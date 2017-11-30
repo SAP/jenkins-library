89d08948236d3b09ef2d3b47a025e6427c689eb2
Daniel Kurzynski <daniel.kurzynski@sap.com>
2017-11-30 10:42:38 +0100

    Add new BashUtilsTest.groovy

--- test/groovy/com/sap/piper/BashUtilsTest.groovy
+++ test/groovy/com/sap/piper/BashUtilsTest.groovy
@@ -7,10 +7,19 @@ class BashUtilsTest {
 
     @Test
     void testEscape() {
-        // input: 'a$b%c%d$e'$?$#$$"'
+        // input: a$b%c%d$e'$?$#$$"
         def input = "a\$b%c%d\$e\'\$?\$#\$\$\""
-        // expect: "'a$b%c%d$e'$?$#$$\"'"
+        // expect: 'a$b%c%d$e\'$?$#$$'
         def expected = "\'a\$b%c%d\$e\\\'\$?\$#\$\$\"\'"
+        Assert.assertEquals(expected, BashUtils.escape(input))
+    }
+
+    @Test
+    void testSimpleEscape() {
+        // input: my file path
+        def input = "my file path"
+        // expect: 'my file path'
+        def expected = "'my file path'"
         Assert.assertEquals(expected, BashUtils.escape(input))
     }
 }
