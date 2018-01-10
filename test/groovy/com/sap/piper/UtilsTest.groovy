fe89155a04a091c5d2f73b927c5fb9f7a3daf1fa
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2018-01-10 10:27:55 +0100

    Initial check-in of lesfurets test improvements (#23)
    Adding lesfurets test framwork improvements via helper classes

---
+++ test/groovy/com/sap/piper/UtilsTest.groovy
@@ -0,0 +1,76 @@
+package com.sap.piper
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+import org.junit.rules.RuleChain
+import util.JenkinsLoggingRule
+import util.JenkinsSetupRule
+import util.SharedLibraryCreator
+
+import static org.hamcrest.Matchers.containsString
+import static org.hamcrest.Matchers.hasSize
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertThat
+
+class UtilsTest extends BasePipelineTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none()
+
+    @Rule
+    public JenkinsSetupRule setUpRule = new JenkinsSetupRule(this, SharedLibraryCreator.lazyLoadedLibrary)
+
+    Utils utils
+
+    @Before
+    void init() throws Exception {
+        utils = new Utils()
+        prepareObjectInterceptors(utils)
+    }
+
+    void prepareObjectInterceptors(object) {
+        object.metaClass.invokeMethod = helper.getMethodInterceptor()
+        object.metaClass.static.invokeMethod = helper.getMethodInterceptor()
+        object.metaClass.methodMissing = helper.getMethodMissingInterceptor()
+    }
+
+    @Test
+    void testGetMandatoryParameterValid() {
+
+        def sourceMap = [test1: 'value1', test2: 'value2']
+
+        def defaultFallbackMap = [myDefault1: 'default1']
+
+        assertEquals('value1', utils.getMandatoryParameter(sourceMap, 'test1', null))
+
+        assertEquals('value1', utils.getMandatoryParameter(sourceMap, 'test1', ''))
+
+        assertEquals('value1', utils.getMandatoryParameter(sourceMap, 'test1', 'customValue'))
+
+    }
+
+    @Test
+    void testGetMandatoryParameterDefaultFallback() {
+
+        def myMap = [test1: 'value1', test2: 'value2']
+
+        assertEquals('', utils.getMandatoryParameter(myMap, 'test3', ''))
+        assertEquals('customValue', utils.getMandatoryParameter(myMap, 'test3', 'customValue'))
+    }
+
+
+    @Test
+    void testGetMandatoryParameterFail() {
+
+        def myMap = [test1: 'value1', test2: 'value2']
+
+        exception.expect(Exception.class)
+
+        exception.expectMessage("ERROR - NO VALUE AVAILABLE FOR")
+
+        utils.getMandatoryParameter(myMap, 'test3', null)
+    }
+}
---
+++ test/groovy/util/JenkinsPropertiesRule.groovy
@@ -0,0 +1,55 @@
+package util
+
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class JenkinsPropertiesRule implements TestRule {
+
+    final BasePipelineTest testInstance
+
+    final String propertyPath
+
+    final Properties configProperties
+
+    JenkinsPropertiesRule(BasePipelineTest testInstance, String propertyPath) {
+        this.testInstance = testInstance
+        this.propertyPath = propertyPath
+        configProperties = loadProperties(propertyPath)
+    }
+
+
+    @Override
+    Statement apply(Statement base, Description description) {
+        return statement(base)
+    }
+
+    private Statement statement(final Statement base) {
+        return new Statement() {
+            @Override
+            void evaluate() throws Throwable {
+
+                testInstance.helper.registerAllowedMethod("readProperties", [Map.class], {
+                    propertyPath ->
+                        if (JenkinsPropertiesRule.this.propertyPath.contains(propertyPath.file)) {
+                            return JenkinsPropertiesRule.this.configProperties
+                        }
+
+                        throw new Exception("Could not find the properties with path $propertyPath")
+                })
+
+                base.evaluate()
+            }
+        }
+    }
+
+    static Properties loadProperties(String path) {
+        def inputStream = new File(path).newInputStream()
+        def properties = new Properties()
+        properties.load(inputStream)
+        inputStream.close()
+        return properties
+    }
+
+}
