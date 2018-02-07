f6d13033171577f5127d3fd7c560c3269eba1e0b
Christopher Fenner <Christopher.Fenner@sap.com>
2018-02-07 12:08:01 +0100

    remove obsolete method

--- src/com/sap/piper/MapUtils.groovy
+++ src/com/sap/piper/MapUtils.groovy
@@ -7,13 +7,4 @@ class MapUtils implements Serializable {
     static isMap(object){
         return object in Map
     }
-
-    @NonCPS
-    static fromList(List list){
-        Map map = [:]
-        for(String key : list){
-            map.put(key, null)
-        }
-        return map
-    }
 }
