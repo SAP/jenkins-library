159ab7ba3dd3edadfbc7e000a8c18fcca19a3ae5
Alejandra Ferreiro Vidal <alejandra.ferreiro.vidal@sap.com>
2018-02-08 15:07:01 +0100

    toolValidate with new config

--- test/groovy/com/sap/piper/UtilsTest.groovy
+++ test/groovy/com/sap/piper/UtilsTest.groovy
@@ -5,22 +5,37 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.rules.ExpectedException
 
+import org.junit.rules.RuleChain
+
+import util.Rules
+import util.JenkinsLoggingRule
+
 import com.sap.piper.Utils
 
+import com.lesfurets.jenkins.unit.BasePipelineTest
 
-class UtilsTest {
+
+class UtilsTest extends BasePipelineTest {
+
+    private ExpectedException thrown = new ExpectedException()
+    private JenkinsLoggingRule jlr = new JenkinsLoggingRule(this)
 
     @Rule
-    public ExpectedException thrown = new ExpectedException().none()
+    public RuleChain rules = Rules.getCommonRules(this)
+                .around(thrown)
+                .around(jlr)
 
-    private utils = new Utils()
+    private utils
     private parameters
+    private cpe
 
 
     @Before
     void setup() {
 
+        utils = new Utils()
         parameters = [:]
+        cpe = loadScript('commonPipelineEnvironment.groovy').commonPipelineEnvironment
     }
 
     @Test
@@ -29,7 +44,7 @@ class UtilsTest {
         thrown.expect(Exception)
         thrown.expectMessage("ERROR - NO VALUE AVAILABLE FOR test")
 
-        utils.getMandatoryParameter(parameters, 'test', null)
+        utils.getMandatoryParameter(parameters, 'test')
     }
 
     @Test
@@ -43,6 +58,102 @@ class UtilsTest {
 
         parameters.put('test', 'value')
 
-        assert utils.getMandatoryParameter(parameters, 'test', null) == 'value'
+        assert utils.getMandatoryParameter(parameters, 'test') == 'value'
+    }
+
+    @Test
+    void getMtaJarFromConfigurationTest() {
+
+        def configuration = [mtaJarLocation: '/config/mta']
+        def environment = [MTA_JAR_LOCATION: '/env/mta']
+
+        def mtaJar = utils.getMtaJar(cpe, 'test', configuration, environment)
+
+        assert mtaJar == '/config/mta/mta.jar'
+        assert jlr.log.contains('[test] MTA JAR "/config/mta/mta.jar" retrieved from configuration.')
+    }
+
+    @Test
+    void getMtaJarFromEnvironmentTest() {
+
+        def environment = [MTA_JAR_LOCATION: '/env/mta']
+
+        def mtaJar = utils.getMtaJar(cpe, 'test', [:], environment)
+
+        assert mtaJar == '/env/mta/mta.jar'
+        assert jlr.log.contains('[test] MTA JAR "/env/mta/mta.jar" retrieved from environment.')
+    }
+
+    @Test
+    void getMtaJarFromCurrentWorkingDirectoryTest() {
+
+        def mtaJar = utils.getMtaJar(cpe, 'test', [:], [:])
+
+        assert mtaJar == 'mta.jar'
+        assert jlr.log.contains('[test] Using MTA JAR from current working directory.')
+    }
+
+    @Test
+    void getNeoExecutableFromConfigurationTest() {
+
+        def configuration = [neoHome: '/config/neo']
+        def environment = [NEO_HOME: '/env/neo']
+
+        def neoExecutable = utils.getNeoExecutable(cpe, 'test', configuration, environment)
+
+        assert neoExecutable == '/config/neo/tools/neo.sh'
+        assert jlr.log.contains('[test] Neo executable "/config/neo/tools/neo.sh" retrieved from configuration.')
+    }
+
+    @Test
+    void getNeoExecutableFromEnvironmentTest() {
+
+        def environment = [NEO_HOME: '/env/neo']
+
+        def neoExecutable = utils.getNeoExecutable(cpe, 'test', [:], environment)
+
+        assert neoExecutable == '/env/neo/tools/neo.sh'
+        assert jlr.log.contains('[test] Neo executable "/env/neo/tools/neo.sh" retrieved from environment.')
+    }
+
+    @Test
+    void getNeoExecutableFromCurrentWorkingDirectoryTest() {
+
+        def neoExecutable = utils.getNeoExecutable(cpe, 'test', [:], [:])
+
+        assert neoExecutable == 'neo.sh'
+        assert jlr.log.contains('[test] Using Neo executable from PATH.')
+    }
+
+    @Test
+    void getCmCliExecutableFromConfigurationTest() {
+
+        def configuration = [cmCliHome: '/config/cmclient']
+        def environment = [CM_CLI_HOME: '/env/cmclient']
+
+        def cmCliExecutable = utils.getCmCliExecutable(cpe, 'test', configuration, environment)
+
+        assert cmCliExecutable == '/config/cmclient/bin/cmclient'
+        assert jlr.log.contains('[test] Change Management Command Line Interface "/config/cmclient/bin/cmclient" retrieved from configuration.')
+    }
+
+    @Test
+    void getCmCliExecutableFromEnvironmentTest() {
+
+        def environment = [CM_CLI_HOME: '/env/cmclient']
+
+        def cmCliExecutable = utils.getCmCliExecutable(cpe, 'test', [:], environment)
+
+        assert cmCliExecutable == '/env/cmclient/bin/cmclient'
+        assert jlr.log.contains('[test] Change Management Command Line Interface "/env/cmclient/bin/cmclient" retrieved from environment.')
+    }
+
+    @Test
+    void getCmCliExecutableFromCurrentWorkingDirectoryTest() {
+
+        def cmCliExecutable = utils.getCmCliExecutable(cpe, 'test', [:], [:])
+
+        assert cmCliExecutable == 'cmclient'
+        assert jlr.log.contains('[test] Change Management Command Line Interface retrieved from current working directory.')
     }
 }
--- vars/toolValidate.groovy
+++ vars/toolValidate.groovy
@@ -1,31 +1,60 @@
 import com.sap.piper.FileUtils
 import com.sap.piper.Version
+import com.sap.piper.Utils
+import com.sap.piper.ConfigurationLoader
+import com.sap.piper.ConfigurationMerger
+
 import hudson.AbortException
 
 
 def call(Map parameters = [:]) {
 
-    handlePipelineStepErrors (stepName: 'toolValidate', stepParameters: parameters) {
+    def stepName = 'toolValidate'
 
-        def tool = parameters.tool
-        def home = parameters.home
+    List parameterKeys = [
+        'tool',
+        'home'
+    ]
 
-        if (!tool) throw new IllegalArgumentException("The parameter 'tool' can not be null or empty.")
-        if (!home) throw new IllegalArgumentException("The parameter 'home' can not be null or empty.")
+    List generalConfigurationKeys = [
+        'mtaJarLocation',
+        'neoHome',
+        'cmCliHome'
+    ]
 
-        FileUtils.validateDirectoryIsNotEmpty(home)
+    handlePipelineStepErrors (stepName: stepName, stepParameters: parameters) {
+
+        final script = parameters?.script ?: [commonPipelineEnvironment: commonPipelineEnvironment]
+
+        final Map generalConfiguration = ConfigurationLoader.generalConfiguration(script)
+        final Map configuration = ConfigurationMerger.merge(
+                                      parameters, parameterKeys,
+                                      generalConfiguration, generalConfigurationKeys)
+
+        def utils = new Utils()
+        def tool = utils.getMandatoryParameter(configuration, 'tool')
+        def home = configuration.home
+
+        if (home) FileUtils.validateDirectoryIsNotEmpty(home)
 
         switch(tool) {
-            case 'java': validateJava(home)
-            return
-            case 'mta': validateMta(home)
-            return
-            case 'neo': validateNeo(home)
-            return
-            case 'cm': validateCm(home)
-            return
+            case 'java':
+                validateJava(home)
+                return
+            case 'mta':
+                if (home) validateMta("$home/mta.jar")
+                else validateMta(utils.getMtaJar(this, stepName, configuration, env))
+                return
+            case 'neo':
+                if (home) validateNeo("$home/tools/neo.sh")
+                else validateNeo(utils.getNeoExecutable(this, stepName, configuration, env))
+                return
+            case 'cm':
+                if (home) validateCm("$home/bin/cmclient")
+                else validateCm(utils.getCmCliExecutable(this, stepName, configuration, env))
+                return
             default:
-            throw new AbortException("The tool \'$tool\' is not supported. The following tools are supported: java, mta, neo and cm.")
+                throw new AbortException("The tool '$tool' is not supported. The following tools are supported: java, mta, neo and cm.")
         }
     }
 }
@@ -34,25 +63,28 @@ def validateJava(home) {
     validateTool('Java', home, "$home/bin/java -version 2>&1", new Version(1,8,0))
 }
 
-def validateMta(home) {
-    validateTool('SAP Multitarget Application Archive Builder', home, "$JAVA_HOME/bin/java -jar $home/mta.jar -v", new Version(1, 0, 6))
+def validateMta(executable) {
+    FileUtils.validateFile(executable)
+    validateTool('SAP Multitarget Application Archive Builder', executable, "$JAVA_HOME/bin/java -jar $executable -v", new Version(1, 0, 6))
 }
 
-def validateNeo(home) {
-    validateTool('SAP Cloud Platform Console Client', home, "$home/tools/neo.sh version", new Version(3, 39, 10))
+def validateNeo(executable) {
+    FileUtils.validateFile(executable)
+    validateTool('SAP Cloud Platform Console Client', executable, "$executable version", new Version(3, 39, 10))
 }
 
-def validateCm(home) {
-    validateTool('Change Management Command Line Interface', home, "$home/bin/cmclient -v", new Version(0, 0, 1))
+def validateCm(executable) {
+    FileUtils.validateFile(executable)
+    validateTool('Change Management Command Line Interface', executable, "$executable -v", new Version(0, 0, 1))
 }
 
-private validateTool(name, home, command, expectedVersion) {
+private validateTool(name, executable, command, expectedVersion) {
     echo "[toolValidate] Validating $name version ${expectedVersion.toString()} or compatible version."
     def output
     try {
       output = sh returnStdout: true, script: command
     } catch(AbortException e) {
-      throw new AbortException("The validation of $name failed. Please check $name home '$home': $e.message.")
+      throw new AbortException("The validation of $name failed. Please check '$executable': $e.message.")
     }
     def version = new Version(output)
     if (!version.isCompatibleVersion(expectedVersion)) {
