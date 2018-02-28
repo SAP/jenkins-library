f9d5e6ef42b90e86a24d4b19d180c669f4c3d06b
Christopher Fenner <Christopher.Fenner@sap.com>
2018-02-28 13:18:14 +0100

    add test template

---
+++ template/StepTestTemplateTest.groovy
@@ -0,0 +1,34 @@
+#!groovy
+import com.lesfurets.jenkins.unit.BasePipelineTest
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+
+import util.Rules
+import util.JenkinsStepRule
+
+class StepTestTemplateTest extends BasePipelineTest {
+    private JenkinsStepRule jsr = new JenkinsStepRule(this)
+
+    @Rule
+    public RuleChain rules = Rules
+        .getCommonRules(this)
+        .around(jsr)
+
+    @Before
+    void init() throws Exception {
+    }
+
+    @Test
+    void testStepTestTemplate() throws Exception {
+        jsr.step.call()
+        // asserts
+        assertEquals(true != false)
+        assertTrue(true)
+        assertJobStatusSuccess()
+    }
+}
