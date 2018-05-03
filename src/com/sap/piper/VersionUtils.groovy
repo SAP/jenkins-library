bc6ce8b3ba80ff013a9cf39e864f0c356c6003e7
Alejandra Ferreiro Vidal <alejandra.ferreiro.vidal@sap.com>
2018-05-03 09:45:16 +0200

    add multiple versions verification

--- src/com/sap/piper/VersionUtils.groovy
+++ src/com/sap/piper/VersionUtils.groovy
@@ -5,7 +5,7 @@ import hudson.AbortException
 
 class VersionUtils implements Serializable {
 
-    def static verifyVersion(script, name, executable, version, versionOption) {
+    def static verifyVersion(script, name, executable, String version, versionOption) {
 
         script.echo "Verifying $name version $version or compatible version."
 
@@ -21,5 +21,27 @@ class VersionUtils implements Serializable {
           throw new AbortException("The installed version of $name is ${installedVersion.toString()}. Please install version $version or a compatible version.")
         }
         script.echo "Verification success. $name version ${installedVersion.toString()} is installed."
+    }
+
+    def static verifyVersion(script, name, executable, Map versions, versionOption) {
+
+        def toolVersion
+        try {
+          toolVersion = script.sh returnStdout: true, script: """#!/bin/bash
+                                                                 $executable $versionOption"""
+        } catch(AbortException e) {
+          throw new AbortException("The verification of $name failed. Please check '$executable'. $e.message.")
+        }
+        for (def entry : versions) {
+            if (toolVersion.contains(entry.getKey())) {
+                def installedVersion = new Version(toolVersion)
+                def expectedVersion = entry.getValue()
+                script.echo "Verifying $name version $expectedVersion or compatible version."
+                if (!installedVersion.isCompatibleVersion(new Version(expectedVersion))) {
+                    throw new AbortException("The installed version of $name is ${installedVersion.toString()}. Please install version $expectedVersion or a compatible version.")
+                }
+                script.echo "Verification success. $name version ${installedVersion.toString()} is installed."
+            }
+        }
     }
 }
