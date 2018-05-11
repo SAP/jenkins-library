6e8b89a9b8177e63b16d5f862cfe29c9c26d33ea
Marcus Holl <marcus.holl@sap.com>
2018-05-11 15:20:57 +0200

    isChangeInDevelopment

---
+++ vars/isChangeInDevelopment.groovy
@@ -0,0 +1,48 @@
+import com.sap.piper.GitUtils
+import groovy.transform.Field
+
+@Field def STEP_NAME = 'isChangeInDevelopment'
+
+def call(parameters = [:]) {
+
+
+    Set parameterKeys = [
+        ]
+
+    Set stepConfigurationKeys = [
+        ]
+
+    handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters) {
+
+        GitUtils gitUtils = new GitUtils()
+
+        if( ! gitUtils.insideWorkTree() ) {
+            throw new hudson.AbortException('Cannot retrieve change status. Not is a git work tree.')
+        } else {
+            echo '[INFO] Inside a git work tree.'
+        }
+
+        def changeId = getChangeId(gitUtils, 'HEAD~2')
+        echo "CHANGE-ID: ${changeId}"
+    }
+}
+
+String getChangeId(GitUtils gitUtils, String from = 'origin/master', String to = 'HEAD') {
+
+    def label = 'ChangeDocument\\s?:'
+
+    def log = gitUtils.extractLogLines('.*', from, to)
+
+    echo "LOG: ${log}"
+
+    def changeIds = log.collect { line -> line.replaceAll(label,'').trim() } .unique()
+        changeIds.retainAll { line -> ! line.isEmpty() }
+
+    if( changeIds.size() == 0 ) {
+        throw new hudson.AbortException('Cannot retrieve changeId from git commits.')
+    } else if (changeIds.size() > 1) {
+        throw new hudson.AbortException("Multiple ChangeIds found: ${changeIds}.")
+    }
+
+    return changeIds.get(0)
+}
