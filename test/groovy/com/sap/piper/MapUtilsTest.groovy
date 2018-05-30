f9de5c4bf10af24c55418ba626d60faf48b35ec7
Marcus Holl <marcus.holl@sap.com>
2018-05-30 12:28:18 +0200

    tests

--- test/groovy/com/sap/piper/MapUtilsTest.groovy
+++ test/groovy/com/sap/piper/MapUtilsTest.groovy
@@ -10,4 +10,37 @@ class MapUtilsTest {
         Assert.assertTrue('Map is not recognized as Map', MapUtils.isMap([:]))
         Assert.assertTrue('String is recognized as Map', !MapUtils.isMap('I am not a Map'))
     }
+
+    @Test
+    void testMergeMapStraigtForward(){
+
+        Map a = [a: '1',
+                 c: [d: '1',
+                     e: '2']],
+             b = [b: '2',
+                  c: [d: 'x']];
+
+        Map merged = MapUtils.merge(a, b)
+
+              assert merged == [a: '1',
+                                b: '2',
+                                c: [d: 'x', e: '2']]
+    }
+
+    @Test
+    void testMergeMapNullValues(){
+
+        Map a = [a: '1',
+                 c: [d: '1',
+                     e: '2']],
+             b = [b: '2',
+                  c: null];
+
+        Map merged = MapUtils.merge(a, b)
+
+              assert merged == [a: '1',
+                                b: '2',
+                                c: [d: '1', e: '2']]
+    }
+
 }
