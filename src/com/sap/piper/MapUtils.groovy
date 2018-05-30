22f81e3d97bf6d315d4f2c37d68bfd2418d9c320
Christopher Fenner <Christopher.Fenner@sap.com>
2018-05-30 12:17:29 +0200

    Update MapUtils.groovy

--- src/com/sap/piper/MapUtils.groovy
+++ src/com/sap/piper/MapUtils.groovy
@@ -7,4 +7,22 @@ class MapUtils implements Serializable {
     static boolean isMap(object){
         return object in Map
     }
+    
+    @NonCPS
+    static Map merge(Map a, Map b, skipNull = true/*, override = true*/) {
+        Map result = [:]
+        
+        a = a ?: [:]
+
+        result.putAll(a)
+
+        for(String key : b.keySet())
+            //if(override || a[key] != null)
+            if(isMap(b[key]))
+                result[key] = merge(a[key], b[key])
+            else if(b[key] != null || !skipNull)
+                result[key] = b[key]
+            // else: keep defaults value and omit null values from config
+        return result
+    }
 }
