7c3a7b26b324a5a874c5645649c26b0b513d2060
Oliver Nocon <oliver.nocon@sap.com>
2018-06-06 11:19:19 +0200

    speed up tests
    * use new base class for testing
    * initialize jenkins unit test framework only once for all test classes
    * minor test cleanups

--- test/groovy/DurationMeasureTest.groovy
+++ test/groovy/DurationMeasureTest.groovy
@@ -1,32 +1,29 @@
 #!groovy
-import com.lesfurets.jenkins.unit.BasePipelineTest
-
 import org.junit.Rule
 import org.junit.Test
+import util.BasePiperTest
+
 import static org.junit.Assert.assertTrue
 import org.junit.rules.RuleChain
 
 import util.Rules
 import util.JenkinsStepRule
-import util.JenkinsEnvironmentRule
 
-class DurationMeasureTest extends BasePipelineTest {
+class DurationMeasureTest extends BasePiperTest {
     private JenkinsStepRule jsr = new JenkinsStepRule(this)
-    private JenkinsEnvironmentRule jer = new JenkinsEnvironmentRule(this)
 
     @Rule
     public RuleChain rules = Rules
         .getCommonRules(this)
         .around(jsr)
-        .around(jer)
 
     @Test
     void testDurationMeasurement() throws Exception {
         def bodyExecuted = false
-        jsr.step.call(script: [commonPipelineEnvironment: jer.env], measurementName: 'test') {
+        jsr.step.call(script: nullScript, measurementName: 'test') {
             bodyExecuted = true
         }
-        assertTrue(jer.env.getPipelineMeasurement('test') != null)
+        assertTrue(nullScript.commonPipelineEnvironment.getPipelineMeasurement('test') != null)
         assertTrue(bodyExecuted)
         assertJobStatusSuccess()
     }
