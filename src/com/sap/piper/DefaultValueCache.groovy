2339cbe836511fb2b1ab69a1027b041a9ab481b1
Marcus Holl <marcus.holl@sap.com>
2018-06-07 09:15:35 +0200

    Provide configuration layer for custom configuration

--- src/com/sap/piper/DefaultValueCache.groovy
+++ src/com/sap/piper/DefaultValueCache.groovy
@@ -5,10 +5,15 @@ import com.cloudbees.groovy.cps.NonCPS
 class DefaultValueCache implements Serializable {
     private static DefaultValueCache instance
 
+    // Contains defaults values provided by this library itself
     private Map defaultValues
 
-    private DefaultValueCache(Map defaultValues){
+    // intended for describing e.g. the system landscape on customer side
+    private Map customDefaultValues
+
+    private DefaultValueCache(Map customeDefaultValues, Map customDefaultValues){
         this.defaultValues = defaultValues
+        this.customDefaultValues = customDefaultValues ?: [:]
     }
 
     @NonCPS
@@ -16,13 +21,18 @@ class DefaultValueCache implements Serializable {
         return instance
     }
 
-    static createInstance(Map defaultValues){
-        instance = new DefaultValueCache(defaultValues)
+    static createInstance(Map defaultValues, Map customDefaultValues){
+        instance = new DefaultValueCache(defaultValues, customDefaultValues)
     }
 
     @NonCPS
     Map getDefaultValues(){
         return defaultValues
+    }
+
+    @NonCPS
+    Map getCustomDefaultValues(){
+        return customDefaultValues
     }
 
     static reset(){
