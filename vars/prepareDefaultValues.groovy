bb811afa411ccb17dadbca0913a63ce9fd70afcd
Marcus Holl <marcus.holl@sap.com>
2018-06-07 09:15:35 +0200

    Provide configuration layer for custom configuration

--- vars/prepareDefaultValues.groovy
+++ vars/prepareDefaultValues.groovy
@@ -1,10 +1,26 @@
 import com.sap.piper.DefaultValueCache
 
+import hudson.AbortException
+
 def call(Map parameters = [:]) {
     handlePipelineStepErrors (stepName: 'prepareDefaultValues', stepParameters: parameters) {
         if(!DefaultValueCache.getInstance()) {
             Map defaultValues  = readYaml text: libraryResource('default_pipeline_environment.yml')
-            DefaultValueCache.createInstance(defaultValues)
+            Map customDefaultValues = null
+
+            def customDefaults = null
+
+            try {
+              customDefaults = libraryResource('pipeline_environment.yml')
+            } catch(AbortException e) {
+                // custom defaults file not found, that's OK the file is optional.
+            }
+
+            if( customDefaults) {
+                customDefaultValues = readYaml text: customDefaults
+            }
+            echo "CUSTOM_DEFAULT_VALUES: '${customDefaultValues}'."
+            DefaultValueCache.createInstance(defaultValues, customDefaultValues)
         }
     }
 }
