322a4377630628c251910c7cc0b782e2f49f9a28
Daniel Kurzynski <daniel.kurzynski@sap.com>
2018-06-08 11:55:38 +0200

    Revert "Merge branch 'master' of https://github.com/SAP/jenkins-library"
    This reverts commit 65220efb4eb3b47be935c8e59c31e1db0394a7fc, reversing
    changes made to cf17510641da66290071e6ebafac8dea52e6a133.

--- vars/dockerExecute.groovy
+++ vars/dockerExecute.groovy
@@ -5,55 +5,47 @@ def call(Map parameters = [:], body) {
     def STEP_NAME = 'dockerExecute'
     def PLUGIN_ID_DOCKER_WORKFLOW = 'docker-workflow'
 
-    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters){
         def dockerImage = parameters.dockerImage ?: ''
         Map dockerEnvVars = parameters.dockerEnvVars ?: [:]
         def dockerOptions = parameters.dockerOptions ?: ''
         Map dockerVolumeBind = parameters.dockerVolumeBind ?: [:]
-        def k8s = parameters.k8s ?: true
 
-        if (k8s) {
-            executeDockerOnKubernetes(
-                dockerImage: parameters.dockerImage,
-                dockerEnvVars: parameters.dockerEnvVars,
-                dockerOptions: parameters.dockerOptions,
-                dockerVolumeBind: parameters.dockerVolumeBind) {
-                body()
-            }
-        } else if (dockerImage) {
+        if(dockerImage) {
 
-            if (!isPluginActive(PLUGIN_ID_DOCKER_WORKFLOW)) {
+            if (! isPluginActive(PLUGIN_ID_DOCKER_WORKFLOW) ) {
                 echo "[WARNING][${STEP_NAME}] Docker not supported. Plugin '${PLUGIN_ID_DOCKER_WORKFLOW}' is not installed or not active. Configured docker image '${dockerImage}' will not be used."
                 dockerImage = null
             }
 
             def returnCode = sh script: 'which docker > /dev/null', returnStatus: true
-            if (returnCode != 0) {
+            if(returnCode != 0) {
                 echo "[WARNING][${STEP_NAME}] No docker environment found (command 'which docker' did not return with '0'). Configured docker image '${dockerImage}' will not be used."
                 dockerImage = null
             }
 
             returnCode = sh script: 'docker ps -q > /dev/null', returnStatus: true
-            if (returnCode != 0) {
+            if(returnCode != 0) {
                 echo "[WARNING][$STEP_NAME] Cannot connect to docker daemon (command 'docker ps' did not return with '0'). Configured docker image '${dockerImage}' will not be used."
                 dockerImage = null
             }
+        }
+
+        if(!dockerImage){
+            echo "[INFO][${STEP_NAME}] Running on local environment."
+            body()
+        }else{
             def image = docker.image(dockerImage)
             image.pull()
             image.inside(getDockerOptions(dockerEnvVars, dockerVolumeBind, dockerOptions)) {
                 body()
             }
         }
-
-        if (!dockerImage) {
-            echo "[INFO][${STEP_NAME}] Running on local environment."
-            body()
-        }
     }
 }
 
 @NonCPS
-private isPluginActive(String pluginId) {
+private isPluginActive(String pluginId){
     return Jenkins.instance.pluginManager.plugins.find { p -> p.isActive() && p.getShortName() == pluginId }
 }
 
@@ -65,12 +57,14 @@ private isPluginActive(String pluginId) {
  */
 @NonCPS
 private getDockerOptions(Map dockerEnvVars, Map dockerVolumeBind, def dockerOptions) {
-    def specialEnvironments = ['http_proxy',
-                               'https_proxy',
-                               'no_proxy',
-                               'HTTP_PROXY',
-                               'HTTPS_PROXY',
-                               'NO_PROXY']
+    def specialEnvironments = [
+        'http_proxy',
+        'https_proxy',
+        'no_proxy',
+        'HTTP_PROXY',
+        'HTTPS_PROXY',
+        'NO_PROXY'
+    ]
     def options = []
     if (dockerEnvVars) {
         for (String k : dockerEnvVars.keySet()) {
