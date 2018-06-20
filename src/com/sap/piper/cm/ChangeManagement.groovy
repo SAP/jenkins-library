4bc7ac82cb40d513b0a8779be2a1815edb715841
Alejandra Ferreiro Vidal <alejandra.ferreiro.vidal@sap.com>
2018-06-20 11:46:28 +0200

    add step transportRequestUploadFile

--- src/com/sap/piper/cm/ChangeManagement.groovy
+++ src/com/sap/piper/cm/ChangeManagement.groovy
@@ -28,4 +28,23 @@ public class ChangeManagement implements Serializable {
           throw new ChangeManagementException("Cannot create a transport request for change id '$changeId'. $e.message.")
         }
     }
+
+    void uploadFileToTransportRequest(String changeId, String transportRequestId, String applicationId, String filePath, String endpoint, String username, String password) {
+
+        int rc = script.sh(returnStatus: true,
+                    script:
+                    """#!/bin/bash
+                       cmclient -e '$endpoint' \
+                                -u '$username' \
+                                -p '$password' \
+                                -t SOLMAN \
+                              upload-file-to-transport -cID '$changeId' -tID '$transportRequestId' '$applicationId' '$filePath'
+                    """)
+
+        if(rc == 0) {
+            return
+        } else {
+            throw new ChangeManagementException("Cannot upload file '$filePath' for change document '$changeId' with transport request '$transportRequestId'. Return code from cmclient: $rc.")
+        }
+    }
 }
