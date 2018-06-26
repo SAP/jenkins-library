cf4eea82a63982d68b7012647ed88fc11d21a0bc
Alejandra Ferreiro Vidal <alejandra.ferreiro.vidal@sap.com>
2018-06-26 10:39:46 +0200

    add basic ChangeManagement

---
+++ src/com/sap/piper/cm/ChangeManagementException.groovy
@@ -0,0 +1,10 @@
+package com.sap.piper.cm;
+
+public class ChangeManagementException extends RuntimeException {
+
+	private static final long serialVersionUID = -139169285551665766L
+
+	ChangeManagementException(String message) {
+		super(message)
+	}
+}
