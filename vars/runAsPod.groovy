2c2e1aca6607deb0522bcf4c03fff109fea0da1f
Ramachandra Kamath Arbettu <ramachandra.kamath.arbettu@sap.com>
2018-07-26 15:22:48 +0200

    runAsPod has been added

---
+++ vars/runAsPod.groovy
@@ -0,0 +1,80 @@
+import com.sap.piper.ConfigurationHelper
+import com.sap.piper.ConfigurationLoader
+import com.sap.piper.SysEnv
+import java.util.UUID
+
+def call(Map parameters = [:], body) {
+    def uniqueId = UUID.randomUUID().toString()
+
+    ConfigurationHelper configurationHelper = new ConfigurationHelper(parameters)
+    def script = configurationHelper.getConfigProperty('script')
+    def containersMap = configurationHelper.getConfigProperty('containersMap',[:])
+    def dockerEnvVars = configurationHelper.getConfigProperty('dockerEnvVars',[:])
+    def dockerWorkspace = configurationHelper.getConfigProperty('dockerWorkspace','')
+
+    handleStepErrors(stepName: 'runAsPod', stepParameters: [:]) {
+        def options = [name      : 'dynamic-agent-' + uniqueId,
+                       label     : uniqueId,
+                       containers: getContainerList(script,containersMap,dockerEnvVars, dockerWorkspace)]
+        podTemplate(options) {
+            node(uniqueId) {
+                body()
+            }
+        }
+    }
+}
+
+private getContainerList(script, containersMap,dockerEnvVars, dockerWorkspace ) {
+    def envVars
+    def jnlpAgent = ConfigurationLoader.generalConfiguration(script).jnlpAgent ?: 's4sdk/jenkins-agent-k8s:latest'
+
+
+    envVars = getContainerEnvs(dockerEnvVars, dockerWorkspace)
+    result = []
+    result.push(containerTemplate(name: 'jnlp',
+        image: jnlpAgent,
+        args: '${computer.jnlpmac} ${computer.name}'))
+
+    containersMap.each { k, v ->
+        result.push(containerTemplate(name: v,
+            image: k,
+            alwaysPullImage: true,
+            command: '/usr/bin/tail -f /dev/null',
+            envVars: envVars))
+    }
+    return result
+}
+
+/**
+ * Returns a list of envVar object consisting of set
+ * environment variables, params (Parametrized Build) and working directory.
+ * (Kubernetes-Plugin only!)
+ * @param dockerEnvVars Map with environment variables
+ * @param dockerWorkspace Path to working dir
+ */
+private getContainerEnvs(dockerEnvVars, dockerWorkspace) {
+    def containerEnv = []
+
+    if (dockerEnvVars) {
+        for (String k : dockerEnvVars.keySet()) {
+            containerEnv << envVar(key: k, value: dockerEnvVars[k].toString())
+        }
+    }
+
+    if (dockerWorkspace) containerEnv << envVar(key: "HOME", value: dockerWorkspace)
+
+    // Inherit the proxy information from the master to the container
+    def systemEnv = new SysEnv()
+    def envList = systemEnv.getEnv().keySet()
+    for (String env : envList) {
+        containerEnv << envVar(key: env, value: systemEnv.get(env))
+    }
+
+    // ContainerEnv array can't be empty. Using a stub to avoid failure.
+    if (!containerEnv) containerEnv << envVar(key: "EMPTY_VAR", value: " ")
+
+    return containerEnv
+}
+
+
+
