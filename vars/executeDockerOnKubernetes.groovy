376961b8d901d9ebe8cc7c81972c15d357e4b1e4
Ramachandra Kamath Arbettu <ramachandra.kamath.arbettu@sap.com>
2018-07-27 09:29:53 +0200

    Use runAsPod

--- vars/executeDockerOnKubernetes.groovy
+++ vars/executeDockerOnKubernetes.groovy
@@ -1,9 +1,6 @@
-import com.cloudbees.groovy.cps.NonCPS
-import com.sap.piper.SysEnv
-import org.codehaus.groovy.GroovyException
 import com.sap.piper.ConfigurationMerger
-import java.util.UUID
 import com.sap.piper.Utils
+import org.codehaus.groovy.GroovyException
 
 def call(Map parameters = [:], body) {
     def STEP_NAME = 'executeDockerOnKubernetes'
@@ -28,24 +25,15 @@ def call(Map parameters = [:], body) {
             parameters, parameterKeys,
             stepConfigurationKeys)
 
-        config.uniqueId = UUID.randomUUID().toString()
 
         if (!config.dockerImage) throw new GroovyException('Docker image not specified.')
 
-        def options = [name      : env.jaas_owner + '-jaas',
-                       label     : config.uniqueId,
-                       containers: getContainerList(config)]
-
         stashWorkspace(config)
-        podTemplate(options) {
-            node(config.uniqueId) {
-                echo "Execute container content in Kubernetes pod"
-                utils.unstashAll(config.stashContent)
-                container(name: 'container-exec') {
-                    body()
-                }
-                stashContainer(config)
-            }
+        runAsPod(script: script, containersMap: [config.dockerImage: 'container-exec'], dockerEnvVars: config.dockerEnvVars, dockerWorkspace: config.dockerWorkspace) {
+            echo "Execute container content in Kubernetes pod"
+            utils.unstashAll(config.stashContent)
+            body()
+            stashContainer(config)
         }
         unstashContainer(config)
     }
@@ -87,64 +75,3 @@ private unstashContainer(config) {
     }
 }
 
-private getContainerList(config) {
-    def envVars
-    envVars = getContainerEnvs(config.dockerEnvVars, config.dockerWorkspace)
-    if (config.dindImage) {
-        envVars << envVar(key: 'DOCKER_HOST', value: '2375')
-    }
-
-    result = []
-    result.push(containerTemplate(name: 'jnlp',
-        image: 's4sdk/jenkins-agent-k8s:latest',
-        args: '${computer.jnlpmac} ${computer.name}'))
-    result.push(containerTemplate(name: 'container-exec',
-        image: config.dockerImage,
-        alwaysPullImage: true,
-        command: '/usr/bin/tail -f /dev/null',
-        envVars: envVars))
-    if (config.dindImage) result.push(containerTemplate(name: 'container-dind',
-        image: config.dindImage,
-        privileged: true))
-    return result
-}
-
-/**
- * Returns a list of envVar object consisting of set
- * environment variables, params (Parametrized Build) and working directory.
- * (Kubernetes-Plugin only!)
- * @param dockerEnvVars Map with environment variables
- * @param dockerWorkspace Path to working dir
- */
-private getContainerEnvs(dockerEnvVars, dockerWorkspace) {
-    def containerEnv = []
-
-    if (dockerEnvVars) {
-        for (String k : dockerEnvVars.keySet()) {
-            containerEnv << envVar(key: k, value: dockerEnvVars[k].toString())
-        }
-    }
-    if (params) {
-        for (String k : params.keySet()) {
-            containerEnv << envVar(key: k, value: params[k].toString())
-        }
-    }
-    if (dockerWorkspace) containerEnv << envVar(key: "HOME", value: dockerWorkspace)
-
-    // Inherit the proxy information from the master to the container
-    def systemEnv = new SysEnv()
-    def envList = systemEnv.getEnv().keySet()
-    for (String env : envList) {
-        containerEnv << envVar(key: env, value: systemEnv.get(env))
-    }
-
-    // ContainerEnv array can't be empty. Using a stub to avoid failure.
-    if (!containerEnv) containerEnv << envVar(key: "EMPTY_VAR", value: " ")
-
-    return containerEnv
-}
-
-@NonCPS
-private isPluginActive(String pluginId) {
-    return Jenkins.instance.pluginManager.plugins.find { p -> p.isActive() && p.getShortName() == pluginId }
-}
