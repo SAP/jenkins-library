71857038c4a14afe6cbc74c1230608f8438d1519
Christopher Fenner <Christopher.Fenner@sap.com>
2018-08-15 10:59:06 +0200

    use new config handler

--- vars/influxWriteData.groovy
+++ vars/influxWriteData.groovy
@@ -2,50 +2,58 @@ import com.sap.piper.ConfigurationLoader
 import com.sap.piper.ConfigurationMerger
 import com.sap.piper.JsonUtils
 
+import groovy.transform.Field
+
+@Field def STEP_NAME = 'influxWriteData'
+
+@Field Set GENERAL_CONFIG_KEYS = []
+@Field Set STEP_CONFIG_KEYS = [
+    'influxServer',
+    'influxPrefix'
+]
+@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS.plus([
+    'artifactVersion'
+])
+
 def call(Map parameters = [:]) {
+    handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters, allowBuildFailure: true) {
+        def script = parameters.script ?: [commonPipelineEnvironment: commonPipelineEnvironment]
 
-    def stepName = 'influxWriteData'
+        // load default & individual configuration
+        Map configuration = ConfigurationHelper
+            .loadStepDefaults(this)
+            .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName?:env.STAGE_NAME, STEP_CONFIG_KEYS)
+            .mixin([
+                artifactVersion: commonPipelineEnvironment.getArtifactVersion()
+            ])
+            .mixin(parameters, PARAMETER_KEYS)
+            .use()
 
-    handlePipelineStepErrors (stepName: stepName, stepParameters: parameters, allowBuildFailure: true) {
-
-        def script = parameters.script
-        if (script == null)
-            script = [commonPipelineEnvironment: commonPipelineEnvironment]
-
-        prepareDefaultValues script: script
-
-        Set parameterKeys = [
-            'artifactVersion',
-            'influxServer',
-            'influxPrefix'
-        ]
         Map pipelineDataMap = [
             artifactVersion: commonPipelineEnvironment.getArtifactVersion()
         ]
-        Set stepConfigurationKeys = [
-            'influxServer',
-            'influxPrefix'
-        ]
 
-        Map configuration = ConfigurationMerger.merge(script, stepName, parameters, parameterKeys, pipelineDataMap, stepConfigurationKeys)
+        Map configuration = ConfigurationMerger.merge(script, STEP_NAME, parameters, parameterKeys, pipelineDataMap, stepConfigurationKeys)
 
         def artifactVersion = configuration.artifactVersion
         if (!artifactVersion)  {
             //this takes care that terminated builds due to milestone-locking do not cause an error
-            echo "[${stepName}] no artifact version available -> exiting writeInflux without writing data"
+            echo "[${STEP_NAME}] no artifact version available -> exiting writeInflux without writing data"
             return
         }
 
         def influxServer = configuration.influxServer
         def influxPrefix = configuration.influxPrefix
 
-        echo """[${stepName}]----------------------------------------------------------
+        echo """[${STEP_NAME}]----------------------------------------------------------
 Artifact version: ${artifactVersion}
 Influx server: ${influxServer}
 Influx prefix: ${influxPrefix}
 InfluxDB data: ${script.commonPipelineEnvironment.getInfluxCustomData()}
 InfluxDB data map: ${script.commonPipelineEnvironment.getInfluxCustomDataMap()}
-[${stepName}]----------------------------------------------------------"""
+[${STEP_NAME}]----------------------------------------------------------"""
 
         if (influxServer)
             step([$class: 'InfluxDbPublisher', selectedTarget: influxServer, customPrefix: influxPrefix, customData: script.commonPipelineEnvironment.getInfluxCustomData(), customDataMap: script.commonPipelineEnvironment.getInfluxCustomDataMap()])
