c2476f5e0ca74b6d3f284d32cb813bc31d165bae
Ramachandra Kamath Arbettu <rkamath3@users.noreply.github.com>
2018-08-21 15:45:59 +0200

    Enable support for executing on K8S as a step (#231)
    * Create executeDockerOnKubernetes.groovy
    
    * Update dockerExecute.groovy
    
    * Create SysEnvTest.groovy
    
    * Update default_pipeline_environment.yml
    
    * Update executeDockerOnKubernetes.groovy
    
    * Create utils object
    
    * update docker image
    
    * Update mavenExecute.groovy
    
    * Use pipeline-lib than piper
    
    * Check container name
    
    * Always change ownership to 1000
    
    * Check for map
    
    * Fix command
    
    * Move chmod to docker execute
    
    * Use generic name for the pod
    
    * runAsPod has been added
    
    * Return false if script has no k8smapping
    
    * fix syntax error
    
    * Null checks
    
    * Returnn dockerImage name
    
    * Check method body
    
    * Return container name
    
    * Cleanup echos
    
    * Use runAsPod
    
    * Rename step
    
    * Use official jenkins JNLP agent image
    
    * Construct containersMap
    
    * Check if kubernetes plugin is active
    
    * Support JaaS
    
    * pass script object
    
    * Move configuration to default section
    
    * Use generic flag to check if running in k8s
    
    * fix jnlp agent name
    
    * Solve travis errors
    
    * Improvements to config and changes to name of the method
    
    * Improvements to config
    
    * Fix type
    
    * Rename stash config
    
    * add import
    
    * Fix map order
    
    * Fix jnlp agent name
    
    * cleanup config usage
    
    * Check if config is enabled
    
    * Use nested k8s mapping
    
    * Support custom docker workspace and move flag to env
    
    * Feature/k8s stage (#1)
    
    * Use nested k8s mapping
    
    * Support custom docker workspace and move flag to env
    
    * Check dockerOptions value
    
    * Support local execution
    
    * Add tests for dockerExecute
    
    * Move config to step and Fix tests
    
    * Use step configuration while running as a pod
    
    * Streamline parameter and config initialization
    
    * Streamline parameter and tests
    
    * Cleanup and align variable name
    
    * Use default JNLP agent if one not defined in config
    
    * Add tests for runInsidePod. Ensure lowercase container names.
    
    * Improve tests and remove unused code block
    
    * Fix permission issues
    
    * Perform stashing and unstashing inside container
    
    * Use custom jnlp agent due to user id restriction
    
    * Fix tests after jnlp agent change
    
    * Address review comments
    
    * Initialize script to default value if null
    
    * Address review comments
    
    * Update exeception handling and documentation
    
    * Improve documentation
    
    * correct indent
    
    * Link documents to the index page
    
    * Merge containerExecute and dockerExecuteOnKuberenetes step and address comments.
    
    * Update dockerExecute.md
    
    * Update dockerExecuteOnKubernetes.md
    
    * Update default_pipeline_environment.yml
    
    * update documentation
    
    * Update documentation. Use annotation for singleton
    
    * Update DockerExecuteOnKubernetesTest.groovy
    
    * Update dockerExecute.groovy
    
    * Update dockerExecuteOnKubernetes.groovy
    
    * Improve documentation and test case names
    
    * neoDeploy: switch to chained ConfigurationHelper (#244)
    
    * switch neoDeploy to chained ConfigurationHelper
    
    * update imports
    
    * Improve tests
    
    * Address review comments
    
    * Improve documentation
    
    * made dockerImage non-mandatory parm, improved test
    
    * add comment regarding userid assumption

---
+++ test/groovy/com/sap/piper/k8s/SystemEnvTest.groovy
@@ -0,0 +1,47 @@
+package com.sap.piper.k8s
+
+import com.sap.piper.k8s.SystemEnv
+import org.junit.Before
+import org.junit.Test
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotNull
+
+class SystemEnvTest {
+    SystemEnv env = null;
+    Map systemEnvironmentMock = [:]
+    @Before
+    void setUp() {
+        systemEnvironmentMock = ['HTTP_PROXY' : 'http://my-http-proxy:8080',
+                                 'HTTPS_PROXY': 'http://my-http-proxy:8080',
+                                 'NO_PROXY'   : '*.example.com,localhost',
+                                 'http_proxy' : 'http://my-http-proxy:8080',
+                                 'https_proxy': 'http://my-http-proxy:8080',
+                                 'no_proxy'   : '*.example.com,localhost',]
+        System.metaClass.static.getenv = { String s -> return systemEnvironmentMock.get(s) }
+        env = new SystemEnv()
+    }
+
+    @Test
+    void testget() {
+        String name = 'HTTP_PROXY'
+        assertEquals(systemEnvironmentMock.get(name), env.get(name))
+
+        name = 'HTTPS_PROXY'
+        assertEquals(systemEnvironmentMock.get(name), env.get(name))
+
+    }
+
+    @Test
+    void testgetEnv() {
+        assertNotNull(env)
+        assertEquals(systemEnvironmentMock.keySet(), env.getEnv().keySet())
+    }
+
+    @Test
+    void testremove() {
+        String name = 'HTTP_PROXY'
+        env.remove(name)
+        assertEquals(env.getEnv().containsKey(name),false)
+    }
+}
--- vars/dockerExecute.groovy
+++ vars/dockerExecute.groovy
@@ -1,53 +1,84 @@
 import com.cloudbees.groovy.cps.NonCPS
+import com.sap.piper.ConfigurationHelper
+import com.sap.piper.k8s.ContainerMap
+import com.sap.piper.JenkinsUtils
+import groovy.transform.Field
 
-def call(Map parameters = [:], body) {
+@Field def STEP_NAME = 'dockerExecute'
+@Field def PLUGIN_ID_DOCKER_WORKFLOW = 'docker-workflow'
 
-    def STEP_NAME = 'dockerExecute'
-    def PLUGIN_ID_DOCKER_WORKFLOW = 'docker-workflow'
+@Field Set GENERAL_CONFIG_KEYS = ['jenkinsKubernetes']
 
-    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters){
-        def dockerImage = parameters.dockerImage ?: ''
-        Map dockerEnvVars = parameters.dockerEnvVars ?: [:]
-        def dockerOptions = parameters.dockerOptions ?: ''
-        Map dockerVolumeBind = parameters.dockerVolumeBind ?: [:]
+@Field Set PARAMETER_KEYS = ['dockerImage',
+                             'dockerOptions',
+                             'dockerWorkspace',
+                             'dockerEnvVars',
+                             'dockerVolumeBind']
+@Field Set STEP_CONFIG_KEYS = PARAMETER_KEYS
 
-        if(dockerImage) {
-
-            if (! isPluginActive(PLUGIN_ID_DOCKER_WORKFLOW) ) {
-                echo "[WARNING][${STEP_NAME}] Docker not supported. Plugin '${PLUGIN_ID_DOCKER_WORKFLOW}' is not installed or not active. Configured docker image '${dockerImage}' will not be used."
-                dockerImage = null
+void call(Map parameters = [:], body) {
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+        final script = parameters.script
+        if (script == null)
+            script = [commonPipelineEnvironment: commonPipelineEnvironment]
+        Map config = ConfigurationHelper
+            .loadStepDefaults(this)
+            .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName?:env.STAGE_NAME, STEP_CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixin(parameters, PARAMETER_KEYS)
+            .use()
+        if (isKubernetes() && config.dockerImage) {
+            if (env.POD_NAME && isContainerDefined(config)) {
+                container(getContainerDefined(config)) {
+                    echo "Executing inside a Kubernetes Container"
+                    body()
+                    sh "chown -R 1000:1000 ."
+                }
+            } else {
+                dockerExecuteOnKubernetes(
+                    script: script,
+                    dockerImage: config.dockerImage,
+                    dockerEnvVars: config.dockerEnvVars,
+                    dockerWorkspace: config.dockerWorkspace
+                ){
+                    echo "Executing inside a Kubernetes Pod"
+                    body()
+                }
+            }
+        } else {
+            boolean executeInsideDocker = true
+            if (!JenkinsUtils.isPluginActive(PLUGIN_ID_DOCKER_WORKFLOW)) {
+                echo "[WARNING][${STEP_NAME}] Docker not supported. Plugin '${PLUGIN_ID_DOCKER_WORKFLOW}' is not installed or not active. Configured docker image '${config.dockerImage}' will not be used."
+                executeInsideDocker = false
             }
 
             def returnCode = sh script: 'which docker > /dev/null', returnStatus: true
-            if(returnCode != 0) {
-                echo "[WARNING][${STEP_NAME}] No docker environment found (command 'which docker' did not return with '0'). Configured docker image '${dockerImage}' will not be used."
-                dockerImage = null
+            if (returnCode != 0) {
+                echo "[WARNING][${STEP_NAME}] No docker environment found (command 'which docker' did not return with '0'). Configured docker image '${config.dockerImage}' will not be used."
+                executeInsideDocker = false
             }
 
             returnCode = sh script: 'docker ps -q > /dev/null', returnStatus: true
-            if(returnCode != 0) {
-                echo "[WARNING][$STEP_NAME] Cannot connect to docker daemon (command 'docker ps' did not return with '0'). Configured docker image '${dockerImage}' will not be used."
-                dockerImage = null
+            if (returnCode != 0) {
+                echo "[WARNING][$STEP_NAME] Cannot connect to docker daemon (command 'docker ps' did not return with '0'). Configured docker image '${config.dockerImage}' will not be used."
+                executeInsideDocker = false
             }
-        }
-
-        if(!dockerImage){
-            echo "[INFO][${STEP_NAME}] Running on local environment."
-            body()
-        }else{
-            def image = docker.image(dockerImage)
-            image.pull()
-            image.inside(getDockerOptions(dockerEnvVars, dockerVolumeBind, dockerOptions)) {
+            if (executeInsideDocker && config.dockerImage) {
+                def image = docker.image(config.dockerImage)
+                image.pull()
+                image.inside(getDockerOptions(config.dockerEnvVars, config.dockerVolumeBind, config.dockerOptions)) {
+                    body()
+                }
+            } else {
+                echo "[INFO][${STEP_NAME}] Running on local environment."
                 body()
             }
         }
     }
 }
 
-@NonCPS
-private isPluginActive(String pluginId){
-    return Jenkins.instance.pluginManager.plugins.find { p -> p.isActive() && p.getShortName() == pluginId }
-}
+
 
 /**
  * Returns a string with docker options containing
@@ -84,15 +115,37 @@ private getDockerOptions(Map dockerEnvVars, Map dockerVolumeBind, def dockerOpti
         }
     }
 
-    if (dockerOptions instanceof CharSequence) {
-        options.add(dockerOptions.toString())
-    } else if (dockerOptions instanceof List) {
-        for (String option : dockerOptions) {
-            options.add "${option}"
+    if (dockerOptions) {
+        if (dockerOptions instanceof CharSequence) {
+            options.add(dockerOptions.toString())
+        } else if (dockerOptions instanceof List) {
+            for (String option : dockerOptions) {
+                options.add "${option}"
+            }
+        } else {
+            throw new IllegalArgumentException("Unexpected type for dockerOptions. Expected was either a list or a string. Actual type was: '${dockerOptions.getClass()}'")
         }
-    } else {
-        throw new IllegalArgumentException("Unexpected type for dockerOptions. Expected was either a list or a string. Actual type was: '${dockerOptions.getClass()}'")
+    }
+    return options.join(' ')
+}
+
+
+boolean isContainerDefined(config) {
+    Map containerMap = ContainerMap.instance.getMap()
+
+    if (!containerMap.containsKey(env.POD_NAME)) {
+        return false
     }
 
-    return options.join(' ')
+    return containerMap.get(env.POD_NAME).containsKey(config.dockerImage)
+}
+
+
+def getContainerDefined(config) {
+    return ContainerMap.instance.getMap().get(env.POD_NAME).get(config.dockerImage).toLowerCase()
+}
+
+
+boolean isKubernetes() {
+    return Boolean.valueOf(env.ON_K8S)
 }
