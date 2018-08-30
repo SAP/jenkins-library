9b2c7e5413211e0de8ee5bce49bf0f6f3916e5b0
Marcus Holl <marcus.holl@sap.com>
2018-08-30 17:18:17 +0200

    enrich DefaultValueCache with project config
    currently the project layer is initialized with <null> all the time.

--- src/com/sap/piper/DefaultValueCache.groovy
+++ src/com/sap/piper/DefaultValueCache.groovy
@@ -5,12 +5,13 @@ import com.cloudbees.groovy.cps.NonCPS
 class DefaultValueCache implements Serializable {
     private static DefaultValueCache instance
 
-    enum Level { DEFAULTS }
+    enum Level { DEFAULTS, PROJECT }
 
     private Map<Level, Map> configurations = [:]
 
-    private DefaultValueCache(Map defaultValues){
+    private DefaultValueCache(Map defaultValues, Map projectConfiguration){
         this.configurations.put(Level.DEFAULTS, defaultValues)
+        this.configurations.put(Level.PROJECT, projectConfiguration)
     }
 
     @NonCPS
@@ -18,14 +19,20 @@ class DefaultValueCache implements Serializable {
         return instance
     }
 
-    static createInstance(Map defaultValues){
-        instance = new DefaultValueCache(defaultValues)
+    static createInstance(Map defaultValues, Map projectConfiguration){
+        instance = new DefaultValueCache(defaultValues, projectConfiguration)
     }
 
     @NonCPS
     Map getDefaultValues(){
         return configurations.get(Level.DEFAULTS)
     }
+
+    @NonCPS
+    Map getProjectConfiguration(){
+        return configurations.get(Level.PROJECT)
+    }
+
 
     static reset(){
         instance = null
