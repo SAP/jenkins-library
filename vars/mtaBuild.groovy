847d8b22ad11a83c2bd2e1886bc3a0c90e224551
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-08-31 11:20:23 +0200

    update

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -35,7 +35,11 @@ def call(Map parameters = [:]) {
 
         new Utils().pushToSWA([step: STEP_NAME], configuration)
 
-        def mtarFileName = dockerExecute(script: script, dockerImage: configuration.dockerImage, dockerOptions: configuration.dockerOptions) {
+        echo configuration //fixme debug
+
+        def mtarFileName = ""
+
+        dockerExecute(script: script, dockerImage: configuration.dockerImage, dockerOptions: configuration.dockerOptions) {
             def java = new ToolDescriptor('Java', 'JAVA_HOME', '', '/bin/', 'java', '1.8.0', '-version 2>&1')
             java.verify(this, configuration)
 
@@ -64,7 +68,7 @@ def call(Map parameters = [:]) {
                 error "Property 'ID' not found in mta.yaml file at: '${pwd()}'"
             }
 
-            def mtarFileName = "${id}.mtar"
+            mtarFileName = "${id}.mtar"
             def mtaJar = mta.getCall(this, configuration)
             def buildTarget = configuration.buildTarget
 
@@ -77,8 +81,6 @@ def call(Map parameters = [:]) {
                 export PATH=./node_modules/.bin:${PATH}
                 $mtaCall
                 """
-
-            return mtarFileName
         }
 
         def mtarFilePath = "${pwd()}/${mtarFileName}"
