615b1f89ac12f50e4c4b2d5e77ff13c7b130319e
Marcus Holl <marcus.holl@sap.com>
2018-09-03 13:09:12 +0200

    [refactoring] artifact set version: check for clean worktree

--- vars/artifactSetVersion.groovy
+++ vars/artifactSetVersion.groovy
@@ -32,7 +32,7 @@ def call(Map parameters = [:], Closure body = null) {
         def gitUtils = parameters.juStabGitUtils ?: new GitUtils()
 
         if (gitUtils.insideWorkTree()) {
-            if (sh(returnStatus: true, script: 'git diff --quiet HEAD') != 0)
+            if (gitUtils.isWorkTreeDirty())
                 error "[${STEP_NAME}] Files in the workspace have been changed previously - aborting ${STEP_NAME}"
         }
 
