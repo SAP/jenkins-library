11af9385a87b43b5abd53313fa97e731e0d15a64
Christopher Fenner <Christopher.Fenner@sap.com>
2018-10-04 16:40:24 +0200

    handle test repositories

--- src/com/sap/piper/GitUtils.groovy
+++ src/com/sap/piper/GitUtils.groovy
@@ -37,3 +37,18 @@ String[] extractLogLines(String filter = '',
         ?.findAll { line -> line ==~ /${filter}/ }
 
 }
+
+static void handleTestRepository(Script steps, Map config){
+    def stashName = "testContent-${UUID.randomUUID()}".toString()
+    def options = [url: config.testRepository]
+    if (config.gitSshKeyCredentialsId)
+        options.put('credentialsId', config.gitSshKeyCredentialsId)
+    if (config.gitBranch)
+        options.put('branch', config.gitBranch)
+    // checkout test repository
+    steps.git options
+    // stash test content
+    steps.stash stashName
+    // alter stashContent
+    config.stashContent = [stashName]
+}
