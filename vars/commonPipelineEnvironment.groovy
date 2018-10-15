1dc90efb6d9724186c91ccd9cdddb3a52ac78a9e
Oliver Nocon <oliver.nocon@sap.com>
2018-10-15 15:28:31 +0200

    enhance commonPipelineEnvironment with additional information
    add additional git information to `commonPipelineEnvironment`:
    * https url
    * git folder / github organization
    * git repository
    * git branch

--- vars/commonPipelineEnvironment.groovy
+++ vars/commonPipelineEnvironment.groovy
@@ -7,6 +7,10 @@ class commonPipelineEnvironment implements Serializable {
     //stores the gitCommitId as well as additional git information for the build during pipeline run
     String gitCommitId
     String gitSshUrl
+    String gitHttpsUrl
+    String gitFolder
+    String gitRepo
+    String gitBranch
 
     //stores properties for a pipeline which build an artifact and then bundles it into a container
     private Map appContainerProperties = [:]
@@ -30,6 +34,10 @@ class commonPipelineEnvironment implements Serializable {
 
         gitCommitId = null
         gitSshUrl = null
+        gitHttpsUrl = null
+        gitFolder = null
+        gitRepo = null
+        gitBranch = null
 
         influxCustomData = [:]
         influxCustomDataMap = [pipeline_data: [:], step_data: [:]]
