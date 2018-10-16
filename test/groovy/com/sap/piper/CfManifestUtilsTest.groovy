e369ce8aedfab8c4d2d9aa58097dfb273076bf46
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-10-16 10:37:09 +0200

    Add more tests

--- test/groovy/com/sap/piper/CfManifestUtilsTest.groovy
+++ test/groovy/com/sap/piper/CfManifestUtilsTest.groovy
@@ -1,15 +1,30 @@
 package com.sap.piper
 
-import org.junit.Assert
 import org.junit.Test
 
+import static org.junit.Assert.*
+
 class CfManifestUtilsTest {
-    Map testFixture = [applications: [[buildpacks: ['sap_java_buildpack']]]]
 
     @Test
     void testManifestTransform() {
+        Map testFixture = [applications: [[buildpacks: ['sap_java_buildpack']]]]
         Map expected = [applications: [[buildpack: 'sap_java_buildpack']]]
         def actual = CfManifestUtils.transform(testFixture)
-        Assert.assertEquals(expected, actual)
+        assertEquals(expected, actual)
+    }
+
+    @Test(expected = RuntimeException)
+    void testManifestTransformMultipleBuildpacks() {
+        Map testFixture = [applications: [[buildpacks: ['sap_java_buildpack', 'another_buildpack']]]]
+        CfManifestUtils.transform(testFixture)
+    }
+
+    @Test
+    void testManifestTransformShouldNotChange() {
+        Map testFixture = [applications: [[buildpack: 'sap_java_buildpack']]]
+        Map expected = [applications: [[buildpack: 'sap_java_buildpack']]]
+        def actual = CfManifestUtils.transform(testFixture)
+        assertEquals(expected, actual)
     }
 }
