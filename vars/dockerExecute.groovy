b76b377afff6491ab3c177f0804d147374ec75cc
Christopher Fenner <Christopher.Fenner@sap.com>
2018-10-22 15:03:36 +0200

    add container names

--- vars/dockerExecute.groovy
+++ vars/dockerExecute.groovy
@@ -113,13 +113,13 @@ void call(Map parameters = [:], body) {
                 } else {
                     def networkName = "sidecar-${UUID.randomUUID()}"
                     sh "docker network create ${networkName}"
-                    
-
                     def sidecarImage = docker.image(config.sidecarImage)
                     sidecarImage.pull()
+                    config.sidecarOptions.push("--name ${config.sidecarName}")
                     config.sidecarOptions.push("--network ${networkName}")
                     sidecarImage.withRun(getDockerOptions(config.sidecarEnvVars, config.sidecarVolumeBind, config.sidecarOptions)) { c ->
                         config.dockerOptions = config.dockerOptions?:[]
+                        config.dockerOptions.add("--name ${config.dockerName}")
                         config.dockerOptions.add("--network ${networkName}")
                         image.inside(getDockerOptions(config.dockerEnvVars, config.dockerVolumeBind, config.dockerOptions)) {
                             echo "[INFO][${STEP_NAME}] Running with sidecar container."
