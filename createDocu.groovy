0d4f4039b91dee1e2175eb1b660cfac87bc3c9ba
Marcus Holl <marcus.holl@sap.com>
2018-10-29 15:25:51 +0100

    Exception handing when rendering steps

--- createDocu.groovy
+++ createDocu.groovy
@@ -329,7 +329,13 @@ for (step in steps) {
 }
 
 for(step in stepDescriptors) {
-  renderStep(step.key, step.value)
+  try {
+    renderStep(step.key, step.value)
+    System.err << "[INFO] Step '${step.key}' has been rendered.\n"
+  } catch(Exception e) {
+    exceptionCaught = true
+    System.err << "${e.getClass().getName()} caught while rendering step '${step}': ${e.getMessage()}.\n"
+  }
 }
 
 if(exceptionCaught) {
