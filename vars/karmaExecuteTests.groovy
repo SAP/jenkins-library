158e53c17c368a15376fb8ceb266588aa202ff05
Christopher Fenner <Christopher.Fenner@sap.com>
2018-11-05 16:12:03 +0100

    Update karmaExecuteTests.groovy

--- vars/karmaExecuteTests.groovy
+++ vars/karmaExecuteTests.groovy
@@ -9,20 +9,20 @@ import groovy.transform.Field
 
 @Field String STEP_NAME = 'karmaExecuteTests'
 @Field Set GENERAL_CONFIG_KEYS = [
-    'containerPortMappings', //port mappings required for containers. This will only take effect inside a Kubernetes pod, format [[containerPort: 1111, hostPort: 1111]]
-    'dockerEnvVars', //envVars to be set in the execution container if required
-    'dockerImage', //Docker image for code execution
-    'dockerName', //name of the Docker container. If not on Kubernetes pod, this will define the network-alias to the NPM container and is thus required for accessing the server, example http://karma:9876 (default).
-    'dockerWorkspace', //user home directory for Docker execution. This will only take effect inside a Kubernetes pod.
+    'containerPortMappings', /** port mappings required for containers. This will only take effect inside a Kubernetes pod, format [[containerPort: 1111, hostPort: 1111]] */
+    'dockerEnvVars', /** envVars to be set in the execution container if required */
+    'dockerImage', /** Docker image for code execution */
+    'dockerName', /** name of the Docker container. If not on Kubernetes pod, this will define the network-alias to the NPM container and is thus required for accessing the server, example http://karma:9876 (default). */
+    'dockerWorkspace', /** user home directory for Docker execution. This will only take effect inside a Kubernetes pod. */
     'failOnError',
     'installCommand',
     'modules',
     'runCommand',
-    'sidecarEnvVars', //envVars to be set in Selenium container if required
-    'sidecarImage', //image for Selenium execution which runs as sidecar to dockerImage
-    'sidecarName', //name of the Selenium container. If not on Kubernetes pod, this will define the network-alias to the Selenium container and is thus required for accessing the server, example http://selenium:4444 (default)
-    'sidecarVolumeBind', //volume bind. This will not take effect in Kubernetes pod.
-    'stashContent' //list of stash names which are required to be unstashed before test run
+    'sidecarEnvVars', /** envVars to be set in Selenium container if required */
+    'sidecarImage', /** image for Selenium execution which runs as sidecar to dockerImage */
+    'sidecarName', /** name of the Selenium container. If not on Kubernetes pod, this will define the network-alias to the Selenium container and is thus required for accessing the server, example http://selenium:4444 (default) */
+    'sidecarVolumeBind', /** volume bind. This will not take effect in Kubernetes pod. */
+    'stashContent' /** list of stash names which are required to be unstashed before test run */
 ]
 @Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
