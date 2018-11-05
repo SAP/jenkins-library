e96b87d681a184c1e2839f5243227ac824ec325f
Marcus Holl <marcus.holl@sap.com>
2018-11-05 10:52:44 +0100

    Abandon backtick syntax in bash

--- createDocu.sh
+++ createDocu.sh
@@ -3,4 +3,4 @@
 export CLASSPATH_FILE='cp.txt'
 mvn dependency:build-classpath -Dmdep.outputFile=${CLASSPATH_FILE} > /dev/null 2>&1
 #~/Library/groovy-2.4.13/bin/groovy -cp src:`cat $CLASSPATH_FILE` createDocu
-groovy -cp "src:`cat $CLASSPATH_FILE`" createDocu "${@}"
+groovy -cp "src:$(cat $CLASSPATH_FILE)" createDocu "${@}"
