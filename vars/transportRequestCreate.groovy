e58c3a81340140aa716f4626d08ed7be065591cd
Marcus Holl <marcus.holl@sap.com>
2018-11-23 08:29:41 +0100

    Provide docu for transportRequestCreate in the step itself.

--- vars/transportRequestCreate.groovy
+++ vars/transportRequestCreate.groovy
@@ -43,6 +43,11 @@ void call(parameters = [:]) {
             .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
             .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName?:env.STAGE_NAME, STEP_CONFIG_KEYS)
             .mixin(parameters, PARAMETER_KEYS)
+            /**
+              * Where/how the transport request is created (via SAP Solution Manager, ABAP).
+              *  @possibleValues `SOLMAN`, `CTS`, `NONE`
+              */
+            .withMandatoryProperty('changeManagement/type')
 
 
         Map configuration =  configHelper.use()
@@ -51,14 +56,51 @@ void call(parameters = [:]) {
         if(backendType == BackendType.NONE) return
 
         configHelper
+            /**
+              * Options forwarded to JVM used by the CM client, like `JAVA_OPTS`.
+              */
             .withMandatoryProperty('changeManagement/clientOpts')
+            /**
+              * The credentials to connect to the service endpoint (Solution Manager, ABAP System).
+              */
             .withMandatoryProperty('changeManagement/credentialsId')
+            /**
+              * The service endpoint (Solution Manager, ABAP System).
+              */
             .withMandatoryProperty('changeManagement/endpoint')
+            /**
+              * The starting point for retrieving the change document id.
+              */
             .withMandatoryProperty('changeManagement/git/from')
+            /**
+              * The end point for retrieving the change document id.
+              */
             .withMandatoryProperty('changeManagement/git/to')
+            /**
+              * Specifies what part of the commit is scanned. By default the body of the commit message is scanned.
+              * @possibleValues see `git log --help`
+              */
             .withMandatoryProperty('changeManagement/git/format')
+            /**
+              * For type `SOLMAN` only. A pattern used for identifying lines holding the change document id.
+              * @possibleValues regex pattern
+              * @mandatory `SOLMAN` only
+              */
+            .withMandatoryProperty('changeManagement/changeDocumentLabel', null, { backendType == BackendType.SOLMAN})
+            /**
+              * for type `CTS` only. Typically `W` (workbench) or `C` customizing.
+              * @mandatory `CTS` only
+              */
             .withMandatoryProperty('transportType', null, { backendType == BackendType.CTS})
+            /**
+              * For type `CTS` only. The system receiving the transport request.
+              * @mandatory `CTS` only
+              */
             .withMandatoryProperty('targetSystem', null, { backendType == BackendType.CTS})
+            /**
+              * For type `CTS` only. The description of the transport request.
+              * @mandatory `CTS` only
+              */
             .withMandatoryProperty('description', null, { backendType == BackendType.CTS})
 
         def changeDocumentId = null
@@ -71,7 +113,18 @@ void call(parameters = [:]) {
             changeDocumentId = getChangeDocumentId(cm, script, configuration)
 
             configHelper.mixin([changeDocumentId: changeDocumentId?.trim() ?: null], ['changeDocumentId'] as Set)
+                        /**
+                          * for `SOLMAN` only. Outlines how the artifact is handled.
+                          * For CTS use case: `SID~Type/Client`, e.g. `XX1~ABAP/100`, for SOLMAN use case: `SID~Typ`,
+                          * e.g. `J01~JAVA`.
+                          * @mandatory `SOLMAN` only.
+                          */
                         .withMandatoryProperty('developmentSystemId')
+                        /**
+                          * for `SOLMAN` only. The id of the change document to that the transport request is bound to.
+                          * Typically this value is provided via commit message in the commit history.
+                          * @mandatory `SOLMAN` only, can be provided via git commit history.
+                          */
                         .withMandatoryProperty('changeDocumentId',
                             "Change document id not provided (parameter: \'changeDocumentId\' or via commit history).")
         }
