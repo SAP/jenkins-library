1c9a66f83013ee83a9042c30019b179e8c6d97a5
Oliver Nocon <oliver.nocon@sap.com>
2018-11-28 14:22:34 +0100

    update stage handling incl. analytics

--- vars/piperStageWrapper.groovy
+++ vars/piperStageWrapper.groovy
@@ -24,33 +24,32 @@ void call(Map parameters = [:], body) {
     stageLocking(config) {
         withNode(config) {
             try {
+
+                //Add general stage stashes to config.stashContent
+                config.stashContent += script.commonPipelineEnvironment.configuration.stageStashes?.get(stageName)?.unstash ?: []
+
                 utils.unstashAll(config.stashContent)
 
                 if (Boolean.valueOf(env.ON_K8S) && containerMap.size() > 0) {
                     withEnv(["POD_NAME=${stageName}"]) {
                         dockerExecute(script: script, containerMap: containerMap) {
-                            executeStage(body, stageName, config)
+                            executeStage(body, stageName, config, utils)
                         }
                     }
                 } else {
-                    executeStage(body, stageName, config)
+                    executeStage(body, stageName, config, utils)
                 }
             } finally {
+                echo "Current build result in stage $stageName is ${currentBuild.currentResult}."
+                //Perform stashing of selected files in workspace
+                utils.stashList(script.commonPipelineEnvironment.configuration.stageStashes?.get(stageName)?.stashes ?: [])
                 deleteDir()
             }
         }
     }
 }
 
-String stageExitFilePath(String stageName, Map config) {
-    return "${config.extensionLocation}${stageName.replace(' ', '_').toLowerCase()}.groovy"
-}
-
-String globalStageExitFilePath(String stageName, Map config) {
-    return "${config.globalExtensionLocation}${stageName.replace(' ', '_').toLowerCase()}.groovy"
-}
-
-void stageLocking(Map config, Closure body) {
+private void stageLocking(Map config, Closure body) {
     if (config.stageLocking) {
         lock(resource: "${env.JOB_NAME}/${config.ordinal}", inversePrecedence: true) {
             milestone config.ordinal
@@ -61,47 +60,46 @@ void stageLocking(Map config, Closure body) {
     }
 }
 
-void withNode(Map config, Closure body) {
-    if (config.withNode) {
-        node(config.nodeLabel) {
+private void executeStage(originalStage, stageName, config, utils) {
+
+    boolean projectExtensions
+    boolean globalExtensions
+    def startTime = System.currentTimeMillis()
+
+    try {
+        /* Defining the sources where to look for a project extension and a repository extension.
+        * Files need to be named like the executed stage to be recognized.
+        */
+        def projectInterceptorFile = "${config.projectExtensionsDirectory}${stageName}.groovy"
+        def globalInterceptorFile = "${config.globalExtensionsDirectory}${stageName}.groovy"
+        projectExtensions = fileExists(projectInterceptorFile)
+        globalExtensions = fileExists(globalInterceptorFile)
+        // Pre-defining the real originalStage in body variable, might be overwritten later if extensions exist
+        def body = originalStage
+
+        // First, check if a global extension exists via a dedicated repository
+        if (globalExtensions) {
+            Script globalInterceptorScript = load(globalInterceptorFile)
+            echo "[${STEP_NAME}] Found global interceptor '${globalInterceptorFile}' for ${stageName}."
+            // If we call the global interceptor, we will pass on originalStage as parameter
+            body = {
+                globalInterceptorScript(body, stageName, config)
+            }
+        }
+
+        // Second, check if a project extension (within the same repository) exists
+        if (projectExtensions) {
+            Script projectInterceptorScript = load(projectInterceptorFile)
+            echo "[${STEP_NAME}] Running project interceptor '${projectInterceptorFile}' for ${stageName}."
+            // If we call the project interceptor, we will pass on body as parameter which contains either originalStage or the repository interceptor
+            projectInterceptorScript(body, stageName, config)
+        } else {
+            //TODO: assign projectInterceptorScript to body as done for globalInterceptorScript, currently test framework does not seem to support this case. Further investigations needed.
             body()
         }
-    } else {
-        body()
+
+    } finally {
+        def duration = System.currentTimeMillis() - startTime
+        utils.pushToSWA(eventType: 'library-os-stage', stageName: stageName, custom1: "${currentBuild.currentResult}", custom2: "${startTime}", custom3: "${duration}", custom4: "${projectExtensions}", custom5: "${globalExtensions}")
     }
-}
-
-void executeStage(originalStage, stageName, config) {
-
-    /* Defining the sources where to look for a project extension and a repository extension.
-    * Files need to be named like the executed stage to be recognized.
-    */
-    def projectInterceptorFile = stageExitFilePath(stageName, config)
-    def globalInterceptorFile = globalStageExitFilePath(stageName, config)
-    boolean projectExtensions = fileExists(projectInterceptorFile)
-    boolean globalExtensions = fileExists(globalInterceptorFile)
-    // Pre-defining the real originalStage in body variable, might be overwritten later if extensions exist
-    def body = originalStage
-
-    if (globalExtensions) {
-        Script globalInterceptorScript = load(globalInterceptorFile)
-        echo "[${STEP_NAME}] Found global interceptor '${globalInterceptorFile}' for ${stageName}."
-        // If we call the global interceptor, we will pass on originalStage as parameter
-        body = {
-            // passing config twice to keep compatibility with https://github.com/SAP/cloud-s4-sdk-pipeline-lib/blob/master/vars/runAsStage.groovy
-            globalInterceptorScript(body, stageName, config, config)
-        }
-    }
-
-    if (projectExtensions) {
-        Script projectInterceptorScript = load(projectInterceptorFile)
-        echo "[${STEP_NAME}] Running project interceptor '${projectInterceptorFile}' for ${stageName}."
-        // If we call the project interceptor, we will pass on body as parameter which contains either originalStage or the repository interceptor
-        // passing config twice to keep compatibility with https://github.com/SAP/cloud-s4-sdk-pipeline-lib/blob/master/vars/runAsStage.groovy
-        projectInterceptorScript(body, stageName, config, config)
-    } else {
-        //TODO: assign projectInterceptorScript to body as done for globalInterceptorScript, currently test framework does not seem to support this case. Further investigations needed.
-        body()
-    }
-
 }
