f68070ca018f03fc88912242cc6d4ce944d75cd5
Daniel Kurzynski <daniel.kurzynski@sap.com>
2018-12-07 14:34:33 +0100

    Only create a node if not using a kubernetes pod

--- vars/piperStageWrapper.groovy
+++ vars/piperStageWrapper.groovy
@@ -27,29 +27,16 @@ void call(Map parameters = [:], body) {
         .use()
 
     stageLocking(config) {
-        node(config.nodeLabel) {
-            try {
-
-                //Add general stage stashes to config.stashContent
-                config.stashContent += script.commonPipelineEnvironment.configuration.stageStashes?.get(stageName)?.unstash ?: []
-
-                utils.unstashAll(config.stashContent)
-
-                def containerMap = ContainerMap.instance.getMap().get(stageName) ?: [:]
-                if (Boolean.valueOf(env.ON_K8S) && containerMap.size() > 0) {
-                    withEnv(["POD_NAME=${stageName}"]) {
-                        dockerExecuteOnKubernetes(script: script, containerMap: containerMap) {
-                            executeStage(script, body, stageName, config, utils)
-                        }
-                    }
-                } else {
+        def containerMap = ContainerMap.instance.getMap().get(stageName) ?: [:]
+        if (Boolean.valueOf(env.ON_K8S) && containerMap.size() > 0) {
+            withEnv(["POD_NAME=${stageName}"]) {
+                dockerExecuteOnKubernetes(script: script, containerMap: containerMap) {
                     executeStage(script, body, stageName, config, utils)
                 }
-            } finally {
-                echo "Current build result in stage $stageName is ${script.currentBuild.currentResult}."
-                //Perform stashing of selected files in workspace
-                utils.stashList(script, script.commonPipelineEnvironment.configuration.stageStashes?.get(stageName)?.stashes ?: [])
-                deleteDir()
+            }
+        } else {
+            node(config.nodeLabel) {
+                executeStage(script, body, stageName, config, utils)
             }
         }
     }
@@ -73,6 +60,11 @@ private void executeStage(script, originalStage, stageName, config, utils) {
     def startTime = System.currentTimeMillis()
 
     try {
+        //Add general stage stashes to config.stashContent
+        config.stashContent += script.commonPipelineEnvironment.configuration.stageStashes?.get(stageName)?.unstash ?: []
+
+        utils.unstashAll(config.stashContent)
+
         /* Defining the sources where to look for a project extension and a repository extension.
         * Files need to be named like the executed stage to be recognized.
         */
@@ -105,6 +97,11 @@ private void executeStage(script, originalStage, stageName, config, utils) {
         }
 
     } finally {
+        echo "Current build result in stage $stageName is ${script.currentBuild.currentResult}."
+        //Perform stashing of selected files in workspace
+        utils.stashList(script, script.commonPipelineEnvironment.configuration.stageStashes?.get(stageName)?.stashes ?: [])
+        deleteDir()
+
         def duration = System.currentTimeMillis() - startTime
         utils.pushToSWA([eventType: 'library-os-stage', stageName: stageName, custom1: "${script.currentBuild.currentResult}", custom2: "${startTime}", custom3: "${duration}", custom4: "${projectExtensions}", custom5: "${globalExtensions}"], config)
     }
