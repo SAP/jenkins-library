bc47719b66b1e5cd0af1cdfa3c1db912f121c490
Christopher Fenner <Christopher.Fenner@sap.com>
2018-12-27 13:05:43 +0100

    Update InfluxWriteDataTest.groovy

--- test/groovy/InfluxWriteDataTest.groovy
+++ test/groovy/InfluxWriteDataTest.groovy
@@ -147,9 +147,9 @@ class InfluxWriteDataTest extends BasePiperTest {
     void testInfluxCustomDataFromCPE() {
         nullScript.commonPipelineEnvironment.reset()
         nullScript.commonPipelineEnvironment.setArtifactVersion('1.2.3')
-        nullScript.commonPipelineEnvironment.setInfluxCustomDataTag('tag1', 'testTag1')
+        nullScript.commonPipelineEnvironment.setInfluxCustomDataTagsEntry('tag1', 'testTag1')
         nullScript.commonPipelineEnvironment.setInfluxCustomDataMapEntry('test_data', 'key1', 'keyValue1')
-        nullScript.commonPipelineEnvironment.setInfluxCustomDataMapTag('test_data', 'tag1', 'tagValue1')
+        nullScript.commonPipelineEnvironment.setInfluxCustomDataMapTagsEntry('test_data', 'tag1', 'tagValue1')
         jsr.step.influxWriteData(
             //juStabUtils: utils,
             script: nullScript,
