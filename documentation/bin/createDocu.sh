4432aa34837b4f977c044e54f84ee90ca177a51b
Marcus Holl <marcus.holl@sap.com>
2019-01-11 13:38:46 +0100

    Remove commented coding line

--- documentation/bin/createDocu.sh
+++ documentation/bin/createDocu.sh
@@ -5,5 +5,4 @@ d=$(dirname "$0")
 
 export CLASSPATH_FILE='cp.txt'
 mvn dependency:build-classpath -Dmdep.outputFile=${CLASSPATH_FILE} > /dev/null 2>&1
-#~/Library/groovy-2.4.13/bin/groovy -cp src:`cat $CLASSPATH_FILE` createDocu
 groovy -cp "src:$(cat $CLASSPATH_FILE)" "${d}createDocu" "${@}"
