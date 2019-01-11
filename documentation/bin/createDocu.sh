7bfb4351b32a3ee03578865e79cff36991d65696
Marcus Holl <marcus.holl@sap.com>
2019-01-11 14:21:00 +0100

    docu script classpath contains the built classes folder
    we do not have only groovy script, but also java files inside src folder.
    Hence we should rely on the target/classes folder in order to see also the
    compiled java classes, and not only the groovy scripts.

--- documentation/bin/createDocu.sh
+++ documentation/bin/createDocu.sh
@@ -4,5 +4,5 @@ d=$(dirname "$0")
 [ !  -z  "$d"  ] &&  d="$d/"
 
 export CLASSPATH_FILE='cp.txt'
-mvn dependency:build-classpath -Dmdep.outputFile=${CLASSPATH_FILE} > /dev/null 2>&1
-groovy -cp "src:$(cat $CLASSPATH_FILE)" "${d}createDocu" "${@}"
+mvn compile dependency:build-classpath -Dmdep.outputFile=${CLASSPATH_FILE} > /dev/null 2>&1
+groovy -cp "target/classes:$(cat $CLASSPATH_FILE)" "${d}createDocu" "${@}"
