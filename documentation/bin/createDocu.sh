35911df158535babec2e4507276c673720356d5b
Marcus Holl <marcus.holl@sap.com>
2019-01-14 08:32:34 +0100

    Create file containing classpath into build dir
    and not into project root. With this appraoch the root folder get not
    filled with temporary content.

--- documentation/bin/createDocu.sh
+++ documentation/bin/createDocu.sh
@@ -3,6 +3,6 @@
 d=$(dirname "$0")
 [ !  -z  "$d"  ] &&  d="$d/"
 
-export CLASSPATH_FILE='cp.txt'
+export CLASSPATH_FILE='target/cp.txt'
 mvn compile dependency:build-classpath -Dmdep.outputFile=${CLASSPATH_FILE} > /dev/null 2>&1
 groovy -cp "target/classes:$(cat $CLASSPATH_FILE)" "${d}createDocu" "${@}"
