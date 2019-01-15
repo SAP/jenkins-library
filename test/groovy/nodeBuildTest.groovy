8428ad104ca007bcdffcf3e40ecbe7b5b315f637
yemengsap <38942126+yemengsap@users.noreply.github.com>
2019-01-15 12:40:09 +0100

    Add nodeBuild

---
+++ test/groovy/nodeBuildTest.groovy
@@ -0,0 +1,59 @@
+import hudson.AbortException
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+import org.junit.rules.RuleChain
+
+import util.BasePiperTest
+import util.JenkinsDockerExecuteRule
+import util.JenkinsReadYamlRule
+import util.JenkinsShellCallRule
+import util.JenkinsStepRule
+import util.Rules
+
+import static org.junit.Assert.assertEquals
+
+class nodeBuildTest extends BasePiperTest {
+
+    private ExpectedException thrown = new ExpectedException().none()
+    private JenkinsShellCallRule shellRule = new JenkinsShellCallRule(this)
+    private JenkinsDockerExecuteRule dockerExecuteRule = new JenkinsDockerExecuteRule(this)
+    private JenkinsStepRule stepRule = new JenkinsStepRule(this)
+    private JenkinsReadYamlRule yamlRule = new JenkinsReadYamlRule(this)
+
+    @Rule
+    public RuleChain ruleChain = Rules
+        .getCommonRules(this)
+        .around(thrown)
+        .around(yamlRule)
+        .around(dockerExecuteRule)
+        .around(shellRule)
+        .around(stepRule)
+
+    @Before
+    void init() {
+        helper.registerAllowedMethod 'fileExists', [String], { s -> s == 'package.json' }
+    }
+
+    @Test
+    void testNodeBuild() {
+        stepRule.step.nodeBuild script: nullScript, dockerImage: 'node:lts-slim', npmScript: 'build'
+        assertEquals 'node:lts-slim', dockerExecuteRule.dockerParams.dockerImage
+    }
+
+    @Test
+    void testNoPackageJson() {
+        helper.registerAllowedMethod 'fileExists', [String], { false }
+        thrown.expect AbortException
+        thrown.expectMessage '[nodeBuild] package.json is not found.'
+        stepRule.step.nodeBuild script: nullScript, dockerImage: 'node:lts-slim', npmScript: 'build'
+    }
+
+    @Test
+    void testNoNpmScript() {
+        thrown.expect AbortException
+        thrown.expectMessage '[nodeBuild] npmScript is not found in configuration.'
+        stepRule.step.nodeBuild script: nullScript, dockerImage: 'node:lts-slim'
+    }
+}
---
+++ vars/nodeBuild.groovy
@@ -0,0 +1,43 @@
+import static com.sap.piper.Prerequisites.checkScript
+
+import com.sap.piper.ConfigurationHelper
+import com.sap.piper.Utils
+
+import groovy.transform.Field
+
+@Field def STEP_NAME = getClass().getName()
+@Field Set GENERAL_CONFIG_KEYS = []
+@Field Set STEP_CONFIG_KEYS = ['dockerImage', 'npmScript']
+@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS + ['dockerOptions']
+
+void call(Map parameters = [:]) {
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+
+        final script = checkScript(this, parameters) ?: this
+
+        // load default & individual configuration
+        Map configuration = ConfigurationHelper.newInstance(this)
+            .loadStepDefaults()
+            .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName?:env.STAGE_NAME, STEP_CONFIG_KEYS)
+            .mixin(parameters, PARAMETER_KEYS)
+            .use()
+
+        new Utils().pushToSWA([
+            step: STEP_NAME,
+            stepParamKey1: 'scriptMissing',
+            stepParam1: parameters?.script == null
+        ], configuration)
+        if (!configuration.npmScript) {
+            error "[${STEP_NAME}] npmScript is not found in configuration."
+        }
+        if (fileExists('package.json')) {
+            dockerExecute(script: script, dockerImage: configuration.dockerImage, dockerOptions: configuration.dockerOptions) {
+                sh """npm run ${configuration.npmScript}"""
+            }
+        } else {
+            error "[${STEP_NAME}] package.json is not found."
+        }
+    }
+}
