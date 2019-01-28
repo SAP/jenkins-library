09a2e3f09089273b3a7240ea3ef8a8f54f68f612
Daniel Kurzynski <daniel.kurzynski@sap.com>
2019-01-28 12:35:35 +0100

    Remove compatibility for neoDeploy

--- vars/neoDeploy.groovy
+++ vars/neoDeploy.groovy
@@ -25,21 +25,6 @@ import static com.sap.piper.Prerequisites.checkScript
     'warAction'
 ])
 
-@Field Map CONFIG_KEY_COMPATIBILITY = [
-    neo   : [
-        host          : 'host',
-        account       : 'account',
-        application   : 'applicationName',
-        credentialsId : 'neoCredentialsId',
-        propertiesFile: 'propertiesFile',
-        runtime       : 'runtime',
-        runtimeVersion: 'runtimeVersion',
-        size          : 'vmSize'
-
-    ],
-    source: 'archivePath'
-]
-
 void call(parameters = [:]) {
     handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
 
@@ -49,17 +34,14 @@ void call(parameters = [:]) {
 
         prepareDefaultValues script: script
 
-        Map stepCompatibilityConfiguration = handleCompatibility(script, parameters)
-
         // load default & individual configuration
         Map configuration = ConfigurationHelper.newInstance(this)
             .loadStepDefaults()
             .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
-            .mixin(stepCompatibilityConfiguration)
-            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS, CONFIG_KEY_COMPATIBILITY)
-            .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName ?: env.STAGE_NAME, STEP_CONFIG_KEYS, CONFIG_KEY_COMPATIBILITY)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName ?: env.STAGE_NAME, STEP_CONFIG_KEYS)
             .addIfEmpty('source', script.commonPipelineEnvironment.getMtarFilePath())
-            .mixin(parameters, PARAMETER_KEYS, CONFIG_KEY_COMPATIBILITY)
+            .mixin(parameters, PARAMETER_KEYS)
             .withMandatoryProperty('neo')
             .withMandatoryProperty('source')
             .withPropertyInValues('deployMode', DeployMode.stringValues())
@@ -73,8 +55,6 @@ void call(parameters = [:]) {
             stepParam2: configuration.warAction == 'rolling-update'?'blue-green':'standard', // ['deploy', 'deploy-mta', 'rolling-update']
             stepParamKey3: 'scriptMissing',
             stepParam3: parameters?.script == null,
-            stepParamKey4: 'legacyConfig',
-            stepParam4: !stepCompatibilityConfiguration.isEmpty(),
         ], configuration)
 
         ToolDescriptor neo = new ToolDescriptor('SAP Cloud Platform Console Client', 'NEO_HOME', 'neoHome', '/tools/', 'neo.sh', null, 'version')
@@ -166,61 +146,6 @@ private deploy(script, utils, Map configuration, NeoCommandHelper neoCommandHelp
 private boolean isAppRunning(NeoCommandHelper commandHelper) {
     def status = sh script: "${commandHelper.statusCommand()} || true", returnStdout: true
     return status.contains('Status: STARTED')
-}
-
-private handleCompatibility(script, parameters) {
-    final Map neoCompatibilityConfiguration = [:]
-
-    // Backward compatibility: ensure old configuration is taken into account
-    // The old configuration in not stage / step specific
-
-    def defaultDeployHost = script.commonPipelineEnvironment.getConfigProperty('DEPLOY_HOST')
-    if (defaultDeployHost) {
-        echo "[WARNING][${STEP_NAME}] A deprecated configuration framework is used for configuring parameter 'DEPLOY_HOST'. This configuration framework will be removed in future versions."
-        neoCompatibilityConfiguration.put('host', defaultDeployHost)
-    }
-
-    def defaultDeployAccount = script.commonPipelineEnvironment.getConfigProperty('CI_DEPLOY_ACCOUNT')
-    if (defaultDeployAccount) {
-        echo "[WARNING][${STEP_NAME}] A deprecated configuration framework is used for configuring parameter 'DEPLOY_ACCOUNT'. This configuration framekwork will be removed in future versions."
-        neoCompatibilityConfiguration.put('account', defaultDeployAccount)
-    }
-
-    if (parameters.deployHost && !parameters.host) {
-        echo "[WARNING][${STEP_NAME}] Deprecated parameter 'deployHost' is used. This will not work anymore in future versions. Use parameter 'host' instead."
-        parameters.put('host', parameters.deployHost)
-    }
-
-    if (parameters.deployAccount && !parameters.account) {
-        echo "[WARNING][${STEP_NAME}] Deprecated parameter 'deployAccount' is used. This will not work anymore in future versions. Use parameter 'account' instead."
-        parameters.put('account', parameters.deployAccount)
-    }
-
-    def credId = script.commonPipelineEnvironment.getConfigProperty('neoCredentialsId')
-    if (credId && !parameters.neoCredentialsId) {
-        echo "[WARNING][${STEP_NAME}] Deprecated parameter 'neoCredentialsId' from old configuration framework is used. This will not work anymore in future versions."
-        parameters.put('neoCredentialsId', credId)
-    }
-
-    if (!neoCompatibilityConfiguration.isEmpty()) {
-        echo "[WARNING][$STEP_NAME] You are using a deprecated configuration framework. This will be removed in " +
-            'futureVersions.\nAdd snippet below to \'./pipeline/config.yml\' and remove ' +
-            'file \'.pipeline/configuration.properties\'.\n' +
-            """|steps:
-                    |    neoDeploy:
-                    |        neo:
-                    |            host: ${neoCompatibilityConfiguration.get('host', '<Add host here>')}
-                    |            account: ${neoCompatibilityConfiguration.get('account', '<Add account here>')}
-                """.stripMargin()
-
-        if (Boolean.getBoolean('com.sap.piper.featureFlag.buildUnstableWhenOldConfigFrameworkIsUsedByNeoDeploy')) {
-            script.currentBuild.setResult('UNSTABLE')
-            echo "[WARNING][$STEP_NAME] Build has been set to unstable since old config framework is used."
-        }
-        return [neo: neoCompatibilityConfiguration]
-    }
-
-    return [:]
 }
 
 private assertPasswordRules(String password) {
