63285466274339aaf447dabf850db6742a403b47
Ye Meng <38942126+yemengsap@users.noreply.github.com>
2019-01-28 15:46:43 +0100

    add configuration

--- vars/nodeBuild.groovy
+++ vars/nodeBuild.groovy
@@ -1,7 +1,33 @@
+import groovy.transform.Field
+
+@Field def STEP_NAME = getClass().getName()
+@Field Set STEP_CONFIG_KEYS = [
+    'dockerImage'
+]
+@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS + ['dockerOptions']
 
 void call(Map parameters = [:]) {
-    final script = checkScript(this, parameters) ?: this
-    dockerExecute(script: script, dockerImage: configuration.dockerImage, dockerOptions: configuration.dockerOptions) {
-        sh '''npm run build'''
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+
+        final script = checkScript(this, parameters) ?: this
+
+        // load default & individual configuration
+        Map configuration = ConfigurationHelper.newInstance(this)
+            .loadStepDefaults()
+            .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName?:env.STAGE_NAME, STEP_CONFIG_KEYS)
+            .mixin(parameters, PARAMETER_KEYS)
+            .use()
+
+        new Utils().pushToSWA([
+            step: STEP_NAME,
+            stepParamKey1: 'scriptMissing',
+            stepParam1: parameters?.script == null
+        ], configuration)
+
+        dockerExecute(script: script, dockerImage: configuration.dockerImage, dockerOptions: configuration.dockerOptions) {
+            sh '''npm run build'''
+        }
     }
 }
