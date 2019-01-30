4f09a13aecd94f73371eacb454e4e8f028439afb
Ye Meng <38942126+yemengsap@users.noreply.github.com>
2019-01-30 16:38:31 +0100

    refactor

--- test/groovy/nodeBuildTest.groovy
+++ test/groovy/nodeBuildTest.groovy
@@ -1,6 +1,7 @@
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
+
 import util.BasePiperTest
 import util.JenkinsDockerExecuteRule
 import util.JenkinsReadYamlRule
@@ -15,11 +16,12 @@ class nodeBuildTest extends BasePiperTest {
     private JenkinsShellCallRule shellRule = new JenkinsShellCallRule(this)
     private JenkinsDockerExecuteRule dockerExecuteRule = new JenkinsDockerExecuteRule(this)
     private JenkinsStepRule stepRule = new JenkinsStepRule(this)
+    private JenkinsReadYamlRule yamlRule = new JenkinsReadYamlRule(this)
 
     @Rule
     public RuleChain ruleChain = Rules
         .getCommonRules(this)
-        .around(new JenkinsReadYamlRule(this))
+        .around(yamlRule)
         .around(dockerExecuteRule)
         .around(shellRule)
         .around(stepRule)
