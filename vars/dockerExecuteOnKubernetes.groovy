SAP/jenkins-library

commit 3c91ddced6d7040f010643ec0f3713a0c9f6243e

Update dockerExecuteOnKubernetes.groovy (#474)

vars/dockerExecuteOnKubernetes.groovy
@@ -110,8 +110,8 @@ private String stashWorkspace(config, prefix) {
         sh "chown -R 1000:1000 ."
         stash(
             name: stashName,
-            include: config.stashIncludes.workspace,
-            exclude: config.stashExcludes.excludes
+            includes: config.stashIncludes.workspace,
+            excludes: config.stashExcludes.excludes
         )
         return stashName
     } catch (AbortException | IOException e) {

commit ad55264b7b95a90f6a3b9162b90729fdea89d32a

Fix regression introduced with #474
Fix stashing behavior to include all files in workspace.
This was for example an issue for PR-voting in Docker pipeline since `Dockerfile` has been excluded from stashing

resources/default_pipeline_environment.yml
@@ -157,7 +157,7 @@ steps:
   dockerExecuteOnKubernetes:
     stashContent: []
     stashIncludes:
-      workspace: '**/*.*'
+      workspace: '**/*'
     stashExcludes:
       workspace: 'nohup.out'
   githubPublishRelease:

vars/dockerExecuteOnKubernetes.groovy
@@ -122,7 +122,7 @@ chown -R 1000:1000 ."""
         stash(
             name: stashName,
             includes: config.stashIncludes.workspace,
-            excludes: config.stashExcludes.excludes
+            excludes: config.stashExcludes.workspace
         )
         return stashName
     } catch (AbortException | IOException e) {