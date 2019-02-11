c5612d8f05c1cdd3b9de8cb0bfc68fdd585af992
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-11 13:25:56 +0100

    Print repo slug

--- consumer-test/runTests.sh
+++ consumer-test/runTests.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+echo Building commit ${TRAVIS_COMMIT}
+echo TRAVIS_PULL_REQUEST_SLUG ${TRAVIS_PULL_REQUEST_SLUG}
+echo TRAVIS_REPO_SLUG ${TRAVIS_REPO_SLUG}
+
 rm -rf workspace
 git clone -b infrastructure-integration-test https://github.com/sap/cloud-s4-sdk-book workspace
 cp -f jenkins.yml workspace
