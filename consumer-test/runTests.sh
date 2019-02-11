3511d83cae24c5c6f12727e55191e18bf2745abc
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-11 14:57:27 +0100

    rename

--- consumer-test/runTests.sh
+++ consumer-test/runTests.sh
@@ -8,5 +8,5 @@ rm -rf workspace
 git clone -b infrastructure-integration-test https://github.com/sap/cloud-s4-sdk-book workspace
 cp -f jenkins.yml workspace
 cd workspace
-echo "@Library(\"piper-lib-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
+echo "@Library(\"piper-library-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CF_PW -e ERP_PW -e BRANCH_NAME=master ppiper/jenkinsfile-runner
