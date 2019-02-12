2037a8db43b7af8c38c161efb74660602f199cac
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-12 15:16:59 +0100

    Configure right repo

--- consumer-test/runTests.sh
+++ consumer-test/runTests.sh
@@ -3,11 +3,13 @@
 set -ex
 
 LIBRARY_VERSION_UNDER_TEST=$(git log --format="%H" -n 1)
+REPOSITORY_UNDER_TEST=${TRAVIS_REPO_SLUG:-SAP/jenkins-library}
 
 rm -rf workspace
 git clone -b consumer-test https://github.com/sap/cloud-s4-sdk-book workspace
 cp -f jenkins.yml workspace
 cd workspace
+sed -i "" -e "s/__REPO_SLUG__/${REPOSITORY_UNDER_TEST}/g" jenkins.yml
 echo "@Library(\"piper-library-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
 git commit --all --author="piper-testing-bot <null@null.com>" --message="Set piper lib version for test"
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e PPIPER_INFRA_IT_CF_USERNAME -e PPIPER_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test ppiper/jenkinsfile-runner
