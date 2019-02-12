c02dc6a9f7533c938ca6f221312e66c86d8391ac
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-12 10:10:06 +0100

    Commit change

--- consumer-test/runTests.sh
+++ consumer-test/runTests.sh
@@ -9,4 +9,5 @@ git clone -b infrastructure-integration-test https://github.com/sap/cloud-s4-sdk
 cp -f jenkins.yml workspace
 cd workspace
 echo "@Library(\"piper-library-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
+git commit --all --author=piper-testing-bot --message="Set piper lib version for test"
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CF_PW -e ERP_PW -e BRANCH_NAME=infrastructure-integration-test ppiper/jenkinsfile-runner
