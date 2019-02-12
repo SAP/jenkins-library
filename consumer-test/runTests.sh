f2436edc61c7ef26f70194d6da097ab33a08860d
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-12 11:28:19 +0100

    env

--- consumer-test/runTests.sh
+++ consumer-test/runTests.sh
@@ -10,4 +10,4 @@ cp -f jenkins.yml workspace
 cd workspace
 echo "@Library(\"piper-library-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
 git commit --all --author="piper-testing-bot <null@null.com>" --message="Set piper lib version for test"
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CF_PW -e ERP_PW -e BRANCH_NAME=infrastructure-integration-test ppiper/jenkinsfile-runner
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e PPIPER_INFRA_IT_CF_USERNAME -e PPIPER_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=infrastructure-integration-test ppiper/jenkinsfile-runner
