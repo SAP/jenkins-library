66a224b90ec7f46b58f5b7711e543bb6136fe2ce
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-13 10:48:28 +0100

    Use new env vars

--- consumer-test/runTests.sh
+++ consumer-test/runTests.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-
-set -ex
+#!/bin/bash -e
 
 LIBRARY_VERSION_UNDER_TEST=$(git log --format="%H" -n 1)
 REPOSITORY_UNDER_TEST=${TRAVIS_REPO_SLUG:-SAP/jenkins-library}
@@ -12,4 +10,5 @@ cd workspace
 sed -i -e "s:__REPO_SLUG__:${REPOSITORY_UNDER_TEST}:g" jenkins.yml
 echo "@Library(\"piper-library-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
 git commit --all --author="piper-testing-bot <null@null.com>" --message="Set piper lib version for test"
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e PPIPER_INFRA_IT_CF_USERNAME -e PPIPER_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test ppiper/jenkinsfile-runner
+
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test ppiper/jenkinsfile-runner
