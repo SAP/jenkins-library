7dfb9e100a5a1c5df3ead7f499fa5c8a1acd52f4
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-14 14:59:25 +0100

    Disable prod deploy in consumer tests on forks

--- consumer-test/runTests.sh
+++ consumer-test/runTests.sh
@@ -3,6 +3,11 @@
 LIBRARY_VERSION_UNDER_TEST=$(git log --format="%H" -n 1)
 REPOSITORY_UNDER_TEST=${TRAVIS_REPO_SLUG:-SAP/jenkins-library}
 
+if [[ ${REPOSITORY_UNDER_TEST} == SAP/jenkins-library ]]
+then
+    BRANCH_NAME=consumer-test
+fi
+
 rm -rf workspace
 git clone -b consumer-test https://github.com/sap/cloud-s4-sdk-book workspace
 cp -f jenkins.yml workspace
@@ -11,4 +16,4 @@ sed -i -e "s:__REPO_SLUG__:${REPOSITORY_UNDER_TEST}:g" jenkins.yml
 echo "@Library(\"piper-library-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
 git commit --all --author="piper-testing-bot <null@null.com>" --message="Set piper lib version for test"
 
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test ppiper/jenkinsfile-runner
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME ppiper/jenkinsfile-runner
