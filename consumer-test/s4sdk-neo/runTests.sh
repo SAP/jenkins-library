b7252ef6276199e0854a50d710e211e7ecbe3987
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-18 11:18:36 +0100

    S4sdk consumer test for neo

--- consumer-test/s4sdk-neo/runTests.sh
+++ consumer-test/s4sdk-neo/runTests.sh
@@ -4,11 +4,15 @@ LIBRARY_VERSION_UNDER_TEST=$(git log --format="%H" -n 1)
 REPOSITORY_UNDER_TEST=${TRAVIS_REPO_SLUG:-SAP/jenkins-library}
 
 rm -rf workspace
-git clone -b consumer-test https://github.com/sap/cloud-s4-sdk-book workspace
+git clone -b consumer-test-neo https://github.com/sap/cloud-s4-sdk-book workspace
 cp -f jenkins.yml workspace
 cd workspace
 sed -i -e "s:__REPO_SLUG__:${REPOSITORY_UNDER_TEST}:g" jenkins.yml
+
+# Force usage of library version under test by setting it in the Jenkinsfile which is then the first definition and thus has the highest precedence
 echo "@Library(\"piper-library-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
-git commit --all --author="piper-testing-bot <null@null.com>" --message="Set piper lib version for test"
+
+# Commit the changed version because artifactSetVersion expects the git repo not to be dirty
+git commit --all --author="piper-testing-bot <piper-testing-bot@example.com>" --message="Set piper lib version for test"
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test ppiper/jenkinsfile-runner
