e78b809ba286428ded65d59008d625ddbc78295a
bernd maier <39129097+sbmaier@users.noreply.github.com>
2019-02-18 13:55:30 +0100

    variable substituion2

--- consumer-test/ui5neo/runTests.sh
+++ consumer-test/ui5neo/runTests.sh
@@ -8,11 +8,8 @@ git clone -b master https://github.com/sbmaier/openui5-sample-app.git workspace
 cp -f jenkins.yml workspace
 cd workspace
 
-#sed -i -e "s:__REPO_SLUG__:${REPOSITORY_UNDER_TEST}:g" jenkins.yml
-#echo "@Library(\"piper-lib-os@$LIBRARY_VERSION_UNDER_TEST\") _" | cat - Jenkinsfile > temp && mv temp Jenkinsfile
 git add jenkins.yml
 git commit --all --author="piper-testing-bot <null@null.com>" --message="Set piper lib version for test"
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test -e LIBRARY_VERSION_UNDER_TEST=${LIBRARY_VERSION_UNDER_TEST} -e REPOSITORY_UNDER_TEST=${REPOSITORY_UNDER_TEST} ppiper/jenkinsfile-runner
-#docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test  ppiper/jenkinsfile-runner
 
