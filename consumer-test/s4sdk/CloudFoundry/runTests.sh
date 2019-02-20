96ca0d905b9d80ad35c3f7313d8124b3bd809a15
Florian Wilhelm <florian.wilhelm02@sap.com>
2019-02-20 13:42:27 +0100

    fix

--- consumer-test/s4sdk/CloudFoundry/runTests.sh
+++ consumer-test/s4sdk/CloudFoundry/runTests.sh
@@ -3,4 +3,4 @@
 source ../prepareTests.sh
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/workspace -v /tmp -e CASC_JENKINS_CONFIG=/workspace/jenkins.yml \
- -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test ppiper/jenkinsfile-runner
+    -e CX_INFRA_IT_CF_USERNAME -e CX_INFRA_IT_CF_PASSWORD -e BRANCH_NAME=consumer-test ppiper/jenkinsfile-runner
