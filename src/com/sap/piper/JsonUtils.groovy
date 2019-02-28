14700ba21046b1aff3eb56eda13aefbfc69f8f7b
Sven Merk <s.merk@sap.com>
2019-02-28 13:01:30 +0100

    Initial checkin of new whitesource step

--- src/com/sap/piper/JsonUtils.groovy
+++ src/com/sap/piper/JsonUtils.groovy
@@ -1,8 +1,14 @@
 package com.sap.piper
 
 import com.cloudbees.groovy.cps.NonCPS
+import groovy.json.JsonSlurperClassic
 
 @NonCPS
 String getPrettyJsonString(object) {
     return groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(object))
+}
+
+@NonCPS
+def parseJsonSerializable(text) {
+    return new JsonSlurperClassic().parseText(text)
 }
