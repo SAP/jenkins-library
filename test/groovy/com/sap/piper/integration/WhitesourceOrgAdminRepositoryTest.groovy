c1189b1ef88a40b1c52b1d352d065d8c6881aacc
Sven Merk <s.merk@sap.com>
2019-03-13 09:58:47 +0100

    Improve coverage

--- test/groovy/com/sap/piper/integration/WhitesourceOrgAdminRepositoryTest.groovy
+++ test/groovy/com/sap/piper/integration/WhitesourceOrgAdminRepositoryTest.groovy
@@ -1,7 +1,6 @@
 package com.sap.piper.integration
 
-import com.lesfurets.jenkins.unit.PipelineTestHelper
-import org.codehaus.groovy.runtime.InvokerHelper
+import hudson.AbortException
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -9,23 +8,28 @@ import org.junit.Test
 import org.junit.rules.RuleChain
 import util.BasePiperTest
 import util.JenkinsEnvironmentRule
+import util.JenkinsErrorRule
 import util.JenkinsLoggingRule
 import util.LibraryLoadingTestExecutionListener
 import util.Rules
 
 import static org.assertj.core.api.Assertions.assertThat
+import static org.hamcrest.Matchers.containsString
 import static org.hamcrest.Matchers.is
+import static org.hamcrest.Matchers.isA
 
 class WhitesourceOrgAdminRepositoryTest extends BasePiperTest {
 
-    private JenkinsLoggingRule jlr = new JenkinsLoggingRule(this)
-    private JenkinsEnvironmentRule jer = new JenkinsEnvironmentRule(this)
+    private JenkinsErrorRule thrown = new JenkinsErrorRule(this)
+    private JenkinsLoggingRule loggingRule = new JenkinsLoggingRule(this)
+    private JenkinsEnvironmentRule environmentRule = new JenkinsEnvironmentRule(this)
 
     @Rule
     public RuleChain ruleChain = Rules
         .getCommonRules(this)
-        .around(jlr)
-        .around(jer)
+        .around(thrown)
+        .around(loggingRule)
+        .around(environmentRule)
 
     WhitesourceOrgAdminRepository repository
 
@@ -95,5 +99,170 @@ class WhitesourceOrgAdminRepositoryTest extends BasePiperTest {
                 userKey    : config.orgAdminUserKey
             ]
         ))
+    }
+
+    @Test
+    void testHttpWhitesourceInternalCallUserKeyVerboseProxy() {
+        def config = [ serviceUrl: "http://some.host.whitesource.com/api/", verbose: true, orgAdminUserKey: "4711"]
+        nullScript.env['HTTP_PROXY'] = "http://test.sap.com:8080"
+        repository.config.putAll(config)
+        def requestBody = ["someJson" : [ "someObject" : "abcdef" ]]
+
+        def requestParams
+        helper.registerAllowedMethod('httpRequest', [Map], { p ->
+            requestParams = p
+        })
+
+        repository.httpWhitesource(requestBody)
+
+        assertThat(requestParams, is(
+            [
+                url        : config.serviceUrl,
+                httpMode   : 'POST',
+                acceptType : 'APPLICATION_JSON',
+                contentType: 'APPLICATION_JSON',
+                requestBody: requestBody,
+                quiet      : false,
+                userKey    : config.orgAdminUserKey,
+                httpProxy  : "http://test.sap.com:8080"
+            ]
+        ))
+
+        assertThat(loggingRule.log, containsString("Sending http request with parameters"))
+        assertThat(loggingRule.log, containsString("Received response"))
+    }
+
+    @Test
+    void testCreateProduct() {
+        def config = [
+            serviceUrl: "http://some.host.whitesource.com/api/",
+            verbose: false,
+            orgAdminUserKey: "4711",
+            orgToken: "abcd1234",
+            productName: "testProduct",
+            emailAddressesOfInitialProductAdmins: ['some@somewhere.com', 'some2@somewhere.com']
+        ]
+        repository.config.putAll(config)
+        def requestBody1 = [
+            requestType: "getOrganizationProductVitals",
+            orgToken: config.orgToken,
+            userKey: "4711"
+        ]
+
+        def requestBody2 = [
+            "requestType" : "setProductAssignments",
+            "productToken" : "54785",
+            "productMembership" : ["userAssignments":[], "groupAssignments":[]],
+            "productAdmins" : ["userAssignments":[[ "email": "some@somewhere.com" ], ["email": "some2@somewhere.com"]]],
+            "alertsEmailReceivers" : ["userAssignments":[]],
+            "userKey": "4711"
+        ]
+
+        def requestParams = []
+        helper.registerAllowedMethod('httpRequest', [Map], { p ->
+            requestParams.add(p)
+            return [ content : "{ \"productToken\" : \"54785\" }" ]
+        })
+
+        repository.createProduct()
+
+        assertThat(requestParams[0], is(
+            [
+                url        : config.serviceUrl,
+                httpMode   : 'POST',
+                acceptType : 'APPLICATION_JSON',
+                contentType: 'APPLICATION_JSON',
+                requestBody: requestBody1,
+                quiet      : false,
+                userKey    : config.orgAdminUserKey,
+                httpProxy  : "http://test.sap.com:8080"
+            ]
+        ))
+
+        assertThat(requestParams[1], is(
+            [
+                url        : config.serviceUrl,
+                httpMode   : 'POST',
+                acceptType : 'APPLICATION_JSON',
+                contentType: 'APPLICATION_JSON',
+                requestBody: requestBody2,
+                quiet      : false,
+                userKey    : config.orgAdminUserKey,
+                httpProxy  : "http://test.sap.com:8080"
+            ]
+        ))
+    }
+
+    @Test
+    void testIssueHttpRequestError() {
+        def config = [ serviceUrl: "http://some.host.whitesource.com/api/", verbose: false, orgAdminUserKey: "4711"]
+        repository.config.putAll(config)
+        def requestBody = ["someJson" : [ "someObject" : "abcdef" ]]
+
+        def requestParams
+        helper.registerAllowedMethod('httpRequest', [Map], { p ->
+            requestParams = p
+            return [content: "{ \"error\" : \"4546\", \"errorMessage\" : \"some text\" } }"]
+        })
+
+        def errorCaught = false
+        try {
+            repository.issueHttpRequest(requestBody)
+        } catch (e) {
+            errorCaught = true
+            assertThat(e, isA(AbortException.class))
+            assertThat(e.getMessage(), equals("[WhiteSource] Request failed with error message 'some text' (4546)."))
+        }
+        assertThat(errorCaught, is(true))
+
+        assertThat(requestParams, is(
+            [
+                url        : config.serviceUrl,
+                httpMode   : 'POST',
+                acceptType : 'APPLICATION_JSON',
+                contentType: 'APPLICATION_JSON',
+                requestBody: requestBody,
+                quiet      : true,
+                userKey    : config.orgAdminUserKey
+            ]
+        ))
+    }
+
+    @Test
+    void testFetchProductMetaInfo() {
+        def config = [ serviceUrl: "http://some.host.whitesource.com/api/", verbose: true, orgAdminUserKey: "4711", orgToken: "12345", productName: "testProduct"]
+        nullScript.env['HTTP_PROXY'] = "http://test.sap.com:8080"
+        repository.config.putAll(config)
+
+        def requestBody = [
+            requestType: "getOrganizationProductVitals",
+            orgToken: config.orgToken,
+            userKey: "4711"
+        ]
+
+        def requestParams
+        helper.registerAllowedMethod('httpRequest', [Map], { p ->
+            requestParams = p
+            return [ content: "{ \"productVitals\" : [ { \"name\": \"testProduct\"} ] }"]
+        })
+
+        def result = repository.fetchProductMetaInfo()
+
+        assertThat(requestParams, is(
+            [
+                url        : config.serviceUrl,
+                httpMode   : 'POST',
+                acceptType : 'APPLICATION_JSON',
+                contentType: 'APPLICATION_JSON',
+                requestBody: requestBody,
+                quiet      : false,
+                userKey    : config.orgAdminUserKey,
+                httpProxy  : "http://test.sap.com:8080"
+            ]
+        ))
+
+        assertThat(result, is([ name: "testProduct"]))
+        assertThat(loggingRule.log, containsString("Sending http request with parameters"))
+        assertThat(loggingRule.log, containsString("Received response"))
     }
 }
