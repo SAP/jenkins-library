ab0275ceac23b9f0bf4aad7e555c2191b9695a0f
Christopher Fenner <Christopher.Fenner@sap.com>
2019-03-14 22:46:06 +0100

    remove accitentially checked-in file

--- vars/tempCodeRunnerFile.groovy
+++
@@ -1,3 +0,0 @@
-def source = 'https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492-linux.zip'
-                def filename = source.tokenize('/').last()
-                println filename.replace('.zip', '')
