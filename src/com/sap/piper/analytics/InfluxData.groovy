32603ac240d6613e01320e3341fa385bd2179390
Christopher Fenner <Christopher.Fenner@sap.com>
2019-03-28 17:04:17 +0100

    add InfluxData object

---
+++ src/com/sap/piper/analytics/InfluxData.groovy
@@ -0,0 +1,41 @@
+package com.sap.piper.analytics
+
+import com.cloudbees.groovy.cps.NonCPS
+
+class InfluxData implements Serializable{
+
+    // each Map in influxCustomDataMap represents a measurement in Influx.
+    // Additional measurements can be added as a new Map entry of influxCustomDataMap
+    protected Map fields = [pipeline_data: [:], step_data: [:]]
+    // each Map in influxCustomDataMapTags represents tags for certain measurement in Influx.
+    // Tags are required in Influx for easier querying data
+    protected Map tags = [pipeline_data: [:], step_data: [:]]
+
+    public Map getFields(){ return fields }
+    public Map getTags(){ return tags }
+
+    protected static InfluxData instance
+
+    @NonCPS
+    public static InfluxData getInstance(){
+        if(!instance) instance = new InfluxData()
+        return instance
+    }
+
+    public static void addField(String measurement, String key, value) {
+        add(getInstance().getFields(), measurement, key, value)
+    }
+
+    public static void addTag(String measurement, String key, value) {
+        add(getInstance().getTags(), measurement, key, value)
+    }
+
+    protected static void add(Map dataMap, String measurement, String field, value) {
+        if (!dataMap[measurement]) dataMap[measurement] = [:]
+        dataMap[measurement][field] = value
+    }
+
+    public static void reset(){
+        instance = null
+    }
+}
