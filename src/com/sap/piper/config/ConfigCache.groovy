0a0255ee44121e5ada1e1c42dc159a8197fa23c9
Marcus Holl <marcus.holl@sap.com>
2019-05-15 12:29:52 +0200

    Initialize inside constructor
    ... this is what constructors are designed for.

--- src/com/sap/piper/config/ConfigCache.groovy
+++ src/com/sap/piper/config/ConfigCache.groovy
@@ -7,36 +7,36 @@ class ConfigCache {
     private static ConfigCache INSTANCE = null
     private static PIPER_OS_DEFAULTS = 'default_pipeline_environment.yml'
 
-    Map piperDefaults = [:]  // resources/default_pipeline_env (immutable
+    final Map piperDefaults  // resources/default_pipeline_env (immutable
 
-    private ConfigCache() {
+    //final Map<String, Map> customDefaults = [:] // next step: custom layers (n), immutable
+
+    //final Map projectConfig = [:] // .pipeline/config.yml immutable
+
+    private ConfigCache(Script steps /*, Set customDefaults */ /*, String projectConfig = '.pipeline/config.yml' */) {
+
+        if(steps == null) throw new NullPointerException('Steps not available.')
+        // next step: make immutable
+        piperDefaults = pruneNulls(steps.readYaml(text: steps.libraryResource(PIPER_OS_DEFAULTS)))
+
+        steps.echo "Loading configuration file '${PIPER_OS_DEFAULTS}'"
+
+        // next step: read customConfig
+
+        // next step: read projec config
     }
-
-    Map<String, Map<?,?>> customDefaults = [:] // custom layers (n), immutable
-
-    Map projectConfig = [:] // .pipeline/config.yml immutable
 
     static synchronized ConfigCache getInstance(Script steps) {
 
         if(INSTANCE == null) {
-            INSTANCE = new ConfigCache()
-            INSTANCE.initialize(steps)
+            INSTANCE = new ConfigCache(steps)
         }
         INSTANCE
     }
 
-    // TODO: Think about reset strategy for testing. In productive coding there should be no reset method.
+    // next step: Think about reset strategy for testing. In productive coding there should be no reset method.
     public static void reset(){
         INSTANCE = null
     }
 
-    void initialize(def steps /*, Set customDefaults */ /*, String projectConfig = '.pipeline/config.yml' */) {
-
-        piperDefaults = pruneNulls(steps.readYaml(text: steps.libraryResource(PIPER_OS_DEFAULTS))) // make immutable
-        steps.echo "Loading configuration file '${PIPER_OS_DEFAULTS}'"
-
-        //customDefaults = // read custome default
-
-        // projectConfig = read config from '.pipeline/config.yml'
-    }
 }
--- test/groovy/com/sap/piper/config/ConfigCacheTest.groovy
+++ test/groovy/com/sap/piper/config/ConfigCacheTest.groovy
@@ -2,6 +2,7 @@ package com.sap.piper.config
 
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 import org.junit.rules.RuleChain
 
 import util.BasePiperTest
@@ -13,18 +14,29 @@ import static org.hamcrest.Matchers.containsString
 import static org.junit.Assert.assertThat
 
 class ConfigCacheTest extends BasePiperTest {
+
     def loggingRule = new JenkinsLoggingRule(this)
+
+    public ExpectedException thrown = ExpectedException.none()
 
     @Rule
     public RuleChain ruleChain = Rules
         .getCommonRules(this)
         .around(new JenkinsReadYamlRule(this))
         .around(loggingRule)
+        .around(thrown)
 
     @Test
     void getPiperDefaultsTest() {
         def configCache = ConfigCache.getInstance(nullScript)
         assert configCache.getPiperDefaults() != null
         assertThat(loggingRule.log, containsString("Loading configuration file 'default_pipeline_environment.yml'"))
+    }
+
+    @Test
+    void callParameterlessConstructorTest() {
+        thrown.expect(NullPointerException)
+        thrown.expectMessage('Steps not available.')
+        new ConfigCache()
     }
 }
