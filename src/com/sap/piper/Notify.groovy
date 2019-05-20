a614bcc8d1af06a80a746d783da4b391b94c12bb
Christopher Fenner <Christopher.Fenner@sap.com>
2019-05-20 14:47:43 +0200

    remove telemetry

--- src/com/sap/piper/Notify.groovy
+++ src/com/sap/piper/Notify.groovy
@@ -6,37 +6,17 @@ class Notify implements Serializable {
     protected static enum Severity { ERROR, WARNING }
 
     protected final static String LIBRARY_NAME = 'piper-lib-os'
-    protected static Utils utils = null
 
-    protected static Utils getUtilsInstance(){
-        this.utils = this.utils ?: new Utils()
-        return this.utils
+    static void warning(Script step, String message, String stepName = null){
+        notify(step, message, stepName, Severity.WARNING)
     }
 
-    static void warning(Map config, Script step, String message, String stepName = null){
-        notify(config, step, message, stepName, Severity.WARNING)
+    static void error(Script step, String message, String stepName = null) {
+        notify(step, message, stepName, Severity.ERROR)
     }
 
-    static void error(Map config, Script step, String message, String stepName = null) {
-        notify(config, step, message, stepName, Severity.ERROR)
-    }
-
-    private static void notify(Map config, Script step, String message, String stepName, Severity severity){
+    private static void notify(Script step, String message, String stepName, Severity severity){
         stepName = stepName ?: step.STEP_NAME
-
-        Telemetry.notify(step, config, [
-            folder: '',
-            repository: '',
-            step: 'Notify',
-            actionName: 'Piper Library OS',
-            eventType: 'notification',
-            jobUrlSha1: Utils.generateSha1(env.JOB_URL),
-            buildUrlSha1: Utils.generateSha1(env.BUILD_URL),
-            stepParam1: LIBRARY_NAME,
-            stepParam2: stepName,
-            stepParam3: message,
-            stepParam4: severity
-        ])
 
         def notification = "[${severity}] ${message} (${LIBRARY_NAME}/${stepName})"
 
--- test/groovy/com/sap/piper/NotifyTest.groovy
+++ test/groovy/com/sap/piper/NotifyTest.groovy
@@ -14,27 +14,21 @@ import org.junit.rules.RuleChain
 
 import util.BasePiperTest
 import util.JenkinsLoggingRule
-import util.JenkinsShellCallRule
 import util.Rules
 
 class NotifyTest extends BasePiperTest {
     private ExpectedException thrown = ExpectedException.none()
     private JenkinsLoggingRule jlr = new JenkinsLoggingRule(this)
-    private JenkinsShellCallRule jscr = new JenkinsShellCallRule(this)
-
-    private Map config
 
     @Rule
     public RuleChain rules = Rules
         .getCommonRules(this)
         .around(jlr)
-        .around(jscr)
         .around(thrown)
 
     @Before
     void init() throws Exception {
         // prepare
-        config = [collectTelemetryData: true]
         nullScript.STEP_NAME = 'anyStep'
         utils.env.JOB_NAME = 'testJob'
         Notify.instance = utils
@@ -43,11 +37,9 @@ class NotifyTest extends BasePiperTest {
     @Test
     void testWarning() {
         // execute test
-        Notify.warning(config, nullScript, "test message")
+        Notify.warning(nullScript, "test message")
         // asserts
         assertThat(jlr.log, containsString('[WARNING] test message (piper-lib-os/anyStep)'))
-        assertThat(jscr.shell, hasItem(containsString('curl -G -v "https://webanalytics.cfapps.eu10.hana.ondemand.com/tracker/log"')))
-        assertThat(jscr.shell, hasItem(containsString('--data-urlencode "custom14=WARNING"')))
     }
 
     @Test
@@ -55,12 +47,6 @@ class NotifyTest extends BasePiperTest {
         thrown.expect(hudson.AbortException)
         thrown.expectMessage('[ERROR] test message (piper-lib-os/anyOtherStep)')
         // execute test
-        try{
-            Notify.error(config, nullScript, "test message", "anyOtherStep")
-        }finally{
-            // asserts
-            assertThat(jscr.shell, hasItem(containsString('curl -G -v "https://webanalytics.cfapps.eu10.hana.ondemand.com/tracker/log"')))
-            assertThat(jscr.shell, hasItem(containsString('--data-urlencode "custom14=ERROR"')))
-        }
+        Notify.error(nullScript, "test message", "anyOtherStep")
     }
 }
