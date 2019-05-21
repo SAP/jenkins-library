d0e6b1a8ed468d5e26c32db828f99cb326ff3ca2
Marcus Holl <marcus.holl@sap.com>
2019-05-21 12:45:46 +0200

    wip

--- documentation/bin/createDocu.groovy
+++ documentation/bin/createDocu.groovy
@@ -579,7 +579,7 @@ def handleStep(stepName, prepareDefaultValuesStep, gse, customDefaults) {
 
     File theStep = new File(stepsDir, "${stepName}.groovy")
     File theStepDocu = new File(stepsDocuDir, "${stepName}.md")
-    File theStepDeps = new File('jenkins_workspace/result.json')
+    File theStepDeps = new File('documentation/jenkins_workspace/result.json')
 
     if(!theStepDocu.exists()) {
         System.err << "[WARNING] step docu input file for step '${stepName}' is missing.\n"
