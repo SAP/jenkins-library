2bac2b6dbc84ac90a4be649c9f222ac7a4e59692
Marcus Holl <marcus.holl@sap.com>
2019-05-21 12:19:32 +0200

    run condence steps within createDocu

--- documentation/bin/createDocu.sh
+++ documentation/bin/createDocu.sh
@@ -5,4 +5,5 @@ d=$(dirname "$0")
 
 export CLASSPATH_FILE='target/cp.txt'
 mvn clean test dependency:build-classpath -Dmdep.outputFile=${CLASSPATH_FILE} > /dev/null 2>&1
+groovy  "${d}steps" -in target/trackedCalls.json --out target/performedCalls.json
 groovy -cp "target/classes:$(cat $CLASSPATH_FILE)" "${d}createDocu" "${@}"
