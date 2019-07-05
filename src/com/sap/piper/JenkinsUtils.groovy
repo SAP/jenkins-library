a89648703239f8cf3da465bfa570500608dc14f2
Christopher Fenner <Christopher.Fenner@sap.com>
2019-07-05 21:40:10 +0200

    use classloader

--- src/com/sap/piper/JenkinsUtils.groovy
+++ src/com/sap/piper/JenkinsUtils.groovy
@@ -20,12 +20,15 @@ static boolean hasTestFailures(build){
 }
 
 static boolean addWarningsNGParser(String id, String name, String regex, String script, String example = ''){
-    if(io.jenkins.plugins.analysis.warnings.groovy.ParserConfiguration.getInstance().contains(id)){
+    def classLoader = this.getClass().getClassLoader()
+    config = classLoader.loadClass('io.jenkins.plugins.analysis.warnings.groovy.ParserConfiguration', true, false)?.getInstance()
+
+    if(config.contains(id)){
         return false
     }else{
-        io.jenkins.plugins.analysis.warnings.groovy.ParserConfiguration.getInstance().setParsers(
-            io.jenkins.plugins.analysis.warnings.groovy.ParserConfiguration.getInstance().getParsers().plus(
-                new io.jenkins.plugins.analysis.warnings.groovy.GroovyParser(id, name, regex, script, example)
+        config.setParsers(
+            config.getParsers().plus(
+                classLoader.loadClass('io.jenkins.plugins.analysis.warnings.groovy.GroovyParser', true, false)?.newInstance(id, name, regex, script, example)
             )
         )
         return true
