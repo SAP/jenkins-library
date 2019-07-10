2b94eb11f554211321a43dd947274b84c6b89099
Christopher Fenner <Christopher.Fenner@sap.com>
2019-07-10 13:28:27 +0200

    add docs

--- vars/piperPublishWarnings.groovy
+++ vars/piperPublishWarnings.groovy
@@ -11,15 +11,39 @@ import groovy.transform.Field
 
 @Field Set GENERAL_CONFIG_KEYS = []
 @Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS.plus([
+    /**
+     * The id of the Groovy script parser. If the id is not present in the current Jenkins configuration it is created.
+     */
     'parserId',
+    /**
+     * The display name for the warnings parsed by the parser.
+     * Only considered if a new parser is created.
+     */
     'parserName',
+    /**
+     * The pattern used to parse the log file.
+     * Only considered if a new parser is created.
+     */
     'parserPattern',
+    /**
+     * The script used to parse the matches produced by the pattern into issues.
+     * Only considered if a new parser is created.
+     * see https://github.com/jenkinsci/analysis-model/blob/master/src/main/java/edu/hm/hafner/analysis/IssueBuilder.java
+     */
     'parserScript',
+    /**
+     * Settings that are passed to the recordIssues step of the warnings-ng plugin.
+     * see https://github.com/jenkinsci/warnings-ng-plugin/blob/master/doc/Documentation.md#configuration
+     */
     'recordIssuesSettings'
 ])
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS.plus([])
 
-/***/
+/**
+ * This step scans the current build log for messages produces by the Piper library steps and published them on the Jenkins job run as *Piper warnings*.
+ *
+ * The default parser detects log entries with the following pattern: `[<SEVERITY>] <MSG> (<LIBRARY>/<STEP>)`
+ */
 @GenerateDocumentation
 void call(Map parameters = [:]) {
     handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters, allowBuildFailure: true) {
