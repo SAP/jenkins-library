bf0ad12fa6b59fca78a2660fac91bb969880e307
Christopher Fenner <Christopher.Fenner@sap.com>
2019-07-10 10:55:54 +0200

    extract recordIssuesSettings to defaults

--- vars/piperPublishWarnings.groovy
+++ vars/piperPublishWarnings.groovy
@@ -1,24 +1,30 @@
+import static com.sap.piper.Prerequisites.checkScript
+
 import com.sap.piper.ConfigurationHelper
+import com.sap.piper.GenerateDocumentation
 import com.sap.piper.JenkinsUtils
 import com.sap.piper.Utils
 
 import groovy.transform.Field
 
-import static com.sap.piper.Prerequisites.checkScript
-
-@Field def STEP_NAME = getClass().getName()
+@Field String STEP_NAME = getClass().getName()
 
 @Field Set GENERAL_CONFIG_KEYS = []
 @Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS.plus([
     'parserId',
     'parserName',
     'parserPattern',
-    'parserScript'
+    'parserScript',
+    'recordIssuesSettings'
 ])
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS.plus([])
 
+/***/
+@GenerateDocumentation
 void call(Map parameters = [:]) {
     handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters, allowBuildFailure: true) {
+
+        //TODO: ensure warnings-ng plugin is installed.
 
         final script = checkScript(this, parameters) ?: this
 
@@ -52,12 +58,12 @@ void call(Map parameters = [:]) {
                 writeFile file: 'buildlog', text: JenkinsUtils.getFullBuildLog(script.currentBuild)
                 // parse log for Piper Notifications
                 recordIssues(
-                    blameDisabled: true,
-                    enabledForFailure: true,
-                    tools: [groovyScript(
-                        parserId: configuration.parserId,
-                        pattern: 'buildlog'
-                    )]
+                    configuration.recordIssuesSettings.plus([
+                        tools: [groovyScript(
+                            parserId: configuration.parserId,
+                            pattern: 'buildlog'
+                        )]
+                    ])
                 )
             }finally{
                 deleteDir()
