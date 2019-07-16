39129aeecb786a3e8c4bee4a3230a936b3b32b30
Marcus Holl <marcus.holl@sap.com>
2019-07-16 11:30:39 +0200

    troubleshoot proxy

--- vars/artifactSetVersion.groovy
+++ vars/artifactSetVersion.groovy
@@ -277,6 +277,8 @@ void call(Map parameters = [:], Closure body = null) {
                      gitPushFlags = gitPushFlags.join(' ')
 
                      sh script: """|#!/bin/bash ${hashbangFlags}
+                                   |unset https_proxy
+                                   |unset http_proxy
                                    |${gitDebug}git ${gitConfig} push ${gitPushFlags} ${gitUrlWithCredentials} ${config.tagPrefix}${newVersion} ${streamhandling}""".stripMargin()
                  }
             } else {
