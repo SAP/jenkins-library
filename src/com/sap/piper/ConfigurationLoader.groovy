7845e18f4d9629f382fdf5ceb92885281ed70e46
Christopher Fenner <Christopher.Fenner@sap.com>
2019-07-29 10:17:56 +0200

    fix NonCPS issues (#796)
    * remove NonCPS statement
    
    * remove NonCPS anntotation
    
    * fix typo
    
    * remove NonCPS anntotation
    
    * remove NonCPS anntotation
    
    * remove NonCPS anntotation
    
    * remove NonCPS anntotation
    
    * remove NonCPS anntotation
    
    * remove Iterable
    
    * remove mixins
    
    * add mixins
    
    * add mixins 2
    
    * add mixins 3
    
    * add NonCPS anntotation
    
    * remove tokenize
    
    * remove closure
    
    * remove closure
    
    * replace closure
    
    * use Object
    
    * use Object
    
    * use Object
    
    * remove object
    
    * remove object
    
    * add logic
    
    * change type
    
    * change type
    
    * remove NonCPS anntotation
    
    * remove NonCPS anntotation
    
    * add import

--- src/com/sap/piper/ConfigurationLoader.groovy
+++ src/com/sap/piper/ConfigurationLoader.groovy
@@ -1,30 +1,23 @@
 package com.sap.piper
 
-import com.cloudbees.groovy.cps.NonCPS
-
 @API(deprecated = true)
 class ConfigurationLoader implements Serializable {
-    @NonCPS
     static Map stepConfiguration(script, String stepName) {
         return loadConfiguration(script, 'steps', stepName, ConfigurationType.CUSTOM_CONFIGURATION)
     }
 
-    @NonCPS
     static Map stageConfiguration(script, String stageName) {
         return loadConfiguration(script, 'stages', stageName, ConfigurationType.CUSTOM_CONFIGURATION)
     }
 
-    @NonCPS
     static Map defaultStepConfiguration(script, String stepName) {
         return loadConfiguration(script, 'steps', stepName, ConfigurationType.DEFAULT_CONFIGURATION)
     }
 
-    @NonCPS
     static Map defaultStageConfiguration(script, String stageName) {
         return loadConfiguration(script, 'stages', stageName, ConfigurationType.DEFAULT_CONFIGURATION)
     }
 
-    @NonCPS
     static Map generalConfiguration(script){
         try {
             return script?.commonPipelineEnvironment?.configuration?.general ?: [:]
@@ -33,17 +26,14 @@ class ConfigurationLoader implements Serializable {
         }
     }
 
-    @NonCPS
     static Map defaultGeneralConfiguration(script){
         return DefaultValueCache.getInstance()?.getDefaultValues()?.general ?: [:]
     }
 
-    @NonCPS
     static Map postActionConfiguration(script, String actionName){
         return loadConfiguration(script, 'postActions', actionName, ConfigurationType.CUSTOM_CONFIGURATION)
     }
 
-    @NonCPS
     private static Map loadConfiguration(script, String type, String entryName, ConfigurationType configType){
         switch (configType) {
             case ConfigurationType.CUSTOM_CONFIGURATION:
