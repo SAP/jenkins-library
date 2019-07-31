3ae1cabafe02abd66c3b73513f59b3c6e998149b
Oliver Feldmann <oliver.feldmann@sap.com>
2019-07-31 12:22:26 +0200

    setupCommonPipelineEnvironment: support yaml config file ending (#811)
    * Allow for yaml file ending
    
    * Format code

--- test/groovy/SetupCommonPipelineEnvironmentTest.groovy
+++ test/groovy/SetupCommonPipelineEnvironmentTest.groovy
@@ -3,17 +3,12 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.yaml.snakeyaml.Yaml
-
-import com.sap.piper.Utils
-
 import util.BasePiperTest
-import util.Rules
-import util.JenkinsReadYamlRule
 import util.JenkinsStepRule
+import util.Rules
 
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertNotNull
-
 
 class SetupCommonPipelineEnvironmentTest extends BasePiperTest {
 
@@ -33,7 +28,7 @@ class SetupCommonPipelineEnvironmentTest extends BasePiperTest {
 
         helper.registerAllowedMethod("readYaml", [Map], { Map parameters ->
             Yaml yamlParser = new Yaml()
-            if(parameters.text) {
+            if (parameters.text) {
                 return yamlParser.load(parameters.text)
             }
             usedConfigFile = parameters.file
@@ -51,6 +46,21 @@ class SetupCommonPipelineEnvironmentTest extends BasePiperTest {
         stepRule.step.setupCommonPipelineEnvironment(script: nullScript)
 
         assertEquals('.pipeline/config.yml', usedConfigFile)
+        assertNotNull(nullScript.commonPipelineEnvironment.configuration)
+        assertEquals('develop', nullScript.commonPipelineEnvironment.configuration.general.productiveBranch)
+        assertEquals('my-maven-docker', nullScript.commonPipelineEnvironment.configuration.steps.mavenExecute.dockerImage)
+    }
+
+    @Test
+    void testWorksAlsoWithYamlFileEnding() throws Exception {
+
+        helper.registerAllowedMethod("fileExists", [String], { String path ->
+            return path.endsWith('.pipeline/config.yaml')
+        })
+
+        stepRule.step.setupCommonPipelineEnvironment(script: nullScript)
+
+        assertEquals('.pipeline/config.yaml', usedConfigFile)
         assertNotNull(nullScript.commonPipelineEnvironment.configuration)
         assertEquals('develop', nullScript.commonPipelineEnvironment.configuration.general.productiveBranch)
         assertEquals('my-maven-docker', nullScript.commonPipelineEnvironment.configuration.steps.mavenExecute.dockerImage)
