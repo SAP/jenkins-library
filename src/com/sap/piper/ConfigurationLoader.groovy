e2cd7c5b4010b7767c88fbf7b841b7cefb2a4d3d
Marcus Holl <marcus.holl@sap.com>
2019-08-08 12:47:28 +0200

    ConfigurationLoader, ConfigurationHelper working without script reference

--- src/com/sap/piper/ConfigurationLoader.groovy
+++ src/com/sap/piper/ConfigurationLoader.groovy
@@ -1,44 +1,104 @@
 package com.sap.piper
 
+// script is present in the signatures in order to keep api compatibility.
+// The script referenced is not used inside the method bodies.
+
 @API(deprecated = true)
 class ConfigurationLoader implements Serializable {
+
+    static Map stepConfiguration(String stepName) {
+        return stepConfiguration(null, stepName)
+    }
+    @Deprecated
+    /** Use stepConfiguration(stepName) instead */
     static Map stepConfiguration(script, String stepName) {
-        return loadConfiguration(script, 'steps', stepName, ConfigurationType.CUSTOM_CONFIGURATION)
+        return loadConfiguration('steps', stepName, ConfigurationType.CUSTOM_CONFIGURATION)
     }
 
+    /*
+     * By default this methods does nothing. With this method we are able to ensure that we do not call the
+     * deprecated methods. Might be usefull during local development.
+     */
+    private static handleDeprecation(script, String methodName) {
+        if(script != null) {
+            def msg = "ConfigurationLoader.${methodName} was called with a script reference." +
+                'This method is deprecated. Use the same method without the script reference'
+            if(Boolean.getBoolean('com.sap.piper.failOnScriptReferenceInConfigurationLoader'))
+                throw new RuntimeException(msg)
+            if(Boolean.getBoolean('com.sap.piper.emitWarningOnScriptReferenceInConfigurationLoader') &&
+                script instanceof Script) script.echo("[WARNING] ${msg}")
+        }
+    }
+
+    static Map stageConfiguration(String stageName) {
+        stageConfiguration(null, stageName)
+    }
+    @Deprecated
+    /** Use stageConfiguration(stageName) instead */
     static Map stageConfiguration(script, String stageName) {
-        return loadConfiguration(script, 'stages', stageName, ConfigurationType.CUSTOM_CONFIGURATION)
+        handleDeprecation(script, 'stageConfiguration')
+        return loadConfiguration('stages', stageName, ConfigurationType.CUSTOM_CONFIGURATION)
     }
 
+    static Map defaultStepConfiguration(String stepName) {
+        defaultStepConfiguration(null, stepName)
+    }
+    @Deprecated
+    /** Use defaultStepConfiguration(stepName) instead */
     static Map defaultStepConfiguration(script, String stepName) {
-        return loadConfiguration(script, 'steps', stepName, ConfigurationType.DEFAULT_CONFIGURATION)
+        handleDeprecation(script, 'defaultStepConfiguration')
+        return loadConfiguration('steps', stepName, ConfigurationType.DEFAULT_CONFIGURATION)
     }
 
+    static Map defaultStageConfiguration(String stageName) {
+        defaultStageConfiguration(null, stageName)
+    }
+    @Deprecated
+    /** Use defaultStageConfiguration(stepName) instead */
     static Map defaultStageConfiguration(script, String stageName) {
-        return loadConfiguration(script, 'stages', stageName, ConfigurationType.DEFAULT_CONFIGURATION)
+        handleDeprecation(script, 'defaultStageConfiguration')
+        return loadConfiguration('stages', stageName, ConfigurationType.DEFAULT_CONFIGURATION)
     }
 
+    static Map generalConfiguration(){
+        generalConfiguration(null)
+    }
+    @Deprecated
+    /** Use generalConfiguration() instead */
     static Map generalConfiguration(script){
+        handleDeprecation(script, 'generalConfiguration')
         try {
-            return script?.commonPipelineEnvironment?.configuration?.general ?: [:]
+            return CommonPipelineEnvironment.getInstance()?.configuration?.general ?: [:]
         } catch (groovy.lang.MissingPropertyException mpe) {
             return [:]
         }
     }
 
+    static Map defaultGeneralConfiguration(){
+        defaultGeneralConfiguration(null)
+    }
+    @Deprecated
+    /** Use defaultGeneralConfiguration() instead */
     static Map defaultGeneralConfiguration(script){
+        handleDeprecation(script, 'defaultGeneralConfiguration')
         return DefaultValueCache.getInstance()?.getDefaultValues()?.general ?: [:]
     }
 
+    static Map postActionConfiguration(String actionName){
+        postActionConfiguration(null, actionName)
+    }
+    @Deprecated
+    /** Use postActionConfiguration() instead */
     static Map postActionConfiguration(script, String actionName){
-        return loadConfiguration(script, 'postActions', actionName, ConfigurationType.CUSTOM_CONFIGURATION)
+        handleDeprecation(script, 'postActionConfiguration')
+        return loadConfiguration('postActions', actionName, ConfigurationType.CUSTOM_CONFIGURATION)
     }
 
-    private static Map loadConfiguration(script, String type, String entryName, ConfigurationType configType){
+    private static Map loadConfiguration(String type, String entryName, ConfigurationType configType){
         switch (configType) {
             case ConfigurationType.CUSTOM_CONFIGURATION:
                 try {
-                    return script?.commonPipelineEnvironment?.configuration?.get(type)?.get(entryName) ?: [:]
+                    return CommonPipelineEnvironment.getInstance()?.configuration?.get(type)?.get(entryName) ?: [:]
                 } catch (groovy.lang.MissingPropertyException mpe) {
                     return [:]
                 }
