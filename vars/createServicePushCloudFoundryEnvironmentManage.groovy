57c8e44f4364d3306ff399b89796668617523b0e
Shanuson <54803480+Shanuson@users.noreply.github.com>
2019-09-30 14:06:39 +0200

    fixed missing annotation

--- vars/createServicePushCloudFoundryEnvironmentManage.groovy
+++ vars/createServicePushCloudFoundryEnvironmentManage.groovy
@@ -1,7 +1,9 @@
+import com.sap.piper.GenerateDocumentation
 import com.sap.piper.BashUtils
 import com.sap.piper.JenkinsUtils
 import com.sap.piper.Utils
 import com.sap.piper.ConfigurationHelper
+
 import groovy.transform.Field
 
 import static com.sap.piper.Prerequisites.checkScript
