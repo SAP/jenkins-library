8be17ceed6914df90705de7e5e80e9bfb849f2b9
Daniel Mieg <daniel.mieg@sap.com>
2019-10-23 12:17:36 +0200

    Change x-csrf-token handling

--- vars/pullSoftwareComponentToAbapCloudPlatform.groovy
+++ vars/pullSoftwareComponentToAbapCloudPlatform.groovy
@@ -96,6 +96,7 @@ private String triggerPull(Map configuration, String url, String authToken) {
             -H 'Authorization: Basic ${authToken}' \
             -H 'Accept: application/json' \
             -H 'Content-Type: application/json' \
+            -H 'x-csrf-token: ${xCsrfToken}‚'
             --cookie ${headerFile} \
             -d '{ \"sc_name\": \"${configuration.repositoryName}\" }'
         """
