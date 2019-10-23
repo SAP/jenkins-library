c4c0aff600666867f7019519f5b9d1f2f08acd9b
OliverNocon <oliver.nocon@sap.com>
2019-10-23 15:03:26 +0200

    Update generator

--- pkg/generator/step-metadata.go
+++ pkg/generator/step-metadata.go
@@ -72,7 +72,7 @@ func {{.CobraCmdFuncName}}() *cobra.Command {
 		Long:   {{ $tick := "` + "`" + `" }}{{ $tick }}{{.Long | longName }}{{ $tick }},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			metadata := {{ .StepName }}Metadata()
-			return PrepareConfig(cmd, &metadata, "{{ .StepName }}", &my{{ .StepName | title}}Options)
+			return PrepareConfig(cmd, &metadata, "{{ .StepName }}", &my{{ .StepName | title}}Options, openPiperFile)
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return {{.StepName}}(my{{ .StepName | title }}Options)
