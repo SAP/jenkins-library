cb9bf9563335027851f6db37c5282cc472981e6a
Christopher Fenner <Christopher.Fenner@sap.com>
2019-11-04 14:19:46 +0100

    adapt test cases

--- cmd/karmaExecuteTests.go
+++ cmd/karmaExecuteTests.go
@@ -13,10 +13,11 @@ func karmaExecuteTests(myKarmaExecuteTestsOptions karmaExecuteTestsOptions) erro
 	// also log stdout as Karma reports into it
 	c.Stdout = log.Entry().Writer()
 	c.Stderr = log.Entry().Writer()
-	return runKarma(myKarmaExecuteTestsOptions, &c)
+	runKarma(myKarmaExecuteTestsOptions, &c)
+	return nil
 }
 
-func runKarma(myKarmaExecuteTestsOptions karmaExecuteTestsOptions, command execRunner) error {
+func runKarma(myKarmaExecuteTestsOptions karmaExecuteTestsOptions, command execRunner) {
 	installCommandTokens := tokenize(myKarmaExecuteTestsOptions.InstallCommand)
 	command.Dir(myKarmaExecuteTestsOptions.ModulePath)
 	err := command.RunExecutable(installCommandTokens[0], installCommandTokens[1:]...)
@@ -36,8 +37,6 @@ func runKarma(myKarmaExecuteTestsOptions karmaExecuteTestsOptions, command execR
 			WithField("command", myKarmaExecuteTestsOptions.RunCommand).
 			Fatal("failed to execute run command")
 	}
-
-	return nil
 }
 
 func tokenize(command string) []string {
--- cmd/karmaExecuteTests_test.go
+++ cmd/karmaExecuteTests_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"testing"
 
+	"github.com/SAP/jenkins-library/pkg/log"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -11,9 +12,7 @@ func TestRunKarma(t *testing.T) {
 		opts := karmaExecuteTestsOptions{ModulePath: "./test", InstallCommand: "npm install test", RunCommand: "npm run test"}
 
 		e := execMockRunner{}
-		err := runKarma(opts, &e)
-
-		assert.NoError(t, err, "error occured but no error expected")
+		runKarma(opts, &e)
 
 		assert.Equal(t, e.dir[0], "./test", "install command dir incorrect")
 		assert.Equal(t, e.calls[0], execCall{exec: "npm", params: []string{"install", "test"}}, "install command/params incorrect")
@@ -24,18 +23,24 @@ func TestRunKarma(t *testing.T) {
 	})
 
 	t.Run("error case install command", func(t *testing.T) {
+		var hasFailed bool
+		log.Entry().Logger.ExitFunc = func(code int) { hasFailed = true }
+
 		opts := karmaExecuteTestsOptions{ModulePath: "./test", InstallCommand: "fail install test", RunCommand: "npm run test"}
 
 		e := execMockRunner{}
-		err := runKarma(opts, &e)
-		assert.Error(t, err, "error expected but none occcured")
+		runKarma(opts, &e)
+		assert.True(t, hasFailed, "expected command to exit with fatal")
 	})
 
 	t.Run("error case run command", func(t *testing.T) {
+		var hasFailed bool
+		log.Entry().Logger.ExitFunc = func(code int) { hasFailed = true }
+
 		opts := karmaExecuteTestsOptions{ModulePath: "./test", InstallCommand: "npm install test", RunCommand: "fail run test"}
 
 		e := execMockRunner{}
-		err := runKarma(opts, &e)
-		assert.Error(t, err, "error expected but none occcured")
+		runKarma(opts, &e)
+		assert.True(t, hasFailed, "expected command to exit with fatal")
 	})
 }
