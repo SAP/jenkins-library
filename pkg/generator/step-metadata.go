282d90775dbe91283569e0afa87ae94490e513c4
Oliver Nocon <33484802+OliverNocon@users.noreply.github.com>
2019-11-06 14:07:41 +0100

    Export general configuration (#956)
    Allow for package external access to general configuration.
    
    Use-case: re-use individual steps

--- pkg/generator/step-metadata.go
+++ pkg/generator/step-metadata.go
@@ -54,7 +54,7 @@ func {{.CobraCmdFuncName}}() *cobra.Command {
 		Long: {{ $tick := "` + "`" + `" }}{{ $tick }}{{.Long | longName }}{{ $tick }},
 		PreRunE: func(cmd *cobra.Command, args []string) error {
 			log.SetStepName("{{ .StepName }}")
-			log.SetVerbose(generalConfig.verbose)
+			log.SetVerbose(GeneralConfig.verbose)
 			return PrepareConfig(cmd, &metadata, "{{ .StepName }}", &my{{ .StepName | title}}Options, openPiperFile)
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
