8581281c5a3cabe80b1c6a9b87c699a74048e481
Daniel Mieg <daniel.mieg@sap.com>
2019-11-18 16:03:57 +0100

    Add documentation

---
+++ test/groovy/CloudFoundryCreateServiceKeyTest.groovy
@@ -0,0 +1,83 @@
+import java.util.Map
+import static org.hamcrest.Matchers.hasItem
+import static org.junit.Assert.assertThat
+
+import org.hamcrest.Matchers
+import static org.hamcrest.Matchers.containsString
+import static org.hamcrest.Matchers.equalTo
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+import org.junit.rules.RuleChain
+
+import util.BasePiperTest
+import util.JenkinsCredentialsRule
+import util.JenkinsStepRule
+import util.JenkinsLoggingRule
+import util.JenkinsReadYamlRule
+import util.JenkinsShellCallRule
+import util.Rules
+
+import hudson.AbortException
+
+public class CloudFoundryCreateServiceKeyTest extends BasePiperTest {
+
+    private ExpectedException thrown = new ExpectedException()
+    private JenkinsStepRule stepRule = new JenkinsStepRule(this)
+    private JenkinsLoggingRule loggingRule = new JenkinsLoggingRule(this)
+    private JenkinsShellCallRule shellRule = new JenkinsShellCallRule(this)
+    private JenkinsCredentialsRule credentialsRule = new JenkinsCredentialsRule(this).withCredentials('test_credentialsId', 'user', 'password')
+
+    @Rule
+    public RuleChain ruleChain = Rules.getCommonRules(this)
+        .around(new JenkinsReadYamlRule(this))
+        .around(thrown)
+        .around(stepRule)
+        .around(loggingRule)
+        .around(credentialsRule)
+        .around(shellRule)
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void success() {
+        shellRule.setReturnValue(JenkinsShellCallRule.Type.REGEX, /.*cf create-service-key.*/, 0 )
+         
+        stepRule.step.cloudFoundryCreateServiceKey(
+            script: nullScript,
+            cloudFoundry: [
+                apiEndpoint: 'api.example.com',
+                credentialsId: 'test_credentialsId',
+                org: 'testOrg',
+                space: 'testSpace',
+                serviceInstance : 'myInstance',
+                serviceKey : 'myServiceKey',
+                serviceKeyConfig : '{ "key" : "value" }' 
+                ]
+        )
+
+        assertThat(shellRule.shell, hasItem(containsString("#!/bin/bash set +x set -e export HOME=null cf login -u 'user' -p 'password' -a api.example.com -o 'testOrg' -s 'testSpace'; cf create-service-key 'myInstance' 'myServiceKey' -c '{ \"key\" : \"value\" }'")))
+    }
+
+    public void fail() {
+        shellRule.setReturnValue(JenkinsShellCallRule.Type.REGEX, /.*cf create-service-key.*/, 1 )
+         
+        stepRule.step.cloudFoundryCreateServiceKey(
+            script: nullScript,
+            cloudFoundry: [
+                apiEndpoint: 'api.example.com',
+                credentialsId: 'test_credentialsId',
+                org: 'testOrg',
+                space: 'testSpace',
+                serviceInstance : 'myInstance',
+                serviceKey : 'myServiceKey',
+                serviceKeyConfig : '{ "key" : "value" }' 
+                ]
+        )
+
+        assertThat(shellRule.shell, hasItem(containsString("[cloudFoundryCreateServiceKey] ERROR: The execution of the create-service-key failed, see the logs above for more details.")))
+    }
+}
