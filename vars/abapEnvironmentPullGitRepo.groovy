1d82a2f502ea04319e9cea996050af7ee3a9812c
Daniel Mieg <daniel.mieg@sap.com>
2019-11-19 12:27:03 +0100

    test

--- vars/abapEnvironmentPullGitRepo.groovy
+++ vars/abapEnvironmentPullGitRepo.groovy
@@ -120,8 +120,8 @@ void call(Map parameters = [:]) {
                         echo status.toString()
                         String responseString = readFile("response.json")
                         echo responseString
-                        Pattern p = Pattern.compile(/\{.*\}$/, Pattern.MULTILINE | Pattern.DOTALL)
-                        Matcher m = p.matcher(responseString)
+                        def p = Pattern.compile(/\{.*\}$/, Pattern.MULTILINE | Pattern.DOTALL)
+                        def m = p.matcher(responseString)
                         String jsonString
                         if (m.find()) {
                             jsonString = m[0]
