3db61e40e3e548d26da9d8c3bc78123d01cea4ce
Christopher Fenner <Christopher.Fenner@sap.com>
2019-12-09 17:49:21 +0100

    refactor(sonar): add sonar cmd to executable

--- cmd/piper.go
+++ cmd/piper.go
@@ -44,6 +44,7 @@ func Execute() {
 	rootCmd.AddCommand(ConfigCommand())
 	rootCmd.AddCommand(VersionCommand())
 	rootCmd.AddCommand(KarmaExecuteTestsCommand())
+	rootCmd.AddCommand(SonarExecuteScanCommand())
 	rootCmd.AddCommand(XsDeployCommand())
 	rootCmd.AddCommand(GithubPublishReleaseCommand())
 
