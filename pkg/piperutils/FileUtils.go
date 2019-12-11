207940cf6468ebce582ef0c30b1d40d011c3f8b5
Marcus Holl <marcus.holl@sap.com>
2019-12-11 08:30:20 +0100

    streamline

--- pkg/piperutils/FileUtils.go
+++ pkg/piperutils/FileUtils.go
@@ -23,12 +23,13 @@ func FileExists(filename string) (bool, error) {
 // Copy ...
 func Copy(src, dst string) (int64, error) {
 
-	if exists, err := FileExists(src); exists && err == nil {
+	exists, err := FileExists(src)
+
+	if err != nil {
+		return 0, err
+	}
+	if !exists {
 		errors.New("Source file '" + src + "' does not exist")
-	} else {
-		if err != nil {
-			return 0, err
-		}
 	}
 
 	source, err := os.Open(src)
