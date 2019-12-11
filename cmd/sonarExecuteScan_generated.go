2dd083be647e13bb1c3a36647cb689eade39cc9d
Christopher Fenner <Christopher.Fenner@sap.com>
2019-12-12 00:23:13 +0100

    update step metadata

--- cmd/sonarExecuteScan_generated.go
+++ cmd/sonarExecuteScan_generated.go
@@ -9,15 +9,20 @@ import (
 )
 
 type sonarExecuteScanOptions struct {
-	Instance                  string `json:"instance,omitempty"`
-	DisableInlineComments     string `json:"disableInlineComments,omitempty"`
-	GithubOrg                 string `json:"githubOrg,omitempty"`
-	LegacyPRHandling          string `json:"legacyPRHandling,omitempty"`
-	GithubRepo                string `json:"githubRepo,omitempty"`
+	Host                      string `json:"host,omitempty"`
+	Token                     string `json:"token,omitempty"`
+	GithubToken               string `json:"githubToken,omitempty"`
+	ChangeID                  string `json:"changeId,omitempty"`
+	DisableInlineComments     bool   `json:"disableInlineComments,omitempty"`
+	LegacyPRHandling          bool   `json:"legacyPRHandling,omitempty"`
+	Owner                     string `json:"owner,omitempty"`
+	Repository                string `json:"repository,omitempty"`
 	GithubAPIURL              string `json:"githubApiUrl,omitempty"`
 	Organization              string `json:"organization,omitempty"`
 	Options                   string `json:"options,omitempty"`
 	CustomTLSCertificateLinks string `json:"customTlsCertificateLinks,omitempty"`
+	PullRequestProvider       string `json:"pullRequestProvider,omitempty"`
+	SonarScannerDownloadURL   string `json:"sonarScannerDownloadUrl,omitempty"`
 	ProjectVersion            string `json:"projectVersion,omitempty"`
 }
 
@@ -46,15 +51,20 @@ func SonarExecuteScanCommand() *cobra.Command {
 }
 
 func addSonarExecuteScanFlags(cmd *cobra.Command) {
-	cmd.Flags().StringVar(&mySonarExecuteScanOptions.Instance, "instance", "SonarCloud", "The name of the SonarQube instance defined in the Jenkins settings.")
-	cmd.Flags().StringVar(&mySonarExecuteScanOptions.DisableInlineComments, "disableInlineComments", os.Getenv("PIPER_disableInlineComments"), "Pull-Request voting only: Disables the pull-request decoration with inline comments. deprecated: only supported in < 7.2")
-	cmd.Flags().StringVar(&mySonarExecuteScanOptions.GithubOrg, "githubOrg", os.Getenv("PIPER_githubOrg"), "Pull-Request voting only: The Github organization. @default: `commonPipelineEnvironment.getGithubOrg()`")
-	cmd.Flags().StringVar(&mySonarExecuteScanOptions.LegacyPRHandling, "legacyPRHandling", os.Getenv("PIPER_legacyPRHandling"), "Pull-Request voting only: Activates the pull-request handling using the [GitHub Plugin](https://docs.sonarqube.org/display/PLUG/GitHub+Plugin) (deprecated). deprecated: only supported in < 7.2")
-	cmd.Flags().StringVar(&mySonarExecuteScanOptions.GithubRepo, "githubRepo", os.Getenv("PIPER_githubRepo"), "Pull-Request voting only: The Github repository. @default: `commonPipelineEnvironment.getGithubRepo()`")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.Host, "host", os.Getenv("PIPER_host"), "")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.Token, "token", os.Getenv("PIPER_token"), "")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.GithubToken, "githubToken", os.Getenv("PIPER_githubToken"), "")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.ChangeID, "changeId", os.Getenv("PIPER_changeId"), "")
+	cmd.Flags().BoolVar(&mySonarExecuteScanOptions.DisableInlineComments, "disableInlineComments", false, "Pull-Request voting only: Disables the pull-request decoration with inline comments. deprecated: only supported in < 7.2")
+	cmd.Flags().BoolVar(&mySonarExecuteScanOptions.LegacyPRHandling, "legacyPRHandling", false, "Pull-Request voting only: Activates the pull-request handling using the [GitHub Plugin](https://docs.sonarqube.org/display/PLUG/GitHub+Plugin) (deprecated). deprecated: only supported in < 7.2")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.Owner, "owner", os.Getenv("PIPER_owner"), "Pull-Request voting only: The Github organization. @default: `commonPipelineEnvironment.getGithubOrg()`")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.Repository, "repository", os.Getenv("PIPER_repository"), "Pull-Request voting only: The Github repository. @default: `commonPipelineEnvironment.getGithubRepo()`")
 	cmd.Flags().StringVar(&mySonarExecuteScanOptions.GithubAPIURL, "githubApiUrl", "https://api.github.com", "Pull-Request voting only: The URL to the Github API. see [GitHub plugin docs](https://docs.sonarqube.org/display/PLUG/GitHub+Plugin#GitHubPlugin-Usage) deprecated: only supported in < 7.2")
 	cmd.Flags().StringVar(&mySonarExecuteScanOptions.Organization, "organization", os.Getenv("PIPER_organization"), "Organization that the project will be assigned to in SonarCloud.io.")
 	cmd.Flags().StringVar(&mySonarExecuteScanOptions.Options, "options", "[]", "A list of options which are passed to the `sonar-scanner`.")
 	cmd.Flags().StringVar(&mySonarExecuteScanOptions.CustomTLSCertificateLinks, "customTlsCertificateLinks", "[]", "List containing download links of custom TLS certificates. This is required to ensure trusted connections to instances with custom certificates.")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.PullRequestProvider, "pullRequestProvider", "GitHub", "")
+	cmd.Flags().StringVar(&mySonarExecuteScanOptions.SonarScannerDownloadURL, "sonarScannerDownloadUrl", "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip", "")
 	cmd.Flags().StringVar(&mySonarExecuteScanOptions.ProjectVersion, "projectVersion", os.Getenv("PIPER_projectVersion"), "The project version that is reported to SonarQube. @default: major number of `commonPipelineEnvironment.getArtifactVersion()`")
 
 }
@@ -66,7 +76,28 @@ func sonarExecuteScanMetadata() config.StepData {
 			Inputs: config.StepInputs{
 				Parameters: []config.StepParameters{
 					{
-						Name:      "instance",
+						Name:      "host",
+						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "token",
+						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "githubToken",
+						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "changeId",
 						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
 						Type:      "string",
 						Mandatory: false,
@@ -75,30 +106,30 @@ func sonarExecuteScanMetadata() config.StepData {
 					{
 						Name:      "disableInlineComments",
 						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
-						Type:      "string",
-						Mandatory: false,
-						Aliases:   []config.Alias{},
-					},
-					{
-						Name:      "githubOrg",
-						Scope:     []string{"GENERAL", "PARAMETERS", "STAGES", "STEPS"},
-						Type:      "string",
+						Type:      "bool",
 						Mandatory: false,
 						Aliases:   []config.Alias{},
 					},
 					{
 						Name:      "legacyPRHandling",
 						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
-						Type:      "string",
+						Type:      "bool",
 						Mandatory: false,
 						Aliases:   []config.Alias{},
 					},
 					{
-						Name:      "githubRepo",
+						Name:      "owner",
 						Scope:     []string{"GENERAL", "PARAMETERS", "STAGES", "STEPS"},
 						Type:      "string",
 						Mandatory: false,
-						Aliases:   []config.Alias{},
+						Aliases:   []config.Alias{{Name: "githubOrg"}},
+					},
+					{
+						Name:      "repository",
+						Scope:     []string{"GENERAL", "PARAMETERS", "STAGES", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{{Name: "githubRepo"}},
 					},
 					{
 						Name:      "githubApiUrl",
@@ -123,6 +154,20 @@ func sonarExecuteScanMetadata() config.StepData {
 					},
 					{
 						Name:      "customTlsCertificateLinks",
+						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "pullRequestProvider",
+						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
+						Type:      "string",
+						Mandatory: false,
+						Aliases:   []config.Alias{},
+					},
+					{
+						Name:      "sonarScannerDownloadUrl",
 						Scope:     []string{"PARAMETERS", "STAGES", "STEPS"},
 						Type:      "string",
 						Mandatory: false,
