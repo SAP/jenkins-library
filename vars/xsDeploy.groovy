264e7838338a5bbf916bba1ab9fd0c8c3d960724
Marcus Holl <marcus.holl@sap.com>
2019-12-16 09:20:15 +0100

    Don't generate docu for xsDeploy from the source file
    expected to be done using the metadata file.

--- vars/xsDeploy.groovy
+++ vars/xsDeploy.groovy
@@ -34,14 +34,6 @@ enum Action {
     }
 }
 
-/**
-  * Performs an XS deployment
-  *
-  * In case of blue-green deployments the step is called for the deployment in the narrower sense
-  * and later again for resuming or aborting. In this case both calls needs to be performed from the
-  * same directory.
-  */
-@GenerateDocumentation
 void call(Map parameters = [:]) {
 
     handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters) {
