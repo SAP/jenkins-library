e64895e0225ef6c635852ec339eb9528c1140351
Marcus Holl <marcus.holl@sap.com>
2019-12-18 10:07:16 +0100

    Don't swallow Exception happening inside dockerExecOnK8S.stashWorkspace (#1071)

--- vars/dockerExecuteOnKubernetes.groovy
+++ vars/dockerExecuteOnKubernetes.groovy
@@ -247,13 +247,24 @@ void executeOnPod(Map config, utils, Closure body, Script script) {
                     }
                     echo "ContainerConfig: ${containerParams}"
                     container(containerParams) {
+                        Exception e
                         try {
                             utils.unstashAll(stashContent)
                             echo "invalidate stash workspace-${config.uniqueId}"
                             stash name: "workspace-${config.uniqueId}", excludes: '**/*', allowEmpty: true
                             body()
+                        } catch(Exception e1) {
+                            e = e1
                         } finally {
-                            stashWorkspace(config, 'container', true, true)
+                            try {
+                                stashWorkspace(config, 'container', true, true)
+                            } catch(Exception e2) {
+                                if(e) {
+                                    e.addSuppressed(e2)
+                                    throw e
+                                }
+                                throw e2
+                            }
                         }
                     }
                 } else {
@@ -316,8 +327,8 @@ chown -R ${runAsUser}:${fsGroup} ."""
         return stashName
     } catch (AbortException | IOException e) {
         echo "${e.getMessage()}"
+        throw e
     }
-    return null
 }
 
 private Map getSecurityContext(Map config) {
