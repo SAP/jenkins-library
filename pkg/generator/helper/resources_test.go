6e4d682424e4e8139781717d5ca0b1d1d9cc5c3d
OliverNocon <oliver.nocon@sap.com>
2020-01-14 16:45:36 +0100

    Fix test

--- pkg/generator/helper/resources_test.go
+++ pkg/generator/helper/resources_test.go
@@ -52,20 +52,25 @@ func TestStructString(t *testing.T) {
 }
 
 func (i *TestStepTestInflux) persist(path, resourceName string) {
-	measurementContent := []config.InfluxMetricContent{
-		{ValType: config.InfluxField, Measurement: "m1" , Name: "field1_1", Value: &i.m1.fields.field1_1},
-		{ValType: config.InfluxField, Measurement: "m1" , Name: "field1_2", Value: &i.m1.fields.field1_2},
-		{ValType: config.InfluxTag, Measurement: "m1" , Name: "tag1_1", Value: &i.m1.tags.tag1_1},
-		{ValType: config.InfluxTag, Measurement: "m1" , Name: "tag1_2", Value: &i.m1.tags.tag1_2},
-		{ValType: config.InfluxField, Measurement: "m2" , Name: "field2_1", Value: &i.m2.fields.field2_1},
-		{ValType: config.InfluxField, Measurement: "m2" , Name: "field2_2", Value: &i.m2.fields.field2_2},
-		{ValType: config.InfluxTag, Measurement: "m2" , Name: "tag2_1", Value: &i.m2.tags.tag2_1},
-		{ValType: config.InfluxTag, Measurement: "m2" , Name: "tag2_2", Value: &i.m2.tags.tag2_2},
+	measurementContent := []struct{
+		measurement string
+		valType     string
+		name        string
+		value       string
+	}{
+		{valType: config.InfluxField, measurement: "m1" , name: "field1_1", value: i.m1.fields.field1_1},
+		{valType: config.InfluxField, measurement: "m1" , name: "field1_2", value: i.m1.fields.field1_2},
+		{valType: config.InfluxTag, measurement: "m1" , name: "tag1_1", value: i.m1.tags.tag1_1},
+		{valType: config.InfluxTag, measurement: "m1" , name: "tag1_2", value: i.m1.tags.tag1_2},
+		{valType: config.InfluxField, measurement: "m2" , name: "field2_1", value: i.m2.fields.field2_1},
+		{valType: config.InfluxField, measurement: "m2" , name: "field2_2", value: i.m2.fields.field2_2},
+		{valType: config.InfluxTag, measurement: "m2" , name: "tag2_1", value: i.m2.tags.tag2_1},
+		{valType: config.InfluxTag, measurement: "m2" , name: "tag2_2", value: i.m2.tags.tag2_2},
 	}
 
 	errCount := 0
 	for _, metric := range measurementContent {
-		err := piperenv.SetResourceParameter(path, resourceName, filepath.Join(metric.Measurement, fmt.Sprintf("%vs", metric.ValType), metric.Name), *metric.Value)
+		err := piperenv.SetResourceParameter(path, resourceName, filepath.Join(metric.measurement, fmt.Sprintf("%vs", metric.valType), metric.name), metric.value)
 		if err != nil {
 			log.Entry().WithError(err).Error("Error persisting influx environment.")
 			errCount++
