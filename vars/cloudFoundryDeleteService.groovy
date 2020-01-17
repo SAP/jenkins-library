78fc4f3b821603acade1025b6fb63e00a52d044c
dominiklendle <d.lendle@sap.com>
2020-01-17 09:43:59 +0100

    added Username flag correctly

--- vars/cloudFoundryDeleteService.groovy
+++ vars/cloudFoundryDeleteService.groovy
@@ -80,7 +80,7 @@ private def executeCreateServicePush(script, Map config) {
             set -e
             export HOME=${config.dockerWorkspace}
 
-            ./piper cloudFoundryDeleteService --user ${BashUtils.quoteAndEscape(CF_USERNAME)} --password ${BashUtils.quoteAndEscape(CF_PASSWORD)} --api ${BashUtils.quoteAndEscape(config.cloudFoundry.apiEndpoint)} --space ${BashUtils.quoteAndEscape(config.cloudFoundry.space)} --org ${BashUtils.quoteAndEscape(config.cloudFoundry.org)} --ServiceName ${BashUtils.quoteAndEscape(config.cloudFoundry.serviceInstance)}
+            ./piper cloudFoundryDeleteService --username ${BashUtils.quoteAndEscape(CF_USERNAME)} --password ${BashUtils.quoteAndEscape(CF_PASSWORD)} --api ${BashUtils.quoteAndEscape(config.cloudFoundry.apiEndpoint)} --space ${BashUtils.quoteAndEscape(config.cloudFoundry.space)} --org ${BashUtils.quoteAndEscape(config.cloudFoundry.org)} --ServiceName ${BashUtils.quoteAndEscape(config.cloudFoundry.serviceInstance)}
             """
             if (returnCode!=0)  {
                 error "[${STEP_NAME}] ERROR: The execution of the delete-service plugin failed, see the logs above for more details."
