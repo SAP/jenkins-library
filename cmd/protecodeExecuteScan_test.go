b4b18f78579c1bef25304392b8f2713c27d79b41
Sascha Vornheder <sascha.vornheder@sap.com>
2020-01-21 13:43:28 +0100

    fixed test

--- cmd/protecodeExecuteScan_test.go
+++ cmd/protecodeExecuteScan_test.go
@@ -125,9 +125,9 @@ func TestGetUrlAndFileNameFromDockerImage(t *testing.T) {
 		protocol    string
 		want        string
 	}{
-		{"scanImage", "registryUrl", "protocol", "remote://registryUrl/scanImage"},
-		{"containerScanImage", "containerRegistryUrl", "protocol", "remote://containerRegistryUrl/containerScanImage"},
-		{"containerScanImage", "registryUrl", "protocol", "remote://registryUrl/containerScanImage"},
+		{"scanImage", "registryUrl", "protocol", "deamon://registryUrl/scanImage"},
+		{"containerScanImage", "containerRegistryUrl", "protocol", "deamon://containerRegistryUrl/containerScanImage"},
+		{"containerScanImage", "registryUrl", "protocol", "deamon://registryUrl/containerScanImage"},
 	}
 
 	for _, c := range cases {
