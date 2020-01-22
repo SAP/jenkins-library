3ca5e677db871370cbc11584dca3df74a8c450b9
Christopher Fenner <Christopher.Fenner@sap.com>
2020-01-22 09:07:36 +0100

    convert struct to payload

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -3,12 +3,14 @@ package telemetry
 import (
 	"crypto/sha1"
 	"fmt"
+	"strings"
 	"time"
 
 	"net/http"
 
 	piperhttp "github.com/SAP/jenkins-library/pkg/http"
 	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/fatih/structs"
 	"github.com/sirupsen/logrus"
 )
 
@@ -20,62 +22,80 @@ import (
 // BaseData ...
 type BaseData struct {
 	Active     bool
-	ActionName string `json:"actionName,omitempty"`
-	EventType  string `json:"eventType,omitempty"`
-	SiteID     string `json:"idsite,omitempty"`
-	URL        string `json:"url,omitempty"`
+	ActionName string `structs:"actionName"`
+	EventType  string `structs:"eventType"`
+	SiteID     string `structs:"idsite"`
+	URL        string `structs:"url"`
 
-	GitOwner               string `json:"e_a,omitempty"`
-	GitOwnerLabel          string `json:"custom1,omitempty"`
-	GitRepository          string `json:"e_2,omitempty"`
-	GitRepositoryLabel     string `json:"custom2,omitempty"`
-	StepName               string `json:"e_3,omitempty"`
-	StepNameLabel          string `json:"custom_3,omitempty"`
-	PipelineURLSha1        string `json:"e_4,omitempty"` // defaults to env.JOB_URl
-	PipelineURLSha1Label   string `json:"custom_4,omitempty"`
-	BuildURLSha1           string `json:"e_5,omitempty"` // defaults to env.BUILD_URL
-	BuildURLSha1Label      string `json:"custom_5,omitempty"`
-	GitPathSha1            string `json:"e_6,omitempty"`
-	GitPathSha1Label       string `json:"custom_6,omitempty"`
-	GitOwnerSha1           string `json:"e_7,omitempty"`
-	GitOwnerSha1Label      string `json:"custom_7,omitempty"`
-	GitRepositorySha1      string `json:"e_8,omitempty"`
-	GitRepositorySha1Label string `json:"custom_8,omitempty"`
-	JobName                string `json:"e_9,omitempty"`
-	JobNameLabel           string `json:"custom_9,omitempty"`
-	StageName              string `json:"e_10,omitempty"`
-	StageNameLabel         string `json:"custom_10,omitempty"`
+	GitOwner               string `structs:"e_a"`
+	GitOwnerLabel          string `structs:"custom1"`
+	GitRepository          string `structs:"e_2"`
+	GitRepositoryLabel     string `structs:"custom2"`
+	StepName               string `structs:"e_3"`
+	StepNameLabel          string `structs:"custom_3"`
+	PipelineURLSha1        string `structs:"e_4"` // defaults to env.JOB_URl
+	PipelineURLSha1Label   string `structs:"custom_4"`
+	BuildURLSha1           string `structs:"e_5"` // defaults to env.BUILD_URL
+	BuildURLSha1Label      string `structs:"custom_5"`
+	GitPathSha1            string `structs:"e_6"`
+	GitPathSha1Label       string `structs:"custom_6"`
+	GitOwnerSha1           string `structs:"e_7"`
+	GitOwnerSha1Label      string `structs:"custom_7"`
+	GitRepositorySha1      string `structs:"e_8"`
+	GitRepositorySha1Label string `structs:"custom_8"`
+	JobName                string `structs:"e_9"`
+	JobNameLabel           string `structs:"custom_9"`
+	StageName              string `structs:"e_10"`
+	StageNameLabel         string `structs:"custom_10"`
 }
 
 // CustomData ...
 type CustomData struct {
-	BuildTool      string `json:"e_11,omitempty"`
-	buildToolLabel string `json:"custom_11,omitempty"`
+	BuildTool      string `structs:"e_11"`
+	buildToolLabel string `structs:"custom_11"`
 	// ...
-	ScanType      string `json:"e_24,omitempty"`
-	scanTypeLabel string `json:"custom_24,omitempty"`
-	Custom25      string `json:"e_25,omitempty"`
-	custom25Label string `json:"custom_25,omitempty"`
-	Custom26      string `json:"e_26,omitempty"`
-	custom26Label string `json:"custom_26,omitempty"`
-	Custom27      string `json:"e_27,omitempty"`
-	custom27Label string `json:"custom_27,omitempty"`
-	Custom28      string `json:"e_28,omitempty"`
-	custom28Label string `json:"custom_28,omitempty"`
-	Custom29      string `json:"e_29,omitempty"`
-	custom29Label string `json:"custom_29,omitempty"`
-	Custom30      string `json:"e_30,omitempty"`
-	Custom30Label string `json:"custom_30,omitempty"`
+	ScanType      string `structs:"e_24"`
+	scanTypeLabel string `structs:"custom_24"`
+	Custom25      string `structs:"e_25"`
+	custom25Label string `structs:"custom_25"`
+	Custom26      string `structs:"e_26"`
+	custom26Label string `structs:"custom_26"`
+	Custom27      string `structs:"e_27"`
+	custom27Label string `structs:"custom_27"`
+	Custom28      string `structs:"e_28"`
+	custom28Label string `structs:"custom_28"`
+	Custom29      string `structs:"e_29"`
+	custom29Label string `structs:"custom_29"`
+	Custom30      string `structs:"e_30"`
+	Custom30Label string `structs:"custom_30"`
 }
 
-var data BaseData
+type Data struct {
+	BaseData
+	CustomData
+}
+
+func (d *Data) toPayloadString() (payload string) {
+	dataMap := structs.Map(d)
+	dataList := make([]string, 0)
+
+	for key, value := range dataMap {
+		dataList = append(dataList, fmt.Sprintf("%v&%v", key, value))
+	}
+
+	payload = strings.Join(dataList, "&")
+
+	return
+}
+
+var baseData BaseData
 var client piperhttp.Sender
 
 // Initialize sets up the base telemetry data and is called in generated part of the steps
 func Initialize(telemetryActive bool, getResourceParameter func(rootPath, resourceName, parameterName string) string, envRootPath, stepName string) {
 	// check if telemetry is disabled
 	if telemetryActive {
-		data = BaseData{Active: telemetryActive}
+		baseData = BaseData{Active: telemetryActive}
 		return
 	}
 
@@ -99,7 +119,7 @@ func Initialize(telemetryActive bool, getResourceParameter func(rootPath, resour
 
 	gitPath := fmt.Sprintf("%v/%v", gitOwner, gitRepo)
 
-	data = BaseData{
+	baseData = BaseData{
 		Active: telemetryActive,
 
 		GitOwner:           gitOwner,
@@ -119,11 +139,19 @@ func Initialize(telemetryActive bool, getResourceParameter func(rootPath, resour
 
 // SWA endpoint
 const endpoint = "https://webanalytics.cfapps.eu10.hana.ondemand.com/tracker/log"
+
+// site ID
 const siteID = "827e8025-1e21-ae84-c3a3-3f62b70b0130"
 
 // SendTelemetry ...
 func SendTelemetry(customData *CustomData) {
-	payload := ""
+	data := Data{BaseData: baseData, CustomData: *customData}
+
+	if data.Active != true {
+		return
+	}
+
+	payload := data.toPayloadString()
 	// Add logic for sending data to SWA
 	client.SendRequest(http.MethodGet, fmt.Sprintf("%v?%v", endpoint, payload), nil, nil, nil)
 }
