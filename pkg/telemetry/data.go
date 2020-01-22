a25f24c7c03c8ce1f2ed6c9e582a1578f38ff30c
Christopher Fenner <Christopher.Fenner@sap.com>
2020-01-22 15:51:25 +0100

    cleanup parameter mapping

--- pkg/telemetry/data.go
+++ pkg/telemetry/data.go
@@ -7,55 +7,65 @@ import (
 
 // BaseData ...
 type BaseData struct {
-	ActionName        string `json:"action_name"`
-	EventType         string `json:"event_type"`
-	SiteID            string `json:"idsite"`
-	URL               string `json:"url"`
-	GitOwner          string `json:"e_a"` // first custom field name is indeed e_a, not e_1
-	GitRepository     string `json:"e_2"`
-	StepName          string `json:"e_3"`
-	PipelineURLSha1   string `json:"e_4"` // defaults to env.JOB_URl
-	BuildURLSha1      string `json:"e_5"` // defaults to env.BUILD_URL
-	GitPathSha1       string `json:"e_6"`
-	GitOwnerSha1      string `json:"e_7"`
-	GitRepositorySha1 string `json:"e_8"`
-	JobName           string `json:"e_9"`
-	StageName         string `json:"e_10"`
+	ActionName string `json:"action_name"`
+	EventType  string `json:"event_type"`
+	SiteID     string `json:"idsite"`
+	URL        string `json:"url"`
+	//GitOwner          string `json:"e_a"` // first custom field name is indeed e_a, not e_1
+	//GitRepository     string `json:"e_2"`
+	StepName        string `json:"e_3"`
+	PipelineURLSha1 string `json:"e_4"` // defaults to env.JOB_URl
+	BuildURLSha1    string `json:"e_5"` // defaults to env.BUILD_URL
+	//GitPathSha1       string `json:"e_6"`
+	//GitOwnerSha1      string `json:"e_7"`
+	//GitRepositorySha1 string `json:"e_8"`
+	//JobName           string `json:"e_9"`
+	StageName string `json:"e_10"`
 }
+
+var baseData BaseData
 
 // BaseMetaData
 type BaseMetaData struct {
-	GitOwnerLabel          string `json:"custom_1"`
-	GitRepositoryLabel     string `json:"custom_2"`
-	StepNameLabel          string `json:"custom_3"`
-	PipelineURLSha1Label   string `json:"custom_4"`
-	BuildURLSha1Label      string `json:"custom_5"`
-	GitPathSha1Label       string `json:"custom_6"`
-	GitOwnerSha1Label      string `json:"custom_7"`
-	GitRepositorySha1Label string `json:"custom_8"`
-	JobNameLabel           string `json:"custom_9"`
-	StageNameLabel         string `json:"custom_10"`
-	BuildToolLabel         string `json:"custom_11"`
-	ScanTypeLabel          string `json:"custom_24"`
+	//GitOwnerLabel          string `json:"custom_1"`
+	//GitRepositoryLabel     string `json:"custom_2"`
+	StepNameLabel        string `json:"custom_3"`
+	PipelineURLSha1Label string `json:"custom_4"`
+	BuildURLSha1Label    string `json:"custom_5"`
+	//GitPathSha1Label       string `json:"custom_6"`
+	//GitOwnerSha1Label      string `json:"custom_7"`
+	//GitRepositorySha1Label string `json:"custom_8"`
+	//JobNameLabel           string `json:"custom_9"`
+	StageNameLabel string `json:"custom_10"`
+}
+
+var baseMetaData BaseMetaData = BaseMetaData{
+	//GitOwnerLabel:          "owner",
+	//GitRepositoryLabel:     "repository",
+	StepNameLabel:        "stepName",
+	PipelineURLSha1Label: "",
+	BuildURLSha1Label:    "",
+	//GitPathSha1Label:       "gitpathsha1",
+	//GitOwnerSha1Label:      "",
+	//GitRepositorySha1Label: "",
+	//JobNameLabel:           "jobName",
+	StageNameLabel: "stageName",
 }
 
 // CustomData ...
 type CustomData struct {
-	BuildTool string `json:"e_11"`
-	// ...
-	ScanType      string `json:"e_24"`
-	Custom25      string `json:"e_25"`
-	Custom26      string `json:"e_26"`
-	Custom27      string `json:"e_27"`
-	Custom28      string `json:"e_28"`
-	Custom29      string `json:"e_29"`
-	Custom30      string `json:"e_30"`
-	custom25Label string `json:"custom_25"`
-	custom26Label string `json:"custom_26"`
-	custom27Label string `json:"custom_27"`
-	custom28Label string `json:"custom_28"`
-	custom29Label string `json:"custom_29"`
-	Custom30Label string `json:"custom_30"`
+	Custom1Label string `json:"custom_11"`
+	Custom2Label string `json:"custom_12"`
+	Custom3Label string `json:"custom_13"`
+	Custom4Label string `json:"custom_14"`
+	Custom5Label string `json:"custom_15"`
+	Custom1      string `json:"e_11"`
+	Custom2      string `json:"e_12"`
+	Custom3      string `json:"e_13"`
+	Custom4      string `json:"e_14"`
+	Custom5      string `json:"e_15"`
+	//Custom30      string `json:"e_30"`
+	//Custom30Label string `json:"custom_30"`
 }
 
 // Data ...
