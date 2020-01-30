51f8e6275a3b3f84643c4e5f835ba93001849deb
dominiklendle <d.lendle@sap.com>
2020-01-30 15:49:59 +0100

    minor changes

--- vars/cloudFoundryDeleteService.groovy
+++ vars/cloudFoundryDeleteService.groovy
@@ -165,9 +165,9 @@ void call(Map parameters = [:]) {
                     set +x
                     set -e
                     export HOME=${config.dockerWorkspace}
+                    """
                     */
                     sh "./piper cloudFoundryDeleteService --Username ${BashUtils.quoteAndEscape(CF_USERNAME)} --Password ${BashUtils.quoteAndEscape(CF_PASSWORD)} --API ${BashUtils.quoteAndEscape(config.cloudFoundry.apiEndpoint)} --Space ${BashUtils.quoteAndEscape(config.cloudFoundry.space)} --Organisation ${BashUtils.quoteAndEscape(config.cloudFoundry.org)} --ServiceName ${BashUtils.quoteAndEscape(config.cloudFoundry.serviceInstance)}"
-                    """
                     if (returnCode!=0)  {
                         error "[${STEP_NAME}] ERROR: The execution of the delete-service plugin failed, see the logs above for more details."
                     }
