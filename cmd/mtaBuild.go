f291ebe29ad5d2d87c955eace32f86379986d2a0
Marcus Holl <marcus.holl@sap.com>
2020-01-31 14:55:21 +0100

    extensions (plural) in case of mtb

--- cmd/mtaBuild.go
+++ cmd/mtaBuild.go
@@ -81,16 +81,19 @@ func runMtaBuild(config mtaBuildOptions, commonPipelineEnvironment *mtaBuildComm
 	var mtaCall = `Echo "Hello MTA"`
 	var options = []string{}
 
-	if len(extensions) != 0 {
-		options = append(options, fmt.Sprintf("--extension=%s", extensions))
-	}
 
 	switch mtaBuildTool {
 	case "classic":
 		options = append(options, fmt.Sprintf("--build-target=%s", buildTarget))
+		if len(extensions) != 0 {
+			options = append(options, fmt.Sprintf("--extension=%s", extensions))
+		}
 		mtaCall = fmt.Sprintf("java -jar %s %s build", mtaJar, strings.Join(options, " "))
 	case "cloudMbt":
 		options = append(options, fmt.Sprintf("--platform %s", platform))
+		if len(extensions) != 0 {
+			options = append(options, fmt.Sprintf("--extensions=%s", extensions))
+		}
 		options = append(options, "--target ./")
 		mtaCall = fmt.Sprintf("mbt build %s", strings.Join(options, " "))
 	default:
