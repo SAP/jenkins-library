dfcbe9e5d2e70857c01504dfcab29affd0157cf0
Daniel Mieg <daniel.mieg@sap.com>
2020-02-03 11:09:41 +0100

    Improve logging

--- cmd/abapEnvironmentPullGitRepo.go
+++ cmd/abapEnvironmentPullGitRepo.go
@@ -156,7 +156,6 @@ func readCfServiceKey(config abapEnvironmentPullGitRepoOptions, c execRunner) (s
 	var abapServiceKey serviceKey
 
 	c.Stderr(log.Entry().Writer())
-	c.Stdout(log.Entry().Writer())
 
 	// Logging into the Cloud Foundry via CF CLI
 	log.Entry().WithField("cfApiEndpoint", config.CfAPIEndpoint).WithField("cfSpace", config.CfSpace).WithField("cfOrg", config.CfOrg).WithField("User", config.Username).Info("Cloud Foundry parameters: ")
