ff4562363c3b6c3d7352f8e1427c6840bf3d5afd
OliverNocon <oliver.nocon@sap.com>
2020-02-04 13:35:42 +0100

    Add missing method

--- src/com/sap/piper/JenkinsUtils.groovy
+++ src/com/sap/piper/JenkinsUtils.groovy
@@ -10,6 +10,11 @@ import org.apache.commons.io.IOUtils
 import org.jenkinsci.plugins.workflow.libs.LibrariesAction
 import org.jenkinsci.plugins.workflow.steps.MissingContextVariableException
 
+@NonCPS
+def getActiveJenkinsInstance() {
+    return Jenkins.getActiveInstance()
+}
+
 @API
 @NonCPS
 static def isPluginActive(pluginId) {
