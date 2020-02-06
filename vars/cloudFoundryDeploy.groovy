ac95834c2e3b70c65477e09c9a1dc7539e43a7c5
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-02-06 15:10:03 +0100

    Update documentation

--- vars/cloudFoundryDeploy.groovy
+++ vars/cloudFoundryDeploy.groovy
@@ -82,6 +82,8 @@ import groovy.transform.Field
     'deployTool',
     /**
      * Defines the type of deployment, either `standard` deployment which results in a system downtime or a zero-downtime `blue-green` deployment.
+     * If 'cf_native' as deployType and 'blue-green' as deployTool is used in combination your manifest.yaml may only contain one application.
+     * If this application has the option 'no-route' active the deployType will be changed to 'standard'.
      * @possibleValues 'standard', 'blue-green'
      */
     'deployType',
