2b8ae691a2e1c98896e68f8a11182bd8b9a89900
Sven Merk <s.merk@sap.com>
2020-02-11 09:13:52 +0100

    Fix test

--- pkg/fortify/fortify_test.go
+++ pkg/fortify/fortify_test.go
@@ -15,24 +15,34 @@ import (
 func TestGetProjectByName(t *testing.T) {
 	// Start a local HTTP server
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if req.URL.Path == "/projects" && req.URL.RawQuery == "fulltextsearch=true&q=name%3Dpython-test-sven" {
+		if req.URL.Path == "/projects" && req.URL.RawQuery == "fulltextsearch=true&q=name%3Dpython-test" {
+			header := rw.Header()
+			header.Add("Content-type", "application/json")
 			rw.Write([]byte(
-				`{"count": 0, "data": [{"createdBy": "string", "creationDate": "2020-02-10T21:22:11.506+0000", "description": "string",
-"id": 4711, "issueTemplateId": "string", "name": "python-test-sven"}, "errorCode": 0, "links": {}, "message": "string",
-"responseCode": 200, "stackTrace": "string", "successCount": 0}`))
+				`{"data": [{"_href": "https://fortify/ssc/api/v1/projects/4711","createdBy": "someUser","name": "python-test",
+				"description": "","id": 4711,"creationDate": "2018-12-03T06:29:38.197+0000","issueTemplateId": "dasdasdasdsadasdasdasdasdas"}],
+				"count": 1,"responseCode": 200,"links": {"last": {"href": "https://fortify/ssc/api/v1/projects?q=name%A3python-test&start=0"},
+				"first": {"href": "https://fortify/ssc/api/v1/projects?q=name%A3python-test&start=0"}}}`))
 			return
 		}
-		if req.URL.Path == "/projectVersions" && req.URL.RawQuery == "fulltextsearch=true&q=name%3D0" {
-			rw.Write([]byte(`{
-				"data": [
-				  {
-					"id": 666,
-					"name": "0"
-				  }
-				],
-				"errorCode": 0,
-				"responseCode": 200
-			  }`))
+		if req.URL.Path == "/projects/4711/versions" {
+			header := rw.Header()
+			header.Add("Content-type", "application/json")
+			rw.Write([]byte(
+				`{"data":[{"latestScanId":null,"serverVersion":17.2,"tracesOutOfDate":false,"attachmentsOutOfDate":false,"description":"",
+				"project":{"id":4711,"name":"python-test","description":"","creationDate":"2018-12-03T06:29:38.197+0000","createdBy":"someUser",
+				"issueTemplateId":"dasdasdasdsadasdasdasdasdas"},"sourceBasePath":null,"mode":"BASIC","masterAttrGuid":"sddasdasda","obfuscatedId":null,
+				"id":10172,"customTagValuesAutoApply":null,"issueTemplateId":"dasdasdasdsadasdasdasdasdas","loadProperties":null,"predictionPolicy":null,
+				"bugTrackerPluginId":null,"owner":"admin","_href":"https://fortify.mo.sap.corp/ssc/api/v1/projectVersions/10172",
+				"committed":true,"bugTrackerEnabled":false,"active":true,"snapshotOutOfDate":false,"issueTemplateModifiedTime":1578411924701,
+				"securityGroup":null,"creationDate":"2018-02-09T16:59:41.297+0000","refreshRequired":false,"issueTemplateName":"someTemplate",
+				"migrationVersion":null,"createdBy":"admin","name":"0","siteId":null,"staleIssueTemplate":false,"autoPredict":null,
+				"currentState":{"id":10172,"committed":true,"attentionRequired":false,"analysisResultsExist":true,"auditEnabled":true,
+				"lastFprUploadDate":"2018-02-09T16:59:53.497+0000","extraMessage":null,"analysisUploadEnabled":true,"batchBugSubmissionExists":false,
+				"hasCustomIssues":false,"metricEvaluationDate":"2018-03-10T00:02:45.553+0000","deltaPeriod":7,"issueCountDelta":0,"percentAuditedDelta":0.0,
+				"criticalPriorityIssueCountDelta":0,"percentCriticalPriorityIssuesAuditedDelta":0.0},"assignedIssuesCount":0,"status":null}],
+				"count":1,"responseCode":200,"links":{"last":{"href":"https://fortify.mo.sap.corp/ssc/api/v1/projects/4711/versions?start=0"},
+				"first":{"href":"https://fortify.mo.sap.corp/ssc/api/v1/projects/4711/versions?start=0"}}}`))
 			return
 		}
 	}))
@@ -49,12 +59,12 @@ func TestGetProjectByName(t *testing.T) {
 		)
 		sys := NewSystemInstanceForClient(client, "test2456", 60*time.Second)
 
-		result, err := sys.GetProjectByName("python-test-sven")
+		result, err := sys.GetProjectByName("python-test")
 		assert.NoError(t, err, "GetProjectByName call not successful")
-		assert.Equal(t, "python-test-sven", strings.ToLower(*result.Name), "Expected to receive python-test-sven")
+		assert.Equal(t, "python-test", strings.ToLower(*result.Name), "Expected to get python-test")
 
 		result2, err := sys.GetProjectVersionDetailsByNameAndProjectID(result.ID, "0")
 		assert.NoError(t, err, "GetProjectVersionDetailsByNameAndProjectID call not successful")
-		assert.Equal(t, "0", *result2.Name, "Expected project version with different name")
+		assert.Equal(t, "0", *result2.Name, "Expected to get project version with different name")
 	})
 }
