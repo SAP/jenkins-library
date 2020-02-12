bad44bc2eeea5bbb93a107828df8882744686177
Marcus Holl <marcus.holl@sap.com>
2020-02-12 16:03:59 +0100

    wip: settings

---
+++ pkg/maven/settings.go
@@ -0,0 +1,139 @@
+package maven
+
+import (
+	"errors"
+	"fmt"
+	piperhttp "github.com/SAP/jenkins-library/pkg/http"
+	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/piperutils"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var getenv = os.Getenv
+
+// SettingsFileType ...
+type SettingsFileType int
+
+const (
+	// GlobalSettingsFile ...
+	GlobalSettingsFile SettingsFileType = iota
+	// ProjectSettingsFile ...
+	ProjectSettingsFile SettingsFileType = iota
+)
+
+// GetSettingsFile ...
+func GetSettingsFile(settingsFileType SettingsFileType, src string, fileUtils piperutils.FileUtils, httpClient piperhttp.Sender) error {
+
+	var dest string
+	var err error
+
+	switch settingsFileType {
+	case GlobalSettingsFile:
+		dest, err = getGlobalSettingsFileDest()
+	case ProjectSettingsFile:
+		dest, err = getProjectSettingsFileDest()
+	default:
+		return errors.New("Invalid SettingsFileType")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if len(src) > 0 {
+
+		log.Entry().Debugf("Copying file \"%s\" to \"%s\"", src, dest)
+
+		parent := filepath.Dir(dest)
+
+		exists, err := fileUtils.FileExists(parent)
+
+		if err != nil {
+			return err
+		}
+
+		if !exists {
+			if err = fileUtils.MkdirAll(parent, 0664); err != nil {
+				return err
+			}
+		}
+
+		if strings.HasPrefix(src, "http:") || strings.HasPrefix(src, "https:") {
+			if err := getSettingsFileFromURL(src, dest, fileUtils, httpClient); err != nil {
+				return err
+			}
+		} else {
+
+			// for sake os symetry it would be better to use a file protocol prefix here (file:)
+
+			fmt.Printf("Copying file '%s' to '%s'", src, dest)
+			if _, err := fileUtils.Copy(src, dest); err != nil {
+				return err
+			}
+		}
+	}
+	log.Entry().Debugf("File \"%s\" copied to \"%s\"", src, dest)
+	return nil
+}
+
+// getSettingsFileFromURL ...
+func getSettingsFileFromURL(url, file string, fileUtils piperutils.FileUtils, httpClient piperhttp.Sender) error {
+
+	var e error
+
+	//CHECK:
+	// - how does this work with a proxy inbetween?
+	// - how does this work with http 302 (relocated) --> curl -L
+	response, e := httpClient.SendRequest(http.MethodGet, url, nil, nil, nil)
+	if e != nil {
+		return e
+	}
+
+	if response.StatusCode != 200 {
+		return fmt.Errorf("Got %d reponse from download attempt for \"%s\"", response.StatusCode, url)
+	}
+
+	body, e := ioutil.ReadAll(response.Body)
+	if e != nil {
+		return e
+	}
+
+	e = fileUtils.FileWrite(file, body, 0644)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func getGlobalSettingsFileDest() (string, error) {
+
+	m2Home, err := getEnvironmentVariable("M2_HOME")
+	if err != nil {
+		return "", err
+	}
+	return m2Home + "/conf/settings.xml", nil
+}
+
+func getProjectSettingsFileDest() (string, error) {
+	home, err := getEnvironmentVariable("HOME")
+	if err != nil {
+		return "", err
+	}
+	return home + "/.m2/settings.xml", nil
+}
+
+func getEnvironmentVariable(name string) (string, error) {
+
+	envVar := getenv(name)
+
+	if len(envVar) == 0 {
+		return "", fmt.Errorf("Environment variable \"%s\" not set or empty", name)
+	}
+
+	return envVar, nil
+}
---
+++ pkg/maven/settings_test.go
@@ -0,0 +1,171 @@
+package maven
+
+import (
+	"fmt"
+	piperhttp "github.com/SAP/jenkins-library/pkg/http"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestSettings(t *testing.T) {
+
+	httpClient := httpMock{}
+
+	defer func() {
+		getenv = os.Getenv
+	}()
+
+	getenv = func(name string) string {
+		if name == "M2_HOME" {
+			return "/usr/share/maven"
+		} else if name == "HOME" {
+			return "/home/me"
+		}
+		return ""
+	}
+
+	t.Run("Invalid settimgs file type", func(t *testing.T) {
+
+		fileUtils := fileUtilsMock{}
+
+		err := GetSettingsFile(-1, "/dev/null", &fileUtils, &httpClient)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, "Invalid SettingsFileType", err.Error())
+	})
+
+	t.Run("Retrieve global settings file", func(t *testing.T) {
+
+		fileUtils := fileUtilsMock{existingFiles: map[string]string{"/opt/sap/maven/global-settings.xml": ""}}
+
+		err := GetSettingsFile(GlobalSettingsFile, "/opt/sap/maven/global-settings.xml", &fileUtils, &httpClient)
+
+		assert.Nil(t, err)
+		assert.Equal(t, "/usr/share/maven/conf/settings.xml", fileUtils.copiedFiles["/opt/sap/maven/global-settings.xml"])
+	})
+
+	t.Run("Retrieve project settings file", func(t *testing.T) {
+
+		fileUtils := fileUtilsMock{existingFiles: map[string]string{"/opt/sap/maven/project-settings.xml": ""}}
+
+		err := GetSettingsFile(ProjectSettingsFile, "/opt/sap/maven/project-settings.xml", &fileUtils, &httpClient)
+
+		assert.Nil(t, err)
+		assert.Equal(t, "/home/me/.m2/settings.xml", fileUtils.copiedFiles["/opt/sap/maven/project-settings.xml"])
+	})
+
+	t.Run("Retrieve global settings file via http", func(t *testing.T) {
+
+		fileUtils := fileUtilsMock{}
+
+		err := GetSettingsFile(GlobalSettingsFile, "https://example.org/maven/global-settings.xml", &fileUtils, &httpClient)
+
+		assert.Nil(t, err)
+		_, ok := fileUtils.writtenFiles["/usr/share/maven/conf/settings.xml"]
+		assert.True(t, ok)
+	})
+
+	t.Run("Retrieve project settings file via http", func(t *testing.T) {
+
+		fileUtils := fileUtilsMock{}
+
+		err := GetSettingsFile(ProjectSettingsFile, "https://example.org/maven/project-settings.xml", &fileUtils, &httpClient)
+
+		assert.Nil(t, err)
+		_, ok := fileUtils.writtenFiles["/home/me/.m2/settings.xml"]
+		assert.True(t, ok)
+	})
+
+	t.Run("Retrieve project settings file via http invalid protocol", func(t *testing.T) {
+
+		fileUtils := fileUtilsMock{}
+
+		err := GetSettingsFile(ProjectSettingsFile, "httpsss://example.org/maven/project-settings.xml", &fileUtils, &httpClient)
+
+		assert.NotNil(t, err)
+	})
+}
+
+type httpMock struct {
+}
+
+type httpMockResponse struct {
+	StatusCode int
+}
+
+func (h *httpMockResponse) Close() error {
+	return nil
+}
+
+func (h *httpMockResponse) Read(p []byte) (n int, err error) {
+	return 0, io.EOF
+}
+
+func (h *httpMock) SendRequest(method, url string, body io.Reader, header http.Header, cookies []*http.Cookie) (*http.Response, error) {
+
+	res := http.Response{StatusCode: 200}
+	res.Body = &httpMockResponse{}
+	return &res, nil
+}
+
+func (h *httpMock) SetOptions(options piperhttp.ClientOptions) {
+
+}
+
+type fileUtilsMock struct {
+	existingFiles map[string]string
+	writtenFiles  map[string]string
+	copiedFiles   map[string]string
+}
+
+func (f *fileUtilsMock) FileExists(path string) (bool, error) {
+
+	if _, ok := f.existingFiles[path]; ok {
+		return true, nil
+	}
+	return false, nil
+}
+
+func (f *fileUtilsMock) Copy(src, dest string) (int64, error) {
+
+	exists, err := f.FileExists(src)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if !exists {
+		return 0, fmt.Errorf("File '%s' does not exist", src)
+	}
+
+	if f.copiedFiles == nil {
+		f.copiedFiles = make(map[string]string)
+	}
+	f.copiedFiles[src] = dest
+
+	return 0, nil
+}
+
+func (f *fileUtilsMock) FileRead(path string) ([]byte, error) {
+	return []byte(f.existingFiles[path]), nil
+}
+
+func (f *fileUtilsMock) FileWrite(path string, content []byte, perm os.FileMode) error {
+
+	if f.writtenFiles == nil {
+		f.writtenFiles = make(map[string]string)
+	}
+
+	if _, ok := f.writtenFiles[path]; ok {
+		delete(f.writtenFiles, path)
+	}
+	f.writtenFiles[path] = string(content)
+	return nil
+}
+
+func (f *fileUtilsMock) MkdirAll(path string, perm os.FileMode) error {
+	return nil
+}
