e81f40f645bb9a5c989baf9f8227963fd0002a81
Oliver Nocon <33484802+OliverNocon@users.noreply.github.com>
2020-02-18 15:19:50 +0100

    Provide foundation for integration-like tests (#1131)

---
+++ integration/testdata/TestKarmaIntegration/src/frontend/test/karma-qunit.js
@@ -0,0 +1,6 @@
+(function() {
+    "use strict";
+    document.body.innerHTML += '<div id="content"></div>';
+    document.body.innerHTML += '<div id="qunit"></div>';
+    sap.ui.require([ "sap/ui/qunit/qunit-css" ]);
+}());
