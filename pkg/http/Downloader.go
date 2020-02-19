c47171817c087b579d72e541dad2e087d25a0b01
Christopher Fenner <Christopher.Fenner@sap.com>
2020-02-19 14:35:01 +0100

    add Downloader

--- pkg/http/Downloader.go
+++ pkg/http/Downloader.go
@@ -1,95 +1,42 @@
 package http
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/pkg/errors"
 )
 
+//Downloader ...
 type Downloader interface {
-	//SendRequest(method, url string, body io.Reader, header http.Header, cookies []*http.Cookie) (*http.Response, error)
 	SetOptions(options ClientOptions)
-	DownloadFile(url, file, fieldName string, header http.Header, cookies []*http.Cookie) (*http.Response, error)
+	DownloadFile(url, filename string, header http.Header, cookies []*http.Cookie) error
+	SendRequest(method, url string, body io.Reader, header http.Header, cookies []*http.Cookie) (*http.Response, error)
 }
 
-// UploadFile uploads a file's content as multipart-form POST request to the specified URL
-func (c *Client) DownloadFile(url, file, fieldName string, header http.Header, cookies []*http.Cookie) (*http.Response, error) {
-	return c.UploadRequest(http.MethodPost, url, file, fieldName, header, cookies)
+// DownloadFile downloads a file's content as GET request from the specified URL to the specified file
+func (c *Client) DownloadFile(url, filename string, header http.Header, cookies []*http.Cookie) error {
+	return c.DownloadRequest(http.MethodPost, url, filename, header, cookies)
 }
 
 // DownloadRequest ...
-func (c *Client) downloadRequest(url, file, fieldName string, header http.Header, cookies []*http.Cookie) (*http.Response, error) {
-
-	if method != http.MethodGet {
-		return nil, errors.New(fmt.Sprintf("Http method %v is not allowed. Possible values are %v", method, http.MethodGet))
-	}
-
-	// http.MethodGet
-
-	response, err := c.SendRequest(http.MethodGet, url, nil, nil, nil)
+func (c *Client) DownloadRequest(method, url, filename string, header http.Header, cookies []*http.Cookie) error {
+	response, err := c.SendRequest(method, url, nil, nil, nil)
 	if err != nil {
-		return nil, err
+		return errors.Wrapf(err, "HTTP %v request to %v failed with error", method, url)
 	}
 	defer response.Body.Close()
 
-	file, err := os.Create(filename)
+	fileHandler, err := os.Create(filename)
 	if err != nil {
-		return 0, err
+		return errors.Wrapf(err, "unable to create file %v", filename)
 	}
-	defer file.Close()
+	defer fileHandler.Close()
 
-	nBytes, err := io.Copy(file, response.Body)
-	return nBytes, err
-
-	//bodyBuffer := &bytes.Buffer{}
-	//bodyWriter := multipart.NewWriter(bodyBuffer)
-
-	//	fileWriter, err := bodyWriter.CreateFormFile(fieldName, file)
-	//	if err != nil {
-	//		return &http.Response{}, errors.Wrapf(err, "error creating form file %v for field %v", file, fieldName)
-	//	}
-
-	defer response.Body.Close()
-	fileHandle, err := os.Create(file)
+	_, err = io.Copy(fileHandler, response.Body)
 	if err != nil {
-		return 0, err
+		return errors.Wrapf(err, "unable to copy content from url to file", filename)
 	}
-	defer fileHandle.Close()
-
-	nBytes, err := io.Copy(fileHandle, response.Body)
-
-	fileHandle, err := os.Open(file)
-	if err != nil {
-		return &http.Response{}, errors.Wrapf(err, "unable to locate file %v", file)
-	}
-	defer fileHandle.Close()
-
-	_, err = io.Copy(fileWriter, fileHandle)
-	if err != nil {
-		return &http.Response{}, errors.Wrapf(err, "unable to copy file content of %v into request body", file)
-	}
-	err = bodyWriter.Close()
-
-	request, err := c.createRequest(method, url, bodyBuffer, &header, cookies)
-	if err != nil {
-		c.logger.Debugf("New %v request to %v", method, url)
-		return &http.Response{}, errors.Wrapf(err, "error creating %v request to %v", method, url)
-	}
-
-	startBoundary := strings.Index(bodyWriter.FormDataContentType(), "=") + 1
-	boundary := bodyWriter.FormDataContentType()[startBoundary:]
-
-	request.Header.Add("Content-Type", "multipart/form-data; boundary=\""+boundary+"\"")
-	request.Header.Add("Connection", "Keep-Alive")
-
-	response, err := httpClient.Do(request)
-	if err != nil {
-		return response, errors.Wrapf(err, "HTTP %v request to %v failed with error", method, url)
-	}
-
-	return c.handleResponse(response)
+	return err
 }
