e282fa90d5884ae15aee1cdc8c4aa84d97c9a6a0
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-02-19 15:57:37 +0100

    wip

--- integration/integration_nexus_upload_test.go
+++ integration/integration_nexus_upload_test.go
@@ -17,7 +17,7 @@ func TestNexusUpload(t *testing.T) {
 	req := testcontainers.ContainerRequest{
 		Image:        "sonatype/nexus3:3.14.0", //FIXME in 3.14.0 nexus still has a hardcoded admin pw by default. In later versions the password is written to a file in a volueme -> harder to create the testcase
 		ExposedPorts: []string{"8081/tcp"},
-		WaitingFor:   wait.ForLog("Started Sonatype Nexus").WithStartupTimeout(time.Minute * 5),
+		WaitingFor:   wait.ForLog("Started Sonatype Nexus").WithStartupTimeout(time.Minute * 5), // Nexus takes more then one minute to boot
 	}
 	nexusContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
@@ -41,8 +41,28 @@ func TestNexusUpload(t *testing.T) {
 		t.Errorf("Expected status code %d. Got %d.", http.StatusOK, resp.StatusCode)
 	}
 
-	params := []string{"run", ".", "nexusUpload", "--artifacts=[{\\\"id\\\":\\\"blob\\\",\\\"classifier\\\":\\\"blob-1.0\\\",\\\"type\\\":\\\"pom\\\",\\\"file\\\":\\\"pom.xml\\\"}]", "--groupId=foo", "--user=admin", "--password=adminsdf123", "--repository=maven-releases", "--version=1.0", "--url=" + url}
+	// cmd := exec.Command("go", "build", "..", "-o", "pipertestbin")
+	// err = cmd.Run()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// when
+	params := []string{"nexusUpload", `--artifacts=[{"id":"blob","classifier":"blob-1.0","type":"pom","file":"testdata/TestNexusIntegration/pom.xml"}]`, "--groupId=foo", "--user=admin", "--password=admin123", "--repository=maven-releases", "--version=1.0", "--url=" + fmt.Sprintf("%s:%s", ip, port.Port())}
 	c := command.Command{}
-	c.RunExecutable("go", params...)
+	err = c.RunExecutable("../pipertestbin", params...)
+	if err != nil {
+		t.Log(err)
+	}
+
+	// then
+	resp, err = http.Get(fmt.Sprintf("http://%s:%s", ip, port.Port()) + "/repository/maven-releases/foo/blob/1.0/blob-1.0.pom")
+	if resp.StatusCode != http.StatusOK {
+		t.Log("Test failed")
+		t.Log(err)
+	}
+	if resp != nil {
+		t.Log(resp)
+	}
 
 }
