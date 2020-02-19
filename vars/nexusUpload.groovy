3ee259b64d89a670cd3c77dee159c7b75308b8ac
Stephan Aßmus <stephan.assmus@sap.com>
2020-02-19 16:36:12 +0100

    echo env

--- vars/nexusUpload.groovy
+++ vars/nexusUpload.groovy
@@ -52,6 +52,7 @@ void call(Map parameters = [:]) {
             "PIPER_parametersJSON=${groovy.json.JsonOutput.toJson(parameters)}",
         ]) {
             // get context configuration
+            echo "environment: ${env.PIPER_parametersJSON}￿"
             Map config = readJSON (text: sh(returnStdout: true, script: "./piper getConfig --contextConfig --stepMetadata '${METADATA_FILE}'"))
 
             echo "config decoded from ENV: $config"
