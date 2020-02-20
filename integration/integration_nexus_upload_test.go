b71e235c9d1dc8f2b8c32972c54c6f0c3b955515
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-02-20 18:03:29 +0100

    test nexus 2 and 3

--- integration/integration_nexus_upload_test.go
+++ integration/integration_nexus_upload_test.go
@@ -72,3 +72,58 @@ func TestNexusUpload(t *testing.T) {
 	assert.NoError(t, err, "Downloading artifact failed")
 	assert.Equal(t, resp.StatusCode, http.StatusOK)
 }
+
+func TestNexus2Upload(t *testing.T) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "sonatype/nexus:2.14.16-01",
+		ExposedPorts: []string{"8081/tcp"},
+		WaitingFor:   wait.ForLog("org.sonatype.nexus.bootstrap.jetty.JettyServer - Running").WithStartupTimeout(5 * time.Minute), // Nexus takes more than one minute to boot
+	}
+	nexusContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	assert.NoError(t, err)
+	defer nexusContainer.Terminate(ctx)
+	ip, err := nexusContainer.Host(ctx)
+	assert.NoError(t, err)
+	port, err := nexusContainer.MappedPort(ctx, "8081")
+	assert.NoError(t, err, "Could not map port for nexus container")
+	nexusIpAndPort := fmt.Sprintf("%s:%s", ip, port.Port())
+	url := "http://" + nexusIpAndPort + "/nexus/"
+
+	cmd := command.Command{}
+	cmd.Dir("testdata/TestNexusIntegration/")
+
+	piperOptions := []string{
+		"nexusUpload",
+		`--artifacts=[{"artifactId":"myapp-pom","classifier":"","type":"pom","file":"pom.xml"},{"artifactId":"myapp-jar","classifier":"","type":"jar","file":"test.jar"},{"artifactId":"myapp-yaml","classifier":"","type":"yaml","file":"mta.yaml"},{"artifactId":"myapp-mtar","classifier":"","type":"mtar","file":"test.mtar"}]`,
+		"--groupId=mygroup",
+		"--user=admin",
+		"--password=admin123",
+		"--repository=releases",
+		"--version=1.0",
+		"--nexusVersion=2",
+		"--url=" + nexusIpAndPort + "/nexus/",
+	}
+
+	err = cmd.RunExecutable(getPiperExecutable(), piperOptions...)
+	assert.NoError(t, err, "Calling piper with arguments %v failed.", piperOptions)
+
+	resp, err := http.Get(url + "content/repositories/releases/mygroup/myapp-pom/1.0/myapp-pom-1.0.pom")
+	assert.NoError(t, err, "Downloading artifact failed")
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+	resp, err = http.Get(url + "content/repositories/releases/mygroup/myapp-jar/1.0/myapp-jar-1.0.jar")
+	assert.NoError(t, err, "Downloading artifact failed")
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+	resp, err = http.Get(url + "content/repositories/releases/mygroup/myapp-yaml/1.0/myapp-yaml-1.0.yaml")
+	assert.NoError(t, err, "Downloading artifact failed")
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+	resp, err = http.Get(url + "content/repositories/releases/mygroup/myapp-mtar/1.0/myapp-mtar-1.0.mtar")
+	assert.NoError(t, err, "Downloading artifact failed")
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+}
