af386c0e9331d0ade7083d12e2d7c6ee4eea0859
Stephan Aßmus <stephan.assmus@sap.com>
2020-02-20 13:38:26 +0100

    MD5 and SHA1 hashes are generated and uploaded
    ... along the actual artifact.

--- cmd/nexusUpload.go
+++ cmd/nexusUpload.go
@@ -1,10 +1,17 @@
 package cmd
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"hash"
+	"io"
 	"net/http"
+	"os"
 	"strings"
+
+	"crypto/md5"
+	"crypto/sha1"
 
 	piperHttp "github.com/SAP/jenkins-library/pkg/http"
 	"github.com/SAP/jenkins-library/pkg/log"
@@ -42,26 +49,82 @@ func nexusUpload(config nexusUploadOptions, telemetryData *telemetry.CustomData)
 
 		log.Entry().Info("Trying to upload ", artifact.File, " to ", url)
 
-		response, err := client.UploadRequest(http.MethodPut, url, artifact.File, "", nil, nil)
+		uploadHash(&client, artifact.File, url+".md5", md5.New(), 16)
+		uploadHash(&client, artifact.File, url+".sha1", sha1.New(), 20)
+
+		var file *os.File
+		file, err = os.Open(artifact.File)
 		if err != nil {
-			if response != nil && response.StatusCode == 400 {
-				log.Entry().Info("Artifact already exits, deleting and retrying...")
-				response, err = client.SendRequest(http.MethodDelete, url, nil, nil, nil)
-				if err != nil {
-					panic(fmt.Sprintf("Failed to delete artifact: %s", err))
-				} else {
-					response, err = client.UploadRequest(http.MethodPut, url, artifact.File, "", nil, nil)
-				}
-			}
-			if err != nil {
-				//				log.Entry().Info("Failed to upload artifact")
-				panic(fmt.Sprintf("Failed to upload artifact: %s", err))
-			}
+			panic(fmt.Sprintf("Failed to open artifact file: %s", err))
 		}
 
-		log.Entry().Info("Response is ", response)
+		defer file.Close()
+
+		_, err = uploadToNexus(&client, file, url)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to upload artifact: %s", err))
+		}
 	}
 
 	//log.Entry().WithField("customKey", "customValue").Info("This is how you write a log message with a custom field ...")
 	return err
+}
+
+func uploadToNexus(client *piperHttp.Client, stream io.Reader, url string) (*http.Response, error) {
+	log.Entry().Info("Upload to url '" + url + "'")
+	response, err := client.SendRequest(http.MethodPut, url, stream, nil, nil)
+	if err != nil {
+		// if response != nil && response.StatusCode == 400 {
+		// 	log.Entry().Info("Artifact already exits, deleting and retrying...\n")
+		// 	response, err = client.SendRequest(http.MethodDelete, url, nil, nil, nil)
+		// 	if err != nil {
+		// 		log.Entry().Info("Failed to delete artifact\n", err)
+		// 		return nil, err
+		// 	}
+		// 	response, err = client.SendRequest(http.MethodPut, url, stream, nil, nil)
+		// }
+		// if err != nil {
+		log.Entry().Info("Failed to upload artifact\n", err)
+		//		return nil, err
+		// }
+	}
+	log.Entry().Info("Response is ", response)
+	return response, nil
+}
+
+func uploadHash(client *piperHttp.Client, filePath, url string, hash hash.Hash, length int) (*http.Response, error) {
+	hashReader, err := generateHashReader(filePath, hash, length)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to get hash: %s", err))
+	}
+	var response *http.Response
+	response, err = uploadToNexus(client, hashReader, url)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to upload md5 hash: %s", err))
+	}
+	return response, nil
+}
+
+func generateHashReader(filePath string, hash hash.Hash, length int) (io.Reader, error) {
+	// Open file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	// Read file and feed the hash
+	if _, err := io.Copy(hash, file); err != nil {
+		return nil, err
+	}
+
+	// Get the 16 bytes hash
+	hashInBytes := hash.Sum(nil)[:length]
+
+	// Convert the bytes to a string
+	hexString := hex.EncodeToString(hashInBytes)
+
+	// Finally create an io.Reader wrapping the string
+	return strings.NewReader(hexString), nil
 }
