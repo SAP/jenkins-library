616d813a60ef06cb96d888b8ce5b75f115c46857
Florian Geckeler <f.geckeler@sap.com>
2020-02-26 17:02:17 +0100

    First implementation spotBugs; depends on mavenExec PR

---
+++ cmd/checkSpotBugs.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"github.com/SAP/jenkins-library/pkg/command"
+	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/maven"
+	"github.com/SAP/jenkins-library/pkg/telemetry"
+)
+
+func checkSpotBugs(config checkSpotBugsOptions, telemetryData *telemetry.CustomData) {
+	c := command.Command{}
+	c.Stdout(log.Entry().Writer())
+	c.Stderr(log.Entry().Writer())
+
+	err := runCheckSpotBugs(&config, telemetryData, &c)
+	if err != nil {
+		log.Entry().WithError(err).Fatal("step execution failed")
+	}
+}
+
+func runCheckSpotBugs(config *checkSpotBugsOptions, telemetryData *telemetry.CustomData, command execRunner) error {
+	var defines []string
+	if config.IncludeFilterFile != "" {
+		defines = append(defines, "-Dspotbugs.includeFilterFile="+config.IncludeFilterFile)
+	}
+	if config.ExcludeFilterFile != "" {
+		defines = append(defines, "-Dspotbugs.excludeFilterFile="+config.ExcludeFilterFile)
+	}
+	mavenOptions := maven.ExecuteOptions{
+		Goals:   "com.github.spotbugs:spotbugs-maven-plugin:3.1.12:spotbugs",
+		Defines: defines,
+	}
+
+	_, err := maven.Execute(&mavenOptions, command)
+	return err
+}
