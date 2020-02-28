22aa87a35c08c209b17532dd3844f1ba5a154709
Florian Geckeler <f.geckeler@sap.com>
2020-02-28 11:10:35 +0100

    Revert last commit

--- pkg/maven/maven_test.go
+++ pkg/maven/maven_test.go
@@ -2,49 +2,51 @@ package maven
 
 import (
 	"errors"
+
 	"github.com/SAP/jenkins-library/pkg/mock"
+
+	"net/http"
+	"testing"
 
 	piperHttp "github.com/SAP/jenkins-library/pkg/http"
 	"github.com/SAP/jenkins-library/pkg/log"
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"testing"
 )
 
 func TestExecute(t *testing.T) {
 	t.Run("should return stdOut", func(t *testing.T) {
 		expectedOutput := "mocked output"
-		e := mock.ExecMockRunner{}
-		e.StdoutReturn = map[string]string{"mvn --file pom.xml --batch-mode": "mocked output"}
+		execMockRunner := mock.ExecMockRunner{}
+		execMockRunner.StdoutReturn = map[string]string{"mvn --file pom.xml --batch-mode": "mocked output"}
 		opts := ExecuteOptions{PomPath: "pom.xml", ReturnStdout: true}
 
-		mavenOutput, _ := Execute(&opts, &e)
+		mavenOutput, _ := Execute(&opts, &execMockRunner)
 
 		assert.Equal(t, expectedOutput, mavenOutput)
 	})
 	t.Run("should not return stdOut", func(t *testing.T) {
 		expectedOutput := ""
-		e := mock.ExecMockRunner{}
-		e.StdoutReturn = map[string]string{"mvn --file pom.xml --batch-mode": "mocked output"}
+		execMockRunner := mock.ExecMockRunner{}
+		execMockRunner.StdoutReturn = map[string]string{"mvn --file pom.xml --batch-mode": "mocked output"}
 		opts := ExecuteOptions{PomPath: "pom.xml", ReturnStdout: false}
 
-		mavenOutput, _ := Execute(&opts, &e)
+		mavenOutput, _ := Execute(&opts, &execMockRunner)
 
 		assert.Equal(t, expectedOutput, mavenOutput)
 	})
 	t.Run("should log that command failed if executing maven failed", func(t *testing.T) {
 		var hasFailed bool
 		log.Entry().Logger.ExitFunc = func(int) { hasFailed = true }
-		e := mock.ExecMockRunner{ShouldFailOnCommand: map[string]error{"mvn --file pom.xml --batch-mode": errors.New("error case")}}
+		execMockRunner := mock.ExecMockRunner{ShouldFailOnCommand: map[string]error{"mvn --file pom.xml --batch-mode": errors.New("error case")}}
 		opts := ExecuteOptions{PomPath: "pom.xml", ReturnStdout: false}
 
-		output, _ := Execute(&opts, &e)
+		output, _ := Execute(&opts, &execMockRunner)
 
 		assert.True(t, hasFailed, "failed to execute run command")
 		assert.Equal(t, output, "")
 	})
 	t.Run("should have all configured parameters in the exec call", func(t *testing.T) {
-		e := mock.ExecMockRunner{}
+		execMockRunner := mock.ExecMockRunner{}
 		opts := ExecuteOptions{PomPath: "pom.xml", ProjectSettingsFile: "settings.xml",
 			GlobalSettingsFile: "anotherSettings.xml", M2Path: ".m2/",
 			Goals: []string{"flatten", "install"}, Defines: []string{"-Da=b"},
@@ -54,10 +56,10 @@ func TestExecute(t *testing.T) {
 			"-Dmaven.repo.local=.m2/", "--file", "pom.xml", "-q", "-Da=b", "--batch-mode",
 			"-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn", "flatten", "install"}
 
-		mavenOutput, _ := Execute(&opts, &e)
+		mavenOutput, _ := Execute(&opts, &execMockRunner)
 
-		assert.Equal(t, len(e.Calls[0].Params), len(expectedParameters))
-		assert.Equal(t, e.Calls[0], mock.ExecCall{Exec: "mvn", Params: expectedParameters})
+		assert.Equal(t, len(execMockRunner.Calls[0].Params), len(expectedParameters))
+		assert.Equal(t, execMockRunner.Calls[0], mock.ExecCall{Exec: "mvn", Params: expectedParameters})
 		assert.Equal(t, "", mavenOutput)
 	})
 }
