5ce54552d4a767d0d3992adac707376408c3073e
Florian Geckeler <f.geckeler@sap.com>
2020-02-28 14:28:12 +0100

    typo

--- cmd/checkSpotBugs_test.go
+++ cmd/checkSpotBugs_test.go
@@ -11,11 +11,11 @@ import (
 func TestCheckSpotBugs(t *testing.T) {
 	t.Run("mavenExecute should have include and exclude filters", func(t *testing.T) {
 		execMockRunner := mock.ExecMockRunner{}
-		config := checkSpotBugsOptions{ExcludeFilterFile: "exludeFilter.xml", IncludeFilterFile: "includeFilter.xml"}
+		config := checkSpotBugsOptions{ExcludeFilterFile: "excludeFilter.xml", IncludeFilterFile: "includeFilter.xml"}
 
 		err := runCheckSpotBugs(&config, nil, &execMockRunner)
 
 		assert.Nil(t, err)
-		assert.Equal(t, mock.ExecCall{Exec: "mvn", Params: []string{"-Dspotbugs.includeFilterFile=includeFilter.xml", "-Dspotbugs.excludeFilterFile=exludeFilter.xml", "--batch-mode", "com.github.spotbugs:spotbugs-maven-plugin:3.1.12:spotbugs"}}, execMockRunner.Calls[0])
+		assert.Equal(t, mock.ExecCall{Exec: "mvn", Params: []string{"-Dspotbugs.includeFilterFile=includeFilter.xml", "-Dspotbugs.excludeFilterFile=excludeFilter.xml", "--batch-mode", "com.github.spotbugs:spotbugs-maven-plugin:3.1.12:spotbugs"}}, execMockRunner.Calls[0])
 	})
 }
