29c88e65643041e73e25c1db9e48952db216682e
Florian Geckeler <f.geckeler@sap.com>
2020-03-06 11:00:16 +0100

    Implement mavenStaticCodeChecks

--- cmd/mavenStaticCodeChecks.go
+++ cmd/mavenStaticCodeChecks.go
@@ -1,23 +1,18 @@
 package cmd
 
 import (
+	"strings"
+
 	"github.com/SAP/jenkins-library/pkg/command"
 	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/maven"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
 )
 
 func mavenStaticCodeChecks(config mavenStaticCodeChecksOptions, telemetryData *telemetry.CustomData) {
-	// for command execution use Command
 	c := command.Command{}
-	// reroute command output to logging framework
 	c.Stdout(log.Entry().Writer())
 	c.Stderr(log.Entry().Writer())
-
-	// for http calls import  piperhttp "github.com/SAP/jenkins-library/pkg/http"
-	// and use a  &piperhttp.Client{} in a custom system
-	// Example: step checkmarxExecuteScan.go
-
-	// error situations should stop execution through log.Entry().Fatal() call which leads to an os.Exit(1) in the end
 	err := runMavenStaticCodeChecks(&config, telemetryData, &c)
 	if err != nil {
 		log.Entry().WithError(err).Fatal("step execution failed")
@@ -25,6 +20,62 @@ func mavenStaticCodeChecks(config mavenStaticCodeChecksOptions, telemetryData *t
 }
 
 func runMavenStaticCodeChecks(config *mavenStaticCodeChecksOptions, telemetryData *telemetry.CustomData, command execRunner) error {
-	log.Entry().WithField("LogField", "Log field content").Info("This is just a demo for a simple step.")
-	return nil
+	var defines []string
+
+	if testModulesExcludes := maven.GetTestModulesExcludes(); testModulesExcludes != nil {
+		defines = append(defines, testModulesExcludes...)
+	}
+	if config.MavenModulesExcludes != nil {
+		for _, module := range config.MavenModulesExcludes {
+			defines = append(defines, "-pl")
+			defines = append(defines, "!"+module)
+		}
+	}
+
+	spotBugsMavenParameters := getSpotBugsMavenParameters(config)
+	pmdMavenParameters := getPmdMavenParameters(config)
+
+	defines = append(defines, spotBugsMavenParameters.Defines...)
+	defines = append(defines, pmdMavenParameters.Defines...)
+
+	finalMavenOptions := maven.ExecuteOptions{
+		Goals:   append(spotBugsMavenParameters.Goals, pmdMavenParameters.Goals...),
+		Defines: defines,
+	}
+	_, err := maven.Execute(&finalMavenOptions, command)
+	return err
+}
+
+func getSpotBugsMavenParameters(config *mavenStaticCodeChecksOptions) *maven.ExecuteOptions {
+	var defines []string
+	if config.SpotBugsIncludeFilterFile != "" {
+		defines = append(defines, "-Dspotbugs.excludeFilterFile="+config.SpotBugsIncludeFilterFile)
+	}
+	if config.SpotBugsExcludeFilterFile != "" {
+		defines = append(defines, "-Dspotbugs.excludeFilterFile="+config.SpotBugsExcludeFilterFile)
+	}
+
+	mavenOptions := maven.ExecuteOptions{
+		Goals:   []string{"com.github.spotbugs:spotbugs-maven-plugin:3.1.12:spotbugs"},
+		Defines: defines,
+	}
+
+	return &mavenOptions
+}
+
+func getPmdMavenParameters(config *mavenStaticCodeChecksOptions) *maven.ExecuteOptions {
+	var defines []string
+	if config.PmdExcludes != nil {
+		defines = append(defines, "-Dpmd.excludes="+strings.Join(config.PmdExcludes, ","))
+	}
+	if config.PmdRuleSets != nil {
+		defines = append(defines, "-Dpmd.rulesets="+strings.Join(config.PmdRuleSets, ","))
+	}
+
+	mavenOptions := maven.ExecuteOptions{
+		Goals:   []string{"org.apache.maven.plugins:maven-pmd-plugin:3.13.0:pmd"},
+		Defines: defines,
+	}
+
+	return &mavenOptions
 }
