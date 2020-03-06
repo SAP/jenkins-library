d7ad6300d5f88b0940e1c5299d703fad40ca5765
dominiklendle <d.lendle@sap.com>
2020-03-06 10:10:23 +0100

    Changed Groovy File

--- vars/cloudFoundryCreateServiceKey.groovy
+++ vars/cloudFoundryCreateServiceKey.groovy
@@ -1,5 +1,6 @@
 import com.sap.piper.GenerateDocumentation
 import com.sap.piper.BashUtils
+import com.sap.piper.PiperGoUtils
 import com.sap.piper.JenkinsUtils
 import com.sap.piper.Utils
 import com.sap.piper.ConfigurationHelper
@@ -8,58 +9,51 @@ import groovy.transform.Field
 
 import static com.sap.piper.Prerequisites.checkScript
 
-@Field String STEP_NAME = 'cloudFoundryCreateServiceKey'
+@Field def STEP_NAME = getClass().getName()
+@Field String METADATA_FILE = 'metadata/cloudFoundryDeleteService.yaml'
 
-@Field Set STEP_CONFIG_KEYS = [
-    'cloudFoundry',
-        /**
-         * Cloud Foundry API endpoint.
-         * @parentConfigKey cloudFoundry
-         */
-        'apiEndpoint',
-        /**
-         * Cloud Foundry credentials.
-         * @parentConfigKey cloudFoundry
-         */
-        'credentialsId',
-        /**
-         * Cloud Foundry target organization.
-         * @parentConfigKey cloudFoundry
-         */
-        'org',
-        /**
-         * Cloud Foundry target space.
-         * @parentConfigKey cloudFoundry
-         */
-        'space',
-        /**
-         * Cloud Foundry service instance, for which the service key will be created.
-         * @parentConfigKey cloudFoundry
-         */
-        'serviceInstance',
-        /**
-         * Cloud Foundry service key, which will be created.
-         * @parentConfigKey cloudFoundry
-         */
-        'serviceKey',
-        /**
-         * Cloud Foundry service key configuration.
-         * @parentConfigKey cloudFoundry
-         */
-        'serviceKeyConfig',
-    /** @see dockerExecute */
-    'dockerImage',
-    /** @see dockerExecute */
-    'dockerWorkspace'
-]
+void call(Map parameters = [:]) {
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters, failOnError: true) {
 
-@Field Set GENERAL_CONFIG_KEYS = STEP_CONFIG_KEYS
-@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
-@Field Map CONFIG_KEY_COMPATIBILITY = [cloudFoundry: [apiEndpoint: 'cfApiEndpoint', credentialsId: 'cfCredentialsId', org: 'cfOrg', space: 'cfSpace', serviceInstance: 'cfServiceInstance', serviceKey: 'cfServiceKey', serviceKeyConfig: 'cfServiceKeyConfig']]
+        def script = checkScript(this, parameters) ?: this
+
+        Map config
+        def utils = parameters.juStabUtils ?: new Utils()
+
+        script.commonPipelineEnvironment.writeToDisk(script)
+
+        writeFile(file: METADATA_FILE, text: libraryResource(METADATA_FILE))
+
+        withEnv([
+            "PIPER_parametersJSON=${groovy.json.JsonOutput.toJson(parameters)}",
+        ]) {
+            // get context configuration
+            config = readJSON (text: sh(returnStdout: true, script: "./piper getConfig --contextConfig --stepMetadata '${METADATA_FILE}'"))
+            // execute step
+            dockerExecute(
+                script: script,
+                dockerImage: config.dockerImage,
+                dockerWorkspace: config.dockerWorkspace
+            ) {
+                withCredentials([usernamePassword(
+                    credentialsId: config.cfCredentialsId,
+                    passwordVariable: 'PIPER_password',
+                    usernameVariable: 'PIPER_username'
+                )]) {
+                    sh "./piper cloudFoundryCreateServiceKey"
+                }
+            }
+        }
+    }
+}
+
+
+
+
 
 /**
  * Step that creates a service key for a specified service instance on Cloud Foundry
- */
+ 
 @GenerateDocumentation
 void call(Map parameters = [:]) {
     handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters) {
@@ -109,3 +103,4 @@ private def executeCreateServiceKey(script, Map config) {
         }
     }
 }
+*/
