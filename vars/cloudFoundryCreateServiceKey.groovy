d15aea8f885c79a241f5d6276a3f1f0651af272b
dominiklendle <d.lendle@sap.com>
2020-03-09 13:18:57 +0100

    Minor changes

--- vars/cloudFoundryCreateServiceKey.groovy
+++ vars/cloudFoundryCreateServiceKey.groovy
@@ -10,56 +10,7 @@ import groovy.transform.Field
 import static com.sap.piper.Prerequisites.checkScript
 
 @Field def STEP_NAME = getClass().getName()
-//@Field String METADATA_FILE = 'metadata/cloudFoundryCreateServiceKey.yaml'
-
-@Field Set STEP_CONFIG_KEYS = [
-    'cloudFoundry',
-        /**
-         * Cloud Foundry API endpoint.
-         * @parentConfigKey cloudFoundry
-         */
-        'apiEndpoint',
-        /**
-         * Cloud Foundry credentials.
-         * @parentConfigKey cloudFoundry
-         */
-        'credentialsId',
-        /**
-         * Cloud Foundry target organization.
-         * @parentConfigKey cloudFoundry
-         */
-        'org',
-        /**
-         * Cloud Foundry target space.
-         * @parentConfigKey cloudFoundry
-         */
-        'space',
-        /**
-         * Cloud Foundry service instance, for which the service key will be created.
-         * @parentConfigKey cloudFoundry
-         */
-        'serviceInstance',
-        /**
-         * Cloud Foundry service key, which will be created.
-         * @parentConfigKey cloudFoundry
-         */
-        'serviceKeyName',
-        /**
-         * Cloud Foundry service key configuration.
-         * @parentConfigKey cloudFoundry
-         */
-        'serviceKeyConfig',
-    /** @see dockerExecute */
-    'dockerImage',
-    /** @see dockerExecute */
-    'dockerWorkspace'
-]
-
-
-@Field Set GENERAL_CONFIG_KEYS = STEP_CONFIG_KEYS
-@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
-@Field Map CONFIG_KEY_COMPATIBILITY = [cloudFoundry: [apiEndpoint: 'cfApiEndpoint', credentialsId: 'cfCredentialsId', org: 'cfOrg', space: 'cfSpace', serviceInstance: 'cfServiceInstance', serviceKeyName: 'cfServiceKeyName', serviceKeyConfig: 'cfServiceKeyConfig']]
-
+@Field String METADATA_FILE = 'metadata/cloudFoundryCreateServiceKey.yaml'
 
 void call(Map parameters = [:]) {
     handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters, failOnError: true) {
