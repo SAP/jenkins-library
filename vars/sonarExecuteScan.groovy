954a9aa95257094f95fc4292a38ecd8d6ea28d17
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2020-03-09 12:45:54 +0100

    Update sonarExecuteScan.groovy

--- vars/sonarExecuteScan.groovy
+++ vars/sonarExecuteScan.groovy
@@ -136,7 +136,7 @@ void call(Map parameters = [:]) {
                         // prefix options
                         config.options = config.options.collect { it.startsWith('-D') ? it : "-D${it}" }
 
-                        sh "PATH=\$PATH:'${env.WORKSPACE}/.sonar-scanner/bin' sonar-scanner ${config.options.join(' ')}"
+                        sh "PATH=\$PATH:'${env.WORKSPACE.replaceAll(' ', '\\ ' )}/.sonar-scanner/bin' sonar-scanner ${config.options.join(' ')}"
                 }
             } finally {
                 sh 'rm -rf .sonar-scanner .certificates .scannerwork'
