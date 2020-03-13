bc0a8eeb4e4b81c16251a03288e0a7832e768d9b
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-03-13 16:32:35 +0100

    Add comments

--- pkg/mock/fileUtils.go
+++ pkg/mock/fileUtils.go
@@ -11,6 +11,7 @@ type FilesMock struct {
 	Files []string
 }
 
+// FileExists ...
 func (f FilesMock) FileExists(filename string) (bool, error) {
 
 	for _, file := range f.Files {
@@ -23,18 +24,22 @@ func (f FilesMock) FileExists(filename string) (bool, error) {
 	return false, nil
 }
 
+// Copy ...
 func (f FilesMock) Copy(src, dst string) (int64, error) {
 	return 0, errors.New("Not implemented")
 }
 
+// FileRead ...
 func (f FilesMock) FileRead(path string) ([]byte, error) {
 	return nil, errors.New("Not implemented")
 }
 
+// FileWrite ...
 func (f FilesMock) FileWrite(path string, content []byte, perm os.FileMode) error {
 	return errors.New("Not implemented")
 }
 
+// MkdirAll ...
 func (f FilesMock) MkdirAll(path string, perm os.FileMode) error {
 	return errors.New("Not implemented")
 }
