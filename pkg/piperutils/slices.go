6aa0d3e7369c89d25c47e0959152d565362bb6af
Christopher Fenner <Christopher.Fenner@sap.com>
2020-03-17 15:59:50 +0100

    add slice prefix function

--- pkg/piperutils/slices.go
+++ pkg/piperutils/slices.go
@@ -1,6 +1,8 @@
 package piperutils
 
-import ()
+import (
+	"strings"
+)
 
 //ContainsInt check wether the element is part of the slice
 func ContainsInt(s []int, e int) bool {
@@ -10,4 +12,16 @@ func ContainsInt(s []int, e int) bool {
 		}
 	}
 	return false
+}
+
+//Prefix adds a prefix to each element of the slice
+func Prefix(slice []string, prefix string) []string {
+	for idx, element := range slice {
+		element = strings.TrimSpace(element)
+		if !strings.HasPrefix(element, prefix) {
+			element = prefix + element
+		}
+		slice[idx] = element
+	}
+	return slice
 }
--- pkg/piperutils/slices_test.go
+++ pkg/piperutils/slices_test.go
@@ -1,8 +1,9 @@
 package piperutils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestContainsInt(t *testing.T) {
@@ -14,4 +15,15 @@ func TestContainsInt(t *testing.T) {
 	assert.Equal(t, true, ContainsInt(intList, 1), "True expected but returned false")
 	assert.Equal(t, true, ContainsInt(intList, 4), "True expected but returned false")
 	assert.Equal(t, false, ContainsInt(intList, 13), "False expected but returned true")
+}
+
+func TestPrefix(t *testing.T) {
+	// init
+	s := []string{"tree", "pie", "juice"}
+	// test
+	s = Prefix(s, "apple")
+	// assert
+	assert.Contains(t, s, "appletree")
+	assert.Contains(t, s, "applejuice")
+	assert.Contains(t, s, "applepie")
 }
