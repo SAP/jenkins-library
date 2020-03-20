b39080409c24a9903df2048368dbaf2803cb33b6
Florian Geckeler <43751896+fgeckeler@users.noreply.github.com>
2020-03-20 17:01:20 +0100

    Add DownloadCacheUtils for handling download cache docker container (#1298)

---
+++ src/com/sap/piper/DownloadCacheUtils.groovy
@@ -0,0 +1,49 @@
+package com.sap.piper
+
+
+class DownloadCacheUtils {
+
+    static boolean isEnabled(Script script) {
+        script.node('master') {
+            String network = script.env.DL_CACHE_NETWORK
+            String host = script.env.DL_CACHE_HOSTNAME
+            return (network.asBoolean() && host.asBoolean())
+        }
+    }
+
+    static String getDockerOptions(Script script) {
+        script.node('master') {
+            String dockerNetwork = script.env.DL_CACHE_NETWORK
+            if (!dockerNetwork) {
+                return ''
+            }
+            return " --network=$dockerNetwork"
+        }
+    }
+
+    static String getGlobalMavenSettingsForDownloadCache(Script script) {
+        String globalSettingsFilePath = '.pipeline/global_settings.xml'
+        if (script.fileExists(globalSettingsFilePath)) {
+            return globalSettingsFilePath
+        }
+
+        String hostname = ''
+        script.node('master') {
+            hostname = script.env.DL_CACHE_HOSTNAME // set by cx-server
+        }
+
+        if (!hostname) {
+            return ''
+        }
+
+        String mavenSettingsTemplate = script.libraryResource("mvn_download_cache_proxy_settings.xml")
+        String mavenSettings = mavenSettingsTemplate.replace('__HOSTNAME__', hostname)
+
+        if (!script.fileExists('.pipeline')) {
+            script.sh "mkdir .pipeline"
+        }
+
+        script.writeFile file: globalSettingsFilePath, text: mavenSettings
+        return globalSettingsFilePath
+    }
+}
