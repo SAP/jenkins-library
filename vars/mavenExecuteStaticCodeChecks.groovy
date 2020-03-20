86d994de9420e2fbf18cb6a815174a5b42b442dc
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-03-20 16:47:43 +0100

    Use piper step to execute go

--- vars/mavenExecuteStaticCodeChecks.groovy
+++ vars/mavenExecuteStaticCodeChecks.groovy
@@ -1,12 +1,9 @@
-import com.sap.piper.Utils
 import groovy.transform.Field
 
-@Field String GO_COMMAND = 'mavenExecuteStaticCodeChecks'
 @Field String METADATA_FILE = 'metadata/mavenStaticCodeChecks.yaml'
 @Field String STEP_NAME = getClass().getName()
-@Field String METADATA_FOLDER = '.pipeline' // metadata file contains already the "metadata" folder level, hence we end up in a folder ".pipeline/metadata"
 
 void call(Map parameters = [:]) {
-    def utils = parameters.juStabUtils ?: new Utils()
-    utils.runGoStepWithDocker(this, parameters)
+    List credentials = []
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
 }
