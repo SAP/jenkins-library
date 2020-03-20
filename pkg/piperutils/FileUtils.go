f515e755fd2d420d6c4c3616b436cb93f5de5b2f
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2020-03-20 10:51:02 +0100

    Update FileUtils.go

--- pkg/piperutils/FileUtils.go
+++ pkg/piperutils/FileUtils.go
@@ -73,7 +73,28 @@ func (f Files) Copy(src, dst string) (int64, error) {
 
 // Unzip will decompress a zip archive, moving all files and folders
 // within the zip file (parameter 1) to an output directory (parameter 2).
-// from https://golangcode.com/unzip-files-in-go/
+// from https://golangcode.com/unzip-files-in-go/ with the following license:
+// MIT License
+// 
+// Copyright (c) 2017 Edd Turtle
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 func Unzip(src, dest string) ([]string, error) {
 
 	var filenames []string
