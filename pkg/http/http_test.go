4fe9b3240645104f124b3bc4ffd502dbe1fc0f64
Stephan Aßmus <stephan.assmus@sap.com>
2020-03-25 15:05:12 +0100

    Fix flaky check on exact error message

--- pkg/http/http_test.go
+++ pkg/http/http_test.go
@@ -253,10 +253,8 @@ func TestTransportTimout(t *testing.T) {
 		// test
 		_, err := client.SendRequest(http.MethodGet, svr.URL, &buffer, nil, nil)
 		// assert
-		assert.EqualError(t, err,
-			fmt.Sprintf("error opening %v: Get %v: net/http: timeout awaiting response headers",
-				svr.URL, svr.URL),
-			"expected request to fail")
+		assert.Error(t, err, "expected request to fail")
+		assert.Contains(t, err.Error(), "timeout awaiting response headers")
 	})
 	t.Run("timeout is not hit on transport level", func(t *testing.T) {
 		// init
