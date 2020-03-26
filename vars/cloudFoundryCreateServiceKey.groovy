c59fc2417b6a959d43e59dfd40d4014829e04951
dominiklendle <d.lendle@sap.com>
2020-03-26 11:08:11 +0100

    Changed groovy

--- vars/cloudFoundryCreateServiceKey.groovy
+++ vars/cloudFoundryCreateServiceKey.groovy
@@ -9,7 +9,7 @@ import groovy.transform.Field
 
 import static com.sap.piper.Prerequisites.checkScript
 
-@Field def STEP_NAME = getClass().getName()
+@Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/cloudFoundryCreateServiceKey.yaml'
 
 void call(Map parameters = [:]) {
