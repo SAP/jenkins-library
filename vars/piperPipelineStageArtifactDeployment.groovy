b6bb7b80237e9bf98ce9c4e8aeebfdb27fa5851d
Stephan Aßmus <stephan.assmus@sap.com>
2020-03-27 11:58:02 +0100

    Move params processing around

--- vars/piperPipelineStageArtifactDeployment.groovy
+++ vars/piperPipelineStageArtifactDeployment.groovy
@@ -14,44 +14,10 @@ import static com.sap.piper.Prerequisites.checkScript
 @Field Set GENERAL_CONFIG_KEYS = []
 @Field Set STAGE_STEP_KEYS = [
     /** Parameters for deployment to a Nexus Repository Manager. */
-    'nexus',
-        /**
-         * Version of Nexus. Can be nexus2 or nexus3.
-         * @parentConfigKey nexus
-         * @default `nexus3`
-         * @possibleValues `nexus2`, `nexus3`
-         */
-        'version',
-        /**
-         * URL of the Nexus. The scheme part of the URL will not be considered,
-         * because only http is supported.
-         * @parentConfigKey nexus
-         */
-        'url',
-        /**
-         * Name of the Nexus repository.
-         * @parentConfigKey nexus
-         */
-        'repository',
-        /**
-         * List of additional classifiers that should be deployed to Nexus.
-         * Each item is a map of a type and a classifier name.
-         * @parentConfigKey nexus
-         */
-        'additionalClassifiers',
-        /**
-         * ID to the credentials which is used to connect to Nexus.
-         * Anonymous deployments do not require a credentialsId.
-         * @parentConfigKey nexus
-         * @possibleValues Jenkins credential id
-         */
-        'credentialsId',
+    'nexus'
 ]
 @Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS.plus(STAGE_STEP_KEYS)
-@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS.plus([
-    /** Artifact ID of the main build artifact. */
-    'artifactId',
-])
+@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
 
 /**
  * This stage is responsible for releasing/deploying artifacts to a Nexus Repository Manager.<br />
@@ -91,22 +57,15 @@ void call(Map parameters = [:]) {
             version: nexusConfig.version,
             repository: nexusConfig.repository,
             url: nexusConfig.url,
+            additionalClassifiers: nexusConfig.additionalClassifiers,
         ]
 
         nexusUploadParams = DownloadCacheUtils.injectDownloadCacheInMavenParameters(script as Script, nexusUploadParams)
 
-        // Set artifactId if provided via parameters, fall-back to artifactId from CPE if set
-        if (nexusConfig.artifactId) {
-            nexusUploadParams.artifactId = nexusConfig.artifactId
-        } else if (script.commonPipelineEnvironment.configuration.artifactId) {
+        // Set artifactId from CPE if set
+        if (script.commonPipelineEnvironment.configuration.artifactId) {
             nexusUploadParams.artifactId = script.commonPipelineEnvironment.configuration.artifactId
         }
-        // Replace 'additionalClassifiers' List with JSON encoded String
-        if (nexusConfig.additionalClassifiers) {
-            nexusUploadParams.additionalClassifiers = "${toJson(nexusConfig.additionalClassifiers as List)}"
-        }
-
-        nexusUploadParams.verbose = true
 
         // The withEnv wrapper can be removed before merging to master.
         withEnv(['REPOSITORY_UNDER_TEST=SAP/jenkins-library','LIBRARY_VERSION_UNDER_TEST=stage-artifact-deployment']) {
