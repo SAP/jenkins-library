1566294a92db055bef900a514bde5cef201dcbe5
dominiklendle <d.lendle@sap.com>
2020-03-30 11:23:50 +0200

    Adapted Groovy imports

--- vars/cloudFoundryCreateServiceKey.groovy
+++ vars/cloudFoundryCreateServiceKey.groovy
@@ -1,10 +1,5 @@
-import com.sap.piper.GenerateDocumentation
-import com.sap.piper.BashUtils
 import com.sap.piper.PiperGoUtils
-import com.sap.piper.JenkinsUtils
 import com.sap.piper.Utils
-import com.sap.piper.ConfigurationHelper
-
 import groovy.transform.Field
 
 import static com.sap.piper.Prerequisites.checkScript
