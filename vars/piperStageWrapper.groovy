d1bd8ca5b4619424f0a1cb48306ba63368c12343
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2020-03-30 09:35:01 +0200

    extract condition

--- vars/piperStageWrapper.groovy
+++ vars/piperStageWrapper.groovy
@@ -80,7 +80,7 @@ private void executeStage(script, originalStage, stageName, config, utils, telem
         def body = originalStage
 
         // First, check if a global extension exists via a dedicated repository
-        if ((script.env.PIPER_DISABLE_EXTENSIONS==null || globalExtensions) && (!Boolean.valueOf(script.env.PIPER_DISABLE_EXTENSIONS) && globalExtensions)) {
+        if (globalExtensions && allowExtensions()) {
             echo "[${STEP_NAME}] Found global interceptor '${globalInterceptorFile}' for ${stageName}."
             // If we call the global interceptor, we will pass on originalStage as parameter
             DebugReport.instance.globalExtensions.put(stageName, "Overwrites")
@@ -95,7 +95,7 @@ private void executeStage(script, originalStage, stageName, config, utils, telem
         }
 
         // Second, check if a project extension (within the same repository) exists
-        if ((script.env.PIPER_DISABLE_EXTENSIONS==null || projectExtensions) && (!Boolean.valueOf(script.env.PIPER_DISABLE_EXTENSIONS) && projectExtensions)) {
+        if (projectExtensions && allowExtensions()) {
             echo "[${STEP_NAME}] Running project interceptor '${projectInterceptorFile}' for ${stageName}."
             // If we call the project interceptor, we will pass on body as parameter which contains either originalStage or the repository interceptor
             if (projectExtensions && globalExtensions) {
@@ -187,4 +187,8 @@ private void validateInterceptor(Script interceptor, String extensionFileName) {
 private boolean isOldInterceptorInterfaceUsed(Script interceptor) {
     MetaMethod method = interceptor.metaClass.pickMethod("call", [Closure.class, String.class, Map.class, Map.class] as Class[])
     return method != null
+}
+
+private boolean allowExtensions(){
+    return env.PIPER_DISABLE_EXTENSIONS == null || Boolean.valueOf(env.PIPER_DISABLE_EXTENSIONS) == true
 }
