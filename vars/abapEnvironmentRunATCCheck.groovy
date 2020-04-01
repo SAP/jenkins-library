b5ca329cb85b10bad1433eb1aebb84d037f31068
dominiklendle <d.lendle@sap.com>
2020-04-01 17:03:36 +0200

    corrected metadata file

--- vars/abapEnvironmentRunATCCheck.groovy
+++ vars/abapEnvironmentRunATCCheck.groovy
@@ -5,7 +5,7 @@ import groovy.transform.Field
 import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
-@Field String METADATA_FILE = 'metadata/abapEnvironemtnRunATCCheck.yaml'
+@Field String METADATA_FILE = 'metadata/abapEnvironmentRunATCCheck.yaml'
 
 void call(Map parameters = [:]) {
         List credentials = [
