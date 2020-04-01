c0bed1f198925fea8e3cfd2c1c20a9a9491bdc96
OliverNocon <oliver.nocon@sap.com>
2020-04-01 13:43:19 +0200

    fix syntax error

--- cmd/artifactPrepareVersion.go
+++ cmd/artifactPrepareVersion.go
@@ -255,7 +255,7 @@ func pushChanges(config *artifactPrepareVersionOptions, newVersion string, repos
 			// handling compatibility: try to use ssh in case no credentials are available
 			log.Entry().Info("git username/password missing - switching to ssh")
 			remoteURL := strings.Replace(urls[0], "https://", "git@", 1)
-			remoteURL := strings.Replace(remoteURL, ":", "/", 1)
+			remoteURL = strings.Replace(remoteURL, ":", "/", 1)
 			pushOptions.RemoteName = remoteURL
 			log.Entry().Infof("using remote '%v'", remoteURL)
 			log.Entry().Info("Relying on environment to provide ssh credentials")
