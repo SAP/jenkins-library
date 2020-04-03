d816539ce7a77e53ca5e25a33a79a887c001ab26
Oliver Nocon <33484802+OliverNocon@users.noreply.github.com>
2020-04-03 08:46:18 +0200

    Update pkg/versioning/maven_test.go
    Co-Authored-By: Stephan Aßmus <stephan.assmus@sap.com>

--- pkg/versioning/maven_test.go
+++ pkg/versioning/maven_test.go
@@ -83,7 +83,7 @@ func TestMavenSetVersion(t *testing.T) {
 		expectedOptions := maven.ExecuteOptions{
 			PomPath:             "path/to/pom.xml",
 			Defines:             []string{"-DnewVersion=1.2.4}", "-DgroupId=testGroup}", "-DartifactId='*'", "-DoldVersion='*'", "-DgenerateBackupPoms=false"},
-			Goals:               []string{"versions:set"},
+			Goals:               []string{"org.codehaus.mojo:versions-maven-plugin:2.7:set"},
 			ProjectSettingsFile: "project-settings.xml",
 			GlobalSettingsFile:  "global-settings.xml",
 			M2Path:              "m2/path",
