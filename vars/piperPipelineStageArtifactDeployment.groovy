ded6152b56d6dc691a36a449f1dc87f45398d694
Stephan Aßmus <stephan.assmus@sap.com>
2020-04-04 11:44:02 +0200

    nexusUpload: Remove config processing from Groovy layer (#1364)

--- vars/piperPipelineStageArtifactDeployment.groovy
+++ vars/piperPipelineStageArtifactDeployment.groovy
@@ -1,4 +1,3 @@
-import com.sap.piper.ConfigurationHelper
 import com.sap.piper.GenerateStageDocumentation
 import com.sap.piper.ReportAggregator
 import groovy.transform.Field
@@ -9,10 +8,7 @@ import static com.sap.piper.Prerequisites.checkScript
 
 @Field Set GENERAL_CONFIG_KEYS = []
 @Field Set STAGE_STEP_KEYS = []
-@Field Set STAGE_CONFIG_KEYS = STAGE_STEP_KEYS.plus([
-    /** Parameters for deployment to a Nexus Repository Manager. */
-    'nexus'
-])
+@Field Set STAGE_CONFIG_KEYS = STAGE_STEP_KEYS
 @Field Set PARAMETER_KEYS = STAGE_CONFIG_KEYS
 
 /**
@@ -22,14 +18,6 @@ import static com.sap.piper.Prerequisites.checkScript
 void call(Map parameters = [:]) {
     String stageName = 'artifactDeployment'
     final script = checkScript(this, parameters) ?: this
-
-    Map config = ConfigurationHelper.newInstance(this)
-        .loadStepDefaults()
-        .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
-        .mixinStageConfig(script.commonPipelineEnvironment, stageName, STAGE_CONFIG_KEYS)
-        .mixin(parameters, PARAMETER_KEYS)
-        .withMandatoryProperty('nexus')
-        .use()
 
     piperStageWrapper(stageName: stageName, script: script) {
 
@@ -41,21 +29,8 @@ void call(Map parameters = [:]) {
             commonPipelineEnvironment.setValue('unstableSteps', unstableSteps)
         }
 
-        Map nexusConfig = config.nexus as Map
-
-        // Pull additionalClassifiers param from resolved config here for legacy compatibility.
-        // The parameter will become obsolete soon.
-        Map nexusUploadParams = [
-            script: script,
-            additionalClassifiers: nexusConfig.additionalClassifiers,
-        ]
-
-        if (nexusConfig.credentialsId) {
-            nexusUploadParams.nexusCredentialsId = nexusConfig.credentialsId
-        }
-
         withEnv(["STAGE_NAME=${stageName}"]) {
-            nexusUpload(nexusUploadParams)
+            nexusUpload(script: script)
         }
 
         ReportAggregator.instance.reportDeploymentToNexus()
