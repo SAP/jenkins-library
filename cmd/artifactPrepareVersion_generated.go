d392507edfec8509ff083da845b976300b49885e
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-04-08 17:32:27 +0200

    Mask secrets

--- cmd/artifactPrepareVersion_generated.go
+++ cmd/artifactPrepareVersion_generated.go
@@ -112,7 +112,13 @@ Configuration of this pattern is done via ` + "`" + `versioningType: library` + 
 			startTime = time.Now()
 			log.SetStepName("artifactPrepareVersion")
 			log.SetVerbose(GeneralConfig.Verbose)
-			return PrepareConfig(cmd, &metadata, "artifactPrepareVersion", &stepConfig, config.OpenPiperFile)
+			err := PrepareConfig(cmd, &metadata, "artifactPrepareVersion", &stepConfig, config.OpenPiperFile)
+			if err != nil {
+				return err
+			}
+			log.RegisterSecret(stepConfig.Password)
+			log.RegisterSecret(stepConfig.Username)
+			return nil
 		},
 		Run: func(cmd *cobra.Command, args []string) {
 			telemetryData := telemetry.CustomData{}
