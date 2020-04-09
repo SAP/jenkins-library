c910ecd10da4e8a8f7bccece6a06965fb158a820
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-04-09 14:49:42 +0200

    Convert maven parameters

--- vars/mavenExecute.groovy
+++ vars/mavenExecute.groovy
@@ -7,6 +7,19 @@ import groovy.transform.Field
 void call(Map parameters = [:]) {
     List credentials = [ ]
     parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(parameters.script, parameters)
+
+    if (parameters.flags  && parameters.flags instanceof CharSequence) {
+        parameters.flags  = [parameters.flags]
+    }
+
+    if (parameters.defines  && parameters.defines  instanceof CharSequence) {
+        parameters.defines = [parameters.defines ]
+    }
+
+    if (parameters.goals  && parameters.goals  instanceof CharSequence) {
+        parameters.goals = [parameters.goals ]
+    }
+
     echo parameters.toString()
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
 }
