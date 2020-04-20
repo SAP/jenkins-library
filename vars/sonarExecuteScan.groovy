ba4b56fec143a51c38b5558ae717c4252e396bb1
Christopher Fenner <Christopher.Fenner@sap.com>
2020-04-20 10:18:58 +0200

    archive reports

--- vars/sonarExecuteScan.groovy
+++ vars/sonarExecuteScan.groovy
@@ -73,6 +73,7 @@ void call(Map parameters = [:]) {
                         }
                     }
                 }
+                archiveArtifacts artifacts: "sonarExecuteScan_*.json", allowEmptyArchive: true
                 jenkinsUtils.handleStepResults(STEP_NAME, false, false)
             }
         }
