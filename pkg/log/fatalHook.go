70574267db5d0ca599be867cdb059c61e9ae76df
OliverNocon <oliver.nocon@sap.com>
2020-04-23 20:18:12 +0200

    Add comments for exported objects

--- pkg/log/fatalHook.go
+++ pkg/log/fatalHook.go
@@ -9,15 +9,20 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// FatalHook provides a logrus hook which persists details about a fatal error into the file system.
+// This is helpful in order to transfer the error details to an orchestrating CI/CD system
+// and by that make it possible to provide better error messages to the user.
 type FatalHook struct {
 	Path          string
 	CorrelationID string
 }
 
+// Levels returns the supported log level of the hook.
 func (f *FatalHook) Levels() []logrus.Level {
 	return []logrus.Level{logrus.FatalLevel}
 }
 
+// Fire persists the error message of the fatal error as json file into the file system.
 func (f *FatalHook) Fire(entry *logrus.Entry) error {
 	details := entry.Data
 	if details == nil {
