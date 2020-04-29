47606af9cd3707c54c55e5a0ce01aa93142b5dc9
Daniel Mieg <daniel.mieg@sap.com>
2020-04-29 09:52:27 +0200

    Add abapPipeline

---
+++ vars/abapPipeline.groovy
@@ -0,0 +1,40 @@
+void call(parameters) {
+    pipeline {
+        agent none
+        //triggers {
+        //    issueCommentTrigger('.*/piper ([a-z]*).*')
+        //}
+        options {
+        //    skipDefaultCheckout()
+            timestamps()
+        }
+        stages {
+            stage('Init') {
+                steps {
+                    library 'piper-lib-os'
+                    piperPipelineStageInit script: parameters.script, customDefaults: ['com.sap.piper/pipeline/stageOrdinals.yml'].plus(parameters.customDefaults ?: [])
+                }
+            }
+            stage('Create Service') {
+                steps {
+                    cloudFoundryCreateService script: parameters.script
+                }
+            }
+            stage('Delete Service') {
+                steps {
+                    cloudFoundryDeleteService script: parameters.script
+                }
+            }
+        }
+        // post {
+        //     /* https://jenkins.io/doc/book/pipeline/syntax/#post */
+        //     success {buildSetResult(currentBuild)}
+        //     aborted {buildSetResult(currentBuild, 'ABORTED')}
+        //     failure {buildSetResult(currentBuild, 'FAILURE')}
+        //     unstable {buildSetResult(currentBuild, 'UNSTABLE')}
+        //     cleanup {
+        //         piperPipelineStagePost script: parameters.script
+        //     }
+        // }
+    }
+}
