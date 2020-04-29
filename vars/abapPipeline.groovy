6f6d9b8e2bf22a12d20406694dcda3d98901f70d
Daniel Mieg <daniel.mieg@sap.com>
2020-04-29 10:48:44 +0200

    Small changes

--- vars/abapPipeline.groovy
+++ vars/abapPipeline.groovy
@@ -9,12 +9,12 @@ void call(parameters) {
             timestamps()
         }
         stages {
-            // stage('Init') {
-            //     steps {
-            //         library 'piper-lib-os'
+            stage('Init') {
+                steps {
+                    library 'piper-lib-os-dev'
             //         piperPipelineStageInit script: parameters.script, customDefaults: ['com.sap.piper/pipeline/stageOrdinals.yml'].plus(parameters.customDefaults ?: [])
-            //     }
-            // }
+                }
+            }
             stage('Create Service') {
                 steps {
                     cloudFoundryCreateService script: parameters.script
