481405dd669fca3707fe6c4008768968efd91e2e
Christopher Fenner <Christopher.Fenner@sap.com>
2020-05-01 20:19:34 +0200

    use piperExecuteBin

--- vars/cloudFoundryDeleteService.groovy
+++ vars/cloudFoundryDeleteService.groovy
@@ -1,56 +1,8 @@
-import com.sap.piper.MapUtils
-import com.sap.piper.PiperGoUtils
-import com.sap.piper.Utils
 import groovy.transform.Field
-
-import static com.sap.piper.Prerequisites.checkScript
 
 @Field def STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/cloudFoundryDeleteService.yaml'
 
 void call(Map parameters = [:]) {
-    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters, failOnError: true) {
-        def script = checkScript(this, parameters) ?: this
-        def utils = parameters.juStabUtils ?: new Utils()
-        parameters.juStabUtils = null
-
-        new PiperGoUtils(this, utils).unstashPiperBin()
-        utils.unstash('pipelineConfigAndTests')
-        script.commonPipelineEnvironment.writeToDisk(script)
-
-        writeFile(file: ".pipeline/tmp/${METADATA_FILE}", text: libraryResource(METADATA_FILE))
-
-        withEnv([
-            "PIPER_parametersJSON=${getParametersJSON(parameters)}",
-        ]) {
-            // get context configuration
-            Map config = readJSON (text: sh(returnStdout: true, script: "./piper getConfig --contextConfig --stepMetadata '.pipeline/tmp/${METADATA_FILE}'"))
-            // execute step
-            dockerExecute(
-                script: script,
-                dockerImage: config.dockerImage,
-                dockerWorkspace: config.dockerWorkspace
-            ) {
-                withCredentials([usernamePassword(
-                    credentialsId: config.cfCredentialsId,
-                    passwordVariable: 'PIPER_password',
-                    usernameVariable: 'PIPER_username'
-                )]) {
-                    sh "./piper cloudFoundryDeleteService"
-                }
-            }
-        }
-    }
-}
-
-String getParametersJSON(Map parameters = [:]){
-    Map stepParameters = [:].plus(parameters)
-    // Remove script parameter etc.
-    stepParameters.remove('script')
-    stepParameters.remove('juStabUtils')
-    stepParameters.remove('jenkinsUtilsStub')
-    // When converting to JSON and back again, entries which had a 'null' value will now have a value
-    // of type 'net.sf.json.JSONNull', for which the Groovy Truth resolves to 'true' in for example if-conditions
-    stepParameters = MapUtils.pruneNulls(stepParameters)
-    return groovy.json.JsonOutput.toJson(stepParameters)
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [[type: 'usernamePassword', id: 'cfCredentialsId', env: ['PIPER_username', 'PIPER_password']]], false, false, true)
 }
--- vars/githubPublishRelease.groovy
+++ vars/githubPublishRelease.groovy
@@ -1,6 +1,3 @@
-import com.sap.piper.MapUtils
-import com.sap.piper.PiperGoUtils
-import com.sap.piper.Utils
 import groovy.transform.Field
 
 import static com.sap.piper.Prerequisites.checkScript
@@ -8,45 +5,6 @@ import static com.sap.piper.Prerequisites.checkScript
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/githubrelease.yaml'
 
-//Metadata maintained in file project://resources/metadata/githubrelease.yaml
-
 void call(Map parameters = [:]) {
-    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
-        def script = checkScript(this, parameters) ?: this
-        def utils = parameters.juStabUtils ?: new Utils()
-        parameters.juStabUtils = null
-
-        // telemetry reporting
-        utils.pushToSWA([step: STEP_NAME], config)
-
-        new PiperGoUtils(this, utils).unstashPiperBin()
-        utils.unstash('pipelineConfigAndTests')
-        script.commonPipelineEnvironment.writeToDisk(script)
-
-        writeFile(file: ".pipeline/tmp/${METADATA_FILE}", text: libraryResource(METADATA_FILE))
-
-        withEnv([
-            "PIPER_parametersJSON=${getParametersJSON(parameters)}",
-        ]) {
-            // get context configuration
-            Map config = readJSON (text: sh(returnStdout: true, script: "./piper getConfig --contextConfig --stepMetadata '.pipeline/tmp/${METADATA_FILE}'"))
-
-            // execute step
-            withCredentials([string(credentialsId: config.githubTokenCredentialsId, variable: 'PIPER_token')]) {
-                sh './piper githubPublishRelease'
-            }
-        }
-    }
-}
-
-String getParametersJSON(Map parameters = [:]){
-    Map stepParameters = [:].plus(parameters)
-    // Remove script parameter etc.
-    stepParameters.remove('script')
-    stepParameters.remove('juStabUtils')
-    stepParameters.remove('jenkinsUtilsStub')
-    // When converting to JSON and back again, entries which had a 'null' value will now have a value
-    // of type 'net.sf.json.JSONNull', for which the Groovy Truth resolves to 'true' in for example if-conditions
-    stepParameters = MapUtils.pruneNulls(stepParameters)
-    return groovy.json.JsonOutput.toJson(stepParameters)
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [[type: 'token', id: 'githubTokenCredentialsId', env: ['PIPER_token']]])
 }
