e549b9fe5ae491d8a247c195c90b1165bb8fdbc1
Christopher Fenner <Christopher.Fenner@sap.com>
2020-05-04 22:22:23 +0200

    mock findFiles

--- test/groovy/AbapEnvironmentPullGitRepoTest.groovy
+++ test/groovy/AbapEnvironmentPullGitRepoTest.groovy
@@ -53,12 +53,9 @@ public class AbapEnvironmentPullGitRepoTest extends BasePiperTest {
 
     @Test
     public void test() {
-        helper.registerAllowedMethod("fileExists", [String.class], { file -> 
-            return true 
-        })
-        helper.registerAllowedMethod("fileExists", [Map.class], { file -> 
-            return true
-        })
+        helper.registerAllowedMethod("fileExists", [String.class], { file -> return false })
+        helper.registerAllowedMethod("fileExists", [Map.class], { file -> return false})
+        helper.registerAllowedMethod('findFiles', [Map.class], {m -> return null})
         helper.registerAllowedMethod("withEnv", [List.class, Closure.class], {arguments, closure ->
             arguments.each {arg ->
                 withEnvArgs.add(arg.toString())
