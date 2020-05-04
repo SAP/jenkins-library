286e99846a60639679e70c21e8932ecbe74153cd
Daniel Mieg <daniel.mieg@sap.com>
2020-05-04 16:08:13 +0200

    Small changes

--- vars/abapPipeline.groovy
+++ vars/abapPipeline.groovy
@@ -8,7 +8,29 @@ void call(parameters) {
             timestamps()
         }
         stages {
+            stage("go binary"){
+                steps {
+                    sh '''
+                        rm -rf jenkins-library
+                        git clone https://github.com/DanielMieg/jenkins-library.git
+                    '''
 
+                    dockerExecute(
+                        script: this,
+                        dockerImage: 'golang',
+                        dockerEnvVars: [GOPATH: '/jenkinsdata/cloudFoundryDeleteService Test Pipeline/workspace']
+                    ) {
+                        sh '''
+                            cd jenkins-library
+                            go build -o piper .
+                            chmod +x piper
+                            cp piper ..
+                            cd ..
+                        '''
+                        stash name: 'piper-bin', includes: 'piper'
+                    }
+                }
+            }
             stage('Init') {
                 steps {
             //     library 'piper-lib-os-dev'
