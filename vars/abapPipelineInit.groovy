21efe20de2e7379ac329ebbb61b0f41dd6fece4d
Daniel Mieg <daniel.mieg@sap.com>
2020-05-04 14:51:25 +0200

    Small changes

--- vars/abapPipelineInit.groovy
+++ vars/abapPipelineInit.groovy
@@ -31,7 +31,7 @@ void call(Map parameters = [:]) {
 
         setupCommonPipelineEnvironment script: script, customDefaults: parameters.customDefaults
 
-        echo parameters.script.commonPipelineEnvironment.configuration
-        echo parameters.script.commonPipelineEnvironment.configurationFile
+        System.out.println(parameters.script.commonPipelineEnvironment.configuration)
+        System.out.println(parameters.script.commonPipelineEnvironment.configurationFile)
     }
 }
