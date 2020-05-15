7a16a5d5546be1079dd4420bf13d5076b79b7736
Daniel Mieg <daniel.mieg@sap.com>
2020-05-15 10:55:46 +0200

    Test

--- vars/abapEnvironmentPipelineInit.groovy
+++ vars/abapEnvironmentPipelineInit.groovy
@@ -13,4 +13,30 @@ import static com.sap.piper.Prerequisites.checkScript
 void call(Map parameters = [:]) {
     def script = checkScript(this, parameters) ?: this
     setupCommonPipelineEnvironment script: script
+
+    script.commonPipelineEnvironment.configuration.runStage = [:]
+    script.commonPipelineEnvironment.configuration.runStep = [:]
+
+    // load default & individual configuration
+    Map config = ConfigurationHelper.newInstance(this)
+        .loadStepDefaults()
+        .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+        .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+        .mixinStageConfig(script.commonPipelineEnvironment, stageName, STEP_CONFIG_KEYS)
+        .mixin(parameters, PARAMETER_KEYS)
+        //.withMandatoryProperty('stageConfigResource')
+        .addIfEmpty('stageConfigResource', 'com.sap.piper/pipeline/stageDefaults.yml')
+        .use()
+
+
+    config.stages = (readYaml(text: libraryResource(config.stageConfigResource))).stages
+
+    //handling of stage and step activation
+    config.stages.each {stage ->
+
+        //activate stage if stage configuration is available
+        if (ConfigurationLoader.stageConfiguration(script, stage.getKey())) {
+            script.commonPipelineEnvironment.configuration.runStage[stage.getKey()] = true
+        }
+    }
 }
