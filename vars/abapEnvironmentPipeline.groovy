4737f9ea734acd60fab634b6c69b1d1cf12285e8
Daniel Mieg <daniel.mieg@sap.com>
2020-05-15 11:20:05 +0200

    Test

--- vars/abapEnvironmentPipeline.groovy
+++ vars/abapEnvironmentPipeline.groovy
@@ -32,7 +32,7 @@ void call(parameters) {
             }
 
             stage('Prepare System') {
-                // when {expression {return parameters.script.commonPipelineEnvironment.configuration.runStage?.get(env.STAGE_NAME)}}
+                when {expression {return parameters.script.commonPipelineEnvironment.configuration.runStage?.get(env.STAGE_NAME)}}
                 steps {
                     cloudFoundryCreateService script: parameters.script
                     input message: "Steampunk system ready?"
