2508c1dac7c0b1b849a94fbae3ebce666a44f87a
Sven Merk <s.merk@sap.com>
2020-05-16 00:16:31 +0200

    Fix tests

--- pkg/versioning/pip_test.go
+++ pkg/versioning/pip_test.go
@@ -1,21 +1,3 @@
 package versioning
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestPipInit(t *testing.T) {
-	t.Run("default", func(t *testing.T) {
-		pip := Pip{}
-		pip.init()
-		assert.Equal(t, "VERSION", pip.path)
-	})
-
-	t.Run("no default", func(t *testing.T) {
-		pip := Pip{path: "my/VERSION"}
-		pip.init()
-		assert.Equal(t, "my/VERSION", pip.path)
-	})
-}
+import ()
