9b06e07a2c1ab4edcee4de0c91ae4070b4b39dec
Your Name <kevin.hudemann@sap.com>
2020-05-15 10:43:27 +0200

    Enable DownloadCache

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -1,8 +1,12 @@
 import groovy.transform.Field
+import static com.sap.piper.Prerequisites.checkScript
+import com.sap.piper.DownloadCacheUtils
 
 @Field String STEP_NAME = getClass().getName()
 
 void call(Map parameters = [:]) {
+    final script = checkScript(this, parameters) ?: this
+    parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(script, parameters)
 
     piperExecuteBin parameters, STEP_NAME, "metadata/${STEP_NAME}.yaml", []
 }
