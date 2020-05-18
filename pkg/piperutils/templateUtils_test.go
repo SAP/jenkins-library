44f9c1a55d8ff596d58251b0118aa128a9d573c8
Stephan Aßmus <stephan.assmus@sap.com>
2020-05-18 10:01:56 +0200

    Avoid import cycle

--- pkg/piperutils/templateUtils_test.go
+++ pkg/piperutils/templateUtils_test.go
@@ -1,22 +1,27 @@
 package piperutils
 
 import (
-	"github.com/SAP/jenkins-library/pkg/versioning"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"text/template"
 )
 
+type SomeDescriptor struct {
+	GroupID    string
+	ArtifactID string
+	Version    string
+}
+
 func TestExecuteTemplate(t *testing.T) {
 	t.Run("test success", func(t *testing.T) {
-		context := versioning.MavenDescriptor{GroupID: "com.sap.cp.jenkins", ArtifactID: "piper", Version: "1.2.3"}
+		context := SomeDescriptor{GroupID: "com.sap.cp.jenkins", ArtifactID: "piper", Version: "1.2.3"}
 		result, err := ExecuteTemplate("{{ .GroupID }}-{{ .ArtifactID }}:{{ .Version}}", context)
 		assert.NoError(t, err, "Didn't expect error but got one")
 		assert.Equal(t, "com.sap.cp.jenkins-piper:1.2.3", result, "Expected different result")
 	})
 
 	t.Run("test template error", func(t *testing.T) {
-		context := versioning.MavenDescriptor{GroupID: "com.sap.cp.jenkins", ArtifactID: "piper", Version: "1.2.3"}
+		context := SomeDescriptor{GroupID: "com.sap.cp.jenkins", ArtifactID: "piper", Version: "1.2.3"}
 		_, err := ExecuteTemplate("{{ $+++.+++GroupID }}-{{ .ArtifactID }}:{{ .Version}}", context)
 		assert.Error(t, err, "Expected error but got none")
 	})
@@ -25,7 +30,7 @@ func TestExecuteTemplate(t *testing.T) {
 		functions := template.FuncMap{
 			"testFunc": reverse,
 		}
-		context := versioning.MavenDescriptor{GroupID: "com.sap.cp.jenkins", ArtifactID: "piper", Version: "1.2.3"}
+		context := SomeDescriptor{GroupID: "com.sap.cp.jenkins", ArtifactID: "piper", Version: "1.2.3"}
 		result, err := ExecuteTemplateFunctions("{{ testFunc .GroupID }}-{{ .ArtifactID }}:{{ .Version}}", functions, context)
 		assert.NoError(t, err, "Didn't expect error but got one")
 		assert.Equal(t, "sniknej.pc.pas.moc-piper:1.2.3", result, "Expected different result")
