fa70a4e31bf3b17aec61ed5deb6b01657db83f44
Your Name <kevin.hudemann@sap.com>
2020-05-18 12:02:49 +0200

    Expose step metadata as field

--- vars/mtaBuild.groovy
+++ vars/mtaBuild.groovy
@@ -3,10 +3,11 @@ import static com.sap.piper.Prerequisites.checkScript
 import com.sap.piper.DownloadCacheUtils
 
 @Field String STEP_NAME = getClass().getName()
+@Field String METADATA_FILE = 'metadata/mtaBuild.yaml'
 
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
     parameters = DownloadCacheUtils.injectDownloadCacheInMavenParameters(script, parameters)
 
-    piperExecuteBin parameters, STEP_NAME, "metadata/${STEP_NAME}.yaml", []
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
 }
