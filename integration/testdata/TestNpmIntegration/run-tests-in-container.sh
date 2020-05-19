a5060fb0077f32dc7562c18914347b08e4599352
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-19 11:44:59 +0200

    update

--- integration/testdata/TestNpmIntegration/run-tests-in-container.sh
+++ integration/testdata/TestNpmIntegration/run-tests-in-container.sh
@@ -29,5 +29,8 @@ rm test-log.txt
 popd
 
 pushd registryWithTwoModules
-/test/piper npmExecuteScripts --install --runScripts=ci-build,ci-backend-unit-test --sapNpmRegistry=https://foo.bar
+/test/piper npmExecuteScripts --install --runScripts=ci-build,ci-backend-unit-test --sapNpmRegistry=https://foo.bar  >test-log.txt 2>&1
+grep --quiet "^info  npmExecuteScripts - https://example.com" test-log.txt
+grep --quiet "^info  npmExecuteScripts - https://foo.bar" test-log.txt
+rm test-log.txt
 popd
