45c8e7bedddc0586df626b7614c5750c830f8fb5
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-19 09:27:38 +0200

    fixtests

--- integration/testdata/TestNpmIntegration/run-tests.sh
+++ integration/testdata/TestNpmIntegration/run-tests.sh
@@ -2,4 +2,4 @@
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o piper ../../../
 docker build -t npm-integration-test .
-docker run -v $PWD:/test npm-integration-test /run-tests-in-container.sh
+docker run -v "$PWD":/test npm-integration-test /run-tests-in-container.sh
