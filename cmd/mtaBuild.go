895860e93c67b3f97641d805a0fc4c653c909ab2
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-19 08:23:55 +0200

    remove

--- cmd/mtaBuild.go
+++ cmd/mtaBuild.go
@@ -307,26 +307,6 @@ func createMtaYamlFile(mtaYamlFile, applicationName string, p piperutils.FileUti
 	return nil
 }
 
-func configureNpmRegistry(registryURI string, registryName string, scope string, e execRunner) error {
-	if len(registryURI) == 0 {
-		log.Entry().Debugf("No %s npm registry provided via configuration. Leaving npm config untouched.", registryName)
-		return nil
-	}
-
-	log.Entry().Debugf("Setting %s npm registry to \"%s\"", registryName, registryURI)
-
-	key := "registry"
-	if len(scope) > 0 {
-		key = fmt.Sprintf("%s:registry", scope)
-	}
-
-	if err := e.RunExecutable("npm", "config", "set", key, registryURI); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func handleSettingsFiles(config mtaBuildOptions,
 	p piperutils.FileUtils,
 	httpClient piperhttp.Downloader) error {
