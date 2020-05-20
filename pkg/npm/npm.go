398a4777404bf72782bdd197d5075a415aafb4f0
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-05-20 12:46:33 +0200

    default url

--- pkg/npm/npm.go
+++ pkg/npm/npm.go
@@ -46,7 +46,7 @@ func SetNpmRegistries(options *RegistryOptions, execRunner execRunner) error {
 			}
 		}
 
-		if registry == sapRegistry && registryRequiresConfiguration(preConfiguredRegistry, "https://npm.sap.com") {
+		if registry == sapRegistry && options.SapNpmRegistry != "" && registryRequiresConfiguration(preConfiguredRegistry, "https://npm.sap.com") {
 			log.Entry().Info("npm registry " + registry + " was not configured, setting it to " + options.SapNpmRegistry)
 			err = execRunner.RunExecutable("npm", "config", "set", registry, options.SapNpmRegistry)
 			if err != nil {
