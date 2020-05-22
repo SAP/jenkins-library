ee3e740ba2c74618a3a5bc1166a772e5a13f2cc8
Stephan Aßmus <stephan.assmus@sap.com>
2020-05-22 11:37:48 +0200

    Remove check for Owner
    Turns out in the pipeline run it wasn't retrieved from resources as expected.

--- cmd/fortifyExecuteScan.go
+++ cmd/fortifyExecuteScan.go
@@ -88,9 +88,6 @@ func runFortifyScan(config fortifyExecuteScanOptions, sys fortify.System, comman
 		}
 	}
 
-	if config.Owner == "" {
-		return fmt.Errorf("GitHub organization was not specified via parameter 'owner' and it could not be retried from resources")
-	}
 	if config.Repository == "" {
 		return fmt.Errorf("GitHub repository was not specified via parameter 'repository' and it could not be retried from resources")
 	}
