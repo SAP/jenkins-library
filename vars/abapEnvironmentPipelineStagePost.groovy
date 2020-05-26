e5d5d266158f2b72cc83851f8924ac09bc2adee6
Daniel Mieg <daniel.mieg@sap.com>
2020-05-26 14:35:04 +0200

    Testing Stage Wrapper

--- vars/abapEnvironmentPipelineStagePost.groovy
+++ vars/abapEnvironmentPipelineStagePost.groovy
@@ -4,7 +4,10 @@ import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
 @Field Set GENERAL_CONFIG_KEYS = []
-@Field STAGE_STEP_KEYS = []
+@Field STAGE_STEP_KEYS = [
+    /** Deletes a SAP Cloud Platform ABAP Environment instance via the cloud foundry command line interface */
+    'cloudFoundryDeleteService'
+]
 @Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS.plus(STAGE_STEP_KEYS)
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
 /**
