2fe11117913e053d7b62329340f880db6cc77c7c
Stephan Aßmus <stephan.assmus@sap.com>
2020-05-27 18:32:00 +0200

    Refactor to allow individual config per tool

--- pkg/mock/dockerRunner.go
+++ pkg/mock/dockerRunner.go
@@ -4,7 +4,6 @@ package mock
 
 import (
 	"fmt"
-	"github.com/SAP/jenkins-library/pkg/piperutils"
 	"io"
 	"os"
 )
@@ -17,46 +16,68 @@ type baseRunner interface {
 	RunExecutable(e string, p ...string) error
 }
 
+// DockerExecConfig is the configuration for an individual tool that shall be executed on docker.
+type DockerExecConfig struct {
+	// Image is the fully qualified docker image name that is passed to docker for this tool.
+	Image string
+	// Workspace is the (optional) directory to which the current working directory is mapped
+	// within the docker container.
+	Workspace string
+}
+
+// DockerExecRunner can be used "in place" of another ExecRunner in order to transparently
+// execute commands within a docker container. One use-case is to test locally with tools
+// that are not available on the current platform.
 type DockerExecRunner struct {
-	runner            baseRunner
-	ExecutablesToWrap []string
-	DockerImage       string
-	DockerWorkspace   string
+	// Runner is the ExecRunner to which all executions are forwarded in the end.
+	Runner            baseRunner
+	executablesToWrap map[string]DockerExecConfig
 }
 
 func (d *DockerExecRunner) SetDir(dir string) {
-	d.runner.SetDir(dir)
+	d.Runner.SetDir(dir)
 }
 
 func (d *DockerExecRunner) SetEnv(env []string) {
-	d.runner.SetEnv(env)
+	d.Runner.SetEnv(env)
 }
 
 func (d *DockerExecRunner) Stdout(out io.Writer) {
-	d.runner.Stdout(out)
+	d.Runner.Stdout(out)
 }
 
 func (d *DockerExecRunner) Stderr(err io.Writer) {
-	d.runner.Stderr(err)
+	d.Runner.Stderr(err)
+}
+
+func (d *DockerExecRunner) AddExecConfig(executable string, config DockerExecConfig) error {
+	if executable == "" {
+		return fmt.Errorf("'executable' needs to be provided")
+	}
+	if config.Image == "" {
+		return fmt.Errorf("the DockerExecConfig must specify a docker image")
+	}
+	if d.executablesToWrap == nil {
+		d.executablesToWrap = map[string]DockerExecConfig{}
+	}
+	d.executablesToWrap[executable] = config
+	return nil
 }
 
 func (d *DockerExecRunner) RunExecutable(executable string, parameters ...string) error {
-	if piperutils.ContainsString(d.ExecutablesToWrap, executable) {
-		if d.DockerImage == "" {
-			return fmt.Errorf("no docker image specified")
-		}
+	if config, ok := d.executablesToWrap[executable]; ok {
 		wrappedParameters := []string{"run", "--entrypoint=" + executable}
-		if d.DockerWorkspace != "" {
+		if config.Workspace != "" {
 			currentDir, err := os.Getwd()
 			if err != nil {
 				return fmt.Errorf("failed to get current directory for mounting in docker: %w", err)
 			}
-			wrappedParameters = append(wrappedParameters, "-v", currentDir+":"+d.DockerWorkspace)
+			wrappedParameters = append(wrappedParameters, "-v", currentDir+":"+config.Workspace)
 		}
-		wrappedParameters = append(wrappedParameters, d.DockerImage)
+		wrappedParameters = append(wrappedParameters, config.Image)
 		wrappedParameters = append(wrappedParameters, parameters...)
 		executable = "docker"
 		parameters = wrappedParameters
 	}
-	return d.runner.RunExecutable(executable, parameters...)
+	return d.Runner.RunExecutable(executable, parameters...)
 }
--- pkg/mock/dockerRunner_test.go
+++ pkg/mock/dockerRunner_test.go
@@ -8,15 +8,38 @@ import (
 	"testing"
 )
 
+func TestDockerExecRunnerAddExecConfig(t *testing.T) {
+	t.Run("no executable provided results in error", func(t *testing.T) {
+		dockerRunner := DockerExecRunner{}
+		err := dockerRunner.AddExecConfig("", DockerExecConfig{})
+		assert.Error(t, err, "'executable' needs to be provided")
+	})
+	t.Run("no image provided results in error", func(t *testing.T) {
+		dockerRunner := DockerExecRunner{}
+		err := dockerRunner.AddExecConfig("useful-tool", DockerExecConfig{})
+		assert.Error(t, err, "the DockerExecConfig must specify a docker image")
+	})
+	t.Run("success case", func(t *testing.T) {
+		dockerRunner := DockerExecRunner{}
+		config := DockerExecConfig{Image: "image", Workspace: "/var/home"}
+		err := dockerRunner.AddExecConfig("useful-tool", config)
+		assert.NoError(t, err)
+		if assert.NotNil(t, dockerRunner.executablesToWrap) {
+			assert.Len(t, dockerRunner.executablesToWrap, 1)
+			assert.Equal(t, config, dockerRunner.executablesToWrap["useful-tool"])
+		}
+	})
+}
+
 func TestDockerExecRunnerRunExecutable(t *testing.T) {
-	dockerRunner := DockerExecRunner{
-		DockerWorkspace:   "some/path",
-		DockerImage:       "image",
-		ExecutablesToWrap: []string{"useful-tool"},
-	}
+	dockerRunner := DockerExecRunner{}
+	_ = dockerRunner.AddExecConfig("useful-tool", DockerExecConfig{
+		Image:     "image",
+		Workspace: "some/path",
+	})
 	t.Run("tool execution is wrapped", func(t *testing.T) {
 		mockRunner := ExecMockRunner{}
-		dockerRunner.runner = &mockRunner
+		dockerRunner.Runner = &mockRunner
 		currentDir, err := os.Getwd()
 		assert.NoError(t, err)
 		err = dockerRunner.RunExecutable("useful-tool", "param", "--flag")
@@ -30,7 +53,7 @@ func TestDockerExecRunnerRunExecutable(t *testing.T) {
 	})
 	t.Run("tool execution is not wrapped", func(t *testing.T) {
 		mockRunner := ExecMockRunner{}
-		dockerRunner.runner = &mockRunner
+		dockerRunner.Runner = &mockRunner
 		err := dockerRunner.RunExecutable("another-tool", "param", "--flag")
 		assert.NoError(t, err)
 		if assert.Equal(t, 1, len(mockRunner.Calls)) {
@@ -44,7 +67,7 @@ func TestDockerExecRunnerRunExecutable(t *testing.T) {
 		mockRunner := ExecMockRunner{}
 		mockRunner.ShouldFailOnCommand = map[string]error{}
 		mockRunner.ShouldFailOnCommand["some-tool"] = errors.New("failed")
-		dockerRunner.runner = &mockRunner
+		dockerRunner.Runner = &mockRunner
 		err := dockerRunner.RunExecutable("some-tool")
 		assert.Error(t, err, "failed")
 	})
