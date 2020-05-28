6279f56e335e2aa4c1ea836aa2f9987cbd5a8dd7
Marcus Holl <marcus.holl@sap.com>
2020-05-28 08:29:59 +0200

    Introduce manifest utils for cf

---
+++ pkg/cloudfoundry/ManifestUtils.go
@@ -0,0 +1,226 @@
+package cloudfoundry
+
+import (
+	"fmt"
+	"github.com/ghodss/yaml"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"reflect"
+
+	"github.com/SAP/jenkins-library/pkg/log"
+)
+
+const propApplications = "applications"
+const propBuildpacks = "buildpacks"
+const propBuildpack = "buildpack"
+
+// Manifest ...
+type Manifest struct {
+	self     map[string]interface{}
+	modified bool
+	name     string
+}
+
+var m Manifest
+
+var _readFile = ioutil.ReadFile
+
+// ReadManifest ...
+func ReadManifest(name string) (Manifest, error) {
+
+	log.Entry().Infof("Reading manifest file  '%s'", name)
+
+	m := Manifest{self: make(map[string]interface{}), name: name, modified: false}
+
+	content, err := _readFile(name)
+	if err != nil {
+		return m, errors.Wrapf(err, "cannot read file '%v'", m.name)
+	}
+
+	err = yaml.Unmarshal(content, &m.self)
+
+	if err != nil {
+		return m, errors.Wrapf(err, "Cannot parse yaml file '%s': %s", m.name, string(content))
+	}
+
+	log.Entry().Infof("Manifest file '%s' has been parsed", m.name)
+
+	return m, nil
+}
+
+// WriteManifest ...
+func (m *Manifest) WriteManifest() error {
+
+	d, err := yaml.Marshal(&m.self)
+	if err != nil {
+		return err
+	}
+
+	log.Entry().Debugf("Writing manifest file '%s'", m.name)
+	err = ioutil.WriteFile(m.name, d, 0644)
+
+	if err == nil {
+		m.modified = false
+	}
+
+	log.Entry().Debugf("Manifest file '%s' has been written", m.name)
+	return err
+}
+
+// GetName ...
+func (m Manifest) GetName() string {
+	return m.name
+}
+
+// GetApplications ...
+func (m Manifest) GetApplications() ([]interface{}, error) {
+	return toSlice(m.self)
+}
+
+// ApplicationHasProperty Checks if the application denoted by 'index' has the property 'name'
+func (m Manifest) ApplicationHasProperty(index int, name string) (bool, error) {
+
+	sliced, err := toSlice(m.self[propApplications])
+
+	if err != nil {
+		return false, err
+	}
+
+	if index >= len(sliced) {
+		return false, fmt.Errorf("Index (%d) out of bound. Number of apps: %d", index, len(sliced))
+	}
+
+	_m, err := toMap(sliced[index])
+
+	if err != nil {
+		return false, err
+	}
+
+	_, ok := _m[name]
+
+	return ok, nil
+}
+
+// GetApplicationProperty ...
+func (m Manifest) GetApplicationProperty(index int, name string) (interface{}, error) {
+
+	sliced, err := toSlice(m.self[propApplications])
+
+	if err != nil {
+		return "", err
+	}
+
+	if index >= len(sliced) {
+		return "", fmt.Errorf("Index (%d) out of bound. Number of apps: %d", index, len(sliced))
+	}
+
+	app, err := toMap(sliced[index])
+
+	if err != nil {
+		return "", err
+	}
+
+	if app[name] != nil {
+		return app[name], nil
+	}
+
+	return "", fmt.Errorf("No such property: '%s' available in application at position %d", name, index)
+}
+
+// GetAppName Gets the name of the app at 'index'
+func (m Manifest) GetAppName(index int) (string, error) {
+
+	appName, err := m.GetApplicationProperty(index, "name")
+
+	if err != nil {
+		return "", err
+	}
+
+	if n, ok := appName.(string); ok {
+		return n, nil
+	} else {
+		return "", fmt.Errorf("Cannot cast value for property 'name' to string")
+	}
+
+	return "", fmt.Errorf("Cannot retrieve application name for app at index %d", index)
+}
+
+// Transform ...
+func (m *Manifest) Transform() error {
+
+	sliced, err := toSlice(m.self[propApplications])
+	if err != nil {
+		return err
+	}
+
+	for _, app := range sliced {
+		appAsMap, err := toMap(app)
+		if err != nil {
+			return err
+		}
+
+		err = transformApp(appAsMap, m)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func transformApp(app map[string]interface{}, m *Manifest) error {
+
+	appName := "n/a"
+
+	if n, ok := app["name"].(string); ok {
+		if len(n) > 0 {
+			appName = n
+		}
+	}
+
+	if app[propBuildpacks] == nil {
+		// Revisit: not sure if a build pack is mandatory.
+		// In that case we should check that app.buildpack
+		// is present.
+		return nil
+	}
+
+	buildPacks, err := toSlice(app[propBuildpacks])
+
+	if err != nil {
+		return err
+	}
+
+	if len(buildPacks) > 1 {
+		return fmt.Errorf("More than one Cloud Foundry Buildpack is not supported. Please check manifest file '%s', application '%s'", m.name, appName)
+	}
+
+	if len(buildPacks) == 1 {
+		app[propBuildpack] = buildPacks[0]
+		delete(app, propBuildpacks)
+		m.modified = true
+	}
+
+	return nil
+}
+
+// HasModified ...
+func (m Manifest) HasModified() bool {
+	return m.modified
+}
+func toMap(i interface{}) (map[string]interface{}, error) {
+
+	if m, ok := i.(map[string]interface{}); ok {
+		return m, nil
+	}
+	return nil, fmt.Errorf("Failed to convert %v to map. Was %v", i, reflect.TypeOf(i))
+}
+
+func toSlice(i interface{}) ([]interface{}, error) {
+
+	if s, ok := i.([]interface{}); ok {
+		return s, nil
+	}
+	return nil, fmt.Errorf("Failed to convert %v to slice. Was %v", i, reflect.TypeOf(i))
+}
---
+++ pkg/cloudfoundry/ManifestUtils_test.go
@@ -0,0 +1,111 @@
+package cloudfoundry
+
+import (
+	"testing"
+
+	"fmt"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadManifest(t *testing.T) {
+
+	_readFile = func(filename string) ([]byte, error) {
+		if filename == "myManifest.yaml" {
+			return []byte("applications: [{name: 'manifestAppName'}]"), nil
+		}
+		return []byte{}, fmt.Errorf("File '%s' not found", filename)
+	}
+
+	manifest, err := ReadManifest("myManifest.yaml")
+
+	appName, err := manifest.GetAppName(0)
+	if assert.NoError(t, err) {
+		assert.Equal(t, "manifestAppName", appName)
+	}
+}
+
+func TestNoRoute(t *testing.T) {
+
+	_readFile = func(filename string) ([]byte, error) {
+		if filename == "myManifest.yaml" {
+			return []byte("applications: [{name: 'manifestAppName', no-route: true}]"), nil
+		}
+		return []byte{}, fmt.Errorf("File '%s' not foound", filename)
+	}
+
+	manifest, err := ReadManifest("myManifest.yaml")
+	if !assert.NoError(t, err) {
+		assert.FailNow(t, "Cannot read manifest file")
+	}
+
+	noRoute, err := manifest.GetApplicationProperty(0, "no-route")
+	if assert.NoError(t, err) {
+		noRouteAsBool, ok := noRoute.(bool)
+
+		if assert.True(t, ok) && assert.NoError(t, err) {
+			assert.True(t, noRouteAsBool)
+		}
+	}
+}
+
+func TestTransformGoodCase(t *testing.T) {
+
+	_readFile = func(filename string) ([]byte, error) {
+		if filename == "myManifest.yaml" {
+			return []byte("applications: [{name: 'manifestAppName', no-route: true, buildpacks: [sap_java_buildpack]}]"), nil
+		}
+		return []byte{}, fmt.Errorf("File '%s' not foound", filename)
+	}
+
+	manifest, err := ReadManifest("myManifest.yaml")
+	assert.NoError(t, err)
+
+	err = manifest.Transform()
+
+	assert.NoError(t, err)
+	buildpack, err := manifest.GetApplicationProperty(0, "buildpack")
+	assert.NoError(t, err)
+	buildpacks, err := manifest.GetApplicationProperty(0, "buildpacks")
+
+	assert.Equal(t, "sap_java_buildpack", buildpack)
+	assert.Equal(t, "", buildpacks)
+	assert.True(t, manifest.HasModified())
+
+}
+
+func TestTransformMultipleBuildPacks(t *testing.T) {
+	_readFile = func(filename string) ([]byte, error) {
+		if filename == "myManifest.yaml" {
+			return []byte("no-route: true\napplications: [{name: 'manifestAppName', buildpacks: [sap_java_buildpack, 'another_buildpack']}]"), nil
+		}
+		return []byte{}, fmt.Errorf("File '%s' not foound", filename)
+	}
+
+	manifest, err := ReadManifest("myManifest.yaml")
+	assert.NoError(t, err)
+
+	err = manifest.Transform()
+
+	assert.EqualError(t, err, "More than one Cloud Foundry Buildpack is not supported. Please check manifest file 'myManifest.yaml', application 'manifestAppName'")
+}
+
+func TestTransformUnchanged(t *testing.T) {
+	_readFile = func(filename string) ([]byte, error) {
+		if filename == "myManifest.yaml" {
+			return []byte("applications: [{name: 'manifestAppName', no-route: true, buildpack: sap_java_buildpack}]"), nil
+		}
+		return []byte{}, fmt.Errorf("File '%s' not foound", filename)
+	}
+
+	manifest, err := ReadManifest("myManifest.yaml")
+	assert.NoError(t, err)
+
+	err = manifest.Transform()
+
+	buildpack, err := manifest.GetApplicationProperty(0, "buildpack")
+	assert.NoError(t, err)
+	_, err = manifest.GetApplicationProperty(0, "buildpacks")
+	assert.Equal(t, "sap_java_buildpack", buildpack)
+	assert.EqualError(t, err, "No such property: 'buildpacks' available in application at position 0")
+	assert.False(t, manifest.HasModified())
+}
