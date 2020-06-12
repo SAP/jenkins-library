585a7479b788254f192b8bc7978ddb66241c8b8c
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-06-12 12:15:07 +0200

    make

--- integration/integration_mta_build_test.go
+++ integration/integration_mta_build_test.go
@@ -35,6 +35,7 @@ func TestMavenProject(t *testing.T) {
 	//workaround to use test script util it is possible to set workdir for Exec call
 	testScript := `#!/bin/sh
 cd /test
+apt-get -yqq update; apt-get -yqq install make
 curl -OL https://github.com/SAP/cloud-mta-build-tool/releases/download/v1.0.14/cloud-mta-build-tool_1.0.14_Linux_amd64.tar.gz
 tar xzf cloud-mta-build-tool_1.0.14_Linux_amd64.tar.gz
 mv mbt /usr/bin
