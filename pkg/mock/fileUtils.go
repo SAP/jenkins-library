72edd7b6745f3373f24ae7f4afe383a3cc3b17d7
Stephan Aßmus <stephan.assmus@sap.com>
2020-06-12 17:21:43 +0200

    Flesh out piperutils.Files and mock.FilesMock functionality

--- pkg/mock/fileUtils.go
+++ pkg/mock/fileUtils.go
@@ -3,44 +3,154 @@
 package mock
 
 import (
-	"errors"
+	"fmt"
+	"github.com/bmatcuk/doublestar"
 	"os"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
-// FilesMock ...
+var dirContent []byte
+
+// FilesMock implements the functions from piperutils.Files with an in-memory file system.
 type FilesMock struct {
-	Files []string
+	Files        map[string]*[]byte
+	RemovedFiles map[string]*[]byte
 }
 
-// FileExists ...
-func (f FilesMock) FileExists(filename string) (bool, error) {
-
-	for _, file := range f.Files {
-		if file == filename {
-			return true, nil
-		}
-
+func (f *FilesMock) init() {
+	if f.Files == nil {
+		f.Files = map[string]*[]byte{}
 	}
+	if f.RemovedFiles == nil {
+		f.RemovedFiles = map[string]*[]byte{}
+	}
+}
 
+// AddFile establishes the existence of a virtual file.
+func (f *FilesMock) AddFile(path string, contents []byte) {
+	f.init()
+	f.Files[path] = &contents
+}
+
+// AddDir establishes the existence of a virtual directory.
+func (f *FilesMock) AddDir(path string) {
+	f.init()
+	f.Files[path] = &dirContent
+}
+
+// FileExists returns true if file content has been associated with the given path, false otherwise.
+func (f *FilesMock) FileExists(path string) (bool, error) {
+	if f.Files == nil {
+		return false, nil
+	}
+	content, exists := f.Files[path]
+	if !exists {
+		return false, fmt.Errorf("'%s': %w", path, os.ErrNotExist)
+	}
+	return content != &dirContent, nil
+}
+
+// DirExists returns true, if the given path is a previously added directory, or a parent directory for any of the
+// previously added files.
+func (f *FilesMock) DirExists(path string) (bool, error) {
+	for entry, content := range f.Files {
+		var dirComponents []string
+		if content == &dirContent {
+			dirComponents = strings.Split(entry, string(os.PathSeparator))
+		} else {
+			dirComponents = strings.Split(filepath.Dir(entry), string(os.PathSeparator))
+		}
+		if len(dirComponents) > 0 {
+			dir := ""
+			for i, component := range dirComponents {
+				if i == 0 {
+					dir = component
+				} else {
+					dir = dir + string(os.PathSeparator) + component
+				}
+				if dir == path {
+					return true, nil
+				}
+			}
+		}
+	}
 	return false, nil
 }
 
-// Copy ...
-func (f FilesMock) Copy(src, dst string) (int64, error) {
-	return 0, errors.New("Not implemented")
+// Copy checks if content has been associated with the given src path, and if so copies it under the given path dst.
+func (f *FilesMock) Copy(src, dst string) (int64, error) {
+	f.init()
+	content, exists := f.Files[src]
+	if !exists || content == &dirContent {
+		return 0, fmt.Errorf("cannot copy '%s': %w", src, os.ErrNotExist)
+	}
+	f.AddFile(dst, *content)
+	return int64(len(*content)), nil
 }
 
-// FileRead ...
-func (f FilesMock) FileRead(path string) ([]byte, error) {
-	return nil, errors.New("Not implemented")
+// FileRead returns the content previously associated with the given path via AddFile(), or an error if no
+// content has been associated.
+func (f *FilesMock) FileRead(path string) ([]byte, error) {
+	f.init()
+	content, exists := f.Files[path]
+	if !exists {
+		return nil, fmt.Errorf("could not read '%s'", path)
+	}
+	// check if trying to open a directory for reading
+	if content == &dirContent {
+		return nil, fmt.Errorf("could not read '%s': %w", path, os.ErrInvalid)
+	}
+	return *content, nil
 }
 
-// FileWrite ...
-func (f FilesMock) FileWrite(path string, content []byte, perm os.FileMode) error {
-	return errors.New("Not implemented")
+// FileWrite just forwards to AddFile(), i.e. the content is associated with the given path.
+func (f *FilesMock) FileWrite(path string, content []byte, _ os.FileMode) error {
+	// NOTE: FilesMock could be extended to have a set of paths for which FileWrite should fail.
+	// This is why AddFile() exists separately, to differentiate the notion of setting up the mocking
+	// versus implementing the methods from Files.
+	f.AddFile(path, content)
+	return nil
 }
 
-// MkdirAll ...
-func (f FilesMock) MkdirAll(path string, perm os.FileMode) error {
-	return errors.New("Not implemented")
+// FileRemove deletes the association of the given path with any content and records the removal of the file.
+// If the path has not been registered before, it returns an error.
+func (f *FilesMock) FileRemove(path string) error {
+	if f.Files == nil {
+		return fmt.Errorf("the file '%s' does not exist: %w", path, os.ErrNotExist)
+	}
+	content, exists := f.Files[path]
+	if !exists {
+		return fmt.Errorf("the file '%s' does not exist: %w", path, os.ErrNotExist)
+	}
+	delete(f.Files, path)
+	f.RemovedFiles[path] = content
+	return nil
+}
+
+// MkdirAll creates a directory in the in-memory file system, so that this path is established to exist.
+func (f *FilesMock) MkdirAll(path string, _ os.FileMode) error {
+	// NOTE: FilesMock could be extended to have a set of paths for which MkdirAll should fail.
+	// This is why AddDir() exists separately, to differentiate the notion of setting up the mocking
+	// versus implementing the methods from Files.
+	f.AddDir(path)
+	return nil
+}
+
+// Glob returns an array of path strings which match the given glob-pattern. Double star matching is supported.
+func (f *FilesMock) Glob(pattern string) ([]string, error) {
+	var matches []string
+	if f.Files == nil {
+		return matches, nil
+	}
+	for path := range f.Files {
+		matched, _ := doublestar.Match(pattern, path)
+		if matched {
+			matches = append(matches, path)
+		}
+	}
+	// The order in f.Files is not deterministic, this would result in flaky tests.
+	sort.Strings(matches)
+	return matches, nil
 }
---
+++ pkg/mock/fileUtils_test.go
@@ -0,0 +1,119 @@
+package mock
+
+import (
+	"github.com/stretchr/testify/assert"
+	"path/filepath"
+	"testing"
+)
+
+func TestFilesMockFileExists(t *testing.T) {
+	t.Parallel()
+	t.Run("no init", func(t *testing.T) {
+		files := FilesMock{}
+		path := filepath.Join("some", "path")
+		exists, err := files.FileExists(path)
+		assert.NoError(t, err)
+		assert.False(t, exists)
+	})
+	t.Run("file exists after AddFile()", func(t *testing.T) {
+		files := FilesMock{}
+		path := filepath.Join("some", "path")
+		files.AddFile(path, []byte("dummy content"))
+		exists, err := files.FileExists(path)
+		assert.NoError(t, err)
+		assert.True(t, exists)
+	})
+	t.Run("path is a directory after AddDir()", func(t *testing.T) {
+		files := FilesMock{}
+		path := filepath.Join("some", "path")
+		files.AddDir(path)
+		exists, err := files.FileExists(path)
+		assert.NoError(t, err)
+		assert.False(t, exists)
+	})
+}
+
+func TestFilesMockDirExists(t *testing.T) {
+	t.Parallel()
+	t.Run("no init", func(t *testing.T) {
+		files := FilesMock{}
+		path := filepath.Join("some", "path")
+		exists, err := files.DirExists(path)
+		assert.NoError(t, err)
+		assert.False(t, exists)
+	})
+	t.Run("dir exists after AddDir()", func(t *testing.T) {
+		files := FilesMock{}
+		path := filepath.Join("some", "path")
+		files.AddDir(path)
+		exists, err := files.DirExists(path)
+		assert.NoError(t, err)
+		assert.True(t, exists)
+	})
+
+	t.Run("parent dirs exists after AddFile()", func(t *testing.T) {
+		files := FilesMock{}
+		path := filepath.Join("path", "to", "some", "file")
+		files.AddFile(path, []byte("dummy content"))
+		testDirs := []string{
+			"path",
+			filepath.Join("path", "to"),
+			filepath.Join("path", "to", "some"),
+		}
+		for _, dir := range testDirs {
+			exists, err := files.DirExists(dir)
+			assert.NoError(t, err)
+			assert.True(t, exists, "Should exist: '%s'", dir)
+		}
+	})
+	t.Run("invalid sub-folders do not exist after AddFile()", func(t *testing.T) {
+		files := FilesMock{}
+		path := filepath.Join("path", "to", "some", "file")
+		files.AddFile(path, []byte("dummy content"))
+		testDirs := []string{
+			"to",
+			filepath.Join("to", "some"),
+			"some",
+			filepath.Join("path", "to", "so"),
+		}
+		for _, dir := range testDirs {
+			exists, err := files.DirExists(dir)
+			assert.NoError(t, err)
+			assert.False(t, exists, "Should not exist: '%s'", dir)
+		}
+	})
+}
+
+func TestFilesMockCopy(t *testing.T) {
+	t.Parallel()
+	t.Run("copy a previously added file successfully", func(t *testing.T) {
+		files := FilesMock{}
+		src := filepath.Join("some", "file")
+		content := []byte("dummy content")
+		files.AddFile(src, content)
+		dst := filepath.Join("another", "file")
+		length, err := files.Copy(src, dst)
+		assert.NoError(t, err)
+		assert.Equal(t, length, int64(len(content)))
+	})
+	t.Run("fail to copy non-existing file", func(t *testing.T) {
+		files := FilesMock{}
+		src := filepath.Join("some", "file")
+		dst := filepath.Join("another", "file")
+		length, err := files.Copy(src, dst)
+		assert.EqualError(t, err, "cannot copy '"+src+"': file does not exist")
+		assert.Equal(t, length, int64(0))
+	})
+	t.Run("fail to copy folder", func(t *testing.T) {
+		files := FilesMock{}
+		src := filepath.Join("some", "file")
+		files.AddDir(src)
+		dst := filepath.Join("another", "file")
+		length, err := files.Copy(src, dst)
+		assert.EqualError(t, err, "cannot copy '"+src+"': file does not exist")
+		assert.Equal(t, length, int64(0))
+	})
+}
+
+func TestFilesMockMkdirAll(t *testing.T) {
+}
