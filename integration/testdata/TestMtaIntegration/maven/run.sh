e2965c97392388862b90fa60ccdfb3029d68e04a
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-06-12 11:49:14 +0200

    test

--- integration/testdata/TestMtaIntegration/maven/run.sh
+++ integration/testdata/TestMtaIntegration/maven/run.sh
@@ -4,6 +4,6 @@ pushd ../../../..
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags release -o piper
 popd || exit 1
 docker build -t mta-it .
-docker run --rm -v "$PWD":/project \
+docker run --rm -v "$PWD":/project -u root \
     --mount type=bind,source="$(pwd)"/../../../../piper,target=/piper mta-it \
     /bin/bash -c "/test.sh"
