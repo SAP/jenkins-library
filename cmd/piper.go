d8444d51f206020ce083f74db1c8e2375acdc3ef
Oliver Nocon <33484802+OliverNocon@users.noreply.github.com>
2020-06-22 08:21:38 +0200

    Add possibility to add category to failures (#1692)
    * Add possibility to add category to failures
    
    It is now possible to set the error category within the flow.
    When exiting the program the error category can be used.
    
    There is  a convenience function available for exiting with a previously set category,
    for example
    
    ```
    log.SetErrorCategory(log.ErrorCompliance)
    ...
    log.FatalError(err, "configuration error")
    ```
    
    * extend test
    
    * go mod tidy
    
    * add missing comment
    
    * update information about error categories
    
    * Update DEVELOPMENT.md

--- cmd/piper.go
+++ cmd/piper.go
@@ -94,7 +94,8 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		// in case we end up here we know that something in the PreRunE function went wrong
 		// and thus this indicates a configuration issue
-		log.Entry().WithError(err).WithField("category", "config").Fatal("configuration error")
+		log.SetErrorCategory(log.ErrorConfiguration)
+		log.FatalError(err, "configuration error")
 	}
 }
 
