dc99676c8df76caefe3fa6e56f81ca4c8ae17693
Stephan Aßmus <stephan.assmus@sap.com>
2020-06-24 17:04:58 +0200

    Init container map via YAML resource (#1695)

--- vars/piperExecuteBin.groovy
+++ vars/piperExecuteBin.groovy
@@ -11,7 +11,7 @@ import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
 
-void call(Map parameters = [:], stepName, metadataFile, List credentialInfo, failOnMissingReports = false, failOnMissingLinks = false, failOnError = false) {
+void call(Map parameters = [:], String stepName, String metadataFile, List credentialInfo, boolean failOnMissingReports = false, boolean failOnMissingLinks = false, boolean failOnError = false) {
 
     handlePipelineStepErrorsParameters = [stepName: stepName, stepParameters: parameters]
     if (failOnError) {
@@ -20,32 +20,13 @@ void call(Map parameters = [:], stepName, metadataFile, List credentialInfo, fai
 
     handlePipelineStepErrors(handlePipelineStepErrorsParameters) {
 
-        def stepParameters = [:].plus(parameters)
-
-        def script = checkScript(this, parameters) ?: this
-        stepParameters.remove('script')
-
-        def utils = parameters.juStabUtils ?: new Utils()
-        stepParameters.remove('juStabUtils')
-
+        Script script = checkScript(this, parameters) ?: this
         def jenkinsUtils = parameters.jenkinsUtilsStub ?: new JenkinsUtils()
-        stepParameters.remove('jenkinsUtilsStub')
+        String piperGoPath = parameters.piperGoPath ?: './piper'
 
-        def piperGoPath = parameters.piperGoPath ?: './piper'
-        stepParameters.remove('piperGoPath')
-
-        def piperGoUtils = parameters.piperGoUtils ?: new PiperGoUtils(this, utils)
-        stepParameters.remove('piperGoUtils')
-
-        piperGoUtils.unstashPiperBin()
-        utils.unstash('pipelineConfigAndTests')
-        script.commonPipelineEnvironment.writeToDisk(script)
-
-        writeFile(file: ".pipeline/tmp/${metadataFile}", text: libraryResource(metadataFile))
-
-        // When converting to JSON and back again, entries which had a 'null' value will now have a value
-        // of type 'net.sf.json.JSONNull', for which the Groovy Truth resolves to 'true' in for example if-conditions
-        stepParameters = MapUtils.pruneNulls(stepParameters)
+        prepareExecution(script, parameters)
+        prepareMetadataResource(script, metadataFile)
+        Map stepParameters = prepareStepParameters(parameters)
 
         withEnv([
             "PIPER_parametersJSON=${groovy.json.JsonOutput.toJson(stepParameters)}",
@@ -60,7 +41,7 @@ void call(Map parameters = [:], stepName, metadataFile, List credentialInfo, fai
             // get context configuration
             Map config
             handleErrorDetails(stepName) {
-                config = readJSON(text: sh(returnStdout: true, script: "${piperGoPath} getConfig --contextConfig --stepMetadata '.pipeline/tmp/${metadataFile}'${defaultConfigArgs}${customConfigArg}"))
+                config = getStepContextConfig(script, piperGoPath, metadataFile, defaultConfigArgs, customConfigArg)
                 echo "Context Config: ${config}"
             }
 
@@ -81,6 +62,36 @@ void call(Map parameters = [:], stepName, metadataFile, List credentialInfo, fai
             }
         }
     }
+}
+
+static void prepareExecution(Script script, Map parameters = [:]) {
+    def utils = parameters.juStabUtils ?: new Utils()
+    def piperGoUtils = parameters.piperGoUtils ?: new PiperGoUtils(script, utils)
+    piperGoUtils.unstashPiperBin()
+    utils.unstash('pipelineConfigAndTests')
+    script.commonPipelineEnvironment.writeToDisk(script)
+}
+
+static Map prepareStepParameters(Map parameters) {
+    Map stepParameters = [:].plus(parameters)
+
+    stepParameters.remove('script')
+    stepParameters.remove('jenkinsUtilsStub')
+    stepParameters.remove('piperGoPath')
+    stepParameters.remove('juStabUtils')
+    stepParameters.remove('piperGoUtils')
+
+    // When converting to JSON and back again, entries which had a 'null' value will now have a value
+    // of type 'net.sf.json.JSONNull', for which the Groovy Truth resolves to 'true' in for example if-conditions
+    return MapUtils.pruneNulls(stepParameters)
+}
+
+static void prepareMetadataResource(Script script, String metadataFile) {
+    script.writeFile(file: ".pipeline/tmp/${metadataFile}", text: script.libraryResource(metadataFile))
+}
+
+static Map getStepContextConfig(Script script, String piperGoPath, String metadataFile, String defaultConfigArgs, String customConfigArg) {
+    return script.readJSON(text: script.sh(returnStdout: true, script: "${piperGoPath} getConfig --contextConfig --stepMetadata '.pipeline/tmp/${metadataFile}'${defaultConfigArgs}${customConfigArg}"))
 }
 
 static String getCustomDefaultConfigs() {
