8d482cf299a76fc2a842dee004a1425831e1b37d
OliverNocon <oliver.nocon@sap.com>
2020-07-14 09:25:34 +0200

    Include PR feedback

--- pkg/generator/helper/docuHelper.go
+++ pkg/generator/helper/docuHelper.go
@@ -77,7 +77,7 @@ func readAndAdjustTemplate(docFile io.ReadCloser) string {
 
 // Replaces the docGenStepName placeholder with the content from the yaml
 func docGenStepName(stepData *config.StepData) string {
-	return stepData.Metadata.Name
+	return stepData.Metadata.Name + "\n\n" + stepData.Metadata.Description + "\n"
 }
 
 // Replaces the docGenDescription placeholder with content from the yaml
@@ -87,7 +87,7 @@ func docGenDescription(stepData *config.StepData) string {
 	description += "Description\n\n" + stepData.Metadata.LongDescription + "\n\n"
 
 	description += "## Usage\n\n"
-	description += "We recommend to define values of [step parameters](#parameters) via [config.yml file](../configuration.md). In this case, calling the step is reduced to one simple line.<br />Calling the step can be done either via the Jenkins library step or on the command line.\n\n"
+	description += "We recommend to define values of [step parameters](#parameters) via [config.yml file](../configuration.md). In this case, calling the step is reduced to one simple line.<br />Calling the step can be done either via the Jenkins library step or on the [command line](../cli/index.md).\n\n"
 	description += "### Jenkins pipelines\n\n```groovy\n"
 	description += fmt.Sprintf("%v script: this\n```\n", stepData.Metadata.Name)
 	description += "### Command line\n\n```\n"
@@ -288,9 +288,8 @@ func formatDefault(param config.StepParameters, jenkinsParameters []string) stri
 	case string:
 		if len(v) == 0 {
 			return "`''`"
-		} else {
-			return fmt.Sprintf("`%v`", v)
 		}
+		return fmt.Sprintf("`%v`", v)
 	default:
 		return fmt.Sprintf("`%v`", param.Default)
 	}
