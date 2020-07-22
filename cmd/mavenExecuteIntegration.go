d03eb2f0327623ee9aaefae3edde54a1048d381b
Stephan Aßmus <stephan.assmus@sap.com>
2020-07-22 15:49:56 +0200

    Initial step metadata and generated skeleton

---
+++ cmd/mavenExecuteIntegration.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/SAP/jenkins-library/pkg/command"
+	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/telemetry"
+)
+
+func mavenExecuteIntegration(config mavenExecuteIntegrationOptions, telemetryData *telemetry.CustomData) {
+	// for command execution use Command
+	c := command.Command{}
+	// reroute command output to logging framework
+	c.Stdout(log.Writer())
+	c.Stderr(log.Writer())
+
+	// for http calls import  piperhttp "github.com/SAP/jenkins-library/pkg/http"
+	// and use a  &piperhttp.Client{} in a custom system
+	// Example: step checkmarxExecuteScan.go
+
+	// error situations should stop execution through log.Entry().Fatal() call which leads to an os.Exit(1) in the end
+	err := runMavenExecuteIntegration(&config, telemetryData, &c)
+	if err != nil {
+		log.Entry().WithError(err).Fatal("step execution failed")
+	}
+}
+
+func runMavenExecuteIntegration(config *mavenExecuteIntegrationOptions, telemetryData *telemetry.CustomData, command execRunner) error {
+	log.Entry().WithField("LogField", "Log field content").Info("This is just a demo for a simple step.")
+	return nil
+}
