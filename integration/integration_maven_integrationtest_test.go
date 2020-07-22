86937ba0e9224331981a7c5a299ec18093e054a8
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-07-22 16:35:06 +0200

    tomee it

--- integration/integration_maven_integrationtest_test.go
+++ integration/integration_maven_integrationtest_test.go
@@ -25,23 +25,3 @@ func TestMavenBuildCloudSdkSpringProject(t *testing.T) {
 	container.assertHasFile(t, "/project/application/target/cloud-sdk-spring-archetype-application.jar")
 	container.assertHasFile(t, "/tmp/.m2/repository")
 }
-
-func TestMavenBuildCloudSdkTomeeProject(t *testing.T) {
-	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
-		Image:   "maven:3-openjdk-8-slim",
-		User:    "1000",
-		TestDir: []string{"testdata", "TestMavenIntegration", "cloud-sdk-tomee-archetype"},
-		Mounts:  map[string]string{},
-		Setup:   []string{},
-	})
-
-	err := container.whenRunningPiperCommand("mavenBuild", "")
-	if err != nil {
-		t.Fatalf("Calling piper command filed %s", err)
-	}
-
-	container.assertHasOutput(t, "BUILD SUCCESS")
-	container.assertHasFile(t, "/project/application/target/cloud-sdk-tomee-archetype-application-classes.jar")
-	container.assertHasFile(t, "/project/application/target/cloud-sdk-tomee-archetype-application.war")
-	container.assertHasFile(t, "/tmp/.m2/repository")
-}
