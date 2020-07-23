3a628f2587a9a36d2c9f9dab07a552e1a4b2ca2e
Stephan Aßmus <stephan.assmus@sap.com>
2020-07-23 11:08:01 +0200

    Add collecting test results to Groovy wrapper

--- vars/mavenExecuteIntegration.groovy
+++ vars/mavenExecuteIntegration.groovy
@@ -7,6 +7,15 @@ import static com.sap.piper.Prerequisites.checkScript
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/mavenExecuteIntegration.yaml'
 
+@Field Set GENERAL_CONFIG_KEYS = []
+@Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS
+@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS.plus([
+    /**
+     * Specify a glob pattern where test result files will be located.
+     */
+    'reportLocationPattern',
+])
+
 //Metadata maintained in file project://resources/metadata/mavenExecuteIntegration.yaml
 
 void call(Map parameters = [:]) {
@@ -20,6 +29,10 @@ void call(Map parameters = [:]) {
         parameters = DownloadCacheUtils.injectDownloadCacheInParameters(script, parameters, BuildTool.MAVEN)
     }
 
-    List credentials = []
-    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
+    try {
+        List credentials = []
+        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
+    } finally {
+        testsPublishResults(script: script, junit: true, allowEmptyResults: true, pattern: parameters.reportLocationPattern)
+    }
 }
