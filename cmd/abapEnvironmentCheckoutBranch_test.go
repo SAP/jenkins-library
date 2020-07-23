6d0fbf91070459e096b0cad8c1adbbcbff346fe4
Henrik Müller <henrik.mueller@sap.com>
2020-07-23 09:30:55 +0200

    Add test for polling

--- cmd/abapEnvironmentCheckoutBranch_test.go
+++ cmd/abapEnvironmentCheckoutBranch_test.go
@@ -85,3 +85,69 @@ func TestTriggerCheckout(t *testing.T) {
 		assert.Equal(t, combinedErrorMessage, err.Error(), "Different error message expected")
 	})
 }
+
+func TestPollEntityCheckoutStep(t *testing.T) {
+
+	t.Run("Test poll entity: success case", func(t *testing.T) {
+
+		client := &clientMock{
+			BodyList: []string{
+				`{"d" : { "status" : "S" } }`,
+				`{"d" : { "status" : "R" } }`,
+			},
+			Token:      "myToken",
+			StatusCode: 200,
+		}
+		config := abapEnvironmentCheckoutBranchOptions{
+			CfAPIEndpoint:     "https://api.endpoint.com",
+			CfOrg:             "testOrg",
+			CfSpace:           "testSpace",
+			CfServiceInstance: "testInstance",
+			CfServiceKeyName:  "testServiceKey",
+			Username:          "testUser",
+			Password:          "testPassword",
+			RepositoryName:    "testRepo1",
+		}
+
+		con := abaputils.ConnectionDetailsHTTP{
+			User:       "MY_USER",
+			Password:   "MY_PW",
+			URL:        "https://api.endpoint.com/Entity/",
+			XCsrfToken: "MY_TOKEN",
+		}
+		status, _ := pollEntity(config.RepositoryName, con, client, 0)
+		assert.Equal(t, "S", status)
+	})
+
+	t.Run("Test poll entity: error case", func(t *testing.T) {
+
+		client := &clientMock{
+			BodyList: []string{
+				`{"d" : { "status" : "E" } }`,
+				`{"d" : { "status" : "R" } }`,
+			},
+			Token:      "myToken",
+			StatusCode: 200,
+		}
+		config := abapEnvironmentCheckoutBranchOptions{
+			CfAPIEndpoint:     "https://api.endpoint.com",
+			CfOrg:             "testOrg",
+			CfSpace:           "testSpace",
+			CfServiceInstance: "testInstance",
+			CfServiceKeyName:  "testServiceKey",
+			Username:          "testUser",
+			Password:          "testPassword",
+			RepositoryName:    "testRepo1",
+		}
+
+		con := abaputils.ConnectionDetailsHTTP{
+			User:       "MY_USER",
+			Password:   "MY_PW",
+			URL:        "https://api.endpoint.com/Entity/",
+			XCsrfToken: "MY_TOKEN",
+		}
+		status, _ := pollEntity(config.RepositoryName, con, client, 0)
+		assert.Equal(t, "E", status)
+	})
+
+}
