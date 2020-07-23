e8af84325202f66697d204dca663eb61e92f3e49
Stephan Aßmus <stephan.assmus@sap.com>
2020-07-23 10:49:20 +0200

    Fix CommonStepsTest regarding mavenExecuteIntegration

--- vars/mavenExecuteIntegration.groovy
+++ vars/mavenExecuteIntegration.groovy
@@ -10,13 +10,13 @@ import static com.sap.piper.Prerequisites.checkScript
 //Metadata maintained in file project://resources/metadata/mavenExecuteIntegration.yaml
 
 void call(Map parameters = [:]) {
+    final script = checkScript(this, parameters) ?: this
     // Perhaps the 'sidecarImage' parameter shall also originate from the step configuration.
     // In that case, we would need a way to post-process the parameters *after* resolving the
     // configuration using the piper binary from within piperExecuteBin.
     // Or perhaps the Download Cache should be injected in piperExecuteBin depending on some
     // condition controllable from the caller. That would get rid of some code-duplication.
     if (!parameters.sidecarImage) {
-        final script = checkScript(this, parameters) ?: this
         parameters = DownloadCacheUtils.injectDownloadCacheInParameters(script, parameters, BuildTool.MAVEN)
     }
 
