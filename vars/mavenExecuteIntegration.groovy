f1edcf7537a9fd62f0063245886263e8447f6137
Stephan Aßmus <stephan.assmus@sap.com>
2020-07-30 15:35:48 +0200

    Do not collect test results in wrapper
    This is supposed to happen at the end of a stage

--- vars/mavenExecuteIntegration.groovy
+++ vars/mavenExecuteIntegration.groovy
@@ -7,25 +7,12 @@ import static com.sap.piper.Prerequisites.checkScript
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/mavenExecuteIntegration.yaml'
 
-@Field Set GENERAL_CONFIG_KEYS = []
-@Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS
-@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS.plus([
-    /**
-     * Specify a glob pattern where test result files will be located.
-     */
-    'reportLocationPattern',
-])
-
 //Metadata maintained in file project://resources/metadata/mavenExecuteIntegration.yaml
 
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
     parameters = DownloadCacheUtils.injectDownloadCacheInParameters(script, parameters, BuildTool.MAVEN)
 
-    try {
-        List credentials = []
-        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
-    } finally {
-        testsPublishResults(script: script, junit: [allowEmptyResults: true, pattern: parameters.reportLocationPattern])
-    }
+    List credentials = []
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
 }
