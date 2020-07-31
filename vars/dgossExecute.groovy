a8f01333307f0e34662c3386a0eb9f94b5ece977
Raja Nasrallah <raja.nasrallah@sap.com>
2020-07-31 18:08:18 +0300

    add dgossExecute step

---
+++ vars/dgossExecute.groovy
@@ -0,0 +1,139 @@
+import com.sap.piper.DockerUtils
+import com.sap.piper.GenerateDocumentation
+import com.sap.piper.Utils
+import com.sap.piper.ConfigurationHelper
+
+import groovy.transform.Field
+
+import static com.sap.piper.Prerequisites.checkScript
+
+@Field String STEP_NAME = getClass().getName()
+@Field Set GENERAL_CONFIG_KEYS = [
+    /** For Docker builds only (mandatory): name of the image to be built. */
+    'dockerImageName',
+    /** For Docker builds only: Defines the registry url where the image should be located*/
+    'dockerRegistryUrl',
+]
+@Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS.plus([
+
+    /** For Docker builds only (mandatory): tag of the image to be built. */
+    'dockerImageTag',
+    /**
+     * gossFile Name of the goss file to use. Default value is 'goss.yaml'.
+     */
+    'gossFile',
+    /** For custom build types: Defines the command to be executed within the `dockerImage` in order to execute the build. */
+    'dockerCommand',
+    /** For custom build types: Image to be used for builds in case they should run inside a custom Docker container */
+    'dockerImage',
+])
+@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
+
+/**
+ * This step execute goss validation agains your container
+ */
+@GenerateDocumentation
+void call(Map parameters = [:]) {
+    handlePipelineStepErrors (stepName: STEP_NAME, stepParameters: parameters) {
+        final script = checkScript(this, parameters) ?: this
+        def utils = parameters.juStabUtils ?: new Utils()
+        // handle deprecated parameters
+        // load default & individual configuration
+        Map config = ConfigurationHelper.newInstance(this)
+            .loadStepDefaults()
+            .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, parameters.stageName?:env.STAGE_NAME, STEP_CONFIG_KEYS)
+            .mixin(parameters, PARAMETER_KEYS)
+            .use()
+
+        DockerUtils dockerUtils = new DockerUtils(script)
+        ConfigurationHelper.newInstance(this, config)
+            .withMandatoryProperty('dockerImageName')
+            .withMandatoryProperty('dockerImageTag')
+            .withMandatoryProperty('gossFile')
+
+        def dockerImageNameAndTag = "${config.dockerImageName}:${config.dockerImageTag}"
+
+
+        if (config.dockerRegistryUrl) {
+            dockerImageNameAndTag = config.dockerRegistryUrl + "/" + dockerImageNameAndTag
+        }
+
+        if (dockerUtils.onKubernetes()){
+            runOnK8S(config, dockerImageNameAndTag)
+
+        }else if (dockerUtils.withDockerDaemon()){
+            runOnNode(dockerImageNameAndTag)
+        }else{
+            error "[${STEP_NAME}] No Docker daemon available, dgoss require a runnig docker daemon"
+        }
+    }
+}
+
+def runOnNode(dockerImageNameAndTag){
+    def targetImage = docker.image(dockerImageNameAndTag)
+    docker.image('docker:18.06.3-dind').withRun('--privileged -it --name mydind') { c ->
+        sh "while ! docker exec mydind docker stats --no-stream; do sleep 1; done"
+        docker.image('kiwicom/dgoss').inside(""" --link ${c.id}:docker -v "${pwd()}":/src
+        -e "GOSS_FILES_STRATEGY=cp"
+        -e "DOCKER_HOST=tcp://docker:2375" """){
+            sh """
+                cd /src
+                dgoss run ${dockerImageNameAndTag}
+            """
+        }
+    }
+}
+
+def runOnK8S(config, dockerImageNameAndTag){
+    stash name: '_gossfile',includes: config.gossFile
+    podTemplate(yaml:"""
+apiVersion: v1
+kind: Pod
+metadata:
+  name: dgoss
+  labels:
+    name: dgoss
+spec:
+  volumes:
+  - name: dind-storage
+    emptyDir: {}
+  containers:
+  - name: dind
+    image: docker:18.06.3-dind
+    securityContext:
+      privileged: true
+    volumeMounts:
+      - name: dind-storage
+        mountPath: /var/lib/docker
+  - name: goss
+    image: kiwicom/dgoss
+    env:
+    - name: DOCKER_HOST
+      value: tcp://localhost:2375
+    - name: GOSS_FILES_STRATEGY
+      value: cp
+    command:
+    - cat
+    tty: true
+  - name: jnlp
+    image: docker.wdf.sap.corp:50001/sap-production/jnlp-alpine:3.26.1-sap-02
+    args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
+            """
+    ){
+        node(POD_LABEL){
+            container('goss') {
+                unstash '_gossfile'
+                if (config.gossFile != "goss.yaml"){
+                    sh "cp ${config.gossFile} goss.yaml"
+                }
+                sh "dgoss run ${dockerImageNameAndTag}"
+            }
+        }
+    }
+}
+
+boolean isKubernetes() {
+    return Boolean.valueOf(env.ON_K8S)
+}
