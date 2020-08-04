78868c10c03d970c768ad036fc48b33ca7314ca5
Henrik Müller <henrik.mueller@sap.com>
2020-08-04 16:36:26 +0200

    Adjust pollEntity tests

--- pkg/abaputils/manageGitRepositoryUtils_test.go
+++ pkg/abaputils/manageGitRepositoryUtils_test.go
@@ -8,79 +8,7 @@ import (
 
 func TestPollEntity(t *testing.T) {
 
-	t.Run("Test poll entity multiple repos: success case", func(t *testing.T) {
-
-		client := &ClientMock{
-			BodyList: []string{
-				`{"d" : { "status" : "S" } }`,
-				`{"d" : { "status" : "R" } }`,
-			},
-			Token:      "myToken",
-			StatusCode: 200,
-		}
-
-		options := AbapEnvironmentOptions{
-			CfAPIEndpoint:     "https://api.endpoint.com",
-			CfOrg:             "testOrg",
-			CfSpace:           "testSpace",
-			CfServiceInstance: "testInstance",
-			CfServiceKeyName:  "testServiceKey",
-			Username:          "testUser",
-			Password:          "testPassword",
-		}
-
-		config := AbapEnvironmentPullGitRepoOptions{
-			AbapEnvOptions:  options,
-			RepositoryNames: []string{"testRepo1", "testRepo2"},
-		}
-
-		con := ConnectionDetailsHTTP{
-			User:       "MY_USER",
-			Password:   "MY_PW",
-			URL:        "https://api.endpoint.com/Entity/",
-			XCsrfToken: "MY_TOKEN",
-		}
-		status, _ := PollEntity(config.RepositoryNames[0], con, client, 0)
-		assert.Equal(t, "S", status)
-	})
-
-	t.Run("Test poll entity multiple repos: error case", func(t *testing.T) {
-
-		client := &ClientMock{
-			BodyList: []string{
-				`{"d" : { "status" : "E" } }`,
-				`{"d" : { "status" : "R" } }`,
-			},
-			Token:      "myToken",
-			StatusCode: 200,
-		}
-
-		options := AbapEnvironmentOptions{
-			CfAPIEndpoint:     "https://api.endpoint.com",
-			CfOrg:             "testOrg",
-			CfSpace:           "testSpace",
-			CfServiceInstance: "testInstance",
-			CfServiceKeyName:  "testServiceKey",
-			Username:          "testUser",
-			Password:          "testPassword",
-		}
-
-		config := AbapEnvironmentPullGitRepoOptions{
-			AbapEnvOptions:  options,
-			RepositoryNames: []string{"testRepo1", "testRepo2"},
-		}
-
-		con := ConnectionDetailsHTTP{
-			User:       "MY_USER",
-			Password:   "MY_PW",
-			URL:        "https://api.endpoint.com/Entity/",
-			XCsrfToken: "MY_TOKEN",
-		}
-		status, _ := PollEntity(config.RepositoryNames[0], con, client, 0)
-		assert.Equal(t, "E", status)
-	})
-
-	t.Run("Test poll entity single repo: success case", func(t *testing.T) {
+	t.Run("Test poll entity - success case", func(t *testing.T) {
 
 		client := &ClientMock{
 			BodyList: []string{
@@ -116,7 +44,7 @@ func TestPollEntity(t *testing.T) {
 		assert.Equal(t, "S", status)
 	})
 
-	t.Run("Test poll entity single repo: error case", func(t *testing.T) {
+	t.Run("Test poll entity - error case", func(t *testing.T) {
 
 		client := &ClientMock{
 			BodyList: []string{
