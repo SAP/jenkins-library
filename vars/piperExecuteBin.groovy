8d64e2262c97fd10ca99927f1901a4b66534735b
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-08-04 09:23:26 +0200

    Revert "debug"
    This reverts commit e247dbebc72991d2a2245dbb28a227373688292b.

--- vars/piperExecuteBin.groovy
+++ vars/piperExecuteBin.groovy
@@ -132,7 +132,6 @@ static String getCustomConfigArg(def script) {
 }
 
 void dockerWrapper(script, config, body) {
-    echo "dbg>> config.dockerImage ${config.dockerImage}"
     if (config.dockerImage) {
         Map dockerExecuteParameters = [:].plus(config)
         dockerExecuteParameters.script = script
