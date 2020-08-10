5fc6fee8b8b1e2c77a628314e459ad33e9cb1f4b
Kevin Hudemann <kevin.hudemann@sap.com>
2020-08-10 11:16:55 +0200

    Add groovy wrapper for TemporaryCredentialsUtils
    This change adds the groovy step writeTemporaryCredentials, in order to
    avoid resolving configuration parameters within the piperPipelineStageIntegration
    in the future.
    For the integration tests functionality provided by the SAP Cloud SDK
    Pipeline it needs be possible to configure a credential id pointing to
    Jenkins credentials, which are then temporarily written to a file to be
    available during the execution of the integration tests.
    With the previous implementation solely in the TemporaryCredentialsUtils
    class, it would be required to resolve the credentialId from the
    configuratuion within the piperPipelineStageIntegration, once we fully
    migrate the Cloud SDK integration tests stage.

---
+++ vars/writeTemporaryCredentials.groovy
@@ -0,0 +1,71 @@
+import com.sap.piper.ConfigurationHelper
+import com.sap.piper.ConfigurationLoader
+import com.sap.piper.GenerateDocumentation
+import com.sap.piper.JsonUtils
+import com.sap.piper.TemporaryCredentialsUtils
+import com.sap.piper.Utils
+
+import groovy.transform.Field
+
+import static com.sap.piper.Prerequisites.checkScript
+
+@Field String STEP_NAME = getClass().getName()
+
+@Field Set GENERAL_CONFIG_KEYS = []
+@Field Set STEP_CONFIG_KEYS = [
+    /**
+     * The list of credentials that are written to a temporary file for the execution of the body.
+     * Each element of credentials must be a map containing a property alias and a property credentialId.
+     * You have to ensure that corresponding credential entries exist in your Jenkins configuration.
+     */
+    'credentials',
+    /**
+     * The path to the directory where the credentials file has to be placed.
+     */
+    'credentialsDirectory'
+]
+@Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
+
+/**
+ * Writes credentials to a temporary file and deletes it after the body has been executed.
+ */
+@GenerateDocumentation
+void call(Map parameters = [:], Closure body) {
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+        def script = checkScript(this, parameters) ?: this
+        def stageName = parameters.stageName ?: env.STAGE_NAME
+
+        Map config = ConfigurationHelper.newInstance(this)
+            .loadStepDefaults()
+            .mixin(ConfigurationLoader.defaultStageConfiguration(script, stageName))
+            .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, stageName, STEP_CONFIG_KEYS)
+            .mixin(parameters, PARAMETER_KEYS)
+            .use()
+
+        // telemetry reporting
+        new Utils().pushToSWA([
+            step: STEP_NAME,
+            stepParamKey1: 'scriptMissing',
+            stepParam1: parameters?.script == null
+        ], config)
+
+        if (!config.credentials) {
+            error "[${STEP_NAME}] The execution failed, since no credentials are defined. Please provide credentials as a list of maps.\n"
+        }
+        if (!(config.credentials instanceof List)) {
+            error "[${STEP_NAME}] The execution failed, since credentials is not a list. Please provide credentials as a list of maps. For example:\n" +
+                "credentials: \n" + "  - alias: 'ERP'\n" + "    credentialId: 'erp-credentials'"
+        }
+        if (!config.credentialsDirectory) {
+            error "[${STEP_NAME}] The execution failed, since no credentialsDirectory is defined. Please provide the path for the credentials file.\n"
+        }
+
+        TemporaryCredentialsUtils credUtils = new TemporaryCredentialsUtils(script)
+
+        credUtils.handleTemporaryCredentials(config.credentials, config.credentialsDirectory) {
+            body()
+        }
+    }
+}
