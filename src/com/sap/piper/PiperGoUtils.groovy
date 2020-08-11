479ca042d830838552e7fe9c3240d48bd78af99a
Keshav <anil.keshav@sap.com>
2020-08-11 10:52:22 +0200

    restoring piperGoUtils to download correct piper binary

--- src/com/sap/piper/PiperGoUtils.groovy
+++ src/com/sap/piper/PiperGoUtils.groovy
@@ -46,8 +46,7 @@ class PiperGoUtils implements Serializable {
             }
 
             def fallbackUrl = 'https://github.com/SAP/jenkins-library/releases/latest/download/piper_master'
-            //def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/SAP/jenkins-library/releases/download/${version}/piper"
-            def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/shenoygi/jenkins-library/releases/download/V1.5.7/piper"
+            def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/SAP/jenkins-library/releases/download/${version}/piper"
 
             boolean downloaded = downloadGoBinary(piperBinUrl)
             if (!downloaded) {
