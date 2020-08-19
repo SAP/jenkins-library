7afc5e8777e89281a5e26f76b397f140533d6708
Stephan Aßmus <stephan.assmus@sap.com>
2020-08-19 13:16:21 +0200

    Remove parameter again

--- vars/setupCommonPipelineEnvironment.groovy
+++ vars/setupCommonPipelineEnvironment.groovy
@@ -47,8 +47,6 @@ void call(Map parameters = [:]) {
 
         def script = checkScript(this, parameters)
 
-        script.commonPipelineEnvironment.useTechnicalStageNames = parameters.useTechnicalStageNames == true
-
         String configFile = parameters.get('configFile')
         loadConfigurationFromFile(script, configFile)
 
