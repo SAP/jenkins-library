09636e343f40c035ad1319d7a726b56975846d29
OliverNocon <oliver.nocon@sap.com>
2020-08-19 16:35:02 +0200

    allow overriding dependency resolution setting

--- src/com/sap/piper/WhitesourceConfigurationHelper.groovy
+++ src/com/sap/piper/WhitesourceConfigurationHelper.groovy
@@ -88,7 +88,7 @@ class WhitesourceConfigurationHelper implements Serializable {
                     [name: 'docker.scanTarFiles', value: true, force: true],
                     [name: 'docker.includes', value: /.*.tar/, force: true],
                     [name: 'ignoreSourceFiles', value: true, force: true],
-                    [name: 'python.resolveGlobalPackages', value: true, force: true],
+                    [name: 'python.resolveGlobalPackages', value: true, force: false],
                     [name: 'resolveAllDependencies', value: true, force: false],
                     [name: 'updateType', value: 'OVERRIDE', force: true],
                 ]
