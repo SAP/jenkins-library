c6eb19520dc707db54404e937d18a05041707052
Kevin Hudemann <kevin.hudemann@sap.com>
2020-08-26 17:16:50 +0200

    Fix piperPipelineStageMavenStaticCodeChecks and make it use runStep conditions

--- vars/piperPipelineStageMavenStaticCodeChecks.groovy
+++ vars/piperPipelineStageMavenStaticCodeChecks.groovy
@@ -1,7 +1,9 @@
+import com.sap.piper.ConfigurationHelper
 import com.sap.piper.ConfigurationLoader
 import com.sap.piper.GenerateStageDocumentation
 import com.sap.piper.QualityCheck
 import com.sap.piper.ReportAggregator
+import com.sap.piper.Utils
 
 import static com.sap.piper.Prerequisites.checkScript
 
@@ -9,8 +11,12 @@ import groovy.transform.Field
 
 @Field String STEP_NAME = getClass().getName()
 
+@Field STAGE_STEP_KEYS = [
+    /** Executes static code checks for Maven based projects. The plugins SpotBugs and PMD are used. */
+    'mavenExecuteStaticCodeChecks'
+]
 @Field Set GENERAL_CONFIG_KEYS = []
-@Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS
+@Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS.plus(STAGE_STEP_KEYS)
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
 
 /**
@@ -19,10 +25,27 @@ import groovy.transform.Field
  */
 @GenerateStageDocumentation(defaultStageName = 'mavenExecuteStaticCodeChecks')
 void call(Map parameters = [:]) {
-    String stageName = 'mavenExecuteStaticCodeChecks'
     final script = checkScript(this, parameters) ?: null
 
+    def utils = parameters.juStabUtils ?: new Utils()
+
+    def stageName = parameters.stageName?:env.STAGE_NAME
+
+    Map config = ConfigurationHelper.newInstance(this)
+        .loadStepDefaults()
+        .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+        .mixinStageConfig(script.commonPipelineEnvironment, stageName, STEP_CONFIG_KEYS)
+        .mixin(parameters, PARAMETER_KEYS)
+        .addIfEmpty('mavenExecuteStaticCodeChecks', script.commonPipelineEnvironment.configuration.runStep?.get(stageName)?.mavenExecuteStaticCodeChecks)
+        .use()
+
     piperStageWrapper(stageName: stageName, script: script) {
-        mavenExecuteStaticCodeChecks(script: script)
+
+        // telemetry reporting
+        utils.pushToSWA([step: STEP_NAME], config)
+
+        if (config.mavenExecuteStaticCodeChecks) {
+            mavenExecuteStaticCodeChecks(script: script)
+        }
     }
 }
