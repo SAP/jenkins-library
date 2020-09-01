7431c80f0833bf74b5bfa7efbb7aa54b9811c9c4
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2020-09-01 11:28:08 +0200

    Apply suggestions from code review
    Co-authored-by: Stephan Aßmus <stephan.assmus@sap.com>

--- pkg/protecode/analysis_test.go
+++ pkg/protecode/analysis_test.go
@@ -19,7 +19,7 @@ func TestIsSevere(t *testing.T) {
 			},
 		}
 		// test && assert
-		assert.Equal(t, true, isSevere(vulnerability))
+		assert.True(t, isSevere(vulnerability))
 	})
 	t.Run("with severe cvss v2 vulnerability", func(t *testing.T) {
 		// init
@@ -33,7 +33,7 @@ func TestIsSevere(t *testing.T) {
 			},
 		}
 		// test && assert
-		assert.Equal(t, true, isSevere(vulnerability))
+		assert.True(t, isSevere(vulnerability))
 	})
 	t.Run("with non-severe cvss v3 vulnerability", func(t *testing.T) {
 		// init
@@ -47,7 +47,7 @@ func TestIsSevere(t *testing.T) {
 			},
 		}
 		// test && assert
-		assert.Equal(t, false, isSevere(vulnerability))
+		assert.False(t, isSevere(vulnerability))
 	})
 	t.Run("with non-severe cvss v2 vulnerability", func(t *testing.T) {
 		// init
@@ -61,7 +61,7 @@ func TestIsSevere(t *testing.T) {
 			},
 		}
 		// test && assert
-		assert.Equal(t, false, isSevere(vulnerability))
+		assert.False(t, isSevere(vulnerability))
 	})
 	t.Run("with non-severe vulnerability with missing cvss v3 rating", func(t *testing.T) {
 		// init
@@ -75,7 +75,7 @@ func TestIsSevere(t *testing.T) {
 			},
 		}
 		// test && assert
-		assert.Equal(t, false, isSevere(vulnerability))
+		assert.False(t, isSevere(vulnerability))
 	})
 }
 
@@ -92,36 +92,36 @@ func TestHasSevereVulnerabilities(t *testing.T) {
 		// init
 		data := Result{Components: []Component{{Vulns: []Vulnerability{nonSevere1, severeV3}}}}
 		// test && assert
-		assert.Equal(t, true, HasSevereVulnerabilities(data, ""))
+		assert.True(t, HasSevereVulnerabilities(data, ""))
 	})
 	t.Run("with severe v2 vulnerabilities", func(t *testing.T) {
 		// init
 		data := Result{Components: []Component{{Vulns: []Vulnerability{nonSevere1, severeV2}}}}
 		// test && assert
-		assert.Equal(t, true, HasSevereVulnerabilities(data, ""))
+		assert.True(t, HasSevereVulnerabilities(data, ""))
 	})
 	t.Run("without severe vulnerabilities", func(t *testing.T) {
 		// init
 		data := Result{Components: []Component{{Vulns: []Vulnerability{nonSevere1, nonSevere2}}}}
 		// test && assert
-		assert.Equal(t, false, HasSevereVulnerabilities(data, ""))
+		assert.False(t, HasSevereVulnerabilities(data, ""))
 	})
 	t.Run("with historic vulnerabilities", func(t *testing.T) {
 		// init
 		data := Result{Components: []Component{{Vulns: []Vulnerability{nonSevere1, triaged}}}}
 		// test && assert
-		assert.Equal(t, false, HasSevereVulnerabilities(data, ""))
+		assert.False(t, HasSevereVulnerabilities(data, ""))
 	})
 	t.Run("with excluded vulnerabilities", func(t *testing.T) {
 		// init
 		data := Result{Components: []Component{{Vulns: []Vulnerability{nonSevere1, excluded}}}}
 		// test && assert
-		assert.Equal(t, false, HasSevereVulnerabilities(data, "Cve5,Cve14"))
+		assert.False(t, HasSevereVulnerabilities(data, "Cve5,Cve14"))
 	})
 	t.Run("with historic vulnerabilities", func(t *testing.T) {
 		// init
 		data := Result{Components: []Component{{Vulns: []Vulnerability{nonSevere1, historic}}}}
 		// test && assert
-		assert.Equal(t, false, HasSevereVulnerabilities(data, ""))
+		assert.False(t, HasSevereVulnerabilities(data, ""))
 	})
 }
