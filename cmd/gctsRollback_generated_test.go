41abec6b103a29a41ddbd19d0981ecf3f539aab1
Oliver Feldmann <oliver.feldmann@sap.com>
2020-09-10 14:37:46 +0200

    Fix generator typo (#2008)
    * Fix typo in generator code
    
    * Fix typo in generated code

--- cmd/gctsRollback_generated_test.go
+++ cmd/gctsRollback_generated_test.go
@@ -10,7 +10,7 @@ func TestGctsRollbackCommand(t *testing.T) {
 
 	testCmd := GctsRollbackCommand()
 
-	// only high level testing performed - details are tested in step generation procudure
+	// only high level testing performed - details are tested in step generation procedure
 	assert.Equal(t, "gctsRollback", testCmd.Use, "command name incorrect")
 
 }
