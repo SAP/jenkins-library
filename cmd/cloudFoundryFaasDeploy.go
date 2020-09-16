f1ae7a44a12ad50ad734a1d1d04d0e1d58be6146
Oliver Feldmann <oliver.feldmann@sap.com>
2020-09-16 11:51:57 +0200

    Add success message

--- cmd/cloudFoundryFaasDeploy.go
+++ cmd/cloudFoundryFaasDeploy.go
@@ -76,6 +76,8 @@ func runCloudFoundryFaasDeploy(options *cloudFoundryFaasDeployOptions,
 	xfsrtDeployScript := []string{"faas", "project", "deploy", "-y", "./deploy/values.yaml"}
 	if err := c.RunExecutable("xfsrt-cli", xfsrtDeployScript...); err != nil {
 		return fmt.Errorf("Failed to deploy faas project: %w", err)
+	} else {
+		log.Entry().Info("Deployment successful.")
 	}
 
 	return returnedError
