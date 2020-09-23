e0155a420dec1a0bf6e1dbb4f6a7bec3f64feb6a
Stephan Aßmus <stephan.assmus@sap.com>
2020-09-23 22:13:34 +0200

    Fix checking pom.xml exclude

--- cmd/whitesourceExecuteScan.go
+++ cmd/whitesourceExecuteScan.go
@@ -459,7 +459,7 @@ func executeMavenScanForPomFile(config *ScanOptions, scan *whitesourceScan, util
 	// From the documentation, these are file paths to a module's pom.xml.
 	// For MTA projects, we want to support mixing paths to package.json files and pom.xml files.
 	for _, exclude := range config.BuildDescriptorExcludeList {
-		if !strings.HasPrefix(exclude, "pom.xml") {
+		if !strings.HasSuffix(exclude, "pom.xml") {
 			continue
 		}
 		moduleName := filepath.Dir(exclude)
