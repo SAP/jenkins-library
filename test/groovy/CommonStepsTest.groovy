271c56cb6989ecf5c1c9457422264dc2d12eef1f
dominiklendle <d.lendle@sap.com>
2020-09-24 10:40:06 +0200

    Removing duplicate

--- test/groovy/CommonStepsTest.groovy
+++ test/groovy/CommonStepsTest.groovy
@@ -153,7 +153,6 @@ public class CommonStepsTest extends BasePiperTest{
         'mtaBuild', //implementing new golang pattern without fields
         'nexusUpload', //implementing new golang pattern without fields
         'piperPipelineStageArtifactDeployment', //stage without step flags
-        'abapEnvironmentRunATCCheck', //implementing new golang pattern without fields
         'sonarExecuteScan', //implementing new golang pattern without fields
         'gctsCreateRepository', //implementing new golang pattern without fields
         'gctsRollback', //implementing new golang pattern without fields
