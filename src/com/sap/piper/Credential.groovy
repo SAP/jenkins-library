f9b822fdd542acb4164b00e2488526f6e1c4a98f
Oliver Feldmann <oliver.feldmann@sap.com>
2020-10-07 17:06:46 +0200

    make enum value final
    Co-authored-by: Stephan Aßmus <stephan.assmus@sap.com>

--- src/com/sap/piper/Credential.groovy
+++ src/com/sap/piper/Credential.groovy
@@ -3,7 +3,7 @@ package com.sap.piper
 enum Credential {
     FILE('file'), TOKEN('token'), SECRET_TEXT('secretText'), USERNAME_PASSWORD('usernamePassword'), SSH('ssh')
 
-    private String value
+    private final String value
 
     public Credential(String value) {
         this.value = value
