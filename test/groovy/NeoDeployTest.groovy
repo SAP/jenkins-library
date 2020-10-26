f1fa86f5e7b04fcf4b34464d55b2b62071a213a9
Kondreddy <srinikitha.kondreddy@sap.com>
2020-10-26 10:30:30 +0100

    Add test

--- test/groovy/NeoDeployTest.groovy
+++ test/groovy/NeoDeployTest.groovy
@@ -3,9 +3,12 @@ import com.sap.piper.Utils
 
 import groovy.lang.Script
 import hudson.AbortException
+import util.JenkinsReadJsonRule
 
 import static org.hamcrest.Matchers.allOf
+import static org.hamcrest.Matchers.contains
 import static org.hamcrest.Matchers.containsString
+import static org.hamcrest.Matchers.hasItem
 import static org.hamcrest.Matchers.not
 import static org.junit.Assert.assertThat
 
@@ -52,7 +55,10 @@ class NeoDeployTest extends BasePiperTest {
         .around(shellRule)
         .around(new JenkinsCredentialsRule(this)
         .withCredentials('myCredentialsId', 'anonymous', '********')
-        .withCredentials('CI_CREDENTIALS_ID', 'defaultUser', '********'))
+        .withCredentials('CI_CREDENTIALS_ID', 'defaultUser', '********')
+        .withCredentials('testOauthId', 'clientId', '********'))
+        .around(new JenkinsReadJsonRule(this))
+        .around(new JenkinsPropertiesRule(this, null, new Properties()))
         .around(stepRule)
         .around(new JenkinsLockRule(this))
         .around(new JenkinsWithEnvRule(this))
@@ -415,6 +421,24 @@ class NeoDeployTest extends BasePiperTest {
                 .hasSingleQuotedOption('password', '\\*\\*\\*\\*\\*\\*\\*\\*')
                 .hasSingleQuotedOption('source', '.*'))
 
+    }
+
+    @Test
+    void invalidateCache_Success_Test() {
+
+        nullScript.commonPipelineEnvironment.configuration = [steps: [neoDeploy: [neo: [host: 'test.deploy.host.com', account: 'trialuser123', invalidateCache: true, oauthCredentialId: 'testOauthId', siteId: "12346"]]]]
+        fileExistsRule.registerExistingFile('./target/artifact.war')
+
+        shellRule.setReturnValue(JenkinsShellCallRule.Type.REGEX, "https:\\/\\/oauthasservices-trialuser123\\.test\\.deploy\\.host\\.com\\/oauth2\\/api\\/v1\\/token", "{\"access_token\":\"xxx\"}")
+        shellRule.setReturnValue(JenkinsShellCallRule.Type.REGEX, "fiori\\/api\\/v1\\/csrf", "X-CSRF-Token=xxx")
+        shellRule.setReturnValue(JenkinsShellCallRule.Type.REGEX, "fiori\\/v1\\/operations\\/invalidateCache", "{\"statusCode\":\"200\"}")
+
+        stepRule.step.neoDeploy(script: nullScript,
+            source: archiveName,
+            deployMode: 'mta')
+
+        Assert.assertThat(shellRule.shell, hasItem(containsString("curl -X POST -u \"clientId:********\" --fail \"https://oauthasservices-trialuser123.test.deploy.host.com/oauth2/api/v1/token?grant_type=client_credentials&scope=write,read")))
+        Assert.assertThat(shellRule.shell[3], containsString("#!/bin/bash curl -i -L -c 'cookies.jar' -H 'X-CSRF-Token: Fetch' -H \"Authorization: Bearer xxx\" --fail \"https://sandboxportal-trialuser123.test.deploy.host.com/fiori/api/v1/csrf\""))
     }
 
     @Test
