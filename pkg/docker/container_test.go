017316ba1dc7add5bb107019686a5a662b507157
Fabian Reh <fabian.reh@sap.com>
2020-10-27 12:30:55 +0100

    Adds helm capabilities to the gitopsUpdateDeployment step

--- pkg/docker/container_test.go
+++ pkg/docker/container_test.go
@@ -105,3 +105,69 @@ func TestContainerImageNameTagFromImage(t *testing.T) {
 		})
 	}
 }
+
+func TestContainerImageNameFromImage(t *testing.T) {
+	tt := []struct {
+		image         string
+		expected      string
+		expectedError string
+	}{
+		{image: "", expected: "", expectedError: "failed to parse image name"},
+		{image: "onlyImage", expected: "onlyImage"},
+		{image: "onlyimage", expected: "onlyimage"},
+		{image: "onlyimage:withTag", expected: "onlyimage"},
+		{image: "onlyimage@sha256:152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205", expected: "onlyimage@sha256"},
+		{image: "path/to/image", expected: "path/to/image"},
+		{image: "my.registry.com/onlyimage", expected: "onlyimage"},
+		{image: "my.registry.com:50000/onlyimage", expected: "onlyimage"},
+		{image: "my.registry.com:50000/onlyimage:withTag", expected: "onlyimage"},
+		{image: "my.registry.com:50000/onlyimage@sha256:152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205", expected: "onlyimage@sha256"},
+		{image: "my.registry.com:50000/path/to/image:withTag", expected: "path/to/image"},
+		{image: "my.registry.com:50000/path/to/image@sha256:152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205", expected: "path/to/image@sha256"},
+	}
+
+	for _, test := range tt {
+		t.Run(test.image, func(t *testing.T) {
+			got, err := ContainerImageNameFromImage(test.image)
+			if len(test.expectedError) > 0 {
+				assert.Contains(t, fmt.Sprint(err), test.expectedError)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, test.expected, got)
+		})
+	}
+}
+
+func TestContainerImageTagFromImage(t *testing.T) {
+	tt := []struct {
+		image         string
+		expected      string
+		expectedError string
+	}{
+		{image: "", expected: "", expectedError: "failed to parse image name"},
+		{image: "onlyImage", expected: "", expectedError: "could not extract tag from full image"},
+		{image: "onlyimage", expected: "", expectedError: "could not extract tag from full image"},
+		{image: "onlyimage:withTag", expected: "withTag"},
+		{image: "onlyimage@sha256:152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205", expected: "152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205"},
+		{image: "path/to/image", expected: "", expectedError: "could not extract tag from full image"},
+		{image: "my.registry.com/onlyimage", expected: "", expectedError: "could not extract tag from full image"},
+		{image: "my.registry.com:50000/onlyimage", expected: "", expectedError: "could not extract tag from full image"},
+		{image: "my.registry.com:50000/onlyimage:withTag", expected: "withTag"},
+		{image: "my.registry.com:50000/onlyimage@sha256:152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205", expected: "152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205"},
+		{image: "my.registry.com:50000/path/to/image:withTag", expected: "withTag"},
+		{image: "my.registry.com:50000/path/to/image@sha256:152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205", expected: "152f65865ae43b143b1e42dacdb5e9c473dd70b3adc5b79af7cf585cc8605205"},
+	}
+
+	for _, test := range tt {
+		t.Run(test.image, func(t *testing.T) {
+			got, err := ContainerImageTagFromImage(test.image)
+			if len(test.expectedError) > 0 {
+				assert.Contains(t, fmt.Sprint(err), test.expectedError)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, test.expected, got)
+		})
+	}
+}
