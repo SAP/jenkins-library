4051c8357ca5c2948381aca211e4d4e6d05333e8
Oliver Feldmann <oliver.feldmann@sap.com>
2020-12-14 14:17:45 +0100

    Add test to ensure enum literal does not change

---
+++ test/groovy/com/sap/piper/CredentialTypeTest.groovy
@@ -0,0 +1,18 @@
+package com.sap.piper
+
+import org.junit.Assert
+import org.junit.Test
+
+class CredentialTypeTest {
+
+    @Test
+    void "Check that enum literals have not changed"() {
+        assert "${CredentialType.FILE}" == 'file'
+        assert "${CredentialType.FILE}" == 'file'
+        assert "${CredentialType.TOKEN}" == 'token'
+        assert "${CredentialType.SECRET_TEXT}" == 'secretText'
+        assert "${CredentialType.USERNAME_PASSWORD}" == 'usernamePassword'
+        assert "${CredentialType.SSH}" == 'ssh'
+    }
+
+}
