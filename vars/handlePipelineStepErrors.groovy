b262acd34e5eba80dda86689399731fc4f74a7ca
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2021-01-07 15:24:47 +0100

    Update handlePipelineStepErrors.groovy

--- vars/handlePipelineStepErrors.groovy
+++ vars/handlePipelineStepErrors.groovy
@@ -67,6 +67,7 @@ void call(Map parameters = [:], body) {
 
     def debug1 = cpe?.getValue('unstableSteps')
     echo "DEBUG1: ${debug1} - ${debug1.getClass()} - CPE: ${cpe}"
+    List unstableSteps  = cpe?.getValue('unstableSteps')
     def message = ''
     try {
         if (config.echoDetails)
@@ -104,9 +105,10 @@ void call(Map parameters = [:], body) {
         }
 
         def debug2 = cpe?.getValue('unstableSteps')
+        echo "DEBUG1: ${debug1} - ${debug1.getClass()} - CPE: ${cpe}"
         echo "DEBUG2: ${debug2} - ${debug2.getClass()} - CPE: ${cpe}"
         
-        List unstableSteps = cpe?.getValue('unstableSteps') ?: []
+        //List unstableSteps = cpe?.getValue('unstableSteps') ?: []
 
         // add information about unstable steps to pipeline environment
         // this helps to bring this information to users in a consolidated manner inside a pipeline
