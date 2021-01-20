84e8f4c79f1b323703552a0d2d69066a21b6e39e
Leander Schulz <leander.schulz01@sap.com>
2021-01-20 12:05:11 +0100

    fix iterator ref

--- vars/newmanExecute.groovy
+++ vars/newmanExecute.groovy
@@ -33,7 +33,7 @@ void call(Map parameters = [:]) {
     if (config.cfAppsWithSecrets) {
         config.cfAppsWithSecrets.each {
             echo "[INFO]${STEP_NAME}] Preparing credential for being used by piper-go. key: ${it}, exposed as environment variable PIPER_NEWMAN_USER_${it} and PIPER_NEWMAN_PASSWORD_${it}"
-            credentials << [type: 'usernamePassword', id: ${it}, env: ["PIPER_NEWMAN_USER_${it}", "PIPER_NEWMAN_PASSWORD_${it}"]]
+            credentials << [type: 'usernamePassword', id: "${it}", env: ["PIPER_NEWMAN_USER_${it}", "PIPER_NEWMAN_PASSWORD_${it}"]]
         }
     }
 
