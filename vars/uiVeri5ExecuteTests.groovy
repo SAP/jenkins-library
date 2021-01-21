82d9a48a5b758b328dbd73a46ad8d2e8a80991b2
Leander Schulz <leander.schulz01@sap.com>
2021-01-21 15:16:43 +0100

    add testRepository support

--- vars/uiVeri5ExecuteTests.groovy
+++ vars/uiVeri5ExecuteTests.groovy
@@ -1,9 +1,29 @@
+import com.sap.piper.GitUtils
+import groovy.text.GStringTemplateEngine
 import groovy.transform.Field
+
+import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/uiVeri5ExecuteTests.yaml'
 
+@Field String CONFIG_KEYS = ['testRepository']
+
 void call(Map parameters = [:]) {
+    final script = checkScript(this, parameters) ?: this
+    String stageName = parameters.stageName ?: env.STAGE_NAME
+    Map config = ConfigurationHelper.newInstance(this)
+            .loadStepDefaults([:], stageName)
+            .mixinGeneralConfig(script.commonPipelineEnvironment, CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, stageName, CONFIG_KEYS)
+            .mixin(parameters, PARAMETER_KEYS)
+            .addIfEmpty('seleniumHost', isKubernetes()?'localhost':'selenium')
+            .use()
+
+
+    parameters.config.stashContent = config.testRepository ? [GitUtils.handleTestRepository(this, parameters.config)] : utils.unstashAll(parameters.config.stashContent)
+
     List credentials = [
         [type: 'usernamePassword', id: 'seleniumHubCredentialsId', env: ['PIPER_SELENIUM_HUB_USER', 'PIPER_SELENIUM_HUB_PASSWORD']],
     ]
