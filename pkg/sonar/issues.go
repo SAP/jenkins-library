1ef844b9d349e04c4d811ee98eedcc018fdf37e3
Christopher Fenner <Christopher.Fenner@sap.com>
2021-02-02 13:43:54 +0100

    extract issue service

--- pkg/sonar/issues.go
+++ pkg/sonar/issues.go
@@ -1,15 +1,5 @@
 package sonar
 
-import (
-	"net/http"
-	"strings"
-
-	sonargo "github.com/magicsong/sonargo/sonar"
-	"github.com/pkg/errors"
-
-	"github.com/SAP/jenkins-library/pkg/log"
-)
-
 type IssuesSearchOption struct {
 	Branch       string `url:"branch,omitempty"`       // Description:"Branch key"
 	Organization string `url:"organization,omitempty"` // Description:"Organization key"
@@ -44,40 +34,6 @@ type IssuesSearchOption struct {
 	Types              string `url:"types,omitempty"`              // Description:"Comma-separated list of types.",ExampleValue:"CODE_SMELL,BUG"
 }
 
-func NewIssuesService(host, token, project, organization, branch, pullRequest string, client Sender) *IssueService {
-	// Make sure the given URL end with a slash
-	if !strings.HasSuffix(host, "/") {
-		host += "/"
-	}
-	// Make sure the given URL end with a api/
-	if !strings.HasSuffix(host, "api/") {
-		host += "api/"
-	}
-
-	log.Entry().Debugf("using api client for '%s'", host)
-
-	return &IssueService{
-		Organization: organization,
-		Project:      project,
-		Branch:       branch,
-		PullRequest:  pullRequest,
-		apiClient: &Requester{
-			Client:   client,
-			Host:     host,
-			Username: token,
-			Password: "",
-		},
-	}
-}
-
-type IssueService struct {
-	Organization string
-	Project      string
-	Branch       string
-	PullRequest  string
-	apiClient    *Requester
-}
-
 type issueSeverity string
 
 func (s issueSeverity) ToString() string {
@@ -91,71 +47,3 @@ const (
 	minor    issueSeverity = "MINOR"
 	info     issueSeverity = "INFO"
 )
-
-func (service *IssueService) GetNumberOfBlockerIssues() (int, error) {
-	return service.getIssueCount(blocker)
-}
-
-func (service *IssueService) GetNumberOfCriticalIssues() (int, error) {
-	return service.getIssueCount(critical)
-}
-
-func (service *IssueService) GetNumberOfMajorIssues() (int, error) {
-	return service.getIssueCount(major)
-}
-
-func (service *IssueService) GetNumberOfMinorIssues() (int, error) {
-	return service.getIssueCount(minor)
-}
-
-func (service *IssueService) GetNumberOfInfoIssues() (int, error) {
-	return service.getIssueCount(info)
-}
-
-func (service *IssueService) getIssueCount(severity issueSeverity) (int, error) {
-	options := &IssuesSearchOption{
-		ComponentKeys: service.Project,
-		Severities:    severity.ToString(),
-		Resolved:      "false",
-		Ps:            "1",
-	}
-	if len(service.Branch) > 0 {
-		options.Branch = service.Branch
-	}
-	if len(service.Organization) > 0 {
-		options.Organization = service.Organization
-	}
-	if len(service.PullRequest) > 0 {
-		options.PullRequest = service.PullRequest
-	}
-	result, _, err := service.SearchIssues(options)
-	if err != nil {
-		return -1, errors.Wrapf(err, "failed to fetch the numer of '%s' issues", severity)
-	}
-	return result.Total, nil
-}
-
-// SearchIssues Search for issues.<br>At most one of the following parameters can be provided at the same time: componentKeys, componentUuids, components, componentRootUuids, componentRoots.<br>Requires the 'Browse' permission on the specified project(s).
-func (service *IssueService) SearchIssues(options *IssuesSearchOption) (result *sonargo.IssuesSearchObject, response *http.Response, err error) {
-	request, err := service.apiClient.create("GET", "issues/search", options)
-	if err != nil {
-		return
-	}
-	// use custom HTTP client to send request
-	response, err = service.apiClient.send(request)
-	if err != nil {
-		return
-	}
-	// reuse response verrification from sonargo
-	err = sonargo.CheckResponse(response)
-	if err != nil {
-		return
-	}
-	// decode JSON response
-	result = new(sonargo.IssuesSearchObject)
-	err = service.apiClient.decode(response, result)
-	if err != nil {
-		return nil, response, err
-	}
-	return
-}
