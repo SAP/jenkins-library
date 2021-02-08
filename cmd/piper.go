837469917df58b484c2e1bd5e47f3a6c17f01d7e
Chris <42861202+bluesbrother84@users.noreply.github.com>
2021-02-08 14:22:13 +0100

    Adding new abap Assembly Confirm

--- cmd/piper.go
+++ cmd/piper.go
@@ -125,6 +125,7 @@ func Execute() {
 	rootCmd.AddCommand(IntegrationArtifactDeployCommand())
 	rootCmd.AddCommand(IntegrationArtifactUpdateConfigurationCommand())
 	rootCmd.AddCommand(IntegrationArtifactGetMplStatusCommand())
+	rootCmd.AddCommand(AbapEnvironmentAssembleConfirmCommand())
 
 	addRootFlags(rootCmd)
 	if err := rootCmd.Execute(); err != nil {
