8188775f5080480e3d0ece30a31629b48787e8af
Marcus Holl <marcus.holl@sap.com>
2021-02-12 14:20:30 +0100

    rfc create quickstart

---
+++ pkg/transportrequest/rfc/create.go
@@ -0,0 +1,141 @@
+package rfc
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/Jeffail/gabs/v2"
+	"github.com/SAP/jenkins-library/pkg/command"
+	"github.com/SAP/jenkins-library/pkg/config/validation"
+	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/piperutils"
+	"github.com/pkg/errors"
+	"io"
+	"strings"
+)
+
+// Exec Everything we need for interacting with a proccess
+type Exec interface {
+	command.ExecRunner
+	GetExitCode() int
+}
+
+// Create Provides all the functions we need for creating a transport request
+type Create interface {
+	WithConnection(Connection)
+	WithTransportType(string)
+	WithTargetSystemID(string)
+	WithDescription(string)
+	WithCMOpts([]string)
+	Perform(command Exec) (string, error)
+}
+
+// CreateAction The implementation for creating a transport request
+type CreateAction struct {
+	Connection     Connection
+	TransportType  string
+	TargetSystemID string
+	Description    string
+	CMOpts         []string
+}
+
+// WithConnection Set the connection details
+func (c *CreateAction) WithConnection(con Connection) {
+	c.Connection = con
+}
+
+// WithTransportType Sets the transport type
+func (c *CreateAction) WithTransportType(t string) {
+	c.TransportType = t
+}
+
+// WithTargetSystemID Sets the target system
+func (c *CreateAction) WithTargetSystemID(t string) {
+	c.TargetSystemID = t
+}
+
+// WithDescription Sets the description
+func (c *CreateAction) WithDescription(d string) {
+	c.Description = d
+}
+
+// WithCMOpts sets additional options for calling the
+// cm client tool. E.g. -D options. Useful for troubleshooting
+func (c *CreateAction) WithCMOpts(opts []string) {
+	c.CMOpts = opts
+}
+
+// Perform Creates the transport request
+func (c *CreateAction) Perform(command Exec) (string, error) {
+
+	log.Entry().Infof("Creating new transport request at '%s'", c.Connection.Endpoint)
+
+	missingParameters, err := validation.FindEmptyStringsInConfigStruct(*c)
+
+	if err == nil {
+		// Connection.Client is not used in this case
+		missingParameters, _ := piperutils.RemoveAll(missingParameters, "Connection.Client")
+		notInitialized := len(missingParameters) != 0
+		if notInitialized {
+			err = fmt.Errorf("the following parameters are not available %s", missingParameters)
+		}
+	}
+
+	var transportRequestID string
+
+	if err == nil {
+		if len(c.CMOpts) > 0 {
+			command.SetEnv(
+				[]string{
+					"ABAP_DEVELOPMENT_SERVER" + "=" + c.Connection.Endpoint,
+					"ABAP_DEVELOPMENT_USER" + "=" + c.Connection.User,
+					"ABAP_DEVELOPMENT_PASSWORD" + "=" + c.Connection.Password,
+					"TRANSPORT_DESCRIPTION" + "=" + c.Description,
+					"ABAP_DEVELOPMENT_INSTANCE" + "=" + c.Connection.Instance,
+					"ABAP_DEVELOPMENT_CLIENT" + "=" + c.Connection.Client,
+					//VERBOSE: verbose, TODO: how to handle the verbose flag?
+				},
+			)
+		}
+
+		oldStdout := command.GetStdout()
+		defer func() {
+			command.Stdout(oldStdout)
+		}()
+
+		var cmClientStdout bytes.Buffer
+		w := io.MultiWriter(&cmClientStdout, oldStdout)
+		command.Stdout(w)
+
+		err = command.RunExecutable("cts", "createTransportRequest")
+
+		if err == nil {
+			exitCode := command.GetExitCode()
+
+			if exitCode != 0 {
+				err = fmt.Errorf("Create transport request command returned with exit code '%d'", exitCode)
+			} else {
+				stdoutContent := cmClientStdout.String()
+				var c *gabs.Container
+				c, err = gabs.ParseJSON([]byte(strings.TrimSpace(stdoutContent)))
+				c = c.Path("REQUESTID")
+				if c == nil {
+					log.Entry().Warnf("Invalid response received: '%s'. Maybe unexpected format", stdoutContent)
+				} else {
+					transportRequestID = c.Data().(string)
+				}
+
+				if len(transportRequestID) == 0 {
+					err = fmt.Errorf("No transport request id received.")
+				}
+			}
+		}
+	}
+
+	if err != nil {
+		log.Entry().Warnf("Cannot create transport request at '%s': %s", c.Connection.Endpoint, err.Error())
+	} else {
+		log.Entry().Infof("Transport request '%s' has been created at '%s'", transportRequestID, c.Connection.Endpoint)
+	}
+
+	return transportRequestID, errors.Wrapf(err, "Cannot create transport request at '%s'", c.Connection.Endpoint)
+}
---
+++ pkg/transportrequest/rfc/create_test.go
@@ -0,0 +1,112 @@
+package rfc
+
+import (
+	"bytes"
+	"errors"
+	"github.com/SAP/jenkins-library/pkg/mock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateTransportRequest(t *testing.T) {
+
+	defaultCreateAction := CreateAction{
+		Connection: Connection{
+			Endpoint: "https://example.org/cts",
+			Instance: "001",
+			User:     "me",
+			Password: "******",
+		},
+		TransportType:  "X",
+		TargetSystemID: "XYZ",
+		Description:    "Lorem ipsum",
+		CMOpts:         []string{"-Dx=y", "-Dabc=123"},
+	}
+
+	t.Run("straight forward", func(t *testing.T) {
+		examinee := defaultCreateAction
+		cmd := getExecMock()
+		cmd.StdoutReturn = map[string]string{"cts.*": "{\"REQUESTID\":\"XXXK12345678\"}"}
+		transportRequestId, err := examinee.Perform(cmd)
+
+		if assert.NoError(t, err) {
+			t.Run("assert the call", func(t *testing.T) {
+				assert.Equal(t, []mock.ExecCall{
+					mock.ExecCall{
+						Exec: "cts",
+						Params: []string{
+							"createTransportRequest",
+						},
+					},
+				}, cmd.Calls)
+			})
+			t.Run("assert the transport request id", func(t *testing.T) {
+				assert.Equal(t, "XXXK12345678", transportRequestId)
+			})
+			t.Run("assert the additional environment", func(t *testing.T) {
+
+				expected := []string{
+					"ABAP_DEVELOPMENT_SERVER=https://example.org/cts",
+					"ABAP_DEVELOPMENT_USER=me",
+					"ABAP_DEVELOPMENT_PASSWORD=******",
+					"TRANSPORT_DESCRIPTION=Lorem ipsum",
+					"ABAP_DEVELOPMENT_INSTANCE=001",
+					"ABAP_DEVELOPMENT_CLIENT=",
+				}
+
+				if assert.Len(t, expected, 6) {
+					assert.Subset(t, expected, cmd.Env)
+				}
+			})
+		}
+	})
+
+	t.Run("no transport request id provided via stdout by cm client", func(t *testing.T) {
+		examinee := defaultCreateAction
+		cmd := getExecMock()
+		cmd.StdoutReturn = map[string]string{"cts.*": ""}
+		_, err := examinee.Perform(cmd)
+
+		assert.EqualError(t, err, "Cannot create transport request at 'https://example.org/cts': No transport request id received.")
+	})
+
+	t.Run("create transport request fails with rc not equal zero", func(t *testing.T) {
+		examinee := defaultCreateAction
+		cmd := getExecMock()
+		cmd.ExitCode = 1
+		_, err := examinee.Perform(cmd)
+
+		assert.EqualError(t, err, "Cannot create transport request at 'https://example.org/cts': Create transport request command returned with exit code '1'")
+	})
+
+	t.Run("create transport request fails with error", func(t *testing.T) {
+		examinee := defaultCreateAction
+		cmd := getExecMock()
+		cmd.ShouldFailOnCommand = map[string]error{"cts.*": errors.New("We have a problem")}
+		_, err := examinee.Perform(cmd)
+
+		assert.EqualError(t, err, "Cannot create transport request at 'https://example.org/cts': We have a problem")
+	})
+
+	t.Run("check missing parameters", func(t *testing.T) {
+		examinee := defaultCreateAction
+		examinee.Connection.Endpoint = ""
+		examinee.TransportType = ""
+		cmd := getExecMock()
+
+		_, err := examinee.Perform(cmd)
+
+		if assert.EqualError(t, err, "Cannot create transport request at '': the following parameters are not available [Connection.Endpoint TransportType]") {
+			t.Run("no calls", func(t *testing.T) {
+				assert.Empty(t, cmd.Calls)
+			})
+		}
+	})
+}
+
+func getExecMock() *mock.ExecMockRunner {
+	var out bytes.Buffer
+	e := &mock.ExecMockRunner{}
+	e.Stdout(&out)
+	return e
+}
