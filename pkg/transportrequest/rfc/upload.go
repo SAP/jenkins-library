5a98693b4e5dffa2e085aeaa5e6666dc35ecc2fb
Marcus Holl <marcus.holl@sap.com>
2021-02-18 14:20:57 +0100

    drop variable 'notInitialized'

--- pkg/transportrequest/rfc/upload.go
+++ pkg/transportrequest/rfc/upload.go
@@ -101,8 +101,7 @@ func (action *UploadAction) Perform(command Exec) error {
 	if err != nil {
 		return fmt.Errorf("Cannot check that all required parameters are available for SOLMAN upload: %w", err)
 	}
-	notInitialized := len(parametersWithMissingValues) != 0
-	if notInitialized {
+	if len(parametersWithMissingValues) != 0 {
 		return fmt.Errorf("Cannot perform artifact upload. The following parameters are not available %s", parametersWithMissingValues)
 	}
 
