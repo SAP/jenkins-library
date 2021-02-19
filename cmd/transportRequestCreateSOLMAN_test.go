ce55f5eb8ccf56267a577314c2bc7d88fa0a6660
Marcus Holl <marcus.holl@sap.com>
2021-02-19 15:08:40 +0100

    test transport request create solman good case

--- cmd/transportRequestCreateSOLMAN_test.go
+++ cmd/transportRequestCreateSOLMAN_test.go
@@ -18,10 +18,35 @@ func newTransportRequestCreateSOLMANTestsUtils() transportRequestCreateSOLMANMoc
 	return utils
 }
 
+type createMock struct {
+	received           solman.CreateAction
+	transportRequestID string
+}
+
+func (a *createMock) WithConnection(c solman.Connection) {
+	a.received.Connection = c
+}
+
+func (a *createMock) WithChangeDocumentID(id string) {
+	a.received.ChangeDocumentID = id
+}
+
+func (a *createMock) WithDevelopmentSystemID(id string) {
+	a.received.DevelopmentSystemID = id
+}
+
+func (a *createMock) WithCMOpts(opts []string) {
+	a.received.CMOpts = opts
+}
+
+func (a *createMock) Perform(command solman.Exec) (string, error) {
+	return a.transportRequestID, nil
+}
+
 func TestRunTransportRequestCreateSOLMAN(t *testing.T) {
 	t.Parallel()
 
-	t.Run("happy path", func(t *testing.T) {
+	t.Run("straight forward", func(t *testing.T) {
 		t.Parallel()
 		// init
 		config := transportRequestCreateSOLMANOptions{
@@ -31,17 +56,38 @@ func TestRunTransportRequestCreateSOLMAN(t *testing.T) {
 			Password:            "secret",
 			ChangeDocumentID:    "123",
 			DevelopmentSystemID: "XXX",
-			CmClientOpts: []string{"-Dabc=123", "-Dxyz=456"},
+			CmClientOpts:        []string{"-Dabc=123", "-Dxyz=456"},
 		}
 
 		utils := newTransportRequestCreateSOLMANTestsUtils()
-		create := solman.CreateAction{}
+		create := createMock{transportRequestID: "XYZK12345678"}
 		cpe := &transportRequestCreateSOLMANCommonPipelineEnvironment{}
 
 		// test
 		err := runTransportRequestCreateSOLMAN(&config, &create, nil, utils, cpe)
 
 		// assert
-		assert.NoError(t, err)
+		if assert.NoError(t, err) {
+
+			t.Run("assert received parameters", func(t *testing.T) {
+				assert.Equal(t, solman.CreateAction{
+					Connection: solman.Connection{
+						Endpoint: "https://example.org/solman",
+						User:     "me",
+						Password: "secret",
+					},
+					ChangeDocumentID:    "123",
+					DevelopmentSystemID: "XXX",
+					CMOpts: []string{
+						"-Dabc=123",
+						"-Dxyz=456",
+					},
+				}, create.received)
+			})
+
+			t.Run("assert transport request id on CPE", func(t *testing.T) {
+				assert.Equal(t, "XYZK12345678", cpe.custom.transportRequestID)
+			})
+		}
 	})
 }
