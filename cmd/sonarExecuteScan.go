cb3fa7c293f9d38f63cb58a263291da9cd1df97c
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2021-02-24 15:44:23 +0100

    feat(sonar): add output of measurements (#2218)
    * add sonarqube measurements
    
    * fetch measurements from API
    
    * add api for fetching issue counts
    
    * add debug outputs
    
    * add further severities
    
    * log number of issues
    
    * report failure
    
    * expose method to send request
    
    * Fixed what was broken.
    
    * add debug output
    
    * wip
    
    * correct opaque property
    
    * push client handling to apiClient.go
    
    * use correct API URL
    
    * correct log outputs
    
    * remove logging
    
    * remove option validation
    
    * extend search options
    
    * restructure
    
    * rename api client file
    
    * simplify client usage
    
    * simplify issue client
    
    * write sonar values to influx
    
    * extract issue service
    
    * reorder imports
    
    * add sonar integration test
    
    * allow unknown fields
    
    * add test case
    
    * add test case
    
    * remove
    
    * fix
    
    * Update http.go
    
    * Apply suggestions from code review
    
    * Update cmd/sonarExecuteScan.go
    
    * rework test cases
    
    * use explicit returns
    
    * add task service
    
    * add waitfortask
    
    * fix typo
    
    * remove fixme
    
    * expose poll interval
    
    * rename test cases
    
    * add test cases
    
    * use newAPIClient method
    
    * use waitForTask
    
    * rename services
    
    * finalize code
    
    * handle error
    
    * move defer
    
    * move types
    
    * add test case
    
    * use http.status...
    
    * add test case
    
    * expose api endpoint names
    
    * extract api client
    
    * adjust test cases
    
    * Update integration-tests-pr.yaml
    
    * Update integration-tests.yaml
    
    * improve require message
    
    * Update integration-tests-pr.yaml
    
    * Update integration-tests-pr.yaml

--- cmd/sonarExecuteScan.go
+++ cmd/sonarExecuteScan.go
@@ -10,6 +10,9 @@ import (
 	"strings"
 	"time"
 
+	"github.com/bmatcuk/doublestar"
+	"github.com/pkg/errors"
+
 	"github.com/SAP/jenkins-library/pkg/command"
 	piperhttp "github.com/SAP/jenkins-library/pkg/http"
 	"github.com/SAP/jenkins-library/pkg/log"
@@ -18,8 +21,6 @@ import (
 	StepResults "github.com/SAP/jenkins-library/pkg/piperutils"
 	SonarUtils "github.com/SAP/jenkins-library/pkg/sonar"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
-	"github.com/bmatcuk/doublestar"
-	"github.com/pkg/errors"
 )
 
 type sonarSettings struct {
@@ -76,9 +77,13 @@ func sonarExecuteScan(config sonarExecuteScanOptions, _ *telemetry.CustomData, i
 	// reroute command output to logging framework
 	runner.Stdout(log.Writer())
 	runner.Stderr(log.Writer())
-
-	client := piperhttp.Client{}
-	client.SetOptions(piperhttp.ClientOptions{TransportTimeout: 20 * time.Second})
+	// client for downloading the sonar-scanner
+	downloadClient := &piperhttp.Client{}
+	downloadClient.SetOptions(piperhttp.ClientOptions{TransportTimeout: 20 * time.Second})
+	// client for talking to the SonarQube API
+	apiClient := &piperhttp.Client{}
+	//TODO: implement certificate handling
+	apiClient.SetOptions(piperhttp.ClientOptions{TransportSkipVerification: true})
 
 	sonar = sonarSettings{
 		workingDir:  "./",
@@ -88,7 +93,7 @@ func sonarExecuteScan(config sonarExecuteScanOptions, _ *telemetry.CustomData, i
 	}
 
 	influx.step_data.fields.sonar = false
-	if err := runSonar(config, &client, &runner); err != nil {
+	if err := runSonar(config, downloadClient, &runner, apiClient, influx); err != nil {
 		if log.GetErrorCategory() == log.ErrorUndefined && runner.GetExitCode() == 2 {
 			// see https://github.com/SonarSource/sonar-scanner-cli/blob/adb67d645c3bcb9b46f29dea06ba082ebec9ba7a/src/main/java/org/sonarsource/scanner/cli/Exit.java#L25
 			log.SetErrorCategory(log.ErrorConfiguration)
@@ -98,7 +103,7 @@ func sonarExecuteScan(config sonarExecuteScanOptions, _ *telemetry.CustomData, i
 	influx.step_data.fields.sonar = true
 }
 
-func runSonar(config sonarExecuteScanOptions, client piperhttp.Downloader, runner command.ExecRunner) error {
+func runSonar(config sonarExecuteScanOptions, client piperhttp.Downloader, runner command.ExecRunner, apiClient SonarUtils.Sender, influx *sonarExecuteScanInflux) error {
 	if len(config.ServerURL) > 0 {
 		sonar.addEnvironment("SONAR_HOST_URL=" + config.ServerURL)
 	}
@@ -168,6 +173,36 @@ func runSonar(config sonarExecuteScanOptions, client piperhttp.Downloader, runne
 		}
 		StepResults.PersistReportsAndLinks("sonarExecuteScan", sonar.workingDir, nil, links)
 	}
+
+	taskService := SonarUtils.NewTaskService(taskReport.ServerURL, config.Token, taskReport.TaskID, apiClient)
+	// wait for analysis task to complete
+	err = taskService.WaitForTask()
+	if err != nil {
+		return err
+	}
+	// fetch number of issues by severity
+	issueService := SonarUtils.NewIssuesService(taskReport.ServerURL, config.Token, taskReport.ProjectKey, config.Organization, config.BranchName, config.ChangeID, apiClient)
+	influx.sonarqube_data.fields.blocker_issues, err = issueService.GetNumberOfBlockerIssues()
+	if err != nil {
+		return err
+	}
+	influx.sonarqube_data.fields.critical_issues, err = issueService.GetNumberOfCriticalIssues()
+	if err != nil {
+		return err
+	}
+	influx.sonarqube_data.fields.major_issues, err = issueService.GetNumberOfMajorIssues()
+	if err != nil {
+		return err
+	}
+	influx.sonarqube_data.fields.minor_issues, err = issueService.GetNumberOfMinorIssues()
+	if err != nil {
+		return err
+	}
+	influx.sonarqube_data.fields.info_issues, err = issueService.GetNumberOfInfoIssues()
+	if err != nil {
+		return err
+	}
+	log.Entry().Debugf("Influx values: %v", influx.sonarqube_data.fields)
 	return nil
 }
 
