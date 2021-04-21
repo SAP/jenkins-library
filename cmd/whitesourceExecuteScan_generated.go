f6009aa144c82a32dc327f1fe3daf6f04e0f9aa3
Pavel Krasnousov <pavel.krasnousov@sap.com>
2021-04-21 13:43:49 +0300

    Add default for "dir" option

--- cmd/whitesourceExecuteScan_generated.go
+++ cmd/whitesourceExecuteScan_generated.go
@@ -218,7 +218,7 @@ func addWhitesourceExecuteScanFlags(cmd *cobra.Command, stepConfig *whitesourceE
 	cmd.Flags().BoolVar(&stepConfig.CreateProductFromPipeline, "createProductFromPipeline", true, "Whether to create the related WhiteSource product on the fly based on the supplied pipeline configuration.")
 	cmd.Flags().StringVar(&stepConfig.CustomScanVersion, "customScanVersion", os.Getenv("PIPER_customScanVersion"), "Custom version of the WhiteSource project used as source.")
 	cmd.Flags().StringVar(&stepConfig.CvssSeverityLimit, "cvssSeverityLimit", `-1`, "Limit of tolerable CVSS v3 score upon assessment and in consequence fails the build.")
-	cmd.Flags().StringVar(&stepConfig.Dir, "dir", os.Getenv("PIPER_dir"), "Directory where start to scan.")
+	cmd.Flags().StringVar(&stepConfig.Dir, "dir", `.`, "Directory where start to scan.")
 	cmd.Flags().StringSliceVar(&stepConfig.EmailAddressesOfInitialProductAdmins, "emailAddressesOfInitialProductAdmins", []string{}, "The list of email addresses to assign as product admins for newly created WhiteSource products.")
 	cmd.Flags().StringSliceVar(&stepConfig.Excludes, "excludes", []string{}, "List of file path patterns to exclude in the scan.")
 	cmd.Flags().StringSliceVar(&stepConfig.Includes, "includes", []string{}, "List of file path patterns to include in the scan.")
