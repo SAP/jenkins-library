fd2610c8cb7fb144e57d048ece6c012c6a68f71a
CCFenner <christopher.fenner@sap.com>
2021-04-23 10:21:50 +0200

    extract versioning model to own package

--- pkg/versioning/descriptorUtils.go
+++ pkg/versioning/descriptorUtils.go
@@ -7,17 +7,6 @@ import (
 	"github.com/SAP/jenkins-library/pkg/piperutils"
 )
 
-const (
-	// SchemeMajorVersion is the versioning scheme based on the major version only
-	SchemeMajorVersion = `{{(split "." (split "-" .Version)._0)._0}}`
-	// SchemeMajorMinorVersion is the versioning scheme based on the major version only
-	SchemeMajorMinorVersion = `{{(split "." (split "-" .Version)._0)._0}}.{{(split "." (split "-" .Version)._0)._1}}`
-	// SchemeSemanticVersion is the versioning scheme based on the major.minor.micro version
-	SchemeSemanticVersion = `{{(split "." (split "-" .Version)._0)._0}}.{{(split "." (split "-" .Version)._0)._1}}.{{(split "." (split "-" .Version)._0)._2}}`
-	// SchemeFullVersion is the versioning scheme based on the full version
-	SchemeFullVersion = "{{.Version}}"
-)
-
 // DetermineProjectCoordinatesWithCustomVersion resolves the coordinates of the project for use in 3rd party scan tools
 // It considers a custom version if provided instead of using the GAV version adapted according to the versionScheme
 func DetermineProjectCoordinatesWithCustomVersion(nameTemplate, versionScheme, customVersion string, gav Coordinates) (string, string) {
@@ -36,21 +25,7 @@ func DetermineProjectCoordinates(nameTemplate, versionScheme string, gav Coordin
 		log.Entry().Warnf("Unable to resolve project name: %v", err)
 	}
 
-	var versionTemplate string
-	if versionScheme == "full" {
-		versionTemplate = SchemeFullVersion
-	}
-	if versionScheme == "major" {
-		versionTemplate = SchemeMajorVersion
-	}
-	if versionScheme == "major-minor" {
-		versionTemplate = SchemeMajorMinorVersion
-	}
-	if versionScheme == "semantic" {
-		versionTemplate = SchemeSemanticVersion
-	}
-
-	projectVersion, err := piperutils.ExecuteTemplateFunctions(versionTemplate, sprig.HermeticTxtFuncMap(), gav)
+	projectVersion, err := ApplyVersioningModel(versionScheme, gav)
 	if err != nil {
 		log.Entry().Warnf("Unable to resolve project version: %v", err)
 	}
---
+++ pkg/versioning/versioningModel.go
@@ -0,0 +1,44 @@
+package versioning
+
+import (
+	"github.com/Masterminds/sprig"
+	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/piperutils"
+)
+
+const (
+	// SchemeMajorVersion is the versioning scheme based on the major version only
+	SchemeMajorVersion = `{{(split "." (split "-" .Version)._0)._0}}`
+	// SchemeMajorMinorVersion is the versioning scheme based on the major version only
+	SchemeMajorMinorVersion = `{{(split "." (split "-" .Version)._0)._0}}.{{(split "." (split "-" .Version)._0)._1}}`
+	// SchemeSemanticVersion is the versioning scheme based on the major.minor.micro version
+	SchemeSemanticVersion = `{{(split "." (split "-" .Version)._0)._0}}.{{(split "." (split "-" .Version)._0)._1}}.{{(split "." (split "-" .Version)._0)._2}}`
+	// SchemeFullVersion is the versioning scheme based on the full version
+	SchemeFullVersion = "{{.Version}}"
+)
+
+const (
+	VersioningModelFull       string = "full"
+	VersioningModelSemantic   string = "semantic"
+	VersioningModelMajorMinor string = "major-minor"
+	VersioningModelMajor      string = "major"
+)
+
+func ApplyVersioningModel(model string, version Coordinates) (string, error) {
+	var versioningScheme string
+
+	switch model {
+	case VersioningModelFull:
+		versioningScheme = SchemeFullVersion
+	case VersioningModelSemantic:
+		versioningScheme = SchemeSemanticVersion
+	case VersioningModelMajorMinor:
+		versioningScheme = SchemeMajorMinorVersion
+	case VersioningModelMajor:
+		versioningScheme = SchemeMajorVersion
+	default:
+		log.Entry().Warnf("versioning model not supported: %s", model)
+	}
+
+	return piperutils.ExecuteTemplateFunctions(versioningScheme, sprig.HermeticTxtFuncMap(), version)
+}
