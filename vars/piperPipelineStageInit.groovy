d11a108cbf3b758979da72e5484288e4cab96fed
Kondreddy <srinikitha.kondreddy@sap.com>
2021-04-28 08:54:03 +0200

    Add code review changes

--- vars/piperPipelineStageInit.groovy
+++ vars/piperPipelineStageInit.groovy
@@ -44,9 +44,9 @@ import static com.sap.piper.Prerequisites.checkScript
     'projectName',
     /**
      * Specify to execute artifact versioning in a kubernetes pod.
-     * @possibleValues `true` , `false`
+     * @possibleValues `true`, `false`
      */
-    'runOnPod',
+    'runArtifactVersioningOnPod',
     /**
      *  Defines the library resource containing stage/step initialization settings. Those define conditions when certain steps/stages will be activated. **Caution: changing the default will break the standard behavior of the pipeline - thus only relevant when including `Init` stage into custom pipelines!**
      */
@@ -206,15 +206,13 @@ void call(Map parameters = [:]) {
             if (config.inferBuildTool) {
                 prepareVersionParams.buildTool = buildTool
             }
-            if (env.ON_K8S) {
-                if(!config.runOnPod){
+            if (env.ON_K8S && !config.runArtifactVersioningOnPod) {
                     // We force dockerImage: "" for the K8S case to avoid the execution of artifactPrepareVersion in a K8S Pod.
                     // Since artifactPrepareVersion may need the ".git" folder in order to push a tag, it would need to be part of the stashing.
                     // There are however problems with tar-ing this folder, which results in a failure to copy the stash back -- without a failure of the pipeline.
                     // This then also has the effect that any changes made to the build descriptors by the step (updated version) are not visible in the relevant stashes.
                     // In addition, a mvn executable is available on the Jenkins instance which can be used directly instead of executing the command in a container.
                     prepareVersionParams.dockerImage = ""
-                }
             }
             artifactPrepareVersion prepareVersionParams
         }
