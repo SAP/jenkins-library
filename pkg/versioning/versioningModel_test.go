3a14a91ae506b09dcf704a65d3f378c2e67c83b9
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2021-05-14 09:35:31 +0200

    refactor(versioning): simplify versioning model method (#2825)
    * rename artifactVersion to version
    
    * simplify versioningModel

--- pkg/versioning/versioningModel_test.go
+++ pkg/versioning/versioningModel_test.go
@@ -7,23 +7,23 @@ import (
 func Test_applyVersioningModel(t *testing.T) {
 	type args struct {
 		model   string
-		version Coordinates
+		version string
 	}
 	tests := []struct {
 		name string
 		args args
 		want string
 	}{
-		{"maven - major", args{VersioningModelMajor, Coordinates{Version: "1.2.3-7864387648746"}}, "1"},
-		{"maven - major-minor", args{VersioningModelMajorMinor, Coordinates{Version: "1.2.3-7864387648746"}}, "1.2"},
-		{"maven - semantic", args{VersioningModelSemantic, Coordinates{Version: "1.2.3-7864387648746"}}, "1.2.3"},
-		{"maven - full", args{VersioningModelFull, Coordinates{Version: "1.2.3-7864387648746"}}, "1.2.3-7864387648746"},
-		{"python - major-minor", args{VersioningModelMajorMinor, Coordinates{Version: "2.2.3.20200101"}}, "2.2"},
-		{"leading zero", args{VersioningModelMajor, Coordinates{Version: "0.0.1"}}, "0"},
-		{"trailing zero", args{VersioningModelMajorMinor, Coordinates{Version: "2.0"}}, "2.0"},
-		{"invalid - unknown versioning model", args{"snapshot", Coordinates{Version: "1.2.3-SNAPSHOT"}}, ""},
-		{"invalid - incorrect version", args{VersioningModelMajor, Coordinates{Version: ".2.3"}}, ""},
-		{"invalid - version to short", args{VersioningModelSemantic, Coordinates{Version: "1.2"}}, "1.2.<no value>"},
+		{"maven - major", args{VersioningModelMajor, "1.2.3-7864387648746"}, "1"},
+		{"maven - major-minor", args{VersioningModelMajorMinor, "1.2.3-7864387648746"}, "1.2"},
+		{"maven - semantic", args{VersioningModelSemantic, "1.2.3-7864387648746"}, "1.2.3"},
+		{"maven - full", args{VersioningModelFull, "1.2.3-7864387648746"}, "1.2.3-7864387648746"},
+		{"python - major-minor", args{VersioningModelMajorMinor, "2.2.3.20200101"}, "2.2"},
+		{"leading zero", args{VersioningModelMajor, "0.0.1"}, "0"},
+		{"trailing zero", args{VersioningModelMajorMinor, "2.0"}, "2.0"},
+		{"invalid - unknown versioning model", args{"snapshot", "1.2.3-SNAPSHOT"}, ""},
+		{"invalid - incorrect version", args{VersioningModelMajor, ".2.3"}, ""},
+		{"invalid - version to short", args{VersioningModelSemantic, "1.2"}, "1.2.<no value>"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
