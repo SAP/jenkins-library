40c32598a4bb3979ec6b28bf076a4f29fb079ef6
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2021-05-28 11:12:42 +0200

    feat(jenkins): update jenkins go dependency (#2848)
    * update dependency
    
    * use context
    
    * regenerate mock
    
    * decapitalize error messages
    
    * use getBuildFromQueueID
    
    * add comments about mockery
    
    * update mocks
    
    * update tests
    
    * add context parameter
    
    * pull up context
    
    * update mocks
    
    * update tests

--- pkg/jenkins/jenkins_test.go
+++ pkg/jenkins/jenkins_test.go
@@ -1,17 +1,19 @@
 package jenkins
 
 import (
+	"context"
 	"fmt"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/SAP/jenkins-library/pkg/jenkins/mocks"
+	"github.com/bndr/gojenkins"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 func TestTriggerJob(t *testing.T) {
+	ctx := context.Background()
 	jobName := "ContinuousDelivery/piper-library"
 	jobID := strings.ReplaceAll(jobName, "/", "/job/")
 	jobParameters := map[string]string{}
@@ -21,28 +23,28 @@ func TestTriggerJob(t *testing.T) {
 		queueID := int64(0)
 		jenkins := &mocks.Jenkins{}
 		jenkins.
-			On("BuildJob", jobID, map[string]string{}).
+			On("BuildJob", ctx, jobID, map[string]string{}).
 			Return(queueID, fmt.Errorf(mock.Anything))
 		// test
-		task, err := TriggerJob(jenkins, jobName, jobParameters)
+		build, err := TriggerJob(ctx, jenkins, jobName, jobParameters)
 		// asserts
 		jenkins.AssertExpectations(t)
 		assert.EqualError(t, err, mock.Anything)
-		assert.Nil(t, task)
+		assert.Nil(t, build)
 	})
 	t.Run("error - task already queued", func(t *testing.T) {
 		// init
 		queueID := int64(0)
 		jenkins := &mocks.Jenkins{}
 		jenkins.
-			On("BuildJob", jobID, map[string]string{}).
+			On("BuildJob", ctx, jobID, map[string]string{}).
 			Return(queueID, nil)
 		// test
-		task, err := TriggerJob(jenkins, jobName, jobParameters)
+		build, err := TriggerJob(ctx, jenkins, jobName, jobParameters)
 		// asserts
 		jenkins.AssertExpectations(t)
-		assert.EqualError(t, err, "Unable to queue build")
-		assert.Nil(t, task)
+		assert.EqualError(t, err, "unable to queue build")
+		assert.Nil(t, build)
 	})
 	t.Run("error - task not queued", func(t *testing.T) {
 		// init
@@ -50,38 +52,32 @@ func TestTriggerJob(t *testing.T) {
 		jenkins := &mocks.Jenkins{}
 		jenkins.Test(t)
 		jenkins.
-			On("BuildJob", jobID, map[string]string{}).
+			On("BuildJob", ctx, jobID, map[string]string{}).
 			Return(queueID, nil).
-			On("GetQueueItem", queueID).
+			On("GetBuildFromQueueID", ctx, queueID).
 			Return(nil, fmt.Errorf(mock.Anything))
 		// test
-		task, err := TriggerJob(jenkins, jobName, jobParameters)
+		build, err := TriggerJob(ctx, jenkins, jobName, jobParameters)
 		// asserts
 		jenkins.AssertExpectations(t)
 		assert.EqualError(t, err, mock.Anything)
-		assert.Nil(t, task)
-	})
-}
-
-func TestWaitForBuildToStart(t *testing.T) {
-	jobName := "ContinuousDelivery/piper-library"
-	jobID := strings.ReplaceAll(jobName, "/", "/job/")
-
-	t.Run("error - build not started", func(t *testing.T) {
-		// init
-		buildNumber := int64(43)
-		task := &mocks.Task{}
-		task.On("WaitToStart", time.Millisecond).Return(buildNumber, nil)
-		jenkins := &mocks.Jenkins{}
-		jenkins.
-			On("GetBuild", jobID, buildNumber).
-			Return(nil, fmt.Errorf("Build not started"))
-		// test
-		build, err := WaitForBuildToStart(jenkins, jobName, task, time.Millisecond)
-		// asserts
-		task.AssertExpectations(t)
-		jenkins.AssertExpectations(t)
-		assert.EqualError(t, err, "Build not started")
 		assert.Nil(t, build)
+	})
+	t.Run("success", func(t *testing.T) {
+		// init
+		queueID := int64(43)
+		jenkins := &mocks.Jenkins{}
+		jenkins.Test(t)
+		jenkins.
+			On("BuildJob", ctx, jobID, map[string]string{}).
+			Return(queueID, nil).
+			On("GetBuildFromQueueID", ctx, queueID).
+			Return(&gojenkins.Build{}, nil)
+		// test
+		build, err := TriggerJob(ctx, jenkins, jobName, jobParameters)
+		// asserts
+		jenkins.AssertExpectations(t)
+		assert.NoError(t, err)
+		assert.NotNil(t, build)
 	})
 }
