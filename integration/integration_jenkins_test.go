c4d9b0d615cbdc920beb58170272a39f0e4c833e
CCFenner <christopher.fenner@sap.com>
2021-06-02 16:17:15 +0200

    update integration test case

--- integration/integration_jenkins_test.go
+++ integration/integration_jenkins_test.go
@@ -1,4 +1,3 @@
-// +build integration
 // can be execute with go test -tags=integration ./integration/...
 
 package main
@@ -22,22 +21,29 @@ func TestTriggerJob(t *testing.T) {
 	ctx := context.Background()
 	// ctx = context.WithValue(ctx, "debug", true)
 
+	// os.Setenv("PIPER_INTEGRATION_JENKINS_USER_NAME", "")
+	// os.Setenv("PIPER_INTEGRATION_JENKINS_TOKEN", "")
+	// os.Setenv("PIPER_INTEGRATION_JENKINS_HOST", "")
+	// os.Setenv("PIPER_INTEGRATION_JENKINS_JOB_NAME", "")
+
 	host := os.Getenv("PIPER_INTEGRATION_JENKINS_HOST")
 	user := os.Getenv("PIPER_INTEGRATION_JENKINS_USER_NAME")
 	token := os.Getenv("PIPER_INTEGRATION_JENKINS_TOKEN")
-	job := os.Getenv("PIPER_INTEGRATION_JENKINS_JOB_NAME")
+	jobName := os.Getenv("PIPER_INTEGRATION_JENKINS_JOB_NAME")
 	require.NotEmpty(t, host, "Jenkins host url is missing")
 	require.NotEmpty(t, user, "Jenkins user name is missing")
 	require.NotEmpty(t, token, "Jenkins token is missing")
-	require.NotEmpty(t, job, "Jenkins job name is missing")
+	require.NotEmpty(t, jobName, "Jenkins job name is missing")
 
 	jenx, err := jenkins.Instance(ctx, http.DefaultClient, host, user, token)
 	require.NotNil(t, jenx, "could not connect to Jenkins instance")
 	require.NoError(t, err)
 	// test
-	build, err := jenkins.TriggerJob(ctx, jenx, job, nil)
+	job, getJobErr := jenkins.GetJob(ctx, jenx, jobName)
+	build, triggerJobErr := jenkins.TriggerJob(ctx, jenx, job, nil)
 	// asserts
-	assert.NoError(t, err)
+	assert.NoError(t, getJobErr)
+	assert.NoError(t, triggerJobErr)
 	assert.NotNil(t, build)
 	assert.True(t, build.IsRunning(ctx))
 }
