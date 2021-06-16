39df5a0ed8be0e0ced0029a96f7433490053d1bf
Sven Merk <33895725+nevskrem@users.noreply.github.com>
2021-06-16 23:19:37 +0200

    Update protecodeExecuteScan.go

--- cmd/protecodeExecuteScan.go
+++ cmd/protecodeExecuteScan.go
@@ -67,7 +67,12 @@ func runProtecodeScan(config *protecodeExecuteScanOptions, influx *protecodeExec
 		}
 	} else if len(config.FilePath) > 0 {
 		parts := strings.Split(config.FilePath, "/")
-		(*config).FilePath = strings.Join(parts[:len(parts)-1], "/")
+		pathFragment := strings.Join(parts[:len(parts)-1], "/")
+		if len(pathFragment > 0 {
+			(*config).FilePath = pathFragment
+		} else {
+		        (*config).FilePath = "./"
+		}
 		fileName = parts[len(parts)-1]
 	}
 
