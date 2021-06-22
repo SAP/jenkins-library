22d3520b22f498522e2370c1b136cc6cbb2d8a9c
Sven Merk <s.merk@sap.com>
2021-06-22 10:08:50 +0200

    Replace io.Copy

---
+++ pkg/piperutils/ioUtils.go
@@ -0,0 +1,38 @@
+package piperutils
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/pkg/errors"
+)
+
+func CopyData(dst io.Writer, src io.Reader) (int64, error) {
+	tmp := make([]byte, 256)
+	bytesRead := int64(0)
+	bytesWritten := int64(0)
+	done := false
+	
+	for {
+		n, err := src.Read(tmp)
+		bytesRead += int64(n)
+		if err != nil {
+			if err != io.EOF {
+				return bytesRead, errors.Wrap(err, "read error")
+			}
+			done = true
+		}
+		n, err = dst.Write(tmp[:n])
+		bytesWritten += int64(n)
+		if err != nil {
+			return bytesWritten, errors.Wrap(err, "write error")
+		}
+		if done {
+			break
+		}
+	}
+	if bytesRead != bytesWritten {
+		return bytesRead, errors.New(fmt.Sprintf("transfer error: read %v bytes but wrote %v bytes", bytesRead, bytesWritten))
+	}
+	return bytesWritten, nil
+}
---
+++ pkg/piperutils/ioUtils_test.go
@@ -0,0 +1,57 @@
+package piperutils
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCopyData(t *testing.T) {
+	runInTempDir(t, "copying file succeeds", "dir1", func(t *testing.T) {
+		srcName := "testFile"
+		src, err := os.OpenFile(srcName, os.O_CREATE, 0700)
+		if err != nil {
+			t.Fatal("Failed to create src file")
+		}
+		_, err = src.Write([]byte{byte(1), byte(2), byte(3)})
+		src.Close()
+		src, err = os.OpenFile(srcName, os.O_CREATE, 0700)
+
+		dstName := "testFile2"
+		dst, err := os.OpenFile(dstName, os.O_CREATE, 0700)
+		if err != nil {
+			t.Fatal("Failed to create dst file")
+		}
+
+		result, err := CopyData(dst, src)
+
+		assert.NoError(t, err, "Didn't expert error but got one")
+		assert.Equal(t, int64(3), result, "Expected true but got false")
+	})
+	runInTempDir(t, "copying file succeeds", "dir2", func(t *testing.T) {
+		srcName := "testFile"
+		src, err := os.OpenFile(srcName, os.O_CREATE, 0700)
+		if err != nil {
+			t.Fatal("Failed to create src file")
+		}
+		data := make([]byte, 300)
+		for i := 0; i < 300; i++ {
+			data[i] = byte(i)
+		}
+		_, err = src.Write(data)
+		src.Close()
+		src, err = os.OpenFile(srcName, os.O_CREATE, 0700)
+
+		dstName := "testFile2"
+		dst, err := os.OpenFile(dstName, os.O_CREATE, 0700)
+		if err != nil {
+			t.Fatal("Failed to create dst file")
+		}
+
+		result, err := CopyData(dst, src)
+
+		assert.NoError(t, err, "Didn't expert error but got one")
+		assert.Equal(t, int64(300), result, "Expected true but got false")
+	})
+}
