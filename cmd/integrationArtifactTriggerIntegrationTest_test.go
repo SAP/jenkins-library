0285f7f7a5f519d01e9ff1015da946e5060d5320
Oliver Feldmann <oliver.feldmann@sap.com>
2021-06-29 10:13:09 +0200

    remove unused unit tests

--- cmd/integrationArtifactTriggerIntegrationTest_test.go
+++ cmd/integrationArtifactTriggerIntegrationTest_test.go
@@ -24,30 +24,6 @@ func newIntegrationArtifactTriggerIntegrationTestTestsUtils() integrationArtifac
 }
 
 func TestRunIntegrationArtifactTriggerIntegrationTest(t *testing.T) {
-	//t.Parallel()
-	//t.Run("happy path", func(t *testing.T) {
-	//	t.Parallel()
-	//	// init
-	//	config := integrationArtifactTriggerIntegrationTestOptions{
-	//		Host:                  "https://demo",
-	//		OAuthTokenProviderURL: "https://demo/oauth/token",
-	//		Username:              "demouser",
-	//		Password:              "******",
-	//		IntegrationFlowID:     "CPI_IFlow_Call_using_Cert",
-	//		Platform:              "cf",
-	//	}
-	//
-	//	utils := newintegrationArtifactTriggerIntegrationTestTestsUtils()
-	//	utils.AddFile("file.txt", []byte("dummy content"))
-	//	httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetServiceEndpoint", ResponseBody: ``, TestType: "PositiveAndGetetIntegrationArtifactGetServiceResBody"}
-	//
-	//	// test
-	//	err := runintegrationArtifactTriggerIntegrationTest(&config, nil, utils, &httpClient)
-	//
-	//	// assert
-	//	assert.NoError(t, err)
-	//})
-
 	t.Run("MessageBodyPath good but no ContentType (ERROR) callIFlowURL", func(t *testing.T) {
 		//init
 		iFlowServiceKey := `{
@@ -201,18 +177,4 @@ func TestRunIntegrationArtifactTriggerIntegrationTest(t *testing.T) {
 		//assert
 		assert.NoError(t, err)
 	})
-
-	// t.Run("error path", func(t *testing.T) {
-	// 	t.Parallel()
-	// 	// init
-	// 	config := integrationArtifactTriggerIntegrationTestOptions{}
-
-	// 	utils := newintegrationArtifactTriggerIntegrationTestTestsUtils()
-
-	// 	// test
-	// 	err := runintegrationArtifactTriggerIntegrationTest(&config, nil, utils)
-
-	// 	// assert
-	// 	assert.EqualError(t, err, "cannot run without important file")
-	// })
 }
