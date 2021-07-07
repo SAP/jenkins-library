fea0599ff41521c6a897b7dfcaf21bc80eaf2dd4
Your Name <you@example.com>
2021-07-07 13:02:33 +0200

    remving $

--- cmd/mavenBuild.go
+++ cmd/mavenBuild.go
@@ -140,14 +140,14 @@ func createOrUpdateProjectSettingsXML(projectSettingsFile string, altDeploymentR
 }
 
 func loadRemoteRepoCertificates(certificateList []string, client piperhttp.Downloader, flags *[]string, runner command.ExecRunner, fileUtils piperutils.FileUtils) error {
-	/* if err := fileUtils.Chdir(os.Getenv("$JAVA_HOME")); err != nil {
+	if err := fileUtils.Chdir(os.Getenv("JAVA_HOME")); err != nil {
 		return errors.Wrap(err, "Could not find the java home environment variable ")
 	} else {
 		javaHomePath := getWorkingDirForTrustStore()
 		log.Entry().Infof("current location is %s", javaHomePath)
-	} */
+	}
 
-	if err := fileUtils.Chdir(os.Getenv("$JRE_HOME")); err != nil {
+	if err := fileUtils.Chdir(os.Getenv("JRE_HOME")); err != nil {
 		return errors.Wrap(err, "Could not find the jre home environment variable ")
 	} else {
 		javaJrePath := getWorkingDirForTrustStore()
