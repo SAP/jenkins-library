72a4ef16ab0ee29fe5ba09be289199488d77b329
Mayur Belur Mohan <68264167+mayurmohan@users.noreply.github.com>
2021-07-08 15:21:31 +0530

    Integration Artifact command Fixes (#2974)
    * Add DISCARDED state in query filter
    
    * fix formating
    
    * fomating fixes
    
    * codereview fixes
    
    * Code Review Fixes
    
    * formatting fixes
    
    Co-authored-by: Oliver Feldmann <oliver.feldmann@sap.com>

--- cmd/integrationArtifactGetMplStatus_test.go
+++ cmd/integrationArtifactGetMplStatus_test.go
@@ -45,7 +45,7 @@ func TestRunIntegrationArtifactGetMplStatus(t *testing.T) {
 			assert.EqualValues(t, seOut.custom.iFlowMplStatus, "COMPLETED")
 
 			t.Run("check url", func(t *testing.T) {
-				assert.Equal(t, "https://demo/api/v1/MessageProcessingLogs?$filter=IntegrationArtifact/Id+eq+'flow1'&$orderby=LogEnd+desc&$top=1", httpClient.URL)
+				assert.Equal(t, "https://demo/api/v1/MessageProcessingLogs?$filter=IntegrationArtifact/Id+eq+'flow1'+and+Status+ne+'DISCARDED'&$orderby=LogEnd+desc&$top=1", httpClient.URL)
 			})
 
 			t.Run("check method", func(t *testing.T) {
@@ -75,7 +75,7 @@ func TestRunIntegrationArtifactGetMplStatus(t *testing.T) {
 		err := runIntegrationArtifactGetMplStatus(&config, nil, &httpClient, &seOut)
 		assert.EqualValues(t, seOut.custom.iFlowMplStatus, "")
 		assert.EqualError(t, err, "HTTP GET request to https://demo/api/v1/MessageProcessingLogs?$filter=IntegrationArtifact/"+
-			"Id+eq+'flow1'&$orderby=LogEnd+desc&$top=1 failed with error: "+
+			"Id+eq+'flow1'+and+Status+ne+'DISCARDED'&$orderby=LogEnd+desc&$top=1 failed with error: "+
 			"Unable to get integration flow MPL status, Response Status code:400")
 	})
 }
