fc7fd7c6d8caaddecd4aa3bf9157c296d72c80da
Marc Bormeth <marc.bormeth@sap.com>
2021-07-12 08:21:45 +0200

    Remove PIPER_SONAR_LOAD_CERTIFICATES check

--- cmd/sonarExecuteScan.go
+++ cmd/sonarExecuteScan.go
@@ -362,10 +362,7 @@ func loadCertificates(certificateList []string, client piperhttp.Downloader, run
 		// use local existing trust store
 		sonar.addEnvironment("SONAR_SCANNER_OPTS=-Djavax.net.ssl.trustStore=" + trustStoreFile + " -Djavax.net.ssl.trustStorePassword=changeit")
 		log.Entry().WithField("trust store", trustStoreFile).Info("Using local trust store")
-	} else
-	//TODO: certificate loading is deactivated due to the missing JAVA keytool
-	// see https://github.com/SAP/jenkins-library/issues/1072
-	if os.Getenv("PIPER_SONAR_LOAD_CERTIFICATES") == "true" && len(certificateList) > 0 {
+	} else if len(certificateList) > 0 {
 		// use local created trust store with downloaded certificates
 		keytoolOptions := []string{
 			"-import",
