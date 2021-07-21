23fb3ca9028faec7c10b243d7634d02080b1191f
mayurmohan <68264167+mayurmohan@users.noreply.github.com>
2021-07-21 15:39:51 +0530

    formatting fixes

--- cmd/integrationArtifactGetMplStatus_test.go
+++ cmd/integrationArtifactGetMplStatus_test.go
@@ -83,7 +83,7 @@ func TestRunIntegrationArtifactGetMplStatus(t *testing.T) {
 	t.Run(" Integration flow message processing get Error message test", func(t *testing.T) {
 		clientOptions := piperhttp.ClientOptions{}
 		clientOptions.Token = fmt.Sprintf("Bearer %s", "Demo")
-		httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetMplStatusError",Options: clientOptions, ResponseBody: ``, TestType: "Negative"}
+		httpClient := httpMockCpis{CPIFunction: "IntegrationArtifactGetMplStatusError", Options: clientOptions, ResponseBody: ``, TestType: "Negative"}
 		seOut := integrationArtifactGetMplStatusCommonPipelineEnvironment{}
 		message, err := getIntegrationArtifactMPLError(&seOut, "1000111", &httpClient, "demo")
 		assert.NoError(t, err)
