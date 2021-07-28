8fcfed5189e6d4ef263444c6bb3350c3b54e90ad
mayurmohan <68264167+mayurmohan@users.noreply.github.com>
2021-07-28 16:38:03 +0530

    IntegrationArtifactUnDeploy Command

---
+++ cmd/integrationArtifactUnDeploy.go
@@ -0,0 +1,104 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"github.com/SAP/jenkins-library/pkg/command"
+	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/cpi"
+	piperhttp "github.com/SAP/jenkins-library/pkg/http"
+	"github.com/SAP/jenkins-library/pkg/telemetry"
+	"github.com/pkg/errors"
+)
+
+type integrationArtifactUnDeployUtils interface {
+	command.ExecRunner
+
+	// Add more methods here, or embed additional interfaces, or remove/replace as required.
+	// The integrationArtifactUnDeployUtils interface should be descriptive of your runtime dependencies,
+	// i.e. include everything you need to be able to mock in tests.
+	// Unit tests shall be executable in parallel (not depend on global state), and don't (re-)test dependencies.
+}
+
+type integrationArtifactUnDeployUtilsBundle struct {
+	*command.Command
+
+	// Embed more structs as necessary to implement methods or interfaces you add to integrationArtifactUnDeployUtils.
+	// Structs embedded in this way must each have a unique set of methods attached.
+	// If there is no struct which implements the method you need, attach the method to
+	// integrationArtifactUnDeployUtilsBundle and forward to the implementation of the dependency.
+}
+
+func newIntegrationArtifactUnDeployUtils() integrationArtifactUnDeployUtils {
+	utils := integrationArtifactUnDeployUtilsBundle{
+		Command: &command.Command{},
+	}
+	// Reroute command output to logging framework
+	utils.Stdout(log.Writer())
+	utils.Stderr(log.Writer())
+	return &utils
+}
+
+func integrationArtifactUnDeploy(config integrationArtifactUnDeployOptions, telemetryData *telemetry.CustomData) {
+	// Utils can be used wherever the command.ExecRunner interface is expected.
+	// It can also be used for example as a mavenExecRunner.
+	utils := newIntegrationArtifactUnDeployUtils()
+	utils.Stdout(log.Writer())
+	httpClient := &piperhttp.Client{}
+	// For HTTP calls import  piperhttp "github.com/SAP/jenkins-library/pkg/http"
+	// and use a  &piperhttp.Client{} in a custom system
+	// Example: step checkmarxExecuteScan.go
+
+	// Error situations should be bubbled up until they reach the line below which will then stop execution
+	// through the log.Entry().Fatal() call leading to an os.Exit(1) in the end.
+	err := runIntegrationArtifactUnDeploy(&config, telemetryData, httpClient)
+	if err != nil {
+		log.Entry().WithError(err).Fatal("step execution failed")
+	}
+}
+
+func runIntegrationArtifactUnDeploy(config *integrationArtifactUnDeployOptions, telemetryData *telemetry.CustomData, httpClient piperhttp.Sender) error {
+	clientOptions := piperhttp.ClientOptions{}
+	header := make(http.Header)
+	header.Add("Accept", "application/json")
+	serviceKey, err := cpi.ReadCpiServiceKey(config.APIServiceKey)
+	if err != nil {
+		return err
+	}
+	unDeployURL := fmt.Sprintf("%s/api/v1/IntegrationRuntimeArtifacts('%s')", serviceKey.OAuth.Host, config.IntegrationFlowID)
+	tokenParameters := cpi.TokenParameters{TokenURL: serviceKey.OAuth.OAuthTokenProviderURL, Username: serviceKey.OAuth.ClientID, Password: serviceKey.OAuth.ClientSecret, Client: httpClient}
+	token, err := cpi.CommonUtils.GetBearerToken(tokenParameters)
+	if err != nil {
+		return errors.Wrap(err, "failed to fetch Bearer Token")
+	}
+	clientOptions.Token = fmt.Sprintf("Bearer %s", token)
+	httpClient.SetOptions(clientOptions)
+	httpMethod := "DELETE"
+	unDeployResp, httpErr := httpClient.SendRequest(httpMethod, unDeployURL, nil, header, nil)
+	if httpErr != nil {
+		return errors.Wrapf(httpErr, "HTTP %v request to %v failed with error", httpMethod, unDeployURL)
+	}
+
+	if unDeployResp != nil && unDeployResp.Body != nil {
+		defer unDeployResp.Body.Close()
+	}
+
+	if unDeployResp == nil {
+		return errors.Errorf("did not retrieve a HTTP response")
+	}
+
+	if unDeployResp.StatusCode == http.StatusAccepted {
+		log.Entry().
+			WithField("IntegrationFlowID", config.IntegrationFlowID).
+			Info("successfully undeployed from integration runtime")
+		return nil
+	}
+	responseBody, readErr := ioutil.ReadAll(unDeployResp.Body)
+
+	if readErr != nil {
+		return errors.Wrapf(readErr, "HTTP response body could not be read, response status code: %v", unDeployResp.StatusCode)
+	}
+	log.Entry().Errorf("a HTTP error occurred! Response body: %v, Response status code : %v", responseBody, unDeployResp.StatusCode)
+	return errors.Errorf("integration flow undeployment failed, response Status code: %v", unDeployResp.StatusCode)
+}
---
+++ cmd/integrationArtifactUnDeploy_test.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"github.com/SAP/jenkins-library/pkg/mock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type integrationArtifactUnDeployMockUtils struct {
+	*mock.ExecMockRunner
+	*mock.FilesMock
+}
+
+func newIntegrationArtifactUnDeployTestsUtils() integrationArtifactUnDeployMockUtils {
+	utils := integrationArtifactUnDeployMockUtils{
+		ExecMockRunner: &mock.ExecMockRunner{},
+		FilesMock:      &mock.FilesMock{},
+	}
+	return utils
+}
+
+func TestRunIntegrationArtifactUnDeploy(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Successful undeploy of integration flow test", func(t *testing.T) {
+
+		apiServiceKey := `{
+			"oauth": {
+				"url": "https://demo",
+				"clientid": "demouser",
+				"clientsecret": "******",
+				"tokenurl": "https://demo/oauth/token"
+			}
+		}`
+		config := integrationArtifactUnDeployOptions{
+			APIServiceKey:     apiServiceKey,
+			IntegrationFlowID: "flow1",
+		}
+		httpClient := httpMockCpis{CPIFunction: "PositiveAndUnDeployIntegrationDesigntimeArtifact", ResponseBody: ``, TestType: "Positive"}
+
+		// test
+		err := runIntegrationArtifactUnDeploy(&config, nil, &httpClient)
+
+		// assert
+		assert.NoError(t, err)
+	})
+
+	t.Run("Failed undeploy of integration flow test", func(t *testing.T) {
+		apiServiceKey := `{
+			"oauth": {
+				"url": "https://demo",
+				"clientid": "demouser",
+				"clientsecret": "******",
+				"tokenurl": "https://demo/oauth/token"
+			}
+		}`
+		config := integrationArtifactUnDeployOptions{
+			APIServiceKey:     apiServiceKey,
+			IntegrationFlowID: "flow1",
+		}
+
+		httpClient := httpMockCpis{CPIFunction: "FailedIntegrationRuntimeArtifactUnDeployment", ResponseBody: ``, TestType: "Negative"}
+
+		// test
+		err := runIntegrationArtifactUnDeploy(&config, nil, &httpClient)
+
+		// assert
+		assert.EqualError(t, err, "integration flow undeployment failed, response Status code: 400")
+	})
+}
