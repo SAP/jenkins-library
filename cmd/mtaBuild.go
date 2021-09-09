def36792bd44bdf096e3d85e0cdc576a60b70dcd
Your Name <you@example.com>
2021-09-09 19:19:30 +0200

    force profile

--- cmd/mtaBuild.go
+++ cmd/mtaBuild.go
@@ -212,7 +212,7 @@ func runMtaBuild(config mtaBuildOptions,
 		if err != nil {
 			return err
 		}
-		utils.AppendEnv([]string{"MAVEN_OPTS=-Dmaven.repo.local=" + absolutePath + " -Prelease.build"})
+		utils.AppendEnv([]string{"MAVEN_OPTS=-Dmaven.repo.local=" + absolutePath + " --activate-profiles release.build"})
 	}
 
 	log.Entry().Infof("Executing mta build call: \"%s\"", strings.Join(call, " "))
