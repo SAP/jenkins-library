53e9ce1e57f3eb1e688d6d0fed248350f95411a2
Your Name <you@example.com>
2021-09-13 21:57:57 +0200

    creating parent folder

--- pkg/maven/settings.go
+++ pkg/maven/settings.go
@@ -213,7 +213,7 @@ func CreateNewProjectSettingsXMLWithActiveProfile(activeProfile []string, utils 
 	// 	},
 	// }
 
-	settingsXML := `"<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd\">"
+	settingsXML := `<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd">
 	<activeProfiles>
 	<activeProfile>release.build</activeProfile>
 	</activeProfiles>
