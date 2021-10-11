ae8841e80f43d255301d418faf84a0cb31dae110
CCFenner <christopher.fenner@sap.com>
2021-10-11 09:25:04 +0200

    remove groovy docs for hadolint

--- vars/hadolintExecute.groovy
+++ vars/hadolintExecute.groovy
@@ -5,30 +5,17 @@ import com.sap.piper.JenkinsUtils
 import com.sap.piper.Utils
 import groovy.transform.Field
 
-@Field def STEP_NAME = getClass().getName()
+@Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/hadolint.yaml'
 
 @Field Set GENERAL_CONFIG_KEYS = []
 @Field Set STEP_CONFIG_KEYS = GENERAL_CONFIG_KEYS.plus([
-    /**
-     * Quality Gates to fail the build, see [warnings-ng plugin documentation](https://github.com/jenkinsci/warnings-plugin/blob/master/doc/Documentation.md#quality-gate-configuration).
-     */
     'qualityGates',
-    /**
-     * Name of the result file used locally within the step.
-     */
     'reportFile',
-    /**
-     * Name of the checkstyle report being generated our of the results.
-     */
     'reportName'
 ])
 @Field Set PARAMETER_KEYS = STEP_CONFIG_KEYS
-/**
- * Executes the Haskell Dockerfile Linter which is a smarter Dockerfile linter that helps you build [best practice](https://docs.docker.com/develop/develop-images/dockerfile_best-practices/) Docker images.
- * The linter is parsing the Dockerfile into an abstract syntax tree (AST) and performs rules on top of the AST.
- */
-@GenerateDocumentation
+
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: null
     List credentialInfo = [
