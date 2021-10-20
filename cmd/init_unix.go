8016611f8de69e5db9db413521d5bf92a298b589
Ralf Pannemans <ralf.pannemans@sap.com>
2021-10-20 08:44:05 +0200

    Add generated files
    Co-authored-by: Pavel Busko <pavel.busko@sap.com>

--- cmd/init_unix.go
+++ cmd/init_unix.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // +build !windows
 
 package cmd
@@ -9,3 +10,18 @@ func init() {
 	// creation are altered in unpredictable ways.
 	syscall.Umask(0)
 }
+||||||| parent of 2c68cedf (Add genrated files)
+=======
+//go:build !windows
+// +build !windows
+
+package cmd
+
+import "syscall"
+
+func init() {
+	// unset umask otherwise permissions on file or directory
+	// creation are altered in unpredictable ways.
+	syscall.Umask(0)
+}
+>>>>>>> 2c68cedf (Add genrated files)
