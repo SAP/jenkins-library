29b8aac35c73bf986a353d4e1b20b68f942ee36a
Felix Feldmann <F.Feldmann@sap.com>
2021-11-03 15:52:05 +0100

    Removes GetLog() test

--- pkg/orchestrator/jenkins_test.go
+++ pkg/orchestrator/jenkins_test.go
@@ -1,10 +1,7 @@
 package orchestrator
 
 import (
-	"github.com/SAP/jenkins-library/pkg/log"
-	"io/ioutil"
 	"os"
-	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,68 +42,4 @@ func TestJenkins(t *testing.T) {
 		assert.Equal(t, "main", c.Base)
 		assert.Equal(t, "42", c.Key)
 	})
-}
-
-func TestJenkinsConfigProvider_GetLog(t *testing.T) {
-	defer resetEnv(os.Environ())
-	os.Clearenv()
-	jenkinsHome := getTempDir()
-	buildNumber := "42"
-	jobName := "UnitTest"
-	logPath := jenkinsHome + "/jobs/" + jobName + "/builds/" + buildNumber
-
-	os.Setenv("JENKINS_HOME", jenkinsHome)
-	os.Setenv("BUILD_NUMBER", buildNumber)
-	os.Setenv("JOB_NAME", jobName)
-	os.MkdirAll(logPath, 0700)
-
-	dummyLog := []byte("First line of log\nSecond line of log\nOh Snap an error!\nExiting\n")
-
-	tests := []struct {
-		name    string
-		want    []byte
-		wantErr bool
-	}{
-		{
-			name:    "Get log file -  success",
-			want:    dummyLog,
-			wantErr: false,
-		},
-		{
-			name:    "Get log file -  not found failure",
-			want:    nil,
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			a := &JenkinsConfigProvider{}
-			if !tt.wantErr {
-				// create dummy log file
-				err := ioutil.WriteFile(logPath+"/log", dummyLog, 0644)
-				if err != nil {
-					log.Entry().Errorf("Could not create dummy log file. %v", err)
-				}
-			}
-
-			got, err := a.GetLog()
-
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetLog() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetLog() got = %v, want %v", got, tt.want)
-			}
-			os.RemoveAll(jenkinsHome)
-		})
-	}
-}
-
-func getTempDir() string {
-	tmpFolder, err := ioutil.TempDir(".", "temp-")
-	if err != nil {
-		log.Entry().WithError(err).WithField("path", tmpFolder).Debug("Creating temp directory failed")
-	}
-	return tmpFolder
 }
