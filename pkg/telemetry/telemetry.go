d9091ddc7f22c66aa7fcfe506fe49f812747c880
Felix Feldmann <F.Feldmann@sap.com>
2021-11-11 10:51:19 +0100

    Adds possibility to send telemetry to custom endpoint

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -1,9 +1,13 @@
 package telemetry
 
 import (
+	"bytes"
 	"crypto/sha1"
+	"encoding/json"
 	"fmt"
 	"github.com/SAP/jenkins-library/pkg/orchestrator"
+	"io"
+	"io/ioutil"
 	"time"
 
 	"net/http"
@@ -25,8 +29,6 @@ var LibraryRepository string
 // SiteID ...
 var SiteID string
 
-var client piperhttp.Sender
-
 // SWA baseURL
 const baseURL = "https://webanalytics.cfapps.eu10.hana.ondemand.com"
 
@@ -35,11 +37,15 @@ const endpoint = "/tracker/log"
 
 // Telemetry struct which holds necessary infos about telemetry
 type Telemetry struct {
-	baseData     BaseData
-	baseMetaData BaseMetaData
-	data         Data
-	provider     orchestrator.OrchestratorSpecificConfigProviding
-	disabled     bool
+	baseData             BaseData
+	baseMetaData         BaseMetaData
+	data                 Data
+	provider             orchestrator.OrchestratorSpecificConfigProviding
+	disabled             bool
+	client               piperhttp.Client
+	CustomReportingDsn   string
+	CustomReportingToken string
+	PipelineTelemetry    *PipelineTelemetry
 }
 
 // Initialize sets up the base telemetry data and is called in generated part of the steps
@@ -57,11 +63,7 @@ func (t *Telemetry) Initialize(telemetryDisabled bool, stepName string) {
 	}
 	t.provider = provider
 
-	if client == nil {
-		client = &piperhttp.Client{}
-	}
-
-	client.SetOptions(piperhttp.ClientOptions{MaxRequestDuration: 5 * time.Second, MaxRetries: -1})
+	t.client.SetOptions(piperhttp.ClientOptions{MaxRequestDuration: 5 * time.Second, MaxRetries: -1})
 
 	if len(LibraryRepository) == 0 {
 		LibraryRepository = "https://github.com/n/a"
@@ -119,6 +121,8 @@ func (t *Telemetry) GetData() Data {
 // Send telemetry information to SWA
 func (t *Telemetry) Send() {
 
+	t.sendCustom()
+
 	// skip if telemetry is disabled
 	if t.disabled {
 		return
@@ -128,5 +132,57 @@ func (t *Telemetry) Send() {
 	request.Path = endpoint
 	request.RawQuery = t.data.toPayloadString()
 	log.Entry().WithField("request", request.String()).Debug("Sending telemetry data")
-	client.SendRequest(http.MethodGet, request.String(), nil, nil, nil)
+	t.client.SendRequest(http.MethodGet, request.String(), nil, nil, nil)
+}
+
+func (t *Telemetry) sendCustom() {
+	t.data.ErrorCode = "1"
+	if t.data.ErrorCode == "1" && len(t.CustomReportingDsn) > 0 {
+		// check if reporting is available in the config and then send the payload to the specific URL
+		customClient := &piperhttp.Client{}
+		t.provider.GetBuildUrl()
+		data := t.data
+		data.BuildURLHash = t.provider.GetBuildUrl()
+		data.PipelineURLHash = t.provider.GetBuildUrl()
+
+		var payload []byte
+		var err error
+		if t.PipelineTelemetry != nil {
+			var m map[string]string
+
+			jPipelineTelemetry, _ := json.Marshal(t.PipelineTelemetry)
+			json.Unmarshal(jPipelineTelemetry, &m)
+			jData, _ := json.Marshal(data)
+			json.Unmarshal(jData, &m)
+
+			payload, err = json.Marshal(m)
+			if err != nil {
+				log.Entry().Errorf("error while marshalling reporting details, %v", err)
+			}
+		} else {
+			payload, err = json.Marshal(data)
+			if err != nil {
+				log.Entry().Errorf("error while marshalling reporting details, %v", err)
+			}
+		}
+
+		resp, err := customClient.SendRequest(http.MethodPost, t.CustomReportingDsn, bytes.NewBuffer(payload), nil, nil)
+
+		if resp != nil {
+			if resp.StatusCode != http.StatusOK {
+				// log it to stdout
+				rdr := io.LimitReader(resp.Body, 1000)
+				body, errRead := ioutil.ReadAll(rdr)
+				log.Entry().Infof("%v: error logging failed - %v", resp.Status, string(body))
+				if errRead != nil {
+					log.Entry().Errorf("Error reading response body. %v", errRead)
+				}
+				log.Entry().Errorf("%v: error logging failed - %v, %v", err, resp.Status, string(body))
+			}
+		}
+
+		if err != nil {
+			log.Entry().Errorf("error sending the requests: %v", err)
+		}
+	}
 }
