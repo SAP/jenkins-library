f16b11b804efbdb13ce66edcf28dff7dde02c41e
ffeldmann <f.feldmann@sap.com>
2021-11-19 11:28:15 +0100

    Removes t.Skip() and re-activates integration test (#3299)

--- integration/integration_cnb_test.go
+++ integration/integration_cnb_test.go
@@ -9,7 +9,6 @@ import (
 
 func TestNpmProject(t *testing.T) {
 	t.Parallel()
-	t.Skip()
 	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
 		Image:   "paketobuildpacks/builder:full",
 		User:    "cnb",
