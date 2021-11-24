5f9bf06e080643cb78ac563aa83a0b324462ba7c
Christopher Fenner <Christopher.Fenner@sap.com>
2021-11-24 20:49:57 +0100

    add keytool package

---
+++ pkg/java/keytool.go
@@ -0,0 +1,42 @@
+package java
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/SAP/jenkins-library/pkg/command"
+	"github.com/SAP/jenkins-library/pkg/log"
+)
+
+const defaultTruststorePath = "lib/security/cacerts"
+const DefaultTruststorePassword = "changeit"
+
+func GetDefaultTruststorePath() string {
+	return filepath.Join(os.Getenv("JAVA_HOME"), filepath.FromSlash(defaultTruststorePath))
+}
+
+func ImportCert(runner command.ExecRunner, destTruststore, certfile string) error {
+	options := []string{
+		"-import",
+		"-noprompt",
+		"-storepass", DefaultTruststorePassword,
+		"-keystore", destTruststore,
+		"-file", certfile,
+		"-alias", filepath.Base(certfile),
+	}
+	log.Entry().Infof("Importing certificate. %s", certfile)
+	return runner.RunExecutable("keytool", options...)
+}
+
+func ImportTruststore(runner command.ExecRunner, destTruststore, srcTruststore string) error {
+	options := []string{
+		"-importkeystore",
+		"-noprompt",
+		"-srckeystore", srcTruststore,
+		"-srcstorepass", DefaultTruststorePassword,
+		"-destkeystore", destTruststore,
+		"-deststorepass", DefaultTruststorePassword,
+	}
+	log.Entry().Debugf("Copying existing trust store: %s", srcTruststore)
+	return runner.RunExecutable("keytool", options...)
+}
