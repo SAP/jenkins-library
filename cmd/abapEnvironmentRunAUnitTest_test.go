0e8d6b0f92310b924bc42680a82dc860a5cde47f
dominiklendle <d.lendle@sap.com>
2021-11-26 01:57:18 +0100

    test adaptions

--- cmd/abapEnvironmentRunAUnitTest_test.go
+++ cmd/abapEnvironmentRunAUnitTest_test.go
@@ -356,7 +356,7 @@ func TestRunAbapEnvironmentRunAUnitTest(t *testing.T) {
 		tokenExpected := "myToken"
 
 		client := &abaputils.ClientMock{
-			Body:  `<?xml version="1.0" encoding="utf-8"?><aunit:run xmlns:aunit="http://www.sap.com/adt/api/aunit"><aunit:progress status="FINISHED"/><aunit:time/><atom:link href="/sap/bc/adt/api/abapunit/results/test" rel="http://www.sap.com/adt/relations/api/abapunit/run-result" type="application/vnd.sap.adt.api.junit.run-result.v1+xml" title="JUnit Run Result" xmlns:atom="http://www.w3.org/2005/Atom"/></aunit:run>`,
+			Body:  `<?xml version="1.0" encoding="utf-8"?><aunit:run xmlns:aunit="http://www.sap.com/adt/api/aunit"><aunit:progress status="FINISHED"/><aunit:time/><atom:link href="/sap/bc/adt/api/abapunit/results/test" rel="http://www.sap.com/adt/relations/api/abapunit/run-result" type="application/vnd.sap.adt.api.junit.run-result.v1xml" title="JUnit Run Result" xmlns:atom="http://www.w3.org/2005/Atom"/></aunit:run>`,
 			Token: tokenExpected,
 		}
 
@@ -378,7 +378,7 @@ func TestRunAbapEnvironmentRunAUnitTest(t *testing.T) {
 		tokenExpected := "myToken"
 
 		client := &abaputils.ClientMock{
-			Body:  `<?xml version="1.0" encoding="utf-8"?><aunit:run xmlns:aunit="http://www.sap.com/adt/api/aunit"><aunit:progress status="Not Created"/><aunit:time/><atom:link href="/sap/bc/adt/api/abapunit/results/test" rel="http://www.sap.com/adt/relations/api/abapunit/run-result" type="application/vnd.sap.adt.api.junit.run-result.v1+xml" title="JUnit Run Result" xmlns:atom="http://www.w3.org/2005/Atom"/></aunit:run>`,
+			Body:  `<?xml version="1.0" encoding="utf-8"?><aunit:run xmlns:aunit="http://www.sap.com/adt/api/aunit"><aunit:progress status="Not Created"/><aunit:time/><atom:link href="/sap/bc/adt/api/abapunit/results/test" rel="http://www.sap.com/adt/relations/api/abapunit/run-result" type="application/vnd.sap.adt.api.junit.run-result.v1xml" title="JUnit Run Result" xmlns:atom="http://www.w3.org/2005/Atom"/></aunit:run>`,
 			Token: tokenExpected,
 		}
 
@@ -437,7 +437,7 @@ func TestGenerateHTMLDocumentAUnit(t *testing.T) {
 	t.Run("Test AUnit XML Result", func(t *testing.T) {
 		t.Parallel()
 
-		expectedString := `<!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/xhtml"><head><title>AUnit Results</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><style>table,th,td {border-collapse:collapse;}th,td{padding: 5px;text-align:left;font-size:medium;}</style></head><body><table style="border: 1px solid black"><tr><th>Run title</th><td style="padding-right: 20px"></td><th>System</th><td style="padding-right: 20px"></td><th>Client</th><td style="padding-right: 20px"></td><th>ExecutedBy</th><td style="padding-right: 20px"></td><th>Duration</th><td style="padding-right: 20px">s</td><th>Timestamp</th><td style="padding-right: 20px"></td></tr><tr><th>Failures</th><td style="padding-right: 20px"></td><th>Errors</th><td style="padding-right: 20px"></td><th>Skipped</th><td style="padding-right: 20px"></td><th>Asserts</th><td style="padding-right: 20px"></td><th>Tests</th><td style="padding-right: 20px"></td></tr></table><br><table style="width:100%; border: 1px solid black""><tr style="border: 1px solid black"><th style="border: 1px solid black">Severity</th><th style="border: 1px solid black">File</th><th style="border: 1px solid black">Message</th><th style="border: 1px solid black">Type</th><th style="border: 1px solid black">Text</th></tr><tr><td colspan="5"><b>There are no AUnit findings to be displayed</b></td></tr></table></body></html>`
+		expectedString := `<!DOCTYPE html><html lang="en" xmlns="http://www.w3.org/1999/xhtml"><head><title>AUnit Results</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><style>table,th,td {border-collapse:collapse;}th,td{padding: 5px;text-align:left;font-size:medium;}</style></head><body><table style="border: 1px solid black"><tr><th>Run title</th><td style="padding-right: 20px">Test title</td><th>System</th><td style="padding-right: 20px">Test system</td><th>Client</th><td style="padding-right: 20px">000</td><th>ExecutedBy</th><td style="padding-right: 20px">CC00000</td><th>Duration</th><td style="padding-right: 20px">0.15s</td><th>Timestamp</th><td style="padding-right: 20px">2021-00-00T00:00:00Z</td></tr><tr><th>Failures</th><td style="padding-right: 20px">4</td><th>Errors</th><td style="padding-right: 20px">4</td><th>Skipped</th><td style="padding-right: 20px">4</td><th>Asserts</th><td style="padding-right: 20px">12</td><th>Tests</th><td style="padding-right: 20px">12</td></tr></table><br><table style="width:100%; border: 1px solid black""><tr style="border: 1px solid black"><th style="border: 1px solid black">Severity</th><th style="border: 1px solid black">File</th><th style="border: 1px solid black">Message</th><th style="border: 1px solid black">Type</th><th style="border: 1px solid black">Text</th></tr><tr style="background-color: grey"><td colspan="5"><b>Testcase: my_test for class ZCL_my_test</b></td></tr><tr style="background-color: rgba(227,85,0)"><td style="border: 1px solid black">Failure</td><td style="border: 1px solid black">ZCL_my_test</td><td style="border: 1px solid black">testMessage</td><td style="border: 1px solid black">Assert Error</td><td style="border: 1px solid black">testError</td></tr><tr style="background-color: rgba(227,85,0)"><td style="border: 1px solid black">Failure</td><td style="border: 1px solid black">ZCL_my_test</td><td style="border: 1px solid black">testMessage2</td><td style="border: 1px solid black">Assert Error2</td><td style="border: 1px solid black">testError2</td></tr><tr style="background-color: rgba(227,85,0)"><td style="border: 1px solid black">Failure</td><td style="border: 1px solid black">ZCL_my_test</td><td style="border: 1px solid black">testMessage</td><td style="border: 1px solid black">Assert Failure</td><td style="border: 1px solid black">testFailure</td></tr><tr style="background-color: rgba(227,85,0)"><td style="border: 1px solid black">Failure</td><td style="border: 1px solid black">ZCL_my_test</td><td style="border: 1px solid black">testMessage2</td><td style="border: 1px solid black">Assert Failure2</td><td style="border: 1px solid black">testFailure2</td></tr><tr style="background-color: rgba(255,175,0, 0.2)"><td style="border: 1px solid black">Failure</td><td style="border: 1px solid black">ZCL_my_test</td><td style="border: 1px solid black">testSkipped</td><td style="border: 1px solid black">-</td><td style="border: 1px solid black">testSkipped</td></tr><tr style="background-color: rgba(255,175,0, 0.2)"><td style="border: 1px solid black">Failure</td><td style="border: 1px solid black">ZCL_my_test</td><td style="border: 1px solid black">testSkipped2</td><td style="border: 1px solid black">-</td><td style="border: 1px solid black">testSkipped2</td></tr></table></body></html>`
 
 		result := AUnitResult{
 			XMLName:    xml.Name{Space: "testSpace", Local: "testLocal"},
@@ -452,22 +452,53 @@ func TestGenerateHTMLDocumentAUnit(t *testing.T) {
 			Skipped:    "4",
 			Asserts:    "12",
 			Tests:      "12",
-			Testsuite: {
+			Testsuite: struct {
+				Tests     string `xml:"tests,attr"`
+				Asserts   string `xml:"asserts,attr"`
+				Skipped   string `xml:"skipped,attr"`
+				Errors    string `xml:"errors,attr"`
+				Failures  string `xml:"failures,attr"`
+				Timestamp string `xml:"timestamp,attr"`
+				Time      string `xml:"time,attr"`
+				Hostname  string `xml:"hostname,attr"`
+				Package   string `xml:"package,attr"`
+				Name      string `xml:"name,attr"`
+				Testcase  []struct {
+					Asserts   string `xml:"asserts,attr"`
+					Time      string `xml:"time,attr"`
+					Name      string `xml:"name,attr"`
+					Classname string `xml:"classname,attr"`
+					Error     []struct {
+						Text    string `xml:",chardata"`
+						Type    string `xml:"type,attr"`
+						Message string `xml:"message,attr"`
+					} `xml:"error"`
+					Failure []struct {
+						Text    string `xml:",chardata"`
+						Type    string `xml:"type,attr"`
+						Message string `xml:"message,attr"`
+					} `xml:"failure"`
+					Skipped []struct {
+						Text    string `xml:",chardata"`
+						Message string `xml:"message,attr"`
+					} `xml:"skipped"`
+				} `xml:"testcase"`
+			}{
 				Tests:     "6",
-				Asserts:   "6",
+				Asserts:   "4",
 				Skipped:   "2",
 				Errors:    "2",
 				Failures:  "2",
 				Timestamp: "2021-00-00T00:00:00Z",
-				Time:      "0.1",
+				Time:      "0.15",
 				Hostname:  "0xb",
 				Package:   "testPackage",
 				Name:      "ZCL_testPackage",
-				Testcase  []struct {
-					Asserts:   "2",
-					Time:      "2",
-					Name:      "my_test",
-					Classname: "ZCL_my_test",
+				Testcase: []struct {
+					Asserts   string "xml:\"asserts,attr\""
+					Time      string "xml:\"time,attr\""
+					Name      string "xml:\"name,attr\""
+					Classname string "xml:\"classname,attr\""
 					Error     []struct {
 						Text    string "xml:\",chardata\""
 						Type    string "xml:\"type,attr\""
@@ -482,60 +513,50 @@ func TestGenerateHTMLDocumentAUnit(t *testing.T) {
 						Text    string "xml:\",chardata\""
 						Message string "xml:\"message,attr\""
 					} "xml:\"skipped\""
-				} "xml:\"testcase\""
-			}{},
+				}{{
+					Asserts:   "4",
+					Time:      "0.15",
+					Name:      "my_test",
+					Classname: "ZCL_my_test",
+					Error: []struct {
+						Text    string "xml:\",chardata\""
+						Type    string "xml:\"type,attr\""
+						Message string "xml:\"message,attr\""
+					}{{
+						Text:    "testError",
+						Type:    "Assert Error",
+						Message: "testMessage",
+					}, {
+						Text:    "testError2",
+						Type:    "Assert Error2",
+						Message: "testMessage2",
+					}},
+					Failure: []struct {
+						Text    string "xml:\",chardata\""
+						Type    string "xml:\"type,attr\""
+						Message string "xml:\"message,attr\""
+					}{{
+						Text:    "testFailure",
+						Type:    "Assert Failure",
+						Message: "testMessage",
+					}, {
+						Text:    "testFailure2",
+						Type:    "Assert Failure2",
+						Message: "testMessage2",
+					}},
+					Skipped: []struct {
+						Text    string "xml:\",chardata\""
+						Message string "xml:\"message,attr\""
+					}{{
+						Text:    "testSkipped",
+						Message: "testSkipped",
+					}, {
+						Text:    "testSkipped2",
+						Message: "testSkipped2",
+					}},
+				}},
+			},
 		}
-
-
-
-		/*
-				Tests:     "6",
-				Asserts:   "6",
-				Skipped:   "2",
-				Errors:    "2",
-				Failures:  "2",
-				Timestamp: "2021-00-00T00:00:00Z",
-				Time:      "0.1",
-				Hostname:  "0xb",
-				Package:   "testPackage",
-				Name:      "ZCL_testPackage",
-					Testcase: {
-						Asserts:   "2",
-						Time:      "2",
-						Name:      "my_test",
-						Classname: "ZCL_my_test",
-						Error: {
-							Text:    "testText",
-							Type:    "Assert Error",
-							Message: "Error in ZCL_my_test",
-						}, {
-							Text:    "testText",
-							Type:    "Assert Error",
-							Message: "Error in ZCL_my_test2",
-						},
-						Failure: {
-							Text:    "testText",
-							Type:    "Assert Failure",
-							Message: "Error in ZCL_my_test",
-						}, {
-							Text:    "testText",
-							Type:    "Assert Failure",
-							Message: "Error in ZCL_my_test2",
-						},
-						Skipped: {
-							Text:    "testText",
-							Type:    "Skipped",
-							Message: "Skipped ZCL_my_test",
-						}, {
-							Text:    "testText",
-							Type:    "Skipped",
-							Message: "Skipped ZCL_my_test2",
-						},
-					},
-				},
-			}
-
-		*/
 
 		resultString := generateHTMLDocumentAUnit(&result)
 
