6cc614dfdc9a4eecd95b436fcf31b9f38a9b3375
Rosemarie Bentele <rosemarie.bentele@sap.com>
2021-11-30 14:35:55 +0100

    make abapEnvironmentBuildUtilsBundle powerful

--- cmd/abapEnvironmentBuild.go
+++ cmd/abapEnvironmentBuild.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"encoding/json"
-	"strings"
 	"time"
 
 	abapbuild "github.com/SAP/jenkins-library/pkg/abap/build"
@@ -19,6 +18,11 @@ type abapEnvironmentBuildUtils interface {
 	command.ExecRunner
 
 	FileExists(filename string) (bool, error)
+	abaputils.Communication
+	abapbuild.Publish
+	abapbuild.HTTPSendLoader
+	getMaxRuntime() time.Duration
+	getPollingIntervall() time.Duration
 
 	// Add more methods here, or embed additional interfaces, or remove/replace as required.
 	// The abapEnvironmentBuildUtils interface should be descriptive of your runtime dependencies,
@@ -27,8 +31,13 @@ type abapEnvironmentBuildUtils interface {
 }
 
 type abapEnvironmentBuildUtilsBundle struct {
+	//TODO mein verständnis: command.Command -> ist ein struct, das wird hier embedded, damit "erbt man alles von dem"
 	*command.Command
 	*piperutils.Files
+	*piperhttp.Client
+	*abaputils.AbapUtils
+	maxRuntime       time.Duration
+	pollingIntervall time.Duration
 
 	// Embed more structs as necessary to implement methods or interfaces you add to abapEnvironmentBuildUtils.
 	// Structs embedded in this way must each have a unique set of methods attached.
@@ -36,10 +45,28 @@ type abapEnvironmentBuildUtilsBundle struct {
 	// abapEnvironmentBuildUtilsBundle and forward to the implementation of the dependency.
 }
 
-func newAbapEnvironmentBuildUtils() abapEnvironmentBuildUtils {
+func (aEBUB *abapEnvironmentBuildUtilsBundle) getMaxRuntime() time.Duration {
+	return aEBUB.maxRuntime
+}
+
+func (aEBUB *abapEnvironmentBuildUtilsBundle) getPollingIntervall() time.Duration {
+	return aEBUB.pollingIntervall
+}
+
+func (aEBUB *abapEnvironmentBuildUtilsBundle) PersistReportsAndLinks(stepName, workspace string, reports, links []piperutils.Path) {
+	abapbuild.PersistReportsAndLinks(stepName, workspace, reports, links)
+}
+
+func newAbapEnvironmentBuildUtils(maxRuntime time.Duration, pollingIntervall time.Duration) abapEnvironmentBuildUtils {
 	utils := abapEnvironmentBuildUtilsBundle{
 		Command: &command.Command{},
 		Files:   &piperutils.Files{},
+		Client:  &piperhttp.Client{},
+		AbapUtils: &abaputils.AbapUtils{
+			Exec: &command.Command{},
+		},
+		maxRuntime:       maxRuntime * time.Minute,
+		pollingIntervall: pollingIntervall * time.Second,
 	}
 	// Reroute command output to logging framework
 	utils.Stdout(log.Writer())
@@ -50,14 +77,8 @@ func newAbapEnvironmentBuildUtils() abapEnvironmentBuildUtils {
 func abapEnvironmentBuild(config abapEnvironmentBuildOptions, telemetryData *telemetry.CustomData, cpe *abapEnvironmentBuildCommonPipelineEnvironment) {
 	// Utils can be used wherever the command.ExecRunner interface is expected.
 	// It can also be used for example as a mavenExecRunner.
-	utils := newAbapEnvironmentBuildUtils()
-
-	// TODO das irgendwie in die utils rein?
-	c := command.Command{}
-	var autils = abaputils.AbapUtils{
-		Exec: &c,
-	}
-	client := piperhttp.Client{}
+	utils := newAbapEnvironmentBuildUtils(time.Duration(config.MaxRuntimeInMinutes), time.Duration(config.PollingIntervallInSeconds))
+	//client := piperhttp.Client{}
 
 	// For HTTP calls import  piperhttp "github.com/SAP/jenkins-library/pkg/http"
 	// and use a  &piperhttp.Client{} in a custom system
@@ -65,22 +86,24 @@ func abapEnvironmentBuild(config abapEnvironmentBuildOptions, telemetryData *tel
 
 	// Error situations should be bubbled up until they reach the line below which will then stop execution
 	// through the log.Entry().Fatal() call leading to an os.Exit(1) in the end.
-	err := runAbapEnvironmentBuild(&config, telemetryData, utils, &autils, &client, time.Duration(config.MaxRuntimeInMinutes)*time.Minute, time.Duration(config.PollingIntervallInSeconds)*time.Second, cpe)
+	//err := runAbapEnvironmentBuild(&config, telemetryData, utils, &client, time.Duration(config.MaxRuntimeInMinutes)*time.Minute, time.Duration(config.PollingIntervallInSeconds)*time.Second, cpe)
+	err := runAbapEnvironmentBuild(&config, telemetryData, utils, cpe)
 	if err != nil {
 		log.Entry().WithError(err).Fatal("step execution failed")
 	}
 }
 
-func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData *telemetry.CustomData, utils abapEnvironmentBuildUtils, com abaputils.Communication, client abapbuild.HTTPSendLoader,
-	maxRuntime time.Duration, PollingIntervall time.Duration, cpe *abapEnvironmentBuildCommonPipelineEnvironment) error {
+func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData *telemetry.CustomData, utils abapEnvironmentBuildUtils, cpe *abapEnvironmentBuildCommonPipelineEnvironment) error {
+	//func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData *telemetry.CustomData, utils abapEnvironmentBuildUtils, client abapbuild.HTTPSendLoader,
+	//maxRuntime time.Duration, pollingIntervall time.Duration, cpe *abapEnvironmentBuildCommonPipelineEnvironment) error {
 	conn := new(abapbuild.Connector)
 
 	// TODO wrappe die fehler
-	if err := initConnection(conn, config, com, client); err != nil {
+	//if err := initConnection(conn, config, utils, client, maxRuntime, pollingIntervall); err != nil {
+	if err := initConnection(conn, config, utils); err != nil {
 		return err
 	}
 
-	//stringValues := "[{\"value_id\":\"ID1\",\"value\":\"Value1\"}]"
 	var values abapbuild.Values
 	//TODO delete
 	log.Entry().Infof("config values %s", config.Values)
@@ -121,15 +144,6 @@ func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData 
 	//TODO delete
 	log.Entry().Infof("Values used %s", values.Values)
 
-	//erzeuge value liste
-	// TODO lieber in bfw?
-	//addonDescriptorCPE, _ := abaputils.ConstructAddonDescriptorFromJSON([]byte(config.AddonDescriptor))
-
-	/*values, err := parseValues(config.Values)
-	if err != nil {
-		return err
-	} */
-
 	build := abapbuild.Build{
 		Connector: *conn,
 	}
@@ -137,7 +151,7 @@ func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData 
 	if err := build.Start(config.Phase, values); err != nil {
 		return err
 	}
-	if err := build.Poll(maxRuntime, PollingIntervall); err != nil {
+	if err := build.Poll(); err != nil {
 		return err
 	}
 	if err := build.PrintLogs(); err != nil {
@@ -158,34 +172,30 @@ func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData 
 			if err != nil {
 				return err
 			}
-			if err := result.DownloadWithFilenamePrefix(config.SubDirectoryForDownload, config.FilenamePrefixForDownload); err != nil {
+			if err := result.DownloadWithFilenamePrefixAndTargetDirectory(config.SubDirectoryForDownload, config.FilenamePrefixForDownload); err != nil {
 				return err
 			}
 		}
 	}
 
 	if config.PublishAllDownloadedResultFiles {
-		build.PublishAllDownloadedResults("abapEnvironmentBuild")
+		build.PublishAllDownloadedResults("abapEnvironmentBuild", utils)
 	} else {
-		if err := build.PublishDownloadedResults("abapEnvironmentBuild", config.PublishResultFilenames); err != nil {
+		if err := build.PublishDownloadedResults("abapEnvironmentBuild", config.PublishResultFilenames, utils); err != nil {
 			return err
 		}
 	}
 
-	//TODO values von build nehmen und in die struktur pressen und das wegschreiben
-	//TODO test start
 	type cpeValue struct {
 		ValueID string `json:"value_id"`
 		Value   string `json:"value"`
 	}
 
 	build.GetValues()
-	var testValues []cpeValue
+	var cpeValues []cpeValue
 	byt, _ := json.Marshal(&build.Values)
-	json.Unmarshal(byt, &testValues)
-
-	//Das brauch ich um am ende die Values wegzuschreiben -> muss also eigentlich nach utnen
-	jsonBytes, _ := json.Marshal(testValues)
+	json.Unmarshal(byt, &cpeValues)
+	jsonBytes, _ := json.Marshal(cpeValues)
 	cpe.build.values = string(jsonBytes)
 	//
 	/*
@@ -210,7 +220,8 @@ func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData 
 	return nil
 }
 
-func initConnection(conn *abapbuild.Connector, config *abapEnvironmentBuildOptions, com abaputils.Communication, client abapbuild.HTTPSendLoader) error {
+//func initConnection(conn *abapbuild.Connector, config *abapEnvironmentBuildOptions, utils abapEnvironmentBuildUtils, client abapbuild.HTTPSendLoader, maxRuntime time.Duration, pollingIntervall time.Duration) error {
+func initConnection(conn *abapbuild.Connector, config *abapEnvironmentBuildOptions, utils abapEnvironmentBuildUtils) error {
 	var connConfig abapbuild.ConnectorConfiguration
 	connConfig.CfAPIEndpoint = config.CfAPIEndpoint
 	connConfig.CfOrg = config.CfOrg
@@ -223,13 +234,19 @@ func initConnection(conn *abapbuild.Connector, config *abapEnvironmentBuildOptio
 	connConfig.MaxRuntimeInMinutes = config.MaxRuntimeInMinutes
 	connConfig.CertificateNames = config.CertificateNames
 
-	err := conn.InitBuildFramework(connConfig, com, client)
+	err := conn.InitBuildFramework(connConfig, utils, utils)
 	if err != nil {
 		return errors.Wrap(err, "Connector initialization for communication with the ABAP system failed")
 	}
+	conn.MaxRuntime = utils.getMaxRuntime()            //maxRuntime
+	conn.PollingInterval = utils.getPollingIntervall() //pollingIntervall
+	log.Entry().Infof("MaxRuntime %s", conn.MaxRuntime)
+	log.Entry().Infof("polling intervall %s", conn.PollingInterval)
 	return nil
 }
 
+//TODO delete
+/*
 //TODO delete
 func parseValues(inputValues []string) (abapbuild.Values, error) {
 	var values abapbuild.Values
@@ -268,8 +285,6 @@ func parseValue(inputValue string) (abapbuild.Value, error) {
 	return value, nil
 }
 
-//TODO delete
-/*
 	config.Values = []string{"{value_id:'ID1','value':'Value1'}"}
 	//config.Values = []string{"value_id: PACKAGES, value: /BUILD/AUNIT_DUMMY_TESTS", "value_id: MyId1, value: AunitValue1", "value_id: MyId2, value: AunitValue2"}
 	config.CpeValues = []string{"value_id: PACKAGES, value: /BUILD/AUNIT_DUMMY_TESTS", "value_id: MyId1, value: CPEValue1", "value_id: MyId3, value: CPEValue3"}
