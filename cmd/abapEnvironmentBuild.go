48b0f4b1f2e3bd9d68591d08c2bc2be6e48e0833
Rosemarie Bentele <rosemarie.bentele@sap.com>
2021-12-01 08:24:33 +0100

    check error message

--- cmd/abapEnvironmentBuild.go
+++ cmd/abapEnvironmentBuild.go
@@ -94,6 +94,13 @@ func abapEnvironmentBuild(config abapEnvironmentBuildOptions, telemetryData *tel
 func runAbapEnvironmentBuild(config *abapEnvironmentBuildOptions, telemetryData *telemetry.CustomData, utils abapEnvironmentBuildUtils, cpe *abapEnvironmentBuildCommonPipelineEnvironment) error {
 	conn := new(abapbuild.Connector)
 
+	//TODO checke mal warum da immer der falsche fehler steht..
+
+	log.Entry().Info("blubblub")
+	log.Entry().Info("and more stuff")
+	log.SetErrorCategory(log.ErrorConfiguration)
+	return errors.New("Das ist ein FEHLER!")
+
 	// TODO wrappe die fehler
 	if err := initConnection(conn, config, utils); err != nil {
 		return err
