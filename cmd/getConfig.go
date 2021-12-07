7853def09e3bf0fb7e53ecd193597db15635c65b
Christopher Fenner <Christopher.Fenner@sap.com>
2021-12-07 15:24:22 +0100

    add default metadata resolver

--- cmd/getConfig.go
+++ cmd/getConfig.go
@@ -127,6 +127,9 @@ func getConfig() (config.StepConfig, error) {
 
 	} else {
 		log.Entry().Infof("Printing stepName %s", configOptions.stepName)
+		if GeneralConfig.MetaDataResolver == nil {
+			GeneralConfig.MetaDataResolver = GetAllStepMetadata
+		}
 		metadata, err := config.ResolveMetadata(GeneralConfig.GitHubAccessTokens, GeneralConfig.MetaDataResolver, configOptions.stepMetadata, configOptions.stepName)
 		if err != nil {
 			return stepConfig, errors.Wrapf(err, "failed to resolve metadata")
