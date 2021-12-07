c248941fd860be8056d1d97dcad6502890931e68
Christopher Fenner <Christopher.Fenner@sap.com>
2021-12-07 14:26:35 +0100

    fix something

--- cmd/getConfig.go
+++ cmd/getConfig.go
@@ -127,7 +127,7 @@ func getConfig() (config.StepConfig, error) {
 
 	} else {
 		log.Entry().Infof("Printing stepName %s", configOptions.stepName)
-		metadata, err := config.ResolveMetadata(GeneralConfig.GitHubAccessTokens, GetAllStepMetadata, configOptions.stepMetadata, configOptions.stepName)
+		metadata, err := config.ResolveMetadata(GeneralConfig.GitHubAccessTokens, GeneralConfig.MetaDataResolver, configOptions.stepMetadata, configOptions.stepName)
 		if err != nil {
 			return stepConfig, errors.Wrapf(err, "failed to resolve metadata")
 		}
