ec3ef302e4752a8f13be45a542b40f8f24de8e10
Mahesh Srikrishnan <mahesh.srikrishnan@sap.com>
2021-12-14 10:23:05 +0530

    Change for conflict resolution
    Change-Id: Ic955833eca844f090b7983f99f9d3649ebb981c7

--- test/groovy/CommonStepsTest.groovy
+++ test/groovy/CommonStepsTest.groovy
@@ -205,8 +205,8 @@ public class CommonStepsTest extends BasePiperTest{
         'golangBuild', //implementing new golang pattern without fields
         'apiProxyDownload', //implementing new golang pattern without fields
         'apiKeyValueMapDownload', //implementing new golang pattern without fields
+        'apiProviderDownload', //implementing new golang pattern without fields
         'shellExecute', //implementing new golang pattern without fields
-		'apiProviderDownload', //implementing new golang pattern without fields
     ]
 
     @Test
