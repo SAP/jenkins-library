cd243ee54277e794f95a4eef17a15946654347f4
Siarhei Pazdniakou <siarhei.pazdniakou@sap.com>
2021-12-15 17:07:47 +0300

    feat(gcs): allow upload to gcs from steps (#3034)
    * Upload reports to Google Cloud Storage bucket
    
    * Added tests. Made fixes
    
    * Update step generation. GCS client was moved to GeneralConfig
    
    * Code was refactored
    
    * Fixed issues
    
    * Fixed issues
    
    * Code correction due to PR comments
    
    * Improved gcs client and integration tests
    
    * Integrated gcp config. Updated step metadata
    
    * Fixed issues. Added tests
    
    * Added cpe, vault, aliases resolving for reporting parameters
    
    * Added tests
    
    * Uncommented DeferExitHandler. Removed useless comments
    
    * fixed cloning of config
    
    * Added comments for exported functions. Removed unused mock
    
    * minor fix
    
    * Implemented setting of report name via paramRef
    
    * some refactoring. Writing tests
    
    * Update pkg/config/reporting.go
    
    * Update cmd/sonarExecuteScan_generated.go
    
    * Apply suggestions from code review
    
    * Update pkg/config/reporting.go
    
    * Update pkg/config/reporting.go
    
    * fixed removing valut secret files
    
    * Update pkg/config/reporting.go
    
    * restore order
    
    * restore order
    
    * Apply suggestions from code review
    
    * go generate
    
    * fixed tests
    
    * Update resources/metadata/sonarExecuteScan.yaml
    
    * Update resources.go
    
    * Fixed tests. Code was regenerated
    
    * changed somewhere gcp to gcs. Fixed one test
    
    * move gcsSubFolder to input parameters
    
    * fixed removing valut secret files
    
    * minor fix in integration tests
    
    * fix integration tests
    
    Co-authored-by: Oliver Nocon <33484802+OliverNocon@users.noreply.github.com>
    Co-authored-by: Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
    Co-authored-by: Sven Merk <33895725+nevskrem@users.noreply.github.com>

--- cmd/whitesourceExecuteScan_generated.go
+++ cmd/whitesourceExecuteScan_generated.go
@@ -213,9 +213,9 @@ The step uses the so-called WhiteSource Unified Agent. For details please refer 
 			stepTelemetryData := telemetry.CustomData{}
 			stepTelemetryData.ErrorCode = "1"
 			handler := func() {
-				config.RemoveVaultSecretFiles()
 				commonPipelineEnvironment.persist(GeneralConfig.EnvRootPath, "commonPipelineEnvironment")
 				influx.persist(GeneralConfig.EnvRootPath, "influx")
+				config.RemoveVaultSecretFiles()
 				stepTelemetryData.Duration = fmt.Sprintf("%v", time.Since(startTime).Milliseconds())
 				stepTelemetryData.ErrorCategory = log.GetErrorCategory().String()
 				stepTelemetryData.PiperCommitHash = GitCommit
@@ -793,7 +793,7 @@ func whitesourceExecuteScanMetadata() config.StepData {
 						Name: "commonPipelineEnvironment",
 						Type: "piperEnvironment",
 						Parameters: []map[string]interface{}{
-							{"name": "custom/whitesourceProjectNames"},
+							{"name": "custom/whitesourceProjectNames", "type": "[]string"},
 						},
 					},
 					{
