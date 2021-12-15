c8247b5046fb9a09f6f416c8838d66d63cba88f7
CCFenner <christopher.fenner@sap.com>
2021-12-15 14:10:29 +0100

    update generated sources

--- cmd/abapEnvironmentBuild_generated.go
+++ cmd/abapEnvironmentBuild_generated.go
@@ -65,7 +65,7 @@ func (p *abapEnvironmentBuildCommonPipelineEnvironment) persist(path, resourceNa
 		}
 	}
 	if errCount > 0 {
-		log.Entry().Fatal("failed to persist Piper environment")
+		log.Entry().Error("failed to persist Piper environment")
 	}
 }
 
