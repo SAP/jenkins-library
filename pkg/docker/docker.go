0b45e07c3b5de7f44399292ed0811a31dfaf3e6d
Your Name <you@example.com>
2021-12-20 10:47:37 +0100

    creating directory before write

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/SAP/jenkins-library/pkg/piperutils"
@@ -40,12 +42,12 @@ func CreateDockerConfigJSON(registryURL, username, password, targetPath, configP
 		}
 	} else {
 		// if the targetPath is a directory path then we must create it before we write
-		// if strings.Contains(targetPath, string(os.PathSeparator)) {
-		// 	err := utils.MkdirAll(filepath.Dir(targetPath), 0666)
-		// 	if err != nil {
-		// 		return "", fmt.Errorf("failed to create the Docker config.json file %v:%w", targetPath, err)
-		// 	}
-		// }
+		if strings.Contains(targetPath, string(os.PathSeparator)) {
+			err := utils.MkdirAll(filepath.Dir(targetPath), 0666)
+			if err != nil {
+				return "", fmt.Errorf("failed to create the Docker config.json file %v:%w", targetPath, err)
+			}
+		}
 	}
 
 	credentialsBase64 := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%v:%v", username, password)))
