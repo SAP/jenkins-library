bbe3109419279f1ac6976d6618c631582b739f07
Mayur Belur Mohan <68264167+mayurmohan@users.noreply.github.com>
2021-12-28 14:01:50 +0530

    ApiProxyUpload Command (#3295)
    * ApiProxyUpload Command
    
    * Code Review Fixes
    
    * CodeReview Changes
    
    * CodeReview Fixes
    
    * YAML fixes
    
    * CodeReview Fix
    
    * Code Review Fixes
    
    * CodeReview Fixes
    
    * Code Climate Fixes
    
    * Code Review Fixes
    
    * Code Review Fixes
    
    Co-authored-by: Roland Stengel <r.stengel@sap.com>
    Co-authored-by: Oliver Feldmann <oliver.feldmann@sap.com>

--- cmd/metadata_generated.go
+++ cmd/metadata_generated.go
@@ -26,6 +26,7 @@ func GetAllStepMetadata() map[string]config.StepData {
 		"abapEnvironmentRunAUnitTest":               abapEnvironmentRunAUnitTestMetadata(),
 		"apiKeyValueMapDownload":                    apiKeyValueMapDownloadMetadata(),
 		"apiProxyDownload":                          apiProxyDownloadMetadata(),
+		"apiProxyUpload":                            apiProxyUploadMetadata(),
 		"artifactPrepareVersion":                    artifactPrepareVersionMetadata(),
 		"batsExecuteTests":                          batsExecuteTestsMetadata(),
 		"checkmarxExecuteScan":                      checkmarxExecuteScanMetadata(),
