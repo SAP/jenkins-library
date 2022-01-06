d2aaf43409a76634943a5ec3a6b753e96b2cdd0e
mayurmohan <68264167+mayurmohan@users.noreply.github.com>
2022-01-06 14:25:04 +0530

    include command in metadata

--- cmd/metadata_generated.go
+++ cmd/metadata_generated.go
@@ -24,6 +24,7 @@ func GetAllStepMetadata() map[string]config.StepData {
 		"abapEnvironmentRunATCCheck":                abapEnvironmentRunATCCheckMetadata(),
 		"abapEnvironmentRunAUnitTest":               abapEnvironmentRunAUnitTestMetadata(),
 		"apiKeyValueMapDownload":                    apiKeyValueMapDownloadMetadata(),
+		"apiKeyValueMapUpload":                      apiKeyValueMapUploadMetadata(),
 		"apiProxyDownload":                          apiProxyDownloadMetadata(),
 		"apiProxyUpload":                            apiProxyUploadMetadata(),
 		"artifactPrepareVersion":                    artifactPrepareVersionMetadata(),
