b167b029ea0ece8fac0b4f85f8a679aa897a098a
anilkeshav27 <you@example.com>
2022-01-12 18:19:41 +0100

    changing encoding param

--- pkg/config/vault.go
+++ pkg/config/vault.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/SAP/jenkins-library/pkg/config/interpolation"
 	"github.com/SAP/jenkins-library/pkg/log"
+	CredentialUtils "github.com/SAP/jenkins-library/pkg/piperutils"
 	"github.com/SAP/jenkins-library/pkg/vault"
 	"github.com/hashicorp/vault/api"
 )
@@ -282,7 +283,7 @@ func populateCredentialsAsEnvs(config *StepConfig, secret map[string]string, key
 				os.Setenv(envVariable, secretValue)
 				envVariable = vaultCredentialEnvPrefix + convertEnvVar(secretKey) + "_BASE64"
 				log.Entry().Debugf("Exposing general purpose base64 encoded credential '%v' as '%v'", key, envVariable)
-				os.Setenv(envVariable, secretValue)
+				os.Setenv(envVariable, CredentialUtils.EncodeString(secretValue))
 				matched = true
 			}
 		}
@@ -300,7 +301,7 @@ func populateCredentialsAsEnvs(config *StepConfig, secret map[string]string, key
 					os.Setenv(envVariable, secretValue)
 					envVariable = vaultCredentialEnvPrefixDefault + convertEnvVar(secretKey) + "_BASE64"
 					log.Entry().Debugf("Exposing general purpose base64 encoded credential '%v' as '%v'", key, envVariable)
-					os.Setenv(envVariable, secretValue)
+					os.Setenv(envVariable, CredentialUtils.EncodeString(secretValue))
 					matched = true
 				}
 			}
