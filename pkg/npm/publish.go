6a7063b1ee646be94eb2ac5641507d5964431ef1
anilkeshav27 <you@example.com>
2022-01-18 12:29:50 +0100

    changing scope position

--- pkg/npm/publish.go
+++ pkg/npm/publish.go
@@ -91,7 +91,7 @@ func (exec *Execute) publish(packageJSON, registry, username, password string) e
 		log.Entry().Debug("no registry provided")
 	}
 
-	err := execRunner.RunExecutable("npm", "publish", "--userconfig", npmrc.filepath, "--@pipertest:registry="+registry)
+	err := execRunner.RunExecutable("npm", "publish", "--@pipertest:registry="+registry, "--userconfig", npmrc.filepath)
 	if err != nil {
 		return err
 	}
