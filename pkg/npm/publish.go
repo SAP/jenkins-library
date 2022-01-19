feaa8999dc99e6b56ea43b3314c71b420f9a3929
anilkeshav27 <you@example.com>
2022-01-19 19:28:16 +0100

    error renaming old npmrc file

--- pkg/npm/publish.go
+++ pkg/npm/publish.go
@@ -120,7 +120,10 @@ func (exec *Execute) publish(packageJSON, registry, username, password string, p
 			return err
 		}
 
-		os.Rename(filepath.Join(packageJSON, ".npmrc"), filepath.Join(packageJSON, ".oldnpmrc"))
+		err = os.Rename(filepath.Join(packageJSON, ".npmrc"), filepath.Join(packageJSON, ".oldnpmrc"))
+		if err != nil {
+			return fmt.Errorf("error when renaming current .npmrc file")
+		}
 
 		err = os.Chdir(tmpDirectory)
 		if err != nil {
