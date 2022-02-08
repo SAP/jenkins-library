c0871b1bc3ebb7e00bd224a0a0ae28b3930e31de
Daniel Mieg <daniel.mieg@sap.com>
2022-02-08 17:58:05 +0100

    Format

--- pkg/abaputils/manageGitRepositoryUtils_test.go
+++ pkg/abaputils/manageGitRepositoryUtils_test.go
@@ -9,13 +9,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var logResultSuccess = fmt.Sprintf(`{"d": { "sc_name": "/DMO/SWC", "status": "S", "to_Log_Overview": { "results": [ { "log_index": 1, "log_name": "Main Import", "type_of_found_issues": "Success", "timestamp": "/Date(1644332299000+0000)/", "to_Log_Protocol": { "results": [ { "log_index": 1, "index_no": "1", "log_name": "", "type": "Info", "descr": "Main import", "timestamp": null, "criticality": 0 } ] } } ] } } }`)
-var logResultError = fmt.Sprintf(`{"d": { "sc_name": "/DMO/SWC", "status": "S", "to_Log_Overview": { "results": [ { "log_index": 1, "log_name": "Main Import", "type_of_found_issues": "Error", "timestamp": "/Date(1644332299000+0000)/", "to_Log_Protocol": { "results": [ { "log_index": 1, "index_no": "1", "log_name": "", "type": "Info", "descr": "Main import", "timestamp": null, "criticality": 0 } ] } } ] } } }`)
-
 func TestPollEntity(t *testing.T) {
 
 	t.Run("Test poll entity - success case", func(t *testing.T) {
 
+		var logResultSuccess = fmt.Sprintf(`{"d": { "sc_name": "/DMO/SWC", "status": "S", "to_Log_Overview": { "results": [ { "log_index": 1, "log_name": "Main Import", "type_of_found_issues": "Success", "timestamp": "/Date(1644332299000+0000)/", "to_Log_Protocol": { "results": [ { "log_index": 1, "index_no": "1", "log_name": "", "type": "Info", "descr": "Main import", "timestamp": null, "criticality": 0 } ] } } ] } } }`)
 		client := &ClientMock{
 			BodyList: []string{
 				logResultSuccess,
@@ -53,7 +51,7 @@ func TestPollEntity(t *testing.T) {
 	})
 
 	t.Run("Test poll entity - error case", func(t *testing.T) {
-
+		var logResultError = fmt.Sprintf(`{"d": { "sc_name": "/DMO/SWC", "status": "S", "to_Log_Overview": { "results": [ { "log_index": 1, "log_name": "Main Import", "type_of_found_issues": "Error", "timestamp": "/Date(1644332299000+0000)/", "to_Log_Protocol": { "results": [ { "log_index": 1, "index_no": "1", "log_name": "", "type": "Info", "descr": "Main import", "timestamp": null, "criticality": 0 } ] } } ] } } }`)
 		client := &ClientMock{
 			BodyList: []string{
 				logResultError,
