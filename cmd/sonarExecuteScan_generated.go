599e4fec0d6ef6f9499b0731cd0c385cc65cc323
Linda Siebert <linda.siebert@sap.com>
2022-02-10 09:38:20 +0100

    Change go

--- cmd/sonarExecuteScan_generated.go
+++ cmd/sonarExecuteScan_generated.go
@@ -275,6 +275,9 @@ func sonarExecuteScanMetadata() config.StepData {
 					{Name: "sonarTokenCredentialsId", Description: "Jenkins 'Secret text' credentials ID containing the token used to authenticate with the Sonar Server.", Type: "jenkins"},
 					{Name: "githubTokenCredentialsId", Description: "Jenkins 'Secret text' credentials ID containing the token used to authenticate with the Github Server.", Type: "jenkins"},
 				},
+				Resources: []config.StepResources{
+					{Name: "application", Type: "stash"},
+				},
 				Parameters: []config.StepParameters{
 					{
 						Name:        "instance",
