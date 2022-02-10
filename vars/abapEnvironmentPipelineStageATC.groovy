fb9c5a3e68be8bbfd1aaa3f0ef212fbaa0dcafda
Daniel Bernd <93763187+danManSAP@users.noreply.github.com>
2022-02-10 11:25:50 +0100

    Missimg Import 2

--- vars/abapEnvironmentPipelineStageATC.groovy
+++ vars/abapEnvironmentPipelineStageATC.groovy
@@ -2,6 +2,9 @@ import groovy.transform.Field
 import com.cloudbees.groovy.cps.NonCPS
 import com.sap.piper.GenerateStageDocumentation
 import groovy.transform.Field
+import com.sap.piper.Utils
+import com.sap.piper.ConfigurationHelper
+import com.sap.piper.ConfigurationLoader
 
 import static com.sap.piper.Prerequisites.checkScript
 
