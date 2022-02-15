eea247a367aaed42ee18e8b68d01b7711dfbc6a8
Sven Merk <s.merk@sap.com>
2022-02-15 21:20:41 +0100

    Transport wrapping

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -42,7 +42,6 @@ type CreateIssueOptions struct {
 func NewClient(token, apiURL, uploadURL string, trustedCerts []string) (context.Context, *github.Client, error) {
 	httpClient := piperhttp.Client{}
 	httpClient.SetOptions(piperhttp.ClientOptions{
-		Token: fmt.Sprintf("Bearer %v", token),
 		TransportSkipVerification: true,
 		TrustedCerts: trustedCerts,
 		DoLogRequestBodyOnDebug: true,
@@ -50,7 +49,8 @@ func NewClient(token, apiURL, uploadURL string, trustedCerts []string) (context.
 	})
 	stdClient := httpClient.StandardClient()
 	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, stdClient)
-	//tc := oauth2.NewClient(ctx, &httpClient)
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
 
 	if !strings.HasSuffix(apiURL, "/") {
 		apiURL += "/"
@@ -68,7 +68,7 @@ func NewClient(token, apiURL, uploadURL string, trustedCerts []string) (context.
 		return ctx, nil, err
 	}
 
-	client := github.NewClient(stdClient)
+	client := github.NewClient(tc)
 
 	client.BaseURL = baseURL
 	client.UploadURL = uploadTargetURL
