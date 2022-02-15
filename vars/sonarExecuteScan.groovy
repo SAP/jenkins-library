b0a79ed53ad95ce2c55678ccbda2969a628c742c
Linda Siebert <linda.siebert@sap.com>
2022-02-15 14:55:34 +0100

    Move Artifacts

--- vars/sonarExecuteScan.groovy
+++ vars/sonarExecuteScan.groovy
@@ -73,13 +73,13 @@ void call(Map parameters = [:]) {
                                         withSonarQubeEnv(stepConfig.instance) {
                                             echo "Instance is deprecated - please use serverUrl parameter to set URL to the Sonar backend."
                                             sh "${piperGoPath} ${STEP_NAME}${customDefaultConfig}${customConfigArg}"
+                                            archiveArtifacts artifacts: "sonarscan.json", allowEmptyArchive: true
+                                            jenkinsUtils.handleStepResults(STEP_NAME, false, false)
                                         }
                                     } else {
                                         sh "${piperGoPath} ${STEP_NAME}${customDefaultConfig}${customConfigArg}"
                                     }
-                                    archiveArtifacts artifacts: "sonarscan.json", allowEmptyArchive: true
                                 }
-                                jenkinsUtils.handleStepResults(STEP_NAME, false, false)
                                 readPipelineEnv(script: script, piperGoPath: piperGoPath)
                             }
                         }
