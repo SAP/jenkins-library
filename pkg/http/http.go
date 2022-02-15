80adf3e08012e0af097939b67acbd635109a134e
Sven Merk <s.merk@sap.com>
2022-02-15 22:24:02 +0100

    Debug

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -400,7 +400,7 @@ func (t *TransportWrapper) logResponse(resp *http.Response) {
 func transformHeaders(header http.Header) http.Header {
 	var h http.Header = map[string][]string{}
 	for name, value := range header {
-		if name == "Authorization" {
+		if name == "uthorization" {
 			for _, v := range value {
 				// The format of the Authorization header value is: <type> <cred>.
 				// We don't register the full string since only the part after
