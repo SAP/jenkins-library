e3dc52167f1c155d09d89f22bed933d35bf0e467
TheShifter <mrmixal33@gmail.com>
2022-02-15 11:16:35 +0300

    Rewrite some logic. Minor fix in integration tests for python

--- cmd/pythonBuild.go
+++ cmd/pythonBuild.go
@@ -6,6 +6,7 @@ import (
 	"github.com/SAP/jenkins-library/pkg/log"
 	"github.com/SAP/jenkins-library/pkg/piperutils"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
+	"os"
 )
 
 type pythonBuildUtils interface {
@@ -40,45 +41,59 @@ func pythonBuild(config pythonBuildOptions, telemetryData *telemetry.CustomData)
 }
 
 func runPythonBuild(config *pythonBuildOptions, telemetryData *telemetry.CustomData, utils pythonBuildUtils) error {
-	if len(config.Sources) == 0 {
-		log.Entry().Errorln("link to python project dir is not defined in input parameters")
-	}
 
-	for _, source := range config.Sources {
-		exists, err := utils.DirExists(source)
-		if err != nil {
-			log.Entry().WithError(err).Error("failed to check for python project dir")
-			return fmt.Errorf("failed to check for python project dir: %w", err)
-		}
-		if !exists {
-			log.Entry().WithError(err).Errorf("the python project dir '%v' could not be found: %v", source, err)
-			return fmt.Errorf("the python project dir '%v' could not be found", source)
-		} else {
-			tomlExists, err := utils.FileExists(source + "/pyproject.toml")
+	if len(config.Sources) != 0 {
+		for _, source := range config.Sources {
+			exists, err := utils.DirExists(source)
 			if err != nil {
-				log.SetErrorCategory(log.ErrorConfiguration)
-				return fmt.Errorf("failed to check for important file: %w", err)
+				log.Entry().WithError(err).Error("failed to check for python project dir")
+				return fmt.Errorf("failed to check for python project dir: %w", err)
 			}
-			if !tomlExists {
-				log.SetErrorCategory(log.ErrorConfiguration)
-				return fmt.Errorf("cannot run without important file")
+			if !exists {
+				log.Entry().WithError(err).Errorf("the python project dir '%v' could not be found: %v", source, err)
+				return fmt.Errorf("the python project dir '%v' could not be found", source)
+			} else {
+				tomlExists, err := utils.FileExists(source + "/pyproject.toml")
+				if err != nil {
+					log.SetErrorCategory(log.ErrorConfiguration)
+					return fmt.Errorf("failed to check for important file: %w", err)
+				}
+				if !tomlExists {
+					log.SetErrorCategory(log.ErrorConfiguration)
+					return fmt.Errorf("cannot run without important file")
+				}
+			}
+			err = buildExecute(source, config, utils)
+			if err != nil {
+
 			}
 		}
-
-		var flags []string
-		flags = append(flags, "-m", "build", source)
-		flags = append(flags, config.BuildFlags...)
-
-		if err := utils.RunExecutable("python3", "-m", "pip", "install", "--upgrade", "build"); err != nil {
-			return fmt.Errorf("failed to install 'build': %w", err)
-		}
-
-		log.Entry().Info("starting building python project:", source)
-		err = utils.RunExecutable("python3", flags...)
+	} else {
+		source, err := os.Getwd()
 		if err != nil {
-			log.Entry().Errorln("starting building python project:", source)
+			log.Entry().WithError(err).Error("Getting current working directory failed.")
 		}
+
+		err = buildExecute(source, config, utils)
 	}
 
+	return nil
+}
+
+func buildExecute(source string, config *pythonBuildOptions, utils pythonBuildUtils) error {
+	var flags []string
+	flags = append(flags, "-m", "build")
+
+	if err := utils.RunExecutable("python3", "-m", "pip", "install", "--upgrade", "build"); err != nil {
+		return fmt.Errorf("failed to install 'build': %w", err)
+	}
+	flags = append(flags, source)
+	flags = append(flags, config.BuildFlags...)
+
+	log.Entry().Info("starting building python project:", source)
+	err := utils.RunExecutable("python3", flags...)
+	if err != nil {
+		log.Entry().Errorln("starting building python project can't start:", err)
+	}
 	return nil
 }
--- integration/integration_python_build_test.go
+++ integration/integration_python_build_test.go
@@ -12,8 +12,30 @@ import (
 func TestBuildProject(t *testing.T) {
 	t.Parallel()
 	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
-		Image:   "python:3.9",
-		TestDir: []string{"testdata", "TestPythonIntegration"},
+		Image: "python:3.9",
+	})
+
+	err := container.whenRunningPiperCommand("python3", "-m", "pip", "install", "--upgrade", "build")
+	if err != nil {
+		t.Fatalf("Calling piper command failed %s", err)
+	}
+	var flags []string
+	flags = append(flags, "-m", "build", "./integration/testdata/TestPythonIntegration")
+
+	err = container.whenRunningPiperCommand("python3", flags...)
+	if err != nil {
+		t.Fatalf("Calling piper command failed %s", err)
+	}
+
+	container.assertHasOutput(t, "Successfully built example-package-YOUR-USERNAME-HERE-0.0.1.tar.gz and example_package_YOUR_USERNAME_HERE-0.0.1-py3-none-any.whl")
+	container.assertHasFile(t, "/dist/example-package-YOUR-USERNAME-HERE-0.0.1.tar.gz")
+	container.assertHasFile(t, "/dist/example_package_YOUR_USERNAME_HERE-0.0.1-py3-none-any.whl")
+}
+
+func TestBuildProjectWithoutSource(t *testing.T) {
+	t.Parallel()
+	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
+		Image: "python:3.9",
 	})
 
 	err := container.whenRunningPiperCommand("python3", "-m", "pip", "install", "--upgrade", "build")
@@ -28,7 +50,5 @@ func TestBuildProject(t *testing.T) {
 		t.Fatalf("Calling piper command failed %s", err)
 	}
 
-	container.assertHasOutput(t, "Successfully built example-package-YOUR-USERNAME-HERE-0.0.1.tar.gz and example_package_YOUR_USERNAME_HERE-0.0.1-py3-none-any.whl")
-	container.assertHasFile(t, "/dist/example-package-YOUR-USERNAME-HERE-0.0.1.tar.gz")
-	container.assertHasFile(t, "/dist/example_package_YOUR_USERNAME_HERE-0.0.1-py3-none-any.whl")
+	container.assertHasOutput(t, "link to python project dir is not defined in input parameters")
 }
