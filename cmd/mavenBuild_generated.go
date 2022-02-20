4e4647440512b94cea5a7216a00cfa1dec041f7c
Felix Feldmann <F.Feldmann@sap.com>
2022-02-20 12:59:18 +0100

    Adds generated files

--- cmd/mavenBuild_generated.go
+++ cmd/mavenBuild_generated.go
@@ -89,6 +89,7 @@ func (p *mavenBuildReports) persist(stepConfig mavenBuildOptions, gcpJsonKeyFile
 	gcsClient, err := gcs.NewClient(gcs.WithEnvVars(envVars))
 	if err != nil {
 		log.Entry().Errorf("creation of GCS client failed: %v", err)
+		return
 	}
 	defer gcsClient.Close()
 	structVal := reflect.ValueOf(&stepConfig).Elem()
