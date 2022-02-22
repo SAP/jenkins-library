ffd4a7efb862650e17b01105c7a6becc477f16b2
ffeldmann <f.feldmann@sap.com>
2022-02-22 18:32:09 +0100

    (fix) nil pointer dereference in case credentials are not available (#3564)
    * adds return in gcs upload in case error occurs e.g. no credentials, avoid nil pointer dereference
    
    * Adds generated files
    
    * Updates generated files
    
    Co-authored-by: Oliver Nocon <33484802+OliverNocon@users.noreply.github.com>

--- cmd/golangBuild_generated.go
+++ cmd/golangBuild_generated.go
@@ -95,6 +95,7 @@ func (p *golangBuildReports) persist(stepConfig golangBuildOptions, gcpJsonKeyFi
 	gcsClient, err := gcs.NewClient(gcs.WithEnvVars(envVars))
 	if err != nil {
 		log.Entry().Errorf("creation of GCS client failed: %v", err)
+		return
 	}
 	defer gcsClient.Close()
 	structVal := reflect.ValueOf(&stepConfig).Elem()
