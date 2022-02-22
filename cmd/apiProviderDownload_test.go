f27b38b40eb61bfa8dbe8271c7a0dde08f9ee350
Mahesh Srikrishnan <mahesh.srikrishnan@sap.com>
2022-02-22 14:53:28 +0530

    Added testcase to validate file content
    Change-Id: I21aed481b433450c3b536dbb29d45291f61848d8

--- cmd/apiProviderDownload_test.go
+++ cmd/apiProviderDownload_test.go
@@ -44,8 +44,15 @@ func TestApiProviderDownloadSuccess(t *testing.T) {
 		err := runApiProviderDownload(&config, nil, &httpClient, utilsMock)
 
 		if assert.NoError(t, err) {
+
 			t.Run("Assert file download", func(t *testing.T) {
 				assert.True(t, utilsMock.HasWrittenFile(config.DownloadPath))
+			})
+
+			t.Run("Assert file content", func(t *testing.T) {
+				providerbyteContent, _ := utilsMock.FileRead(config.DownloadPath)
+				providerContent := string(providerbyteContent)
+				assert.Equal(t, providerContent, "{\n\t\t\t\t\"d\": {\n\t\t\t\t\t\"results\": [\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\t\"__metadata\": {\n\t\t\t\t\t\t\t\t\"id\": \"https://roverpoc.it-accd002.cfapps.sap.hana.ondemand.com:443/api/v1/MessageProcessingLogs('AGAS1GcWkfBv-ZtpS6j7TKjReO7t')\",\n\t\t\t\t\t\t\t\t\"uri\": \"https://roverpoc.it-accd002.cfapps.sap.hana.ondemand.com:443/api/v1/MessageProcessingLogs('AGAS1GcWkfBv-ZtpS6j7TKjReO7t')\",\n\t\t\t\t\t\t\t\t\"type\": \"com.sap.hci.api.MessageProcessingLog\"\n\t\t\t\t\t\t\t},\n\t\t\t\t\t\t\t\"MessageGuid\": \"AGAS1GcWkfBv-ZtpS6j7TKjReO7t\",\n\t\t\t\t\t\t\t\"CorrelationId\": \"AGAS1GevYrPodxieoYf4YSY4jd-8\",\n\t\t\t\t\t\t\t\"ApplicationMessageId\": null,\n\t\t\t\t\t\t\t\"ApplicationMessageType\": null,\n\t\t\t\t\t\t\t\"LogStart\": \"/Date(1611846759005)/\",\n\t\t\t\t\t\t\t\"LogEnd\": \"/Date(1611846759032)/\",\n\t\t\t\t\t\t\t\"Sender\": null,\n\t\t\t\t\t\t\t\"Receiver\": null,\n\t\t\t\t\t\t\t\"IntegrationFlowName\": \"flow1\",\n\t\t\t\t\t\t\t\"Status\": \"COMPLETED\",\n\t\t\t\t\t\t\t\"LogLevel\": \"INFO\",\n\t\t\t\t\t\t\t\"CustomStatus\": \"COMPLETED\",\n\t\t\t\t\t\t\t\"TransactionId\": \"aa220151116748eeae69db3e88f2bbc8\"\n\t\t\t\t\t\t}\n\t\t\t\t\t]\n\t\t\t\t}\n\t\t\t}")
 			})
 
 			t.Run("Assert API Provider url", func(t *testing.T) {
