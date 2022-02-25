e473dd3e232c8569f3abb6085f17ec2dc1df6e7e
Linda Siebert <linda.siebert@sap.com>
2022-02-25 10:24:50 +0100

    Test if warning goes away

--- vars/sonarExecuteScan.groovy
+++ vars/sonarExecuteScan.groovy
@@ -72,12 +72,12 @@ void call(Map parameters = [:]) {
                                             sh "${piperGoPath} ${STEP_NAME}${customDefaultConfig}${customConfigArg}"
                                             archiveArtifacts artifacts: "sonarscan.json", allowEmptyArchive: true
                                             jenkinsUtils.handleStepResults(STEP_NAME, false, false)
+                                            readPipelineEnv(script: script, piperGoPath: piperGoPath)
                                         }
                                     } else {
                                         sh "${piperGoPath} ${STEP_NAME}${customDefaultConfig}${customConfigArg}"
                                     }
                                 }
-                                readPipelineEnv(script: script, piperGoPath: piperGoPath)
                             }
                         }
                     }
