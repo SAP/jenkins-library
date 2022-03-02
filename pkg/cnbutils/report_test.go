f22139978da667a1c1a2e00ccf20b00eb3688bb7
Pavel Busko <pavel.busko@sap.com>
2022-03-02 16:07:03 +0100

    added unit tests
    Co-authored-by: Johannes Dillmann <j.dillmann@sap.com>

--- pkg/cnbutils/report_test.go
+++ pkg/cnbutils/report_test.go
@@ -1,1 +1,48 @@
 package cnbutils_test
+
+import (
+	"testing"
+
+	"github.com/SAP/jenkins-library/pkg/cnbutils"
+	"github.com/SAP/jenkins-library/pkg/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDigestFromReport(t *testing.T) {
+	t.Run("return a digest from the report.toml", func(t *testing.T) {
+		mockUtils := &cnbutils.MockUtils{
+			FilesMock: &mock.FilesMock{},
+		}
+		mockUtils.AddFile("/layers/report.toml", []byte(`[build]
+[image]
+digest = "sha256:52eac630560210e5ae13eb10797c4246d6f02d425f32b9430ca00bde697c79ec"
+`))
+		digest, err := cnbutils.DigestFromReport(mockUtils)
+		assert.NoError(t, err)
+		assert.Equal(t, "sha256:52eac630560210e5ae13eb10797c4246d6f02d425f32b9430ca00bde697c79ec", digest)
+	})
+
+	t.Run("fails if digest is empty", func(t *testing.T) {
+		mockUtils := &cnbutils.MockUtils{
+			ExecMockRunner: &mock.ExecMockRunner{},
+			FilesMock:      &mock.FilesMock{},
+		}
+		mockUtils.AddFile("/layers/report.toml", []byte(``))
+
+		digest, err := cnbutils.DigestFromReport(mockUtils)
+		assert.Empty(t, digest)
+		assert.EqualError(t, err, "image digest is empty")
+	})
+
+	t.Run("fails to unmarshal corrupted file", func(t *testing.T) {
+		mockUtils := &cnbutils.MockUtils{
+			ExecMockRunner: &mock.ExecMockRunner{},
+			FilesMock:      &mock.FilesMock{},
+		}
+		mockUtils.AddFile("/layers/report.toml", []byte(`{}`))
+
+		digest, err := cnbutils.DigestFromReport(mockUtils)
+		assert.Empty(t, digest)
+		assert.EqualError(t, err, "(1, 1): parsing error: keys cannot contain { character")
+	})
+}
