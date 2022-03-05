8cfae3e72a817c3b2d42e6f930d3b6b528a840ec
Felix Feldmann <F.Feldmann@sap.com>
2022-03-05 22:36:38 +0100

    Refactors logfile sending logic, renaming of fields, adds proper piper sourcetype

--- pkg/splunk/data.go
+++ pkg/splunk/data.go
@@ -24,23 +24,20 @@ type MonitoringData struct {
 	Duration        string `json:"Duration,omitempty"`
 	ErrorCode       string `json:"ErrorCode,omitempty"`
 	ErrorCategory   string `json:"ErrorCategory,omitempty"`
-	CorrelationID   string `json:"CorrelationID,omitempty"`
+	CorrelationId   string `json:"CorrelationId,omitempty"`
 	CommitHash      string `json:"CommitHash,omitempty"`
 	Branch          string `json:"Branch,omitempty"`
 	GitOwner        string `json:"GitOwner,omitempty"`
 	GitRepository   string `json:"GitRepository,omitempty"`
 }
 
-type LogFileEvents struct {
-	Messages  []string               `json:"messages,omitempty"`  // messages
-	Telemetry map[string]interface{} `json:"telemetry,omitempty"` // telemetryData
-}
-type DetailsLog struct {
-	Host       string        `json:"host"`                 // hostname
-	Source     string        `json:"source,omitempty"`     // optional description of the source of the event; typically the app's name
-	SourceType string        `json:"sourcetype,omitempty"` // optional name of a Splunk parsing configuration; this is usually inferred by Splunk
-	Index      string        `json:"index,omitempty"`      // optional name of the Splunk index to store the event in; not required if the token has a default index set in Splunk
-	Event      LogFileEvents `json:"event,omitempty"`      // throw any useful key/val pairs here}
+type LogFileEvent struct {
+	Event      string `json:"event"`      // messages
+	Host       string `json:"host"`       // hostname
+	Source     string `json:"source"`     // optional description of the source of the event; typically the app's name
+	SourceType string `json:"sourcetype"` // optional name of a Splunk parsing configuration; this is usually inferred by Splunk
+	Index      string `json:"index"`      // optional name of the Splunk index to store the event in; not required if the token has a default index set in Splunk
+
 }
 
 type DetailsTelemetry struct {
--- pkg/splunk/splunk.go
+++ pkg/splunk/splunk.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -24,7 +25,8 @@ type Splunk struct {
 	levels        []logrus.Level
 	tags          map[string]string
 	splunkClient  piperhttp.Client
-	correlationID string
+	correlationId string
+	hostName      string
 	splunkDsn     string
 	splunkIndex   string
 
@@ -35,7 +37,7 @@ type Splunk struct {
 	postMessagesBatchSize int
 }
 
-func (s *Splunk) Initialize(correlationID, dsn, token, index string, sendLogs bool) error {
+func (s *Splunk) Initialize(correlationId, dsn, token, index string, sendLogs bool) error {
 	log.Entry().Debugf("Initializing Splunk with DSN %v", dsn)
 
 	if !strings.HasPrefix(token, "Splunk ") {
@@ -49,14 +51,20 @@ func (s *Splunk) Initialize(correlationID, dsn, token, index string, sendLogs bo
 		MaxRequestDuration:        5 * time.Second,
 		Token:                     token,
 		TransportSkipVerification: true,
-		MaxRetries:                -1,
+		MaxRetries:                5,
 	})
 
+	hostName, err := os.Hostname()
+	if err != nil {
+		log.Entry().WithError(err).Debug("Could not get hostName.")
+		hostName = "n/a"
+	}
+	s.hostName = hostName
 	s.splunkClient = client
 	s.splunkDsn = dsn
 	s.splunkIndex = index
-	s.correlationID = correlationID
-	s.postMessagesBatchSize = 20000
+	s.correlationId = correlationId
+	s.postMessagesBatchSize = 6000
 	s.sendLogs = sendLogs
 
 	return nil
@@ -113,7 +121,7 @@ func (s *Splunk) prepareTelemetry(telemetryData telemetry.Data) MonitoringData {
 		Duration:        telemetryData.CustomData.Duration,
 		ErrorCode:       telemetryData.CustomData.ErrorCode,
 		ErrorCategory:   telemetryData.CustomData.ErrorCategory,
-		CorrelationID:   s.correlationID,
+		CorrelationId:   s.correlationId,
 		CommitHash:      readCommonPipelineEnvironment("git/headCommitId"),
 		Branch:          readCommonPipelineEnvironment("git/branch"),
 		GitOwner:        readCommonPipelineEnvironment("github/owner"),
@@ -138,11 +146,11 @@ func (s *Splunk) SendPipelineStatus(pipelineTelemetryData map[string]interface{}
 	splitted := strings.Split(readLogFile, "\n")
 	messagesLen := len(splitted)
 
-	log.Entry().Debugf("Sending %v messages to Splunk.", messagesLen)
 	log.Entry().Debugf("Sending pipeline telemetry data to Splunk: %v", pipelineTelemetryData)
 	s.postTelemetry(pipelineTelemetryData)
 
 	if s.sendLogs {
+		log.Entry().Debugf("Sending %v messages to Splunk.", messagesLen)
 		for i := 0; i < messagesLen; i += s.postMessagesBatchSize {
 			upperBound := i + s.postMessagesBatchSize
 			if upperBound > messagesLen {
@@ -162,23 +170,24 @@ func (s *Splunk) postTelemetry(telemetryData map[string]interface{}) error {
 		telemetryData = map[string]interface{}{"Empty": "No telemetry available."}
 	}
 	details := DetailsTelemetry{
-		Host:       s.correlationID,
-		SourceType: "_json",
+		Host:       s.correlationId,
+		SourceType: "piper:pipeline:telemetry",
 		Index:      s.splunkIndex,
 		Event:      telemetryData,
 	}
 
 	payload, err := json.Marshal(details)
-	if err != nil {
-		return errors.Wrap(err, "error while marshalling Splunk message details")
-	}
 
-	prettyPayload, err := json.MarshalIndent(payload, "", "    ")
+	prettyPayload, err := json.MarshalIndent(details, "", "    ")
 	if err != nil {
 		log.Entry().WithError(err).Warn("Failed to generate pretty payload json")
 		prettyPayload = nil
 	}
 	log.Entry().Debugf("Sending the follwing payload to Splunk HEC: %s", string(prettyPayload))
+
+	if err != nil {
+		return errors.Wrap(err, "error while marshalling Splunk message details")
+	}
 
 	resp, err := s.splunkClient.SendRequest(http.MethodPost, s.splunkDsn, bytes.NewBuffer(payload), nil, nil)
 
@@ -211,23 +220,26 @@ func (s *Splunk) postTelemetry(telemetryData map[string]interface{}) error {
 
 func (s *Splunk) postLogFile(telemetryData map[string]interface{}, messages []string) error {
 
-	event := LogFileEvents{
-		Messages:  messages,
-		Telemetry: telemetryData,
+	var logfileEvents []string
+	for _, message := range messages {
+		logMessage := LogFileEvent{
+			Event:      message,
+			Host:       s.hostName,
+			Source:     s.correlationId,
+			SourceType: "piper:pipeline:logfile",
+			Index:      s.splunkIndex,
+		}
+		marshalledLogMessage, err := json.Marshal(logMessage)
+		if err != nil {
+			return errors.Wrap(err, "error while marshalling Splunk messages")
+		}
+		logfileEvents = append(logfileEvents, string(marshalledLogMessage))
 	}
-	details := DetailsLog{
-		Host:       s.correlationID,
-		SourceType: "txt",
-		Index:      s.splunkIndex,
-		Event:      event,
-	}
+	// creates payload {"event":"this is a sample event ", "Host":"myHost", "Source":"mySource", "SourceType":"valueA", "Index":"valueB"}{"event":"this is a sample event ", "Host":"myHost", "Source":"mySource", "SourceType":"valueA", "Index":"valueB"}..
+	strout := strings.Join(logfileEvents, ",")
+	payload := strings.NewReader(strout)
 
-	payload, err := json.Marshal(details)
-	if err != nil {
-		return errors.Wrap(err, "error while marshalling Splunk message details")
-	}
-
-	resp, err := s.splunkClient.SendRequest(http.MethodPost, s.splunkDsn, bytes.NewBuffer(payload), nil, nil)
+	resp, err := s.splunkClient.SendRequest(http.MethodPost, s.splunkDsn, payload, nil, nil)
 
 	if resp != nil {
 		if resp.StatusCode != http.StatusOK {
@@ -263,7 +275,7 @@ func (s *Splunk) tryPostMessages(telemetryData MonitoringData, messages []log.Me
 		Telemetry: telemetryData,
 	}
 	details := Details{
-		Host:       s.correlationID,
+		Host:       s.correlationId,
 		SourceType: "_json",
 		Index:      s.splunkIndex,
 		Event:      event,
