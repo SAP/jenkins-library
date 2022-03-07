9a3b800b9d580306f5ade8ce48c479eb4bac19f6
Mayur Belur Mohan <68264167+mayurmohan@users.noreply.github.com>
2022-03-07 15:33:44 +0530

    ApiKeyValueMapUpload Command (#3407)
    * ApiKeyValueMapUpload Command
    
    * include command in metadata
    
    * TestCase Fixes
    
    * CodeReview Fixes
    
    * CodeReview Fixes
    
    * Code Review Fixes
    
    * CodeReview Fixes
    
    * CodeReview Fixes
    
    * CodeReview FIxes
    
    * CodeReview Fixes
    
    * Documenation change
    
    * documentation fix
    
    * Documentation Fix
    
    * Documentation Fix
    
    * documentation Fix
    
    * CodeReview Fixes
    
    * CodeReview Fixes
    
    * Revert changes
    
    * Documentation Fix
    
    * CodeReview FIxes
    
    * Doc Fixes
    
    * Code Review Fixes
    
    * Code Review Fixes
    
    * CodeReview FIxes
    
    * Documentation Fix
    
    * Documentation Changes
    
    * Documentation Fix
    
    * codereview fix
    
    * Documentation Fix
    
    * CodeReview Fixes
    
    * CodeReview Fix
    
    * Documentation FIx
    
    * doc fix
    
    * Doc Fix
    
    * Documentation Fix
    
    * codereview fix
    
    * revert fix
    
    * Code Review Fix
    
    Co-authored-by: Oliver Feldmann <oliver.feldmann@sap.com>

--- cmd/metadata_generated.go
+++ cmd/metadata_generated.go
@@ -25,6 +25,7 @@ func GetAllStepMetadata() map[string]config.StepData {
 		"abapEnvironmentRunATCCheck":                abapEnvironmentRunATCCheckMetadata(),
 		"abapEnvironmentRunAUnitTest":               abapEnvironmentRunAUnitTestMetadata(),
 		"apiKeyValueMapDownload":                    apiKeyValueMapDownloadMetadata(),
+		"apiKeyValueMapUpload":                      apiKeyValueMapUploadMetadata(),
 		"apiProxyDownload":                          apiProxyDownloadMetadata(),
 		"apiProxyUpload":                            apiProxyUploadMetadata(),
 		"artifactPrepareVersion":                    artifactPrepareVersionMetadata(),
