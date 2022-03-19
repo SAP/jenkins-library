69999e52954da85b1ad8ffbaccd96d849d16849a
Oliver Feldmann <oliver.feldmann@sap.com>
2022-03-19 07:55:14 +0100

    Add ans implementation

---
+++ pkg/ans/ans.go
@@ -0,0 +1,150 @@
+package ans
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/SAP/jenkins-library/pkg/xsuaa"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+const (
+	authHeaderKey = "Authorization"
+
+	infoSeverity    = "INFO"
+	noticeSeverity  = "NOTICE"
+	warningSeverity = "WARNING"
+	errorSeverity   = "ERROR"
+	fatalSeverity   = "FATAL"
+
+	exceptionCategory    = "EXCEPTION"
+	alertCategory        = "ALERT"
+	notificationCategory = "NOTIFICATION"
+)
+
+// ANS holds the setup for the xsuaa service to retrieve a bearer token for authorization and
+// the URL to the SAP Alert Notification Service backend
+type ANS struct {
+	XSUAA xsuaa.XSUAA
+	URL   string
+}
+
+// Client to send the event to the SAP Alert Notification Service
+type Client interface {
+	Send(event Event) error
+}
+
+// ServiceKey holds the information about the SAP Alert Notification Service to send the events to
+type ServiceKey struct {
+	Url          string `json:"url"`
+	ClientId     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+	OauthUrl     string `json:"oauth_url"`
+}
+
+// Event structure of the SAP Alert Notification Service
+type Event struct {
+	EventType      string                 `json:"eventType,omitempty"`
+	EventTimestamp int64                  `json:"eventTimestamp,omitempty"`
+	Severity       string                 `json:"severity,omitempty"`
+	Category       string                 `json:"category,omitempty"`
+	Subject        string                 `json:"subject,omitempty"`
+	Body           string                 `json:"body,omitempty"`
+	Priority       int                    `json:"priority,omitempty"`
+	Region         string                 `json:"region,omitempty"`
+	RegionType     string                 `json:"regionType,omitempty"`
+	Tags           map[string]interface{} `json:"tags,omitempty"`
+	Resource       struct {
+		GlobalAccount    string                 `json:"globalAccount,omitempty"`
+		SubAccount       string                 `json:"subAccount,omitempty"`
+		ResourceGroup    string                 `json:"resourceGroup,omitempty"`
+		ResourceName     string                 `json:"resourceName,omitempty"`
+		ResourceType     string                 `json:"resourceType,omitempty"`
+		ResourceInstance string                 `json:"resourceInstance,omitempty"`
+		Tags             map[string]interface{} `json:"tags,omitempty"`
+	} `json:"resource,omitempty"`
+}
+
+// UnmarshallServiceKeyJSON unmarshalls the given json service key string.
+func UnmarshallServiceKeyJSON(serviceKeyJSON string) (ansServiceKey ServiceKey, err error) {
+	err = json.Unmarshal([]byte(serviceKeyJSON), &ansServiceKey)
+	if err != nil {
+		err = errors.Wrap(err, "error unmarshalling ANS serviceKey")
+		return
+	}
+	return
+}
+
+// UnmarshallEventJSON unmarshalls an ANS Event JSON string
+func UnmarshallEventJSON(eventJSON string) (event Event, err error) {
+	err = json.Unmarshal([]byte(eventJSON), &event)
+	if err != nil {
+		err = errors.Wrapf(err, "error unmarshalling ANS event from JSON string %q", eventJSON)
+		return
+	}
+	return
+}
+
+// Send an event to the SAP Alert Notification Service
+func (ans ANS) Send(event Event) error {
+	const eventPath = "/cf/producer/v1/resource-events"
+
+	requestBody, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	header := make(http.Header)
+	err = ans.XSUAA.SetAuthHeaderIfNotPresent(&header)
+	if err != nil {
+		return err
+	}
+
+	entireUrl := ans.URL + eventPath
+
+	httpClient := http.Client{}
+	request, err := http.NewRequest(http.MethodPost, entireUrl, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return err
+	}
+	request.Header.Add(authHeaderKey, header.Get(authHeaderKey))
+	request.Header.Add("Content-Type", "application/json")
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != http.StatusAccepted {
+		return fmt.Errorf("http request to '%s' did not return expected status code %d; instead got %d", entireUrl, http.StatusAccepted, response.StatusCode)
+	}
+
+	return nil
+}
+
+// TranslateLogrusLogLevel takes the logrus log level and translates it to an ANS severity ans category string
+func TranslateLogrusLogLevel(level logrus.Level) (severity, category string) {
+	severity = infoSeverity
+	category = notificationCategory
+	switch level {
+	case logrus.InfoLevel:
+		severity = infoSeverity
+		category = notificationCategory
+	case logrus.DebugLevel:
+		severity = infoSeverity
+		category = notificationCategory
+	case logrus.WarnLevel:
+		severity = warningSeverity
+		category = alertCategory
+	case logrus.ErrorLevel:
+		severity = errorSeverity
+		category = exceptionCategory
+	case logrus.FatalLevel:
+		severity = fatalSeverity
+		category = exceptionCategory
+	case logrus.PanicLevel:
+		severity = fatalSeverity
+		category = exceptionCategory
+	}
+	return
+}
---
+++ pkg/ans/ans_test.go
@@ -0,0 +1,224 @@
+package ans
+
+import (
+	"fmt"
+	"github.com/SAP/jenkins-library/pkg/xsuaa"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestANS_Send(t *testing.T) {
+	testXSUAA := xsuaa.XSUAA{
+		OAuthURL:     "https://my.test.oauth.provider",
+		ClientID:     "myTestClientID",
+		ClientSecret: "super secret",
+		CachedAuthToken: xsuaa.AuthToken{
+			TokenType:   "bearer",
+			AccessToken: "1234",
+			ExpiresIn:   12345,
+		},
+	}
+	type request struct {
+		path       string
+		authHeader string
+		event      Event
+	}
+	tests := []struct {
+		name        string
+		event       Event
+		wantErrf    string
+		wantRequest request
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Straight forward",
+			event: Event{
+				EventType:      "my event",
+				EventTimestamp: 1647526655,
+			},
+			wantRequest: request{
+				path:       "/cf/producer/v1/resource-events",
+				authHeader: "bearer 1234",
+				event: Event{
+					EventType:      "my event",
+					EventTimestamp: 1647526655,
+				},
+			},
+		},
+		{
+			name:     "Wrong status code in response error",
+			wantErrf: "http request to '%s/cf/producer/v1/resource-events' did not return expected status code 202; instead got 200",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var requestedUrlPath string
+			var requestedMethod string
+			var requestedAuthHeader string
+			var requestedContentTypeHeader string
+			var requestedEvent Event
+			// Start a local HTTP server
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				requestedUrlPath = req.URL.String()
+				requestedMethod = req.Method
+				if tt.wantErrf == "" {
+					rw.WriteHeader(http.StatusAccepted)
+				}
+				requestedAuthHeader = req.Header.Get(authHeaderKey)
+				requestedContentTypeHeader = req.Header.Get("Content-Type")
+				requestedBody, err := ioutil.ReadAll(req.Body)
+				require.NoError(t, err)
+				requestedEvent, err = UnmarshallEventJSON(string(requestedBody))
+				require.NoError(t, err)
+			}))
+			ans := ANS{
+				XSUAA: testXSUAA,
+				URL:   server.URL,
+			}
+			err := ans.Send(tt.event)
+			if len(tt.wantErrf) > 0 {
+				require.EqualError(t, err, fmt.Sprintf(tt.wantErrf, server.URL), "An error was expected.")
+			} else {
+				require.NoError(t, err, "No error expected.")
+				assert.Equal(t, tt.wantRequest.path, requestedUrlPath, "Mismatch in requested path")
+				assert.Equal(t, http.MethodPost, requestedMethod, "Mismatch in requested method")
+				assert.Equal(t, tt.wantRequest.authHeader, requestedAuthHeader, "Mismatch in requested auth header")
+				assert.Equal(t, "application/json", requestedContentTypeHeader, "Mismatch in requested content type header")
+				assert.Equal(t, tt.wantRequest.event, requestedEvent, "Mismatch in requested body")
+			}
+		})
+	}
+}
+
+func TestReadServiceKey(t *testing.T) {
+	tests := []struct {
+		name              string
+		serviceKeyJSON    string
+		wantAnsServiceKey ServiceKey
+		wantErr           bool
+	}{
+		{
+			name: "Proper event JSON yields correct event",
+			serviceKeyJSON: `{
+						"url": "https://my.test.backend",
+						"client_id": "myTestClientID",
+						"client_secret": "super secret",
+						"oauth_url": "https://my.test.oauth.provider"
+					}`,
+			wantAnsServiceKey: ServiceKey{
+				Url:          "https://my.test.backend",
+				ClientId:     "myTestClientID",
+				ClientSecret: "super secret",
+				OauthUrl:     "https://my.test.oauth.provider",
+			},
+			wantErr: false,
+		},
+		{
+			name:           "Faulty JSON yields error",
+			serviceKeyJSON: `bli-da-blup`,
+			wantErr:        true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotAnsServiceKey, err := UnmarshallServiceKeyJSON(tt.serviceKeyJSON)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UnmarshallServiceKeyJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, tt.wantAnsServiceKey, gotAnsServiceKey, "Got the wrong ans serviceKey")
+		})
+	}
+}
+
+func TestTranslateLogrusLogLevel(t *testing.T) {
+	tests := []struct {
+		name         string
+		level        logrus.Level
+		wantSeverity string
+		wantCategory string
+	}{
+		{
+			name:         "InfoLevel yields INFO and NOTIFICATION",
+			level:        logrus.InfoLevel,
+			wantSeverity: infoSeverity,
+			wantCategory: notificationCategory,
+		},
+		{
+			name:         "DebugLevel yields INFO and NOTIFICATION",
+			level:        logrus.DebugLevel,
+			wantSeverity: infoSeverity,
+			wantCategory: notificationCategory,
+		},
+		{
+			name:         "WarnLevel yields WARNING and ALERT",
+			level:        logrus.WarnLevel,
+			wantSeverity: warningSeverity,
+			wantCategory: alertCategory,
+		},
+		{
+			name:         "ErrorLevel yields ERROR and EXCEPTION",
+			level:        logrus.ErrorLevel,
+			wantSeverity: errorSeverity,
+			wantCategory: exceptionCategory,
+		},
+		{
+			name:         "FatalLevel yields FATAL and EXCEPTION",
+			level:        logrus.FatalLevel,
+			wantSeverity: fatalSeverity,
+			wantCategory: exceptionCategory,
+		},
+		{
+			name:         "PanicLevel yields FATAL and EXCEPTION",
+			level:        logrus.PanicLevel,
+			wantSeverity: fatalSeverity,
+			wantCategory: exceptionCategory,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotSeverity, gotCategory := TranslateLogrusLogLevel(tt.level)
+			assert.Equal(t, tt.wantSeverity, gotSeverity, "Got wrong severity")
+			assert.Equal(t, tt.wantCategory, gotCategory, "Got wrong category")
+		})
+	}
+}
+
+func TestUnmarshallEventJSON(t *testing.T) {
+	tests := []struct {
+		name      string
+		eventJSON string
+		wantEvent Event
+		wantErr   bool
+	}{
+		{
+			name:      "Proper event JSON yields correct event",
+			eventJSON: `{"eventType": "my event","eventTimestamp":1647526655}`,
+			wantEvent: Event{
+				EventType:      "my event",
+				EventTimestamp: 1647526655,
+			},
+			wantErr: false,
+		},
+		{
+			name:      "Faulty JSON yields error",
+			eventJSON: `bli-da-blup`,
+			wantErr:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotEvent, err := UnmarshallEventJSON(tt.eventJSON)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UnmarshallEventJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, tt.wantEvent, gotEvent, "Gotten Event is not as expected.")
+		})
+	}
+}
