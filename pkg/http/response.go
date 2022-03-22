3f0e41869cc9cc4c4a0a72384498e82ba5c8ff09
Oliver Feldmann <oliver.feldmann@sap.com>
2022-03-22 13:35:08 +0100

    Add reading of response body function

---
+++ pkg/http/response.go
@@ -0,0 +1,22 @@
+package http
+
+import (
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
+)
+
+// ReadResponseBody reads the body of a response and it as a byte slice
+func ReadResponseBody(response *http.Response) ([]byte, error) {
+	if response == nil {
+		return nil, errors.Errorf("did not retrieve an HTTP response")
+	}
+	if response.Body != nil {
+		defer response.Body.Close()
+	}
+	bodyText, readErr := ioutil.ReadAll(response.Body)
+	if readErr != nil {
+		return nil, errors.Wrap(readErr, "HTTP response body could not be read")
+	}
+	return bodyText, nil
+}
---
+++ pkg/http/response_test.go
@@ -0,0 +1,40 @@
+package http
+
+import (
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"testing"
+)
+
+func TestReadResponseBody(t *testing.T) {
+	tests := []struct {
+		name        string
+		response    *http.Response
+		want        []byte
+		wantErrText string
+	}{
+		{
+			name:     "Straight forward",
+			response: httpmock.NewStringResponse(200, "test string"),
+			want:     []byte("test string"),
+		},
+		{
+			name:        "No response error",
+			wantErrText: "did not retrieve an HTTP response",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadResponseBody(tt.response)
+			if tt.wantErrText != "" {
+				require.Error(t, err, "Error expected")
+				assert.EqualError(t, err, tt.wantErrText, "Error is not equal")
+				return
+			}
+			require.NoError(t, err, "No error expected")
+			assert.Equal(t, tt.want, got, "Did not receive expected body")
+		})
+	}
+}
