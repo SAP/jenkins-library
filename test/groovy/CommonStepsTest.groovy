7c884a103d87d96117d7b3bd7031da662e9c7de9
TheShifter <mrmixal33@gmail.com>
2022-03-24 16:00:17 +0300

    add pythonBuild to fieldRelatedWhitelist

--- test/groovy/CommonStepsTest.groovy
+++ test/groovy/CommonStepsTest.groovy
@@ -212,6 +212,7 @@ public class CommonStepsTest extends BasePiperTest{
         'gradleExecuteBuild', //implementing new golang pattern without fields
         'shellExecute', //implementing new golang pattern without fields
         'apiKeyValueMapUpload', //implementing new golang pattern without fields
+        'pythonBuild', //implementing new golang pattern without fields
     ]
 
     @Test
