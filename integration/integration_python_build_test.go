92e4135a185013658eaebf2a3e16a75cb3cb3816
TheShifter <mrmixal33@gmail.com>
2022-03-25 18:27:51 +0300

    update integration test for pythonBuild

--- integration/integration_python_build_test.go
+++ integration/integration_python_build_test.go
@@ -6,49 +6,71 @@
 package main
 
 import (
+	"path/filepath"
 	"testing"
 )
 
 func TestBuildProject(t *testing.T) {
-	t.Parallel()
-	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
+	ctx := context.Background()
+	pwd, err := os.Getwd()
+	assert.NoError(t, err, "Getting current working directory failed.")
+	pwd = filepath.Dir(pwd)
+	err = copyDir(filepath.Join(pwd, "integration", "testdata", "TestPythonIntegration"), tempDir)
+	if err != nil {
+		t.Fatal("Failed to copy test project.")
+	}
+
+	//workaround to use test script util it is possible to set workdir for Exec call
+	testScript := fmt.Sprintf(`#!/bin/sh
+		cd /test
+		/piperbin/piper pythonBuild >test-log.txt 2>&1`)
+	ioutil.WriteFile(filepath.Join(tempDir, "runPiper.sh"), []byte(testScript), 0700)
+
+	reqNode := testcontainers.ContainerRequest{
 		Image: "python:3.9",
+		Cmd:   []string{"tail", "-f"},
+		BindMounts: map[string]string{
+			pwd:     "/piperbin",
+			tempDir: "/test",
+		},
+	}
+
+	nodeContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: reqNode,
+		Started:          true,
 	})
 
-	err := container.whenRunningPiperCommand("python3", "-m", "pip", "install", "--upgrade", "build")
+	code, err := nodeContainer.Exec(ctx, []string{"sh", "/test/runPiper.sh"})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, code)
+
+	content, err := ioutil.ReadFile(filepath.Join(tempDir, "/test-log.txt"))
 	if err != nil {
-		t.Fatalf("Calling piper command failed %s", err)
+		t.Fatal("Could not read test-log.txt.", err)
 	}
-	var flags []string
-	flags = append(flags, "-m", "build", "./integration/testdata/TestPythonIntegration")
+	output := string(content)
 
-	err = container.whenRunningPiperCommand("python3", flags...)
+	assert.Contains(t, output, "info  pythonBuild - running command: python3 setup.py sdist bdist_wheel")
+	assert.Contains(t, output, "info  pythonBuild - running command: python3 -m pip install --upgrade cyclonedx-bom")
+	assert.Contains(t, output, "info  pythonBuild - running command: cyclonedx-bom --e --output bom.xml")
+	assert.Contains(t, output, "info  pythonBuild - SUCCESS")
+
+	//workaround to use test script util it is possible to set workdir for Exec call
+	testScript = fmt.Sprintf(`#!/bin/sh
+		cd /test
+		ls -l >files-list.txt 2>&1`)
+	ioutil.WriteFile(filepath.Join(tempDir, "runPiper.sh"), []byte(testScript), 0700)
+
+	code, err = nodeContainer.Exec(ctx, []string{"sh", "/test/runPiper.sh"})
+	assert.NoError(t, err)
+	assert.Equal(t, 0, code)
+
+	content, err = ioutil.ReadFile(filepath.Join(tempDir, "/files-list.txt"))
 	if err != nil {
-		t.Fatalf("Calling piper command failed %s", err)
+		t.Fatal("Could not read files-list.txt.", err)
 	}
-
-	container.assertHasOutput(t, "Successfully built example-package-YOUR-USERNAME-HERE-0.0.1.tar.gz and example_package_YOUR_USERNAME_HERE-0.0.1-py3-none-any.whl")
-	container.assertHasFile(t, "/dist/example-package-YOUR-USERNAME-HERE-0.0.1.tar.gz")
-	container.assertHasFile(t, "/dist/example_package_YOUR_USERNAME_HERE-0.0.1-py3-none-any.whl")
-}
-
-func TestBuildProjectWithoutSource(t *testing.T) {
-	t.Parallel()
-	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
-		Image: "python:3.9",
-	})
-
-	err := container.whenRunningPiperCommand("python3", "-m", "pip", "install", "--upgrade", "build")
-	if err != nil {
-		t.Fatalf("Calling piper command failed %s", err)
-	}
-	var flags []string
-	flags = append(flags, "-m", "build", "/testdata/TestPythonIntegration")
-
-	err = container.whenRunningPiperCommand("python3", flags...)
-	if err != nil {
-		t.Fatalf("Calling piper command failed %s", err)
-	}
-
-	container.assertHasOutput(t, "link to python project dir is not defined in input parameters")
+	output = string(content)
+	assert.Contains(t, output, "bom.xml")
+	assert.Contains(t, output, "/dist/example-pkg-0.0.1.tar.gz")
+	assert.Contains(t, output, "/dist/example_pkg-0.0.1-py3-none-any.whl")
 }
