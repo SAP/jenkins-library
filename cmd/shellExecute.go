0803e55180986ddfe95a4f03f0553ee442ed9372
anilkeshav27 <you@example.com>
2022-03-28 11:49:02 +0200

    using current directory

--- cmd/shellExecute.go
+++ cmd/shellExecute.go
@@ -106,7 +106,7 @@ func downloadScript(config *shellExecuteOptions, utils shellExecuteUtils, url st
 	log.Entry().Infof("downloading script : %v", url)
 	fileNameParts := strings.Split(url, "/")
 	fileName := fileNameParts[len(fileNameParts)-1]
-	err := utils.DownloadFile(url, filepath.Join(".pipeline", fileName), header, []*http.Cookie{})
+	err := utils.DownloadFile(url, fileName, header, []*http.Cookie{})
 	if err != nil {
 		return "", errors.Wrapf(err, "unable to download script from %v", url)
 	}
@@ -116,5 +116,5 @@ func downloadScript(config *shellExecuteOptions, utils shellExecuteUtils, url st
 		return "", fmt.Errorf("unable to change file permission for script '%v'", fileName)
 	}
 
-	return filepath.Join(".pipeline", fileName), nil
+	return filepath.Join(".", fileName), nil
 }
