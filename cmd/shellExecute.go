8e12963e1e5247f23cc2a3eae2f53779c6b84381
anilkeshav27 <you@example.com>
2022-03-28 11:55:28 +0200

    file permission

--- cmd/shellExecute.go
+++ cmd/shellExecute.go
@@ -111,7 +111,7 @@ func downloadScript(config *shellExecuteOptions, utils shellExecuteUtils, url st
 		return "", errors.Wrapf(err, "unable to download script from %v", url)
 	}
 	log.Entry().Infof("downloaded script %v successfully", url)
-	err = utils.Chmod(filepath.Join(".pipeline", fileName), 0777)
+	err = utils.Chmod(fileName, 0777)
 	if err != nil {
 		return "", fmt.Errorf("unable to change file permission for script '%v'", fileName)
 	}
