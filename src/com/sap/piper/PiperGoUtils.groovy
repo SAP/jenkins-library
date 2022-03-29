fd21bbcc3cd04adb559612b9e1ca5f663150832d
Schmitt <julian.schmitt@sap.com>
2022-03-29 10:05:24 +0200

    try to fix import of fork for Jenkins-Testing

--- src/com/sap/piper/PiperGoUtils.groovy
+++ src/com/sap/piper/PiperGoUtils.groovy
@@ -48,7 +48,7 @@ class PiperGoUtils implements Serializable {
             }
 
             def fallbackUrl = 'https://github.com/SAP/jenkins-library/releases/latest/download/piper_master'
-            def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/SAP/jenkins-library/releases/download/${version}/piper"
+            def piperBinUrl = (version == 'master') ? fallbackUrl : "https://github.com/JulianPSchmitt/jenkins-library/releases/download/${version}/piper"
 
             boolean downloaded = downloadGoBinary(piperBinUrl)
             if (!downloaded) {
