b42de09ec2445a93a5f6f8f020c4012bae5619ad
Schmitt <julian.schmitt@sap.com>
2022-04-07 15:01:07 +0200

    fix groovy wrapper

--- vars/awsS3Upload.groovy
+++ vars/awsS3Upload.groovy
@@ -1,5 +1,4 @@
 import groovy.transform.Field
-import com.sap.piper.PiperGoUtils
 import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
@@ -8,10 +7,8 @@ import static com.sap.piper.Prerequisites.checkScript
 void call(Map parameters = [:]) {
     def script = checkScript(this, parameters) ?: this
 
-    Map stepParams = PiperGoUtils.prepare(this, script).plus(parameters)
-
     List credentials = [
         [type: 'file', id: 'awsFileCredentialsId', env: ['PIPER_jsonKeyFilePath']]
     ]
-    piperExecuteBin(stepParams, STEP_NAME, METADATA_FILE, credentials)
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
 }
