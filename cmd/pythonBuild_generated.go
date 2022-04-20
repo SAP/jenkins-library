10f9ec58e19617cbf03d301f9aa24d30538bb2ed
anilkeshav27 <you@example.com>
2022-04-20 19:26:53 +0200

    not using root user

--- cmd/pythonBuild_generated.go
+++ cmd/pythonBuild_generated.go
@@ -255,7 +255,7 @@ func pythonBuildMetadata() config.StepData {
 				},
 			},
 			Containers: []config.Container{
-				{Name: "python", Image: "python:3.9", Options: []config.Option{{Name: "-u", Value: "0"}}},
+				{Name: "python", Image: "python:3.9"},
 			},
 			Outputs: config.StepOutputs{
 				Resources: []config.StepResources{
