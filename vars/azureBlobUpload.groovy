b1fe35d1480f2f5125f5d93f9e1e80ad720ed16a
Schmitt <julian.schmitt@sap.com>
2022-04-28 14:22:19 +0200

    fix Groovy Wrapper

--- vars/azureBlobUpload.groovy
+++ vars/azureBlobUpload.groovy
@@ -2,7 +2,7 @@ import groovy.transform.Field
 import static com.sap.piper.Prerequisites.checkScript
 
 @Field String STEP_NAME = getClass().getName()
-@Field String METADATA_FILE = 'metadata/awsS3Upload.yaml'
+@Field String METADATA_FILE = 'metadata/azureBlobUpload.yaml'
 
 void call(Map parameters = [:]) {
     def script = checkScript(this, parameters) ?: this
