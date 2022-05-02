2741cfc639236c7c41f464dfd1340d66d599ddec
anilkeshav27 <you@example.com>
2022-05-02 16:03:30 +0200

    clean up

--- pkg/piperenv/artifact.go
+++ pkg/piperenv/artifact.go
@@ -2,8 +2,7 @@ package piperenv
 
 type Artifact struct {
 	LocalPath string `json:"localPath,omitempty"`
-	//ToDo: in future might want to introduce a remote path (to point to where the artifact is pushed after build)
-	Name string `json:"name,omitempty"`
+	Name      string `json:"name,omitempty"`
 }
 
 type Artifacts []Artifact
