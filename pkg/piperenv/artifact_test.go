983eefc1a207c91704d628481b92b2d23684a712
anilkeshav27 <you@example.com>
2022-05-02 16:00:06 +0200

    removing type kind related to PR 3717

--- pkg/piperenv/artifact_test.go
+++ pkg/piperenv/artifact_test.go
@@ -7,31 +7,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Artifacts_FindByKind(t *testing.T) {
-	artifacts := Artifacts([]Artifact{{
-		LocalPath: "target/a.jar",
-		Kind:      "jar",
-	}, {
-		LocalPath: "b",
-		Kind:      "elf",
-	}})
-
-	assert.Len(t, artifacts.FindByKind("garbage"), 0)
-
-	filtered := artifacts.FindByKind("jar")
-	require.Len(t, filtered, 1)
-	assert.Equal(t, "target/a.jar", filtered[0].LocalPath)
-}
-
 func Test_Artifacts_FindByName(t *testing.T) {
 	artifacts := Artifacts([]Artifact{{
 		LocalPath: "target/a.jar",
-		Kind:      "jar",
 		Name:      "a.jar",
 	}})
 
 	assert.Len(t, artifacts.FindByName("garbage"), 0)
-
 	filtered := artifacts.FindByName("a.jar")
 	require.Len(t, filtered, 1)
 	assert.Equal(t, "a.jar", filtered[0].Name)
