338c947517809fa18a35721f336eef384bab90b7
Philipp Stehle <philipp.stehle@sap.com>
2022-05-18 18:19:29 +0200

    replace depreacted strings.Title function

---
+++ pkg/piperutils/strings_test.go
@@ -0,0 +1,15 @@
+package piperutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTitle(t *testing.T) {
+	assert.Equal(t, "TEST", Title("tEST"))
+	assert.Equal(t, "Test", Title("test"))
+	assert.Equal(t, "TEST", Title("TEST"))
+	assert.Equal(t, "Test", Title("Test"))
+	assert.Equal(t, "TEST1 Test2 TEsT3 Test4", Title("TEST1 test2 tEsT3 Test4"))
+}
