594d2149d6b4888c0689e5fed9d6da87a5e38553
Oliver Feldmann <oliver.feldmann@sap.com>
2022-05-25 10:38:29 +0200

    Validate event

--- pkg/ans/event.go
+++ pkg/ans/event.go
@@ -3,8 +3,18 @@ package ans
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+)
+
+var (
+	uni      *ut.UniversalTranslator
+	validate *validator.Validate
 )
 
 const (
@@ -24,12 +34,12 @@ const (
 // Event structure of the SAP Alert Notification Service
 type Event struct {
 	EventType      string                 `json:"eventType,omitempty"`
-	EventTimestamp int64                  `json:"eventTimestamp,omitempty"`
-	Severity       string                 `json:"severity,omitempty"`
-	Category       string                 `json:"category,omitempty"`
+	EventTimestamp int64                  `json:"eventTimestamp,omitempty" validate:"omitempty,min=0"`
+	Severity       string                 `json:"severity,omitempty" validate:"omitempty,oneof=INFO NOTICE WARNING ERROR FATAL"`
+	Category       string                 `json:"category,omitempty" validate:"omitempty,oneof=EXCEPTION ALERT NOTIFICATION"`
 	Subject        string                 `json:"subject,omitempty"`
 	Body           string                 `json:"body,omitempty"`
-	Priority       int                    `json:"priority,omitempty"`
+	Priority       int                    `json:"priority,omitempty" validate:"omitempty,min=1,max=1000"`
 	Tags           map[string]interface{} `json:"tags,omitempty"`
 	Resource       *Resource              `json:"resource,omitempty"`
 }
@@ -45,7 +55,7 @@ type Resource struct {
 // MergeWithJSON unmarshalls an ANS Event JSON string and merges it with the existing receiver Event
 func (event *Event) MergeWithJSON(eventJSON []byte) (err error) {
 	if err = strictUnmarshal(eventJSON, &event); err != nil {
-		err = errors.Wrapf(err, "error unmarshalling ANS event from JSON string %q", eventJSON)
+		return errors.Wrapf(err, "error unmarshalling ANS event from JSON string %q", eventJSON)
 	}
 	return
 }
@@ -54,6 +64,30 @@ func strictUnmarshal(data []byte, v interface{}) error {
 	dec := json.NewDecoder(bytes.NewReader(data))
 	dec.DisallowUnknownFields()
 	return dec.Decode(v)
+}
+
+func (event *Event) Validate() (err error) {
+	validate = validator.New()
+
+	if err = validate.Struct(event); err != nil {
+		translator := newTranslator(validate)
+		errs := err.(validator.ValidationErrors)
+		err = fmt.Errorf("event JSON failed the validation")
+		for _, fieldError := range errs.Translate(translator) {
+			err = errors.Wrap(err, fieldError)
+		}
+	}
+	return
+}
+
+func newTranslator(validate *validator.Validate) ut.Translator {
+	eng := en.New()
+	uni = ut.New(eng, eng)
+
+	translator, _ := uni.GetTranslator("en")
+	en_translations.RegisterDefaultTranslations(validate, translator)
+
+	return translator
 }
 
 // SetSeverityAndCategory takes the logrus log level and sets the corresponding ANS severity and category string
--- pkg/ans/event_test.go
+++ pkg/ans/event_test.go
@@ -1,8 +1,10 @@
 package ans
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -21,7 +23,6 @@ func TestEvent_MergeWithJSON(t *testing.T) {
 				EventType:      "my event",
 				EventTimestamp: 1647526655,
 			},
-			wantErr: false,
 		},
 		{
 			name:      "Merging events includes all parts",
@@ -46,7 +47,6 @@ func TestEvent_MergeWithJSON(t *testing.T) {
 					ResourceInstance: "myResourceInstance",
 				},
 			},
-			wantErr: false,
 		},
 		{
 			name:      "Faulty JSON yields error",
@@ -67,7 +67,6 @@ func TestEvent_MergeWithJSON(t *testing.T) {
 				t.Errorf("MergeWithJSON() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			assert.Equal(t, tt.wantEvent, gotEvent, "Received Event is not as expected.")
 		})
 	}
 }
@@ -123,5 +122,63 @@ func TestEvent_SetLogLevel(t *testing.T) {
 			assert.Equal(t, tt.wantSeverity, event.Severity, "Got wrong severity")
 			assert.Equal(t, tt.wantCategory, event.Category, "Got wrong category")
 		})
+	}
+}
+
+func TestEvent_Validate(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		eventJSON string
+		errMsg    string
+	}{
+		{
+			errMsg:    "Category must be one of [EXCEPTION ALERT NOTIFICATION]",
+			eventJSON: `{"category": "WRONG_CATEGORY"}`,
+		},
+		{
+			errMsg:    "Severity must be one of [INFO NOTICE WARNING ERROR FATAL]",
+			eventJSON: `{"severity": "WRONG_SEVERITY"}`,
+		},
+		{
+			errMsg:    "Priority must be 1,000 or less",
+			eventJSON: `{"priority": 1001}`,
+		},
+		{
+			errMsg:    "Priority must be 1 or greater",
+			eventJSON: `{"priority": -1}`,
+		},
+		{
+			errMsg:    "EventTimestamp must be 0 or greater",
+			eventJSON: `{"eventTimestamp": -1}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.errMsg, func(t *testing.T) {
+			event := defaultEvent()
+			err := event.MergeWithJSON([]byte(tt.eventJSON))
+			require.NoError(t, err)
+			err = event.Validate()
+			wantErrMsg := fmt.Sprintf("%s: %s", tt.errMsg, standardErrMsg)
+			assert.EqualError(t, err, wantErrMsg)
+		})
+	}
+}
+
+const standardErrMsg = "event JSON failed the validation"
+
+func defaultEvent() Event {
+	return Event{
+		EventType:      "MyEvent",
+		EventTimestamp: 1653485928,
+		Severity:       "INFO",
+		Category:       "NOTIFICATION",
+		Subject:        "mySubject",
+		Body:           "myBody",
+		Priority:       123,
+		Resource: &Resource{
+			ResourceName:     "myResourceName",
+			ResourceType:     "myResourceType",
+			ResourceInstance: "myResourceInstance",
+		},
 	}
 }
