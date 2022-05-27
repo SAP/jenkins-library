27e62ab7064c5829cf28ed3d4ed47f180472ac3e
Oliver Feldmann <oliver.feldmann@sap.com>
2022-05-27 11:12:41 +0200

    Make test list test

--- cmd/ansSendEvent_test.go
+++ cmd/ansSendEvent_test.go
@@ -14,131 +14,90 @@ const testTimestamp = 1651585103
 func TestRunAnsSendEvent(t *testing.T) {
 	t.Parallel()
 
-	defaultEvent := ans.Event{
-		EventType: "myEvent",
-		Resource: &ans.Resource{
-			ResourceType: "Pipeline",
-			ResourceName: "Pipeline",
+	log.Entry().Data["stepName"] = "testStep"
+
+	tests := []struct {
+		name       string
+		config     ansSendEventOptions
+		ansMock    ansMock
+		wantErrMsg string
+	}{
+		{
+			name:   "overwriting EventType",
+			config: ansSendEventOptions{AnsServiceKey: goodServiceKey, EventJSON: goodEventJSON},
 		},
-		Subject:        fmt.Sprint("testStep"),
-		Body:           fmt.Sprintf("Call from Piper step: %s", "testStep"),
+		{
+			name:       "bad service key",
+			config:     ansSendEventOptions{AnsServiceKey: `{"forgot": "closing", "bracket": "json"`},
+			wantErrMsg: `error unmarshalling ANS serviceKey: unexpected end of JSON input`,
+		},
+		{
+			name:       "bad event json",
+			config:     ansSendEventOptions{AnsServiceKey: goodServiceKey, EventJSON: `faulty JSON`},
+			wantErrMsg: `error unmarshalling ANS event from JSON string "faulty JSON": invalid character 'u' in literal false (expecting 'l')`,
+		},
+		{
+			name:       "unknown field in json",
+			config:     ansSendEventOptions{AnsServiceKey: goodServiceKey, EventJSON: `{"unknown": "yields error"}`},
+			wantErrMsg: `error unmarshalling ANS event from JSON string "{\"unknown\": \"yields error\"}": json: unknown field "unknown"`,
+		},
+		{
+			name:       "invalid event json",
+			config:     ansSendEventOptions{AnsServiceKey: goodServiceKey, EventJSON: `{"severity": "WRONG_SEVERITY"}`},
+			wantErrMsg: `Severity must be one of [INFO NOTICE WARNING ERROR FATAL]: event JSON failed the validation`,
+		},
+		{
+			name:       "fail to send",
+			config:     ansSendEventOptions{AnsServiceKey: goodServiceKey, EventJSON: goodEventJSON},
+			ansMock:    ansMock{failToSend: true},
+			wantErrMsg: `failed to send`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			defer tt.ansMock.cleanup()
+			if err := runAnsSendEvent(&tt.config, &tt.ansMock); tt.wantErrMsg != "" {
+				assert.EqualError(t, err, tt.wantErrMsg)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, "https://my.test.backend", tt.ansMock.testANS.URL)
+				assert.Equal(t, "myTestClientID", tt.ansMock.testANS.XSUAA.ClientID)
+				assert.Equal(t, "super secret", tt.ansMock.testANS.XSUAA.ClientSecret)
+				assert.Equal(t, "https://my.test.oauth.provider", tt.ansMock.testANS.XSUAA.OAuthURL)
+				assert.Equal(t, defaultEvent, tt.ansMock.testEvent)
+			}
+
+		})
+	}
+}
+
+func defaultEvent() ans.Event {
+	return ans.Event{
+		EventType:      "myEvent",
 		EventTimestamp: testTimestamp,
 		Severity:       "INFO",
 		Category:       "NOTIFICATION",
+		Subject:        "testStep",
+		Body:           "Call from Piper step: testStep",
+		Priority:       123,
+		Resource: &ans.Resource{
+			ResourceName:     "myResourceName",
+			ResourceType:     "myResourceType",
+			ResourceInstance: "myResourceInstance",
+		},
 	}
-	goodServiceKey := `{
+}
+
+const (
+	goodServiceKey = `{
 				"url": "https://my.test.backend",
 				"client_id": "myTestClientID",
 				"client_secret": "super secret",
 				"oauth_url": "https://my.test.oauth.provider"
 			   }`
-
-	log.Entry().Data["stepName"] = "testStep"
-
-	t.Run("happy path - overwriting EventType", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := ansSendEventOptions{
-			AnsServiceKey: goodServiceKey,
-			EventJSON:     `{"eventType": "myEvent"}`,
-		}
-		am := ansMock{}
-		defer am.cleanup()
-
-		// test
-		err := runAnsSendEvent(&config, &am)
-
-		// assert
-		require.NoError(t, err)
-
-		assert.Equal(t, "https://my.test.backend", am.testANS.URL)
-		assert.Equal(t, "myTestClientID", am.testANS.XSUAA.ClientID)
-		assert.Equal(t, "super secret", am.testANS.XSUAA.ClientSecret)
-		assert.Equal(t, "https://my.test.oauth.provider", am.testANS.XSUAA.OAuthURL)
-
-		assert.Equal(t, defaultEvent, am.testEvent)
-	})
-
-	t.Run("error - bad service key", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := ansSendEventOptions{
-			AnsServiceKey: `{
-						"url": "https://my.test.backend",
-						"client_id": "myTestClientID",
-						"client_secret": "super secret",
-						"oauth_url": "https://my.test.oauth.provider"`,
-		}
-
-		// test
-		err := runAnsSendEvent(&config, &ansMock{})
-
-		// assert
-		assert.EqualError(t, err, "error unmarshalling ANS serviceKey: unexpected end of JSON input")
-	})
-
-	t.Run("error - bad event json", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := ansSendEventOptions{
-			AnsServiceKey: goodServiceKey,
-			EventJSON:     `faulty JSON`,
-		}
-
-		// test
-		err := runAnsSendEvent(&config, &ansMock{})
-
-		// assert
-		assert.EqualError(t, err, "error unmarshalling ANS event from JSON string \"faulty JSON\": invalid character 'u' in literal false (expecting 'l')")
-	})
-
-	t.Run("error - unknown field in json", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := ansSendEventOptions{
-			AnsServiceKey: goodServiceKey,
-			EventJSON:     `{"unknown": "yields error"}`,
-		}
-
-		// test
-		err := runAnsSendEvent(&config, &ansMock{})
-
-		// assert
-		assert.EqualError(t, err, `error unmarshalling ANS event from JSON string "{\"unknown\": \"yields error\"}": json: unknown field "unknown"`)
-	})
-
-	t.Run("error - invalid event json", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := ansSendEventOptions{
-			AnsServiceKey: goodServiceKey,
-			EventJSON:     `{"severity": "WRONG_SEVERITY"}`,
-		}
-
-		// test
-		err := runAnsSendEvent(&config, &ansMock{})
-
-		// assert
-		assert.EqualError(t, err, "Severity must be one of [INFO NOTICE WARNING ERROR FATAL]: event JSON failed the validation")
-	})
-
-	t.Run("error - fail to send", func(t *testing.T) {
-		t.Parallel()
-		// init
-		config := ansSendEventOptions{
-			AnsServiceKey: goodServiceKey,
-			EventJSON:     `{"eventType": "myEvent"}`,
-		}
-		am := ansMock{failToSend: true}
-		defer am.cleanup()
-
-		// test
-		err := runAnsSendEvent(&config, &am)
-
-		// assert
-		assert.EqualError(t, err, "failed to send")
-	})
-}
+	goodEventJSON = `{"eventType": "myEvent"}`
+)
 
 type ansMock struct {
 	testANS    ans.ANS
