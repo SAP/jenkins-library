b2a9d737bae94340aa657086d8c433cb7058f664
mayurmohan <68264167+mayurmohan@users.noreply.github.com>
2022-06-21 13:02:57 +0530

    CodeReview Fixes

--- cmd/apiProxyList_test.go
+++ cmd/apiProxyList_test.go
@@ -54,29 +54,6 @@ func TestRunApiProxyList(t *testing.T) {
 		// assert
 		assert.EqualError(t, err, "HTTP GET request to /apiportal/api/1.0/Management.svc/APIProxies failed with error: : Bad Request")
 	})
-
-	t.Run("MakeOdataQuery- Success Test", func(t *testing.T) {
-		config := getDefaultOptionsForApiProxyList()
-		odataFilterInputs := apim.OdataParameters{Filter: config.Filter, Search: config.Search,
-			Top: config.Top, Skip: config.Skip, Orderby: config.Orderby,
-			Select: config.Select, Expand: config.Expand}
-		odataFilters, Err := apim.OdataUtils.MakeOdataQuery(&odataFilterInputs)
-		assert.NoError(t, Err)
-		assert.Equal(t, "?filter=isCopy+eq+false&$orderby=name&$skip=1&$top=4", odataFilters)
-	})
-
-	t.Run("MakeOdataQuery- empty odata filters Test", func(t *testing.T) {
-		config := apiProxyListOptions{
-			APIServiceKey: apimhttp.GetServiceKey(),
-		}
-		odataFilterInputs := apim.OdataParameters{Filter: config.Filter, Search: config.Search,
-			Top: config.Top, Skip: config.Skip, Orderby: config.Orderby,
-			Select: config.Select, Expand: config.Expand}
-		odataFilters, Err := apim.OdataUtils.MakeOdataQuery(&odataFilterInputs)
-		assert.NoError(t, Err)
-		assert.Equal(t, "", odataFilters)
-	})
-
 }
 
 func getDefaultOptionsForApiProxyList() apiProxyListOptions {
---
+++ pkg/apim/APIMUtility_test.go
@@ -0,0 +1,27 @@
+package apim
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOdataQueryInitExisting(t *testing.T) {
+	t.Run("MakeOdataQuery- Success Test", func(t *testing.T) {
+		odataFilterInputs := OdataParameters{Filter: "isCopy eq false", Search: "",
+			Top: 4, Skip: 1, Orderby: "name",
+			Select: "", Expand: ""}
+		odataFilters, Err := OdataUtils.MakeOdataQuery(&odataFilterInputs)
+		assert.NoError(t, Err)
+		assert.Equal(t, "?filter=isCopy+eq+false&$orderby=name&$skip=1&$top=4", odataFilters)
+	})
+
+	t.Run("MakeOdataQuery- empty odata filters Test", func(t *testing.T) {
+		odataFilterInputs := OdataParameters{Filter: "", Search: "",
+			Top: 0, Skip: 0, Orderby: "",
+			Select: "", Expand: ""}
+		odataFilters, Err := OdataUtils.MakeOdataQuery(&odataFilterInputs)
+		assert.NoError(t, Err)
+		assert.Equal(t, "", odataFilters)
+	})
+}
