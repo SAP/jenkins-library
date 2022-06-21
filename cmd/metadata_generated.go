cdea4b7713dd6db1ec089d6e7480b2c188eb4654
Mayur Belur Mohan <68264167+mayurmohan@users.noreply.github.com>
2022-06-21 16:38:00 +0530

    ApiProxyList Command (#3794)
    * ApiProxyList Command
    
    * CodeReview Fixes
    
    * CodeReview Fixes
    
    * CodeReview FIxes
    
    * CodeReview Fixes
    
    * CodeReview FIxes
    
    * CodeReview Fixes
    
    * fixing unit test
    
    * doc fixes
    
    * Update documentation/docs/steps/apiProxyList.md
    
    Co-authored-by: Oliver Feldmann <oliver.feldmann@sap.com>
    
    * CodeReview Fixes
    
    * CodeReview Fixes
    
    * CodeReview Fixes
    
    * codereview fix
    
    Co-authored-by: Oliver Feldmann <oliver.feldmann@sap.com>

--- cmd/metadata_generated.go
+++ cmd/metadata_generated.go
@@ -30,6 +30,7 @@ func GetAllStepMetadata() map[string]config.StepData {
 		"apiProviderDownload":                       apiProviderDownloadMetadata(),
 		"apiProviderUpload":                         apiProviderUploadMetadata(),
 		"apiProxyDownload":                          apiProxyDownloadMetadata(),
+		"apiProxyList":                              apiProxyListMetadata(),
 		"apiProxyUpload":                            apiProxyUploadMetadata(),
 		"artifactPrepareVersion":                    artifactPrepareVersionMetadata(),
 		"awsS3Upload":                               awsS3UploadMetadata(),
