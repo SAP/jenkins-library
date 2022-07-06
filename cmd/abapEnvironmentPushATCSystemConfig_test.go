65bb9ae9779a10b6b8ea2fceb94bea8611d37ea0
Daniel Bernd <93763187+danManSAP@users.noreply.github.com>
2022-07-06 10:41:21 +0200

    Update abapEnvironmentPushATCSystemConfig_test.go
    Unit Test - new attributes

--- cmd/abapEnvironmentPushATCSystemConfig_test.go
+++ cmd/abapEnvironmentPushATCSystemConfig_test.go
@@ -72,7 +72,7 @@ Content-Length: 465
 odata-version: 4.0
 cache-control: no-cache, no-store, must-revalidate
 		
-{"@odata.context":"$metadata#configuration/$entity","@odata.metadataEtag":"W/\"20220211135922\"","root_id":"1","conf_id":"aef8f52b-fe16-1edc-a3fe-27a1e0226c7b","conf_name":"Z_CONFIG_VIA_PIPELINE_STEP","checkvariant":"ABAP_CLOUD_DEVELOPMENT_DEFAULT","pseudo_comment_policy":"SP","last_changed_by":"CC0000000017","last_changed_at":"2022-03-02T11:16:51.336172Z","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"SAP__Messages":[]}
+{"@odata.context":"$metadata#configuration/$entity","@odata.metadataEtag":"W/\"20220211135922\"","root_id":"1","conf_id":"aef8f52b-fe16-1edc-a3fe-27a1e0226c7b","conf_name":"Z_CONFIG_VIA_PIPELINE_STEP","checkvariant":"ABAP_CLOUD_DEVELOPMENT_DEFAULT","pseudo_comment_policy":"SP","last_changed_by":"CC0000000017","last_changed_at":"2022-03-02T11:16:51.336172Z","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"transport_check_policy":"C","check_tasks":"X","check_requests":"","check_tocs":"X","SAP__Messages":[]}
 --B772E21DAA42B9571C778276B829D6C21
 Content-Type: application/http
 Content-Length: 428
@@ -146,7 +146,7 @@ Content-Length: 465
 odata-version: 4.0
 cache-control: no-cache, no-store, must-revalidate
 		
-{"@odata.context":"$metadata#configuration/$entity","@odata.metadataEtag":"W/\"20220211135922\"","root_id":"1","conf_id":"aef8f52b-fe16-1edc-a3fe-27a1e0226c7b","conf_name":"Z_CONFIG_VIA_PIPELINE_STEP","checkvariant":"ABAP_CLOUD_DEVELOPMENT_DEFAULT","pseudo_comment_policy":"SP","last_changed_by":"CC0000000017","last_changed_at":"2022-03-02T11:16:51.336172Z","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"SAP__Messages":[]}
+{"@odata.context":"$metadata#configuration/$entity","@odata.metadataEtag":"W/\"20220211135922\"","root_id":"1","conf_id":"aef8f52b-fe16-1edc-a3fe-27a1e0226c7b","conf_name":"Z_CONFIG_VIA_PIPELINE_STEP","checkvariant":"ABAP_CLOUD_DEVELOPMENT_DEFAULT","pseudo_comment_policy":"SP","last_changed_by":"CC0000000017","last_changed_at":"2022-03-02T11:16:51.336172Z","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"transport_check_policy":"C","check_tasks":"X","check_requests":"","check_tocs":"X","SAP__Messages":[]}
 --B772E21DAA42B9571C778276B829D6C21
 Content-Type: application/http
 Content-Length: 428
@@ -217,7 +217,7 @@ Content-ID: 1
 PATCH configuration(root_id='1',conf_id=4711) HTTP/1.1
 Content-Type: application/json
 
-{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false}
+{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"transport_check_policy":"C","check_tasks":"X","check_requests":"","check_tocs":"X"}
 
 --changeset
 Content-Type: application/http
@@ -242,6 +242,10 @@ Content-Type: application/json
 			"inform_findings": "1",
 			"is_default": false,
 			"is_proxy_variant": false,
+			"transport_check_policy": "C",
+			"check_tasks": "X",
+			"check_requests": "",
+			"check_tocs": "X",
 			"_priorities": [
 				{
 					"test": "CL_CI_TEST_AMDP_HDB_MIGRATION",
@@ -276,7 +280,7 @@ Content-ID: 1
 PATCH configuration(root_id='1',conf_id=4711) HTTP/1.1
 Content-Type: application/json
 
-{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false}
+{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"transport_check_policy":"C","check_tasks":"X","check_requests":"","check_tocs":"X"}
 
 --changeset
 Content-Type: application/http
@@ -311,6 +315,10 @@ Content-Type: application/json
 			"inform_findings": "1",
 			"is_default": false,
 			"is_proxy_variant": false,
+			"transport_check_policy": "C",
+			"check_tasks": "X",
+			"check_requests": "",
+			"check_tocs": "X",			
 			"_priorities": [
 				{
 					"test": "CL_CI_TEST_AMDP_HDB_MIGRATION",
@@ -350,7 +358,7 @@ Content-ID: 1
 PATCH configuration(root_id='1',conf_id=4711) HTTP/1.1
 Content-Type: application/json
 
-{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false}
+{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"transport_check_policy":"C","check_tasks":"X","check_requests":"","check_tocs":"X"}
 
 --changeset--
 
@@ -364,7 +372,11 @@ Content-Type: application/json
 			"block_findings": "0",
 			"inform_findings": "1",
 			"is_default": false,
-			"is_proxy_variant": false
+			"is_proxy_variant": false,
+			"transport_check_policy": "C",
+			"check_tasks": "X",
+			"check_requests": "",
+			"check_tocs": "X"
 		}
 		`
 
@@ -391,7 +403,7 @@ Content-ID: 1
 PATCH configuration(root_id='1',conf_id=4711) HTTP/1.1
 Content-Type: application/json
 
-{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false}
+{"conf_name":"UNITTEST_PIPERSTEP","conf_id":"4711","checkvariant":"SAP_CLOUD_PLATFORM_ATC_DEFAULT","pseudo_comment_policy":"MK","block_findings":"0","inform_findings":"1","is_default":false,"is_proxy_variant":false,"transport_check_policy":"C","check_tasks":"X","check_requests":"","check_tocs":"X"}
 
 --changeset--
 
@@ -406,6 +418,10 @@ Content-Type: application/json
 			"inform_findings": "1",
 			"is_default": false,
 			"is_proxy_variant": false,
+			"transport_check_policy": "C",
+			"check_tasks": "X",
+			"check_requests": "",
+			"check_tocs": "X",
 			"_priorities": [
 			]
 		}
@@ -433,6 +449,10 @@ Content-Type: application/json
 			"inform_findings": "1",
 			"is_default": false,
 			"is_proxy_variant": false,
+			"transport_check_policy": "C",
+			"check_tasks": "X",
+			"check_requests": "",
+			"check_tocs": "X",
 			"_priorities": [
 				{
 					"test": "CL_CI_TEST_AMDP_HDB_MIGRATION",
@@ -542,6 +562,10 @@ func TestRunAbapEnvironmentPushATCSystemConfig(t *testing.T) {
 			"inform_findings": "1",
 			"is_default": false,
 			"is_proxy_variant": false,
+			"transport_check_policy": "C",
+			"check_tasks": "X",
+			"check_requests": "",
+			"check_tocs": "X",
 			"_priorities": [
 				{
 					"test": "CL_CI_TEST_AMDP_HDB_MIGRATION",
@@ -604,6 +628,10 @@ func TestRunAbapEnvironmentPushATCSystemConfig(t *testing.T) {
 			"inform_findings": "1",
 			"is_default": false,
 			"is_proxy_variant": false,
+			"transport_check_policy": "C",
+			"check_tasks": "X",
+			"check_requests": "",
+			"check_tocs": "X",
 			"_priorities": [
 				{
 					"test": "CL_SOMECLASS",
