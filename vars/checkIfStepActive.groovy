a7fe867d34e879184c80e7523cbf7400b5ee0644
Ashly Mathew <ashly.mathew@sap.com>
2022-07-21 17:34:47 +0200

    Add a basic groovy wrapper copycat golang

---
+++ vars/checkIfStepActive.groovy
@@ -0,0 +1,11 @@
+import groovy.transform.Field
+
+@Field String STEP_NAME = getClass().getName()
+@Field String METADATA_FILE = ""
+
+void call(Map parameters = [:]) {
+    List credentials = [
+        [type: 'usernamePassword', id: 'golangPrivateModulesGitTokenCredentialsId', env: ['PIPER_privateModulesGitUsername', 'PIPER_privateModulesGitToken']]
+    ]
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
+}
