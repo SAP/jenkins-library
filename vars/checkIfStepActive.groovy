6c37a79ac34e4fce1dd610c3282efef9fd1aa477
Ashly Mathew <ashly.mathew@sap.com>
2022-07-27 17:26:01 +0200

    Add unstash method

--- vars/checkIfStepActive.groovy
+++ vars/checkIfStepActive.groovy
@@ -1,6 +1,8 @@
 import groovy.transform.Field
 import static com.sap.piper.Prerequisites.checkScript
+import com.sap.piper.Utils
 
+/Users/I560834/Dev/jenkins-library/vars/piperExecuteBin.groovy
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = ""
 
@@ -9,7 +11,12 @@ void call(Map parameters = [:]) {
     //     [type: 'usernamePassword', id: 'checkIfStepActiveCredentialsId', env: ['PIPER_username', 'PIPER_password']]
     // ]
     //piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
+
     Script script = checkScript(this, parameters) ?: this
+    def utils = parameters.juStabUtils ?: new Utils()
+
+    piperExecuteBin.prepareExecution(script, utils, parameters)
+
     String piperGoPath = parameters.piperGoPath ?: './piper'
     String stageConfig = parameters.stageConfig
     String stageOutputFile = parameters.stageOutputFile
