9b8f00ba153c20b39ca4746b61710c31cf72a1c6
Ashly Mathew <ashly.mathew@sap.com>
2022-07-27 16:56:16 +0200

    Groovy call shell script

--- vars/checkIfStepActive.groovy
+++ vars/checkIfStepActive.groovy
@@ -4,8 +4,12 @@ import groovy.transform.Field
 @Field String METADATA_FILE = ""
 
 void call(Map parameters = [:]) {
-    List credentials = [
-        [type: 'usernamePassword', id: 'checkIfStepActiveCredentialsId', env: ['PIPER_username', 'PIPER_password']]
-    ]
-    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
+    // List credentials = [
+    //     [type: 'usernamePassword', id: 'checkIfStepActiveCredentialsId', env: ['PIPER_username', 'PIPER_password']]
+    // ]
+    //piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
+    String piperGoPath = parameters.piperGoPath ?: './piper'
+    String stageConfig = parameters.stageConfig
+    String stageOutputFile = parameters.stageOutputFile
+    script.sh(returnStdout: true, script: "${piperGoPath} checkIfStepActive --stageConfig ${stageConfig} --useV1 --stageOutputFile ${stageOutputFile}")
 }
