fc859120e7b3de09e52e8971a195cb7795bc7ee1
Ashly Mathew <ashly.mathew@sap.com>
2022-07-27 14:50:36 +0200

    Groovy credentials

--- vars/checkIfStepActive.groovy
+++ vars/checkIfStepActive.groovy
@@ -4,8 +4,8 @@ import groovy.transform.Field
 @Field String METADATA_FILE = ""
 
 void call(Map parameters = [:]) {
-    //List credentials = [
-      //  [type: 'usernamePassword', id: 'golangPrivateModulesGitTokenCredentialsId', env: ['PIPER_privateModulesGitUsername', 'PIPER_privateModulesGitToken']]
-    //]
+    List credentials = [
+        [type: 'usernamePassword', id: 'checkIfStepActiveCredentialsId', env: ['PIPER_username', 'PIPER_password']]
+    ]
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
 }
