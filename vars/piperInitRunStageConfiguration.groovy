dfda8a2099220d9d5787f020046fca5ee237c7a9
Raman_Susla <catmorte@gmail.com>
2022-08-25 12:37:06 +0300

    change checkIfStepActive groovy wrapper + comments

--- vars/piperInitRunStageConfiguration.groovy
+++ vars/piperInitRunStageConfiguration.groovy
@@ -61,7 +61,7 @@ void call(Map parameters = [:]) {
     def resource = libraryResource(config.stageConfigResource)
     config.stages = (readYaml(text: resource)).spec.stages
     writeFile(file: ".pipeline/stage_conditions.yaml", text: resource)
-    def success = piperExecuteBin.checkIfStepActive parameters:parameters, script:script, piperGoPath:piperGoPath, stageConfig:".pipeline/stage_conditions.yaml", stepOutputFile:".pipeline/step_out.json", stageOutputFile:".pipeline/stage_out.json"
+    def success = piperExecuteBin.checkIfStepActive(parameters, script, piperGoPath, ".pipeline/stage_conditions.yaml", ".pipeline/step_out.json", ".pipeline/stage_out.json")
     if (!success) {
         throw new Exception("checkIfStepActive finished with error")
     }
