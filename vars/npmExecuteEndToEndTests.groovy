62fe822553ecf1f9b6ffffd03a13d3202334889d
Srinikitha Kondreddy <srinikitha.kondreddy@sap.com>
2022-08-30 15:40:44 +0200

    Conditional check with on_k8s env variable

--- vars/npmExecuteEndToEndTests.groovy
+++ vars/npmExecuteEndToEndTests.groovy
@@ -141,7 +141,7 @@ void call(Map parameters = [:]) {
                 }
             }
             e2ETests["E2E Tests ${index > 1 ? index : ''}"] = {
-                if (env.POD_NAME) {
+                if (env.POD_NAME || env.ON_K8S) {
                     dockerExecuteOnKubernetes(script: script, containerMap: ContainerMap.instance.getMap().get(stageName) ?: [:]) {
                         e2eTest.call()
                     }
