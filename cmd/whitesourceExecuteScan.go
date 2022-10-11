57b29e55f593a883f375c9a4490fa02a914dc945
nevskrem <s.merk@sap.com>
2022-10-11 16:23:51 +0200

    Fix nil reference

--- cmd/whitesourceExecuteScan.go
+++ cmd/whitesourceExecuteScan.go
@@ -693,8 +693,8 @@ func checkProjectSecurityViolations(config *ScanOptions, cvssSeverityLimit float
 		for _, alert := range alerts {
 			if result, err := alert.ContainedIn(assessments); err == nil && result == false {
 				filteredAlerts = append(filteredAlerts, alert)
-			} else {
-				log.Entry().Debugf("Matched assessment with status %v and analysis %v to vulnerability %v affecting packages %v", alert.Status, alert.Analysis, alert.Vulnerability, alert.Purls)
+			} else if len(alert.Vulnerability.Name) > 0 {
+				log.Entry().Debugf("Matched assessment with status %v and analysis %v to vulnerability %v affecting packages %v", alert.Status, alert.Analysis, alert.Vulnerability.Name, alert.Purls)
 				assessedAlerts = append(assessedAlerts, alert)
 			}
 		}
