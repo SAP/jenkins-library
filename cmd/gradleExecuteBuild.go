34e23d43afe34d0ae9c997754e0ef4c263320876
raman-susla-epam <104915202+raman-susla-epam@users.noreply.github.com>
2022-11-04 20:06:26 +0300

    Update gradleExecuteBuild.go

--- cmd/gradleExecuteBuild.go
+++ cmd/gradleExecuteBuild.go
@@ -2,13 +2,16 @@ package cmd
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"text/template"
+
 	"github.com/SAP/jenkins-library/pkg/command"
 	"github.com/SAP/jenkins-library/pkg/gradle"
 	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/piperenv"
 	"github.com/SAP/jenkins-library/pkg/piperutils"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
-	"text/template"
 )
 
 const (
@@ -18,59 +21,54 @@ const (
 var (
 	bomGradleTaskName = "cyclonedxBom"
 	publishTaskName   = "publish"
+	pathToModuleFile  = "./build/publications/maven/module.json"
 )
 
 const publishInitScriptContentTemplate = `
-rootProject {
-	println  "Init script publishing"
-	def username = "{{.RepositoryUsername}}"
-	def password = "{{.RepositoryPassword}}"
-	println  username.substring(0, 1) + " " + username.substring(1, username.length());
-	println  password.substring(0, 1) + " " + password.substring(1, password.length());
-	println  "{{.ArtifactID}}"
-	println  "{{.ArtifactVersion}}"
-	println  "{{.ArtifactGroupID}}"
-	println  "{{.RepositoryURL}}"
-    apply plugin: 'maven-publish'
-    apply plugin: 'java'
-
-    publishing {
-        publications {
-            maven(MavenPublication) {
-                versionMapping {
-                    usage('java-api') {
-                        fromResolutionOf('runtimeClasspath')
+{{ if .ApplyPublishingForAllProjects}}allprojects{{else}}rootProject{{ end }} {
+    def gradleExecuteBuild_skipPublishingProjects = [{{ if .ApplyPublishingForAllProjects}}{{range .ExcludePublishingForProjects}} "{{.}}",{{end}}{{end}} ];
+    if (!gradleExecuteBuild_skipPublishingProjects.contains(project.name)) {
+        apply plugin: 'maven-publish'
+        apply plugin: 'java'
+        publishing {
+            publications {
+                maven(MavenPublication) {
+                    versionMapping {
+                        usage('java-api') {
+                            fromResolutionOf('runtimeClasspath')
+                        }
+                        usage('java-runtime') {
+                            fromResolutionResult()
+                        }
                     }
-                    usage('java-runtime') {
-                        fromResolutionResult()
-                    }
+                    {{- if .ArtifactGroupID}}
+                    groupId = '{{.ArtifactGroupID}}'
+                    {{- end }}
+                    {{- if .ApplyPublishingForAllProjects }}
+                    {{else if .ArtifactID}}
+                    artifactId = '{{.ArtifactID}}'
+                    {{- end }}
+                    {{- if .ArtifactVersion}}
+                    version = '{{.ArtifactVersion}}'
+                    {{- end }}
+                    from components.java
                 }
-				{{- if .ArtifactGroupID}}
-				groupId = '{{.ArtifactGroupID}}'
-				{{- end }}
-				{{- if .ArtifactID}}
-				artifactId = '{{.ArtifactID}}'
-				{{- end }}
-				{{- if .ArtifactVersion}}
-				version = '{{.ArtifactVersion}}'
-				{{- end }}
-                from components.java
             }
-        }
-        repositories {
-            maven {
-                credentials {
-                    username = "{{.RepositoryUsername}}"
-                    password = "{{.RepositoryPassword}}"
+            repositories {
+                maven {
+                    credentials {
+                        username = "{{.RepositoryUsername}}"
+                        password = "{{.RepositoryPassword}}"
+                    }
+                    url = "{{.RepositoryURL}}"
                 }
-                url = "{{.RepositoryURL}}"
             }
         }
     }
 }
 `
 
-const bomInitScriptContent = `
+const bomInitScriptContentTemplate = `
 initscript {
   repositories {
     mavenCentral()
@@ -83,20 +81,36 @@ initscript {
   }
 }
 
-rootProject {
-    apply plugin: 'java'
-    apply plugin: 'maven'
-    apply plugin: org.cyclonedx.gradle.CycloneDxPlugin
+{{ if .ApplyCreateBOMForAllProjects}}allprojects{{else}}rootProject{{ end }} {
+    def gradleExecuteBuild_skipBOMProjects = [{{ if .ApplyCreateBOMForAllProjects}}{{range .ExcludeCreateBOMForProjects}} "{{.}}",{{end}}{{end}} ];
+    if (!gradleExecuteBuild_skipBOMProjects.contains(project.name)) {
+        apply plugin: 'java'
+        apply plugin: org.cyclonedx.gradle.CycloneDxPlugin
 
-    cyclonedxBom {
-        outputName = "` + gradleBomFilename + `"
-        outputFormat = "xml"
-        schemaVersion = "1.2"
-        includeConfigs = ["runtimeClasspath"]
-        skipConfigs = ["compileClasspath", "testCompileClasspath"]
+        cyclonedxBom {
+            outputName = "` + gradleBomFilename + `"
+            outputFormat = "xml"
+            schemaVersion = "1.2"
+            includeConfigs = ["runtimeClasspath"]
+            skipConfigs = ["compileClasspath", "testCompileClasspath"]
+        }
     }
 }
 `
+
+// PublishedArtifacts contains information about published artifacts
+type PublishedArtifacts struct {
+	Elements []Element `json:"variants,omitempty"`
+}
+
+type Element struct {
+	Name      string     `json:"name,omitempty"`
+	Artifacts []Artifact `json:"files,omitempty"`
+}
+
+type Artifact struct {
+	Name string `json:"name,omitempty"`
+}
 
 type gradleExecuteBuildUtils interface {
 	command.ExecRunner
@@ -120,15 +134,15 @@ func newGradleExecuteBuildUtils() gradleExecuteBuildUtils {
 	return &utils
 }
 
-func gradleExecuteBuild(config gradleExecuteBuildOptions, telemetryData *telemetry.CustomData) {
+func gradleExecuteBuild(config gradleExecuteBuildOptions, telemetryData *telemetry.CustomData, pipelineEnv *gradleExecuteBuildCommonPipelineEnvironment) {
 	utils := newGradleExecuteBuildUtils()
-	err := runGradleExecuteBuild(&config, telemetryData, utils)
+	err := runGradleExecuteBuild(&config, telemetryData, utils, pipelineEnv)
 	if err != nil {
 		log.Entry().WithError(err).Fatalf("step execution failed: %v", err)
 	}
 }
 
-func runGradleExecuteBuild(config *gradleExecuteBuildOptions, telemetryData *telemetry.CustomData, utils gradleExecuteBuildUtils) error {
+func runGradleExecuteBuild(config *gradleExecuteBuildOptions, telemetryData *telemetry.CustomData, utils gradleExecuteBuildUtils, pipelineEnv *gradleExecuteBuildCommonPipelineEnvironment) error {
 	log.Entry().Info("BOM file creation...")
 
 	if config.CreateBOM {
@@ -150,7 +164,7 @@ func runGradleExecuteBuild(config *gradleExecuteBuildOptions, telemetryData *tel
 
 	log.Entry().Info("Publishing of artifacts to staging repository...")
 	if config.Publish {
-		if err := publishArtifacts(config, utils); err != nil {
+		if err := publishArtifacts(config, utils, pipelineEnv); err != nil {
 			return err
 		}
 	}
@@ -159,11 +173,15 @@ func runGradleExecuteBuild(config *gradleExecuteBuildOptions, telemetryData *tel
 }
 
 func createBOM(config *gradleExecuteBuildOptions, utils gradleExecuteBuildUtils) error {
+	createBOMInitScriptContent, err := getInitScriptContent(config, bomInitScriptContentTemplate)
+	if err != nil {
+		return fmt.Errorf("failed to get BOM init script content: %v", err)
+	}
 	gradleOptions := &gradle.ExecuteOptions{
 		BuildGradlePath:   config.Path,
 		Task:              bomGradleTaskName,
 		UseWrapper:        config.UseWrapper,
-		InitScriptContent: bomInitScriptContent,
+		InitScriptContent: createBOMInitScriptContent,
 	}
 	if _, err := gradle.Execute(gradleOptions, utils); err != nil {
 		log.Entry().WithError(err).Errorf("failed to create BOM: %v", err)
@@ -172,8 +190,8 @@ func createBOM(config *gradleExecuteBuildOptions, utils gradleExecuteBuildUtils)
 	return nil
 }
 
-func publishArtifacts(config *gradleExecuteBuildOptions, utils gradleExecuteBuildUtils) error {
-	publishInitScriptContent, err := getPublishInitScriptContent(config)
+func publishArtifacts(config *gradleExecuteBuildOptions, utils gradleExecuteBuildUtils, pipelineEnv *gradleExecuteBuildCommonPipelineEnvironment) error {
+	publishInitScriptContent, err := getInitScriptContent(config, publishInitScriptContentTemplate)
 	if err != nil {
 		return fmt.Errorf("failed to get publish init script content: %v", err)
 	}
@@ -187,11 +205,16 @@ func publishArtifacts(config *gradleExecuteBuildOptions, utils gradleExecuteBuil
 		log.Entry().WithError(err).Errorf("failed to publish artifacts: %v", err)
 		return err
 	}
+	artifacts, err := getPublishedArtifactsNames(pathToModuleFile, utils)
+	if err != nil {
+		return fmt.Errorf("failed to get published artifacts: %v", err)
+	}
+	pipelineEnv.custom.artifacts = artifacts
 	return nil
 }
 
-func getPublishInitScriptContent(options *gradleExecuteBuildOptions) (string, error) {
-	tmpl, err := template.New("resources").Parse(publishInitScriptContentTemplate)
+func getInitScriptContent(options *gradleExecuteBuildOptions, templateContent string) (string, error) {
+	tmpl, err := template.New("resources").Parse(templateContent)
 	if err != nil {
 		return "", err
 	}
@@ -203,4 +226,33 @@ func getPublishInitScriptContent(options *gradleExecuteBuildOptions) (string, er
 	}
 
 	return generatedCode.String(), nil
+}
+
+func getPublishedArtifactsNames(file string, utils gradleExecuteBuildUtils) (piperenv.Artifacts, error) {
+	artifacts := piperenv.Artifacts{}
+	publishedArtifacts := PublishedArtifacts{}
+	exists, err := utils.FileExists(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check existence of the file '%s': %v", file, err)
+	}
+	if !exists {
+		return nil, fmt.Errorf("failed to get '%s': file does not exist", file)
+	}
+	content, err := utils.ReadFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read '%s': %v", file, err)
+	}
+	err = json.Unmarshal(content, &publishedArtifacts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal '%s': %v", file, err)
+	}
+	for _, element := range publishedArtifacts.Elements {
+		if element.Name != "apiElements" {
+			continue
+		}
+		for _, artifact := range element.Artifacts {
+			artifacts = append(artifacts, piperenv.Artifact{Name: artifact.Name})
+		}
+	}
+	return artifacts, nil
 }
