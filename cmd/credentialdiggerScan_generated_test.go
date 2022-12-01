66755b2625463f68def81bdd277c0239b5101e3f
Marco Rosa <marco.rosa@sap.com>
2022-12-01 18:13:12 +0100

    Add credentialdiggerScan implementation and tests

---
+++ cmd/credentialdiggerScan_generated_test.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCredentialdiggerScanCommand(t *testing.T) {
+	t.Parallel()
+
+	testCmd := CredentialdiggerScanCommand()
+
+	// only high level testing performed - details are tested in step generation procedure
+	assert.Equal(t, "credentialdiggerScan", testCmd.Use, "command name incorrect")
+
+}
