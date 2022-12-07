4d92a0c7173114f8c666d96a8be835f8c2c16c65
Sumit Kulhadia <sumit.kulhadia@sap.com>
2022-12-07 09:40:40 +0100

    cnbBuild: temporary remove experimental createBOM feature
    Co-authored-by: Pavel Busko <pavel.busko@sap.com>

--- cmd/cnbBuild.go
+++ cmd/cnbBuild.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
 
 	"github.com/SAP/jenkins-library/pkg/buildsettings"
 	"github.com/SAP/jenkins-library/pkg/certutils"
@@ -625,15 +624,15 @@ func runCnbBuild(config *cnbBuildOptions, cnbTelemetry *cnbBuildTelemetry, utils
 	commonPipelineEnvironment.container.imageDigest = digest
 	commonPipelineEnvironment.container.imageDigests = append(commonPipelineEnvironment.container.imageDigests, digest)
 
-	if config.CreateBOM {
-		bomFilename := fmt.Sprintf("bom-%s-%s.xml", targetImage.ContainerImageName, strings.TrimLeft(digest, "sha256:"))
-		imageName := fmt.Sprintf("%s:%s", containerImage, targetImage.ContainerImageTag)
-		err = cnbutils.MergeSBOMFiles("/layers/sbom/launch/**/sbom.syft.json", bomFilename, imageName, dockerConfigFile, utils)
-		if err != nil {
-			log.SetErrorCategory(log.ErrorBuild)
-			return errors.Wrap(err, "failed to create SBoM file")
-		}
-	}
+	// if config.CreateBOM {
+	// 	bomFilename := fmt.Sprintf("bom-%s-%s.xml", targetImage.ContainerImageName, strings.TrimLeft(digest, "sha256:"))
+	// 	imageName := fmt.Sprintf("%s:%s", containerImage, targetImage.ContainerImageTag)
+	// 	err = cnbutils.MergeSBOMFiles("/layers/sbom/launch/**/sbom.syft.json", bomFilename, imageName, dockerConfigFile, utils)
+	// 	if err != nil {
+	// 		log.SetErrorCategory(log.ErrorBuild)
+	// 		return errors.Wrap(err, "failed to create SBoM file")
+	// 	}
+	// }
 
 	if len(config.PreserveFiles) > 0 {
 		if pathType != pathEnumArchive {
