b904591f6e013dcae3468372aafd5e422512230c
Alex Gerstner <alex.gerstner@sap.com>
2022-12-12 15:58:03 +0100

    Add export step

--- cmd/piper.go
+++ cmd/piper.go
@@ -192,6 +192,7 @@ func Execute() {
 	rootCmd.AddCommand(AnsSendEventCommand())
 	rootCmd.AddCommand(ApiProviderListCommand())
 	rootCmd.AddCommand(TmsUploadCommand())
+	rootCmd.AddCommand(TmsExportCommand())
 
 	addRootFlags(rootCmd)
 
