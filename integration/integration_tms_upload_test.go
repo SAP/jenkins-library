554b5055eaed1634f810305f4cd9a2687ba7557d
Alex Gerstner <alex.gerstner@sap.com>
2022-12-12 15:48:26 +0100

    integration test

---
+++ integration/integration_tms_upload_test.go
@@ -0,0 +1,76 @@
+//go:build integration
+// +build integration
+
+// can be executed with
+// go test -v -tags integration -run TestTmsUploadIntegration ./integration/...
+
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestTmsUploadIntegration(t *testing.T) {
+	//Reading TMS credentials from environment
+	tmsServiceKey := os.Getenv("PIPER_tmsServiceKey")
+	if len(tmsServiceKey) == 0 {
+		tmsServiceKey := os.Getenv("PIPER_TMSSERVICEKEY")
+		if len(tmsServiceKey) == 0 {
+			t.Fatal("No tmsServiceKey maintained")
+		}
+	}
+
+	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
+		Image:       "devxci/mbtci-java11-node14",
+		User:        "root",
+		TestDir:     []string{"testdata", "TestTmsUploadIntegration"},
+		Environment: map[string]string{"PIPER_tmsServiceKey": tmsServiceKey},
+	})
+	defer container.terminate(t)
+
+	err := container.whenRunningPiperCommand("tmsUpload",
+		"--tmsServiceKey="+tmsServiceKey,
+		"--mtaPath=scv_x.mtar",
+		"--nodeName=PIPER-TEST",
+		"--customDescription=Piper node upload integration test",
+		"--nodeExtDescriptorMapping={\"PIPER-TEST\":\"scv_x.mtaext\", \"PIPER-PROD\":\"scv_x.mtaext\"}",
+		"--mtaVersion=1.0.0")
+	if err != nil {
+		t.Fatalf("Piper command failed %s", err)
+	}
+
+	container.assertHasOutput(t, "tmsUpload - SUCCESS")
+}
+
+func TestTmsExportIntegration(t *testing.T) {
+	//Reading TMS credentials from environment
+	tmsServiceKey := os.Getenv("PIPER_tmsServiceKey")
+	if len(tmsServiceKey) == 0 {
+		tmsServiceKey := os.Getenv("PIPER_TMSSERVICEKEY")
+		if len(tmsServiceKey) == 0 {
+			t.Fatal("No tmsServiceKey maintained")
+		}
+	}
+
+	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
+		Image:       "devxci/mbtci-java11-node14",
+		User:        "root",
+		TestDir:     []string{"testdata", "TestTmsUploadIntegration"},
+		Environment: map[string]string{"PIPER_tmsServiceKey": tmsServiceKey},
+	})
+	defer container.terminate(t)
+
+	err := container.whenRunningPiperCommand("tmsExport",
+		"--tmsServiceKey="+tmsServiceKey,
+		"--mtaPath=scv_x.mtar",
+		"--nodeName=PIPER-TEST",
+		"--customDescription=Piper node export integration test",
+		"--nodeExtDescriptorMapping={\"PIPER-TEST\":\"scv_x.mtaext\", \"PIPER-PROD\":\"scv_x.mtaext\"}",
+		"--mtaVersion=1.0.0")
+	if err != nil {
+		t.Fatalf("Piper command failed %s", err)
+	}
+
+	container.assertHasOutput(t, "tmsUpload - SUCCESS")
+}
