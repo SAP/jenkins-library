7bf51caecef98d5218b185238793dc12fb317e41
Alex Gerstner <alex.gerstner@sap.com>
2023-01-11 12:00:32 +0100

    namedUser param

--- vars/tmsExport.groovy
+++ vars/tmsExport.groovy
@@ -51,12 +51,13 @@ void call(Map parameters = [:]) {
         [type: 'token', id: 'credentialsId', env: ['PIPER_tmsServiceKey']]
     ]
 
-    /**
-    def jenkinsUtils = parameters.jenkinsUtilsStub ?: new JenkinsUtils()
-    def namedUser = jenkinsUtils.getJobStartedByUserId()
-    if (namedUser) {
-        parameters.namedUser = namedUser
-    }
-    */
+    if (!parameters.namedUser) {
+        def jenkinsUtils = new JenkinsUtils()
+        def namedUser = jenkinsUtils.getJobStartedByUserId()
+        if (namedUser) {
+            parameters.namedUser = namedUser
+        }
+    }    
+    
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials, false, false, true)
 }
