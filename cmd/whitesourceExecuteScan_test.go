492d338ffc767e8a96bfe748facd5eaa85b6a839
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2023-01-13 22:24:47 +0600

    Add test case

--- cmd/whitesourceExecuteScan_test.go
+++ cmd/whitesourceExecuteScan_test.go
@@ -334,6 +334,28 @@ func TestResolveProjectIdentifiers(t *testing.T) {
 			assert.Equal(t, "mock-product-token", config.ProductToken)
 		}
 	})
+	t.Run("success - with version from default (projectName is filled)", func(t *testing.T) {
+		// init
+		config := ScanOptions{
+			BuildTool: "mta",
+			// CustomScanVersion: "latest",
+			VersioningModel: "major-minor",
+			ProductName:     "mock-product",
+			ProjectName:     "mock-project",
+			Version:         "1.2.3",
+		}
+		utilsMock := newWhitesourceUtilsMock()
+		systemMock := ws.NewSystemMock("ignored")
+		scan := newWhitesourceScan(&config)
+		// test
+		err := resolveProjectIdentifiers(&config, scan, utilsMock, systemMock)
+		// assert
+		if assert.NoError(t, err) {
+			assert.Equal(t, "mock-project", scan.AggregateProjectName)
+			assert.Equal(t, "1.2", config.Version)
+			assert.Equal(t, "mock-product-token", config.ProductToken)
+		}
+	})
 	t.Run("retrieves token for configured project name", func(t *testing.T) {
 		// init
 		config := ScanOptions{
