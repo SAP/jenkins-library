97f85078193fb5b7cefd02ddcfea275ddcfed2ac
Alex Gerstner <alex.gerstner@sap.com>
2023-01-24 18:38:07 +0100

    Extend integration tests

--- integration/integration_tms_upload_test.go
+++ integration/integration_tms_upload_test.go
@@ -7,25 +7,30 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
-func TestTmsUploadIntegration1(t *testing.T) {
+func readEnv() string {
 	//Reading TMS credentials from environment
 	tmsServiceKey := os.Getenv("PIPER_tmsServiceKey")
 	if len(tmsServiceKey) == 0 {
 		tmsServiceKey := os.Getenv("PIPER_TMSSERVICEKEY")
 		if len(tmsServiceKey) == 0 {
-			t.Fatal("No tmsServiceKey maintained")
+			fmt.Println("No tmsServiceKey maintained")
 		}
 	}
+	return tmsServiceKey
+}
+
+func TestTmsUploadIntegrationBin(t *testing.T) {
+	tmsServiceKey := readEnv()
 
 	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
-		Image:       "devxci/mbtci-java11-node14",
-		User:        "root",
-		TestDir:     []string{"testdata", "TestTmsUploadIntegration"},
-		Environment: map[string]string{"PIPER_tmsServiceKey": tmsServiceKey},
+		Image:   "devxci/mbtci-java11-node14",
+		User:    "root",
+		TestDir: []string{"testdata", "TestTmsUploadIntegration/bin_param"},
 	})
 	defer container.terminate(t)
 
@@ -35,10 +40,33 @@ func TestTmsUploadIntegration1(t *testing.T) {
 		"--nodeName=PIPER-TEST",
 		"--customDescription=Piper integration test",
 		"--nodeExtDescriptorMapping={\"PIPER-TEST\":\"scv_x.mtaext\", \"PIPER-PROD\":\"scv_x.mtaext\"}",
-		"--mtaVersion=1.0.0")
+		"--mtaVersion=1.0.0",
+		"-v")
 	if err != nil {
 		t.Fatalf("Piper command failed %s", err)
 	}
 
+	container.assertHasOutput(t, "tmsUpload - File uploaded successfully")
 	container.assertHasOutput(t, "tmsUpload - SUCCESS")
+}
+
+func TestTmsUploadIntegrationYaml(t *testing.T) {
+	tmsServiceKey := readEnv()
+
+	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
+		Image:       "devxci/mbtci-java11-node14",
+		User:        "root",
+		TestDir:     []string{"testdata", "TestTmsUploadIntegration/yaml"},
+		Environment: map[string]string{"PIPER_tmsServiceKey": tmsServiceKey},
+	})
+	defer container.terminate(t)
+
+	err := container.whenRunningPiperCommand("tmsUpload")
+	if err != nil {
+		t.Fatalf("Piper command failed %s", err)
+	}
+
+	container.assertHasOutput(t, "tmsUpload - File uploaded successfully")
+	container.assertHasOutput(t, "tmsUpload - SUCCESS")
+	container.assertHasNoOutput(t, "Config value for 'nodeExtDescriptorMapping' is of unexpected type map, expected string")
 }
