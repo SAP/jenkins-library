af458bec02eb815642fa123a8adcd13963cd1af3
DanielMieg <daniel.mieg@sap.com>
2023-02-09 11:45:06 +0100

    Change to default cf create-service

--- cmd/abapEnvironmentCreateSystem_test.go
+++ cmd/abapEnvironmentCreateSystem_test.go
@@ -15,7 +15,7 @@ func TestRunAbapEnvironmentCreateSystem(t *testing.T) {
 	cf := cloudfoundry.CFUtils{Exec: m}
 	u := &uuidMock{}
 
-	t.Run("Create service with generated manifest", func(t *testing.T) {
+	t.Run("Create service with cf create-service", func(t *testing.T) {
 		defer cfMockCleanup(m)
 		config := abapEnvironmentCreateSystemOptions{
 			CfAPIEndpoint:     "https://api.endpoint.com",
@@ -27,12 +27,11 @@ func TestRunAbapEnvironmentCreateSystem(t *testing.T) {
 			CfServiceInstance: "testName",
 			CfServicePlan:     "testPlan",
 		}
-		wd, _ := os.Getwd()
 		err := runAbapEnvironmentCreateSystem(&config, nil, cf, u)
 		if assert.NoError(t, err) {
 			assert.Equal(t, []mock.ExecCall{
 				{Execution: (*mock.Execution)(nil), Async: false, Exec: "cf", Params: []string{"login", "-a", "https://api.endpoint.com", "-o", "testOrg", "-s", "testSpace", "-u", "testUser", "-p", "testPassword"}},
-				{Execution: (*mock.Execution)(nil), Async: false, Exec: "cf", Params: []string{"create-service-push", "--no-push", "--service-manifest", wd + "/generated_service_manifest-my-uuid.yml"}},
+				{Execution: (*mock.Execution)(nil), Async: false, Exec: "cf", Params: []string{"create-service", config.CfService, config.CfServicePlan, config.CfServiceInstance, "-c", "{\"is_development_allowed\":false}", "--wait"}},
 				{Execution: (*mock.Execution)(nil), Async: false, Exec: "cf", Params: []string{"logout"}}},
 				m.Calls)
 		}
@@ -130,7 +129,7 @@ repositories:
   plan: testPlan
 `
 
-		resultBytes, err := generateManifestYAML(&config)
+		resultBytes, err := generateServiceParameterString(&config)
 
 		if assert.NoError(t, err) {
 			result := string(resultBytes)
@@ -174,17 +173,11 @@ repositories:
 		addonYMLBytes := []byte(addonYML)
 		err := ioutil.WriteFile("addon.yml", addonYMLBytes, 0644)
 
-		expectedResult := `create-services:
-- broker: testService
-  name: testName
-  parameters: '{"admin_email":"user@example.com","is_development_allowed":true,"sapsystemname":"H02","size_of_persistence":4,"size_of_runtime":4,"addon_product_name":"myProduct","addon_product_version":"1.2.3","parent_saas_appname":"addon_test"}'
-  plan: testPlan
-`
+		expectedResult := "{\"admin_email\":\"user@example.com\",\"is_development_allowed\":true,\"sapsystemname\":\"H02\",\"size_of_persistence\":4,\"size_of_runtime\":4,\"addon_product_name\":\"myProduct\",\"addon_product_version\":\"1.2.3\",\"parent_saas_appname\":\"addon_test\"}"
 
-		resultBytes, err := generateManifestYAML(&config)
+		result, err := generateServiceParameterString(&config)
 
 		if assert.NoError(t, err) {
-			result := string(resultBytes)
 			assert.Equal(t, expectedResult, result, "Result not as expected")
 		}
 	})
@@ -224,17 +217,11 @@ repositories:
 		addonYMLBytes := []byte(addonYML)
 		err := ioutil.WriteFile("addon.yml", addonYMLBytes, 0644)
 
-		expectedResult := `create-services:
-- broker: testService
-  name: testName
-  parameters: '{"admin_email":"user@example.com","is_development_allowed":true,"sapsystemname":"H02","size_of_persistence":4,"size_of_runtime":4}'
-  plan: testPlan
-`
+		expectedResult := "{\"admin_email\":\"user@example.com\",\"is_development_allowed\":true,\"sapsystemname\":\"H02\",\"size_of_persistence\":4,\"size_of_runtime\":4}"
 
-		resultBytes, err := generateManifestYAML(&config)
+		result, err := generateServiceParameterString(&config)
 
 		if assert.NoError(t, err) {
-			result := string(resultBytes)
 			assert.Equal(t, expectedResult, result, "Result not as expected")
 		}
 	})
@@ -253,7 +240,7 @@ repositories:
 			AbapSystemID:                   "H02",
 			AbapSystemIsDevelopmentAllowed: false,
 			AbapSystemSizeOfPersistence:    4,
-			AbapSystemSizeOfRuntime:        4,
+			AbapSystemSizeOfRuntime:        6,
 			AddonDescriptorFileName:        "addon.yml",
 			IncludeAddon:                   true,
 		}
@@ -275,17 +262,11 @@ repositories:
 		addonYMLBytes := []byte(addonYML)
 		err := ioutil.WriteFile("addon.yml", addonYMLBytes, 0644)
 
-		expectedResult := `create-services:
-- broker: testService
-  name: testName
-  parameters: '{"admin_email":"user@example.com","is_development_allowed":false,"sapsystemname":"H02","size_of_persistence":4,"size_of_runtime":4,"addon_product_name":"myProduct","addon_product_version":"1.2.3","parent_saas_appname":"addon_test"}'
-  plan: testPlan
-`
+		expectedResult := "{\"admin_email\":\"user@example.com\",\"is_development_allowed\":true,\"sapsystemname\":\"H02\",\"size_of_persistence\":4,\"size_of_runtime\":4,\"addon_product_name\":\"myProduct\",\"addon_product_version\":\"1.2.3\",\"parent_saas_appname\":\"addon_test\"}"
 
-		resultBytes, err := generateManifestYAML(&config)
+		result, err := generateServiceParameterString(&config)
 
 		if assert.NoError(t, err) {
-			result := string(resultBytes)
 			assert.Equal(t, expectedResult, result, "Result not as expected")
 		}
 	})
