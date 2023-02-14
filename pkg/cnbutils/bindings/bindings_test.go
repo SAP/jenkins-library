99d7dd11542dd6fb6aa4294a50eeb6af5449bcab
Ralf Pannemans <ralf.pannemans@sap.com>
2023-02-14 13:50:14 +0100

    Adapt and enhance existing mapping tests

--- pkg/cnbutils/bindings/bindings_test.go
+++ pkg/cnbutils/bindings/bindings_test.go
@@ -21,7 +21,7 @@ func TestProcessBindings(t *testing.T) {
 		return utils
 	}
 
-	t.Run("writes bindings to files", func(t *testing.T) {
+	t.Run("DEPRECATED: writes bindings to files", func(t *testing.T) {
 		var utils = mockUtils()
 		httpmock.Activate()
 		defer httpmock.DeactivateAndReset()
@@ -86,13 +86,92 @@ func TestProcessBindings(t *testing.T) {
 		}
 	})
 
+	t.Run("writes bindings to files", func(t *testing.T) {
+		var utils = mockUtils()
+		httpmock.Activate()
+		defer httpmock.DeactivateAndReset()
+		httpmock.RegisterResponder(http.MethodGet, "http://test-url.com/binding", httpmock.NewStringResponder(200, "from url content"))
+		client := &piperhttp.Client{}
+		client.SetOptions(piperhttp.ClientOptions{MaxRetries: -1, UseDefaultTransport: true})
+		err := bindings.ProcessBindings(utils, client, "/tmp/platform", map[string]interface{}{
+			"a": map[string]interface{}{
+				"type": "test",
+				"data": []map[string]interface{}{
+					{
+						"key":     "inline.yaml",
+						"content": "my inline content",
+					},
+					{
+						"key":  "from-file.yaml",
+						"file": "/tmp/somefile.yaml",
+					},
+					{
+						"key":     "from-url.yaml",
+						"fromUrl": "http://test-url.com/binding",
+					},
+				},
+			},
+			"b": map[string]interface{}{
+				"type": "test2",
+				"data": []map[string]interface{}{
+					{
+						"key":     "inline2.yaml",
+						"content": "my inline content2",
+					},
+				},
+			},
+		})
+
+		if assert.NoError(t, err) {
+			if assert.True(t, utils.HasFile("/tmp/platform/bindings/a/type")) {
+				content, err := utils.FileRead("/tmp/platform/bindings/a/type")
+				if assert.NoError(t, err) {
+					assert.Equal(t, string(content), "test")
+				}
+
+				if assert.True(t, utils.HasFile("/tmp/platform/bindings/a/inline.yaml")) {
+					content, err = utils.FileRead("/tmp/platform/bindings/a/inline.yaml")
+					if assert.NoError(t, err) {
+						assert.Equal(t, string(content), "my inline content")
+					}
+				}
+
+				assert.True(t, utils.HasCopiedFile("/tmp/somefile.yaml", "/tmp/platform/bindings/a/from-file.yaml"))
+
+				if assert.True(t, utils.HasFile("/tmp/platform/bindings/a/from-url.yaml")) {
+					content, err := utils.FileRead("/tmp/platform/bindings/a/from-url.yaml")
+					if assert.NoError(t, err) {
+						assert.Equal(t, string(content), "from url content")
+					}
+				}
+			}
+			if assert.True(t, utils.HasFile("/tmp/platform/bindings/b/type")) {
+				content, err := utils.FileRead("/tmp/platform/bindings/b/type")
+				if assert.NoError(t, err) {
+					assert.Equal(t, string(content), "test2")
+				}
+
+				if assert.True(t, utils.HasFile("/tmp/platform/bindings/b/inline2.yaml")) {
+					content, err = utils.FileRead("/tmp/platform/bindings/b/inline2.yaml")
+					if assert.NoError(t, err) {
+						assert.Equal(t, string(content), "my inline content2")
+					}
+				}
+			}
+		}
+	})
+
 	t.Run("fails with the name being invalid", func(t *testing.T) {
 		var utils = mockUtils()
 		err := bindings.ProcessBindings(utils, &piperhttp.Client{}, "/tmp/platform", map[string]interface{}{
 			"..": map[string]interface{}{
-				"key":     "inline.yaml",
-				"type":    "inline",
-				"content": "my inline content",
+				"type": "inline",
+				"data": []map[string]interface{}{
+					{
+						"key":     "inline.yaml",
+						"content": "my inline content",
+					},
+				},
 			},
 		})
 
@@ -105,9 +184,13 @@ func TestProcessBindings(t *testing.T) {
 		var utils = mockUtils()
 		err := bindings.ProcessBindings(utils, &piperhttp.Client{}, "/tmp/platform", map[string]interface{}{
 			"binding": map[string]interface{}{
-				"key":     "test/test.yaml",
-				"type":    "inline",
-				"content": "my inline content",
+				"type": "inline",
+				"data": []map[string]interface{}{
+					{
+						"key":     "test/test.yaml",
+						"content": "my inline content",
+					},
+				},
 			},
 		})
 
@@ -120,15 +203,19 @@ func TestProcessBindings(t *testing.T) {
 		var utils = mockUtils()
 		err := bindings.ProcessBindings(utils, &piperhttp.Client{}, "/tmp/platform", map[string]interface{}{
 			"binding": map[string]interface{}{
-				"key":     "test.yaml",
-				"type":    "both",
-				"content": "my inline content",
-				"file":    "/tmp/somefile.yaml",
+				"type": "both",
+				"data": []map[string]interface{}{
+					{
+						"key":     "test.yaml",
+						"content": "my inline content",
+						"file":    "/tmp/somefile.yaml",
+					},
+				},
 			},
 		})
 
 		if assert.Error(t, err) {
-			assert.Equal(t, "only one of 'content', 'file' or 'fromUrl' can be set for a binding", err.Error())
+			assert.Equal(t, "only one of 'content', 'file' or 'fromUrl' can be set for binding 'test.yaml'", err.Error())
 		}
 	})
 
@@ -142,7 +229,22 @@ func TestProcessBindings(t *testing.T) {
 		})
 
 		if assert.Error(t, err) {
-			assert.Equal(t, "one of 'file', 'content' or 'fromUrl' properties must be specified for binding", err.Error())
+			assert.Equal(t, "'data' must be specified for binding 'test.yaml'", err.Error())
+		}
+	})
+
+	t.Run("fails with empty data being specified", func(t *testing.T) {
+		var utils = mockUtils()
+		err := bindings.ProcessBindings(utils, &piperhttp.Client{}, "/tmp/platform", map[string]interface{}{
+			"binding": map[string]interface{}{
+				"key":  "test.yaml",
+				"type": "none",
+				"data": []map[string]interface{}{},
+			},
+		})
+
+		if assert.Error(t, err) {
+			assert.Equal(t, "one of 'file', 'content' or 'fromUrl' properties must be specified for binding 'test.yaml'", err.Error())
 		}
 	})
 
