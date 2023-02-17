5913d55543d5019a0072973ebc4ada6a81f7e4d8
Alex Gerstner <alex.gerstner@sap.com>
2023-02-17 18:12:54 +0100

    Extend tests

--- integration/integration_tms_upload_test.go
+++ integration/integration_tms_upload_test.go
@@ -2,7 +2,7 @@
 // +build integration
 
 // can be executed with
-// go test -v -tags integration -run TestTmsUploadIntegration ./integration/...
+// go test -v -tags integration -run TestTmsUploadIntegration ./integration
 
 package main
 
@@ -10,23 +10,73 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func readEnv() string {
+var tmsServiceKey string
+
+func readEnv() {
 	//Reading TMS credentials from environment
-	tmsServiceKey := os.Getenv("PIPER_tmsServiceKey")
+	tmsServiceKey = os.Getenv("PIPER_tmsServiceKey")
 	if len(tmsServiceKey) == 0 {
-		tmsServiceKey := os.Getenv("PIPER_TMSSERVICEKEY")
-		if len(tmsServiceKey) == 0 {
-			fmt.Println("No tmsServiceKey maintained")
-		}
+		fmt.Println("Env. variable PIPER_tmsServiceKey is not provided")
+		os.Exit(1)
 	}
-	return tmsServiceKey
 }
 
-func TestTmsUploadIntegrationBin(t *testing.T) {
-	tmsServiceKey := readEnv()
+func TestTmsUploadIntegrationBinSuccess(t *testing.T) {
+	// success case: run cmd without nodeExtDescriptorMapping
+	readEnv()
+	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
+		Image:       "devxci/mbtci-java11-node14",
+		User:        "root",
+		TestDir:     []string{"testdata", "TestTmsUploadIntegration/bin_param"},
+		Environment: map[string]string{"PIPER_tmsServiceKey": tmsServiceKey},
+	})
+	defer container.terminate(t)
 
+	err := container.whenRunningPiperCommand("tmsUpload",
+		"--mtaPath=scv_x.mtar",
+		"--nodeName=PIPER-TEST",
+		"--customDescription=Piper integration test",
+		"--mtaVersion=1.0.0",
+		"-v")
+	if err != nil {
+		t.Fatalf("Piper command failed %s", err)
+	}
+	container.assertHasOutput(t, "tmsUpload - File uploaded successfully")
+	container.assertHasOutput(t, "tmsUpload - Node upload executed successfully")
+	container.assertHasOutput(t, "tmsUpload - SUCCESS")
+}
+
+func TestTmsUploadIntegrationBinNoDescriptionSuccess(t *testing.T) {
+	// success case: run cmd without --nodeExtDescriptorMapping and --customDescription
+	readEnv()
+	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
+		Image:       "devxci/mbtci-java11-node14",
+		User:        "root",
+		TestDir:     []string{"testdata", "TestTmsUploadIntegration/bin_param"},
+		Environment: map[string]string{"PIPER_tmsServiceKey": tmsServiceKey},
+	})
+	defer container.terminate(t)
+
+	err := container.whenRunningPiperCommand("tmsUpload",
+		"--mtaPath=scv_x.mtar",
+		"--nodeName=PIPER-TEST",
+		"--mtaVersion=1.0.0",
+		"-v")
+	if err != nil {
+		t.Fatalf("Piper command failed %s", err)
+	}
+	container.assertHasOutput(t, "tmsUpload - File uploaded successfully")
+	container.assertHasOutput(t, "tmsUpload - Node upload executed successfully")
+	container.assertHasOutput(t, "tmsUpload - SUCCESS")
+}
+
+func TestTmsUploadIntegrationBinFailParam(t *testing.T) {
+	// run cmd with nodeExtDescriptorMapping
+	readEnv()
 	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
 		Image:   "devxci/mbtci-java11-node14",
 		User:    "root",
@@ -35,24 +85,20 @@ func TestTmsUploadIntegrationBin(t *testing.T) {
 	defer container.terminate(t)
 
 	err := container.whenRunningPiperCommand("tmsUpload",
-		"--tmsServiceKey="+tmsServiceKey,
 		"--mtaPath=scv_x.mtar",
 		"--nodeName=PIPER-TEST",
 		"--customDescription=Piper integration test",
 		"--nodeExtDescriptorMapping={\"PIPER-TEST\":\"scv_x.mtaext\", \"PIPER-PROD\":\"scv_x.mtaext\"}",
 		"--mtaVersion=1.0.0",
 		"-v")
-	if err != nil {
-		t.Fatalf("Piper command failed %s", err)
-	}
 
-	container.assertHasOutput(t, "tmsUpload - File uploaded successfully")
-	container.assertHasOutput(t, "tmsUpload - SUCCESS")
+	assert.Error(t, err, "Did expect error")
+	container.assertHasOutput(t, "Error: unknown flag: --nodeExtDescriptorMapping")
 }
 
 func TestTmsUploadIntegrationYaml(t *testing.T) {
-	tmsServiceKey := readEnv()
-
+	// success case: run with config.yaml
+	readEnv()
 	container := givenThisContainer(t, IntegrationTestDockerExecRunnerBundle{
 		Image:       "devxci/mbtci-java11-node14",
 		User:        "root",
@@ -67,6 +113,7 @@ func TestTmsUploadIntegrationYaml(t *testing.T) {
 	}
 
 	container.assertHasOutput(t, "tmsUpload - File uploaded successfully")
+	container.assertHasOutput(t, "tmsUpload - MTA extension descriptor updated successfully")
+	container.assertHasOutput(t, "tmsUpload - Node upload executed successfully")
 	container.assertHasOutput(t, "tmsUpload - SUCCESS")
-	container.assertHasNoOutput(t, "Config value for 'nodeExtDescriptorMapping' is of unexpected type map, expected string")
 }
