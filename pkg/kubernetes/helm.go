1f427e428b144599ffb40dcd6847cf44d18b8c7f
Gergely Czuczy <gergely.czuczy@sap.com>
2023-03-10 13:01:00 +0100

    helmExecute can now upload to a GSC bucket as a repo properly

--- pkg/kubernetes/helm.go
+++ pkg/kubernetes/helm.go
@@ -3,12 +3,22 @@ package kubernetes
 import (
 	"fmt"
 	"io"
+	"os"
 	"net/http"
 	"path/filepath"
 	"strings"
+	"context"
 
 	piperhttp "github.com/SAP/jenkins-library/pkg/http"
 	"github.com/SAP/jenkins-library/pkg/log"
+
+	// google auth
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/option"
+
+	"helm.sh/helm/v3/pkg/cli"
+	"helm.sh/helm/v3/pkg/repo"
+	"helm.sh/helm/v3/pkg/getter"
 )
 
 // HelmExecutor is used for mock
@@ -61,6 +71,8 @@ type HelmExecuteOptions struct {
 	SourceRepositoryPassword  string   `json:"sourceRepositoryPassword,omitempty"`
 	HelmCommand               string   `json:"helmCommand,omitempty"`
 	CustomTLSCertificateLinks []string `json:"customTlsCertificateLinks,omitempty"`
+	GCPJsonKeyFilePath        string   `json:"gcpKeyFilePath,omitempty"`
+	GCSBucketId               string   `json:"gcsBucketId,omitempty"`
 }
 
 // NewHelmExecutor creates HelmExecute instance
@@ -418,33 +430,143 @@ func (h *HelmExecute) RunHelmPublish() (string, error) {
 		return "", fmt.Errorf("there's no target repository for helm chart publishing configured")
 	}
 
-	repoClientOptions := piperhttp.ClientOptions{
-		Username:     h.config.TargetRepositoryUser,
-		Password:     h.config.TargetRepositoryPassword,
-		TrustedCerts: h.config.CustomTLSCertificateLinks,
-	}
-
-	h.utils.SetOptions(repoClientOptions)
-
 	binary := fmt.Sprintf("%s-%s.tgz", h.config.DeploymentName, h.config.PublishVersion)
-
 	separator := "/"
-
 	if strings.HasSuffix(h.config.TargetRepositoryURL, "/") {
 		separator = ""
 	}
-
 	targetURL := fmt.Sprintf("%s%s%s", h.config.TargetRepositoryURL, separator, binary)
-
 	log.Entry().Infof("publishing artifact: %s", targetURL)
 
-	response, err := h.utils.UploadRequest(http.MethodPut, targetURL, binary, "", nil, nil, "binary")
-	if err != nil {
-		return "", fmt.Errorf("couldn't upload artifact: %w", err)
-	}
+	if len(h.config.GCPJsonKeyFilePath)>0 && len(h.config.GCSBucketId)>0 {
+		ctx := context.Background()
+		gcs, err := storage.NewClient(ctx, option.WithCredentialsFile(h.config.GCPJsonKeyFilePath))
+		if err != nil {
+			return "", err
+		}
 
-	if !(response.StatusCode == 200 || response.StatusCode == 201) {
-		return "", fmt.Errorf("couldn't upload artifact, received status code %d", response.StatusCode)
+		bucket := gcs.Bucket(h.config.GCSBucketId)
+		// check whether it exists:
+		_, err = bucket.Attrs(ctx)
+		if err != nil {
+			return "", err
+		}
+
+		log.Entry().Debug("Checking whether artifact already exists in registry")
+		// first check whether the chart is already present
+		helmSettings := cli.New()
+		helmProviders := getter.All(helmSettings)
+
+		checkExisting, err := repo.FindChartInRepoURL(h.config.TargetRepositoryURL,
+			h.config.DeploymentName, h.config.PublishVersion,
+			"", "", "",	helmProviders)
+		if err == nil {
+			return "", fmt.Errorf("Chart already present in the repository")
+		}
+		fmt.Printf("!!! '%s' '%s' '%s' -> %s\n", h.config.TargetRepositoryURL,
+			h.config.DeploymentName, h.config.PublishVersion, checkExisting)
+
+		// take care of the index
+		// 1) fetch the index
+		// 2) load the new index
+		// 3) generate one for the new artifact
+		// 4) merge them
+		// 5) upload the new index
+		// only upload the new artifact once we've successfully merged the
+		// it into the index
+		log.Entry().Infof("Adding new artifact %s to registry in bucket %s", binary, h.config.GCSBucketId)
+		{
+			indexdir, err := os.MkdirTemp("./", "index-")
+			if err != nil {
+				return "", err
+			}
+			indexOld := filepath.Join(indexdir, "index-old.yaml")
+			indexNew := filepath.Join(indexdir, "index-new.yaml")
+			// fetch the current index
+			if err = downloadFileFromBucket(bucket, "index.yaml", indexOld, ctx); err != nil {
+				return "", err
+			}
+
+			// load it
+			currIndex, err := repo.LoadIndexFile(indexOld)
+			if err != nil {
+				return "", err
+			}
+
+			// Index the artifact in a temp directory
+			tmpdir, err := os.MkdirTemp("./", "helmpkg-")
+			if err != nil {
+				return "", err
+			}
+			if err = os.Link(binary, filepath.Join(tmpdir, binary)); err != nil {
+				return "", err
+			}
+
+			cwd, err := os.Getwd()
+			if err != nil {
+				return "", err
+			}
+			// generate the index
+			if err = os.Chdir(tmpdir); err != nil {
+				return "", err
+			}
+			newIndex, err := repo.IndexDirectory(".", h.config.TargetRepositoryURL)
+			if err != nil {
+				os.Chdir(cwd)
+				return "", nil
+			}
+			os.Chdir(cwd)
+
+			log.Entry().Debug("Merging new artifact into existing index")
+			// merge them
+			//currIndex.Merge(newIndex)
+			newIndex.Merge(currIndex)
+
+			// write it
+			if err = newIndex.WriteFile(indexNew, 0644); err != nil {
+				return "", err;
+			}
+
+			// upload the new artifact
+			log.Entry().Debugf("Uploading artifact %s to bucket %s", binary, h.config.GCSBucketId)
+			_, err = uploadFileToBucket(bucket, binary, binary, ctx)
+			if err != nil {
+				return "", err
+			}
+
+			log.Entry().Infof("Uploading new index to bucket %s", h.config.GCSBucketId)
+			// upload it
+			indexObject, err := uploadFileToBucket(bucket, indexNew, "index.yaml", ctx)
+			if err != nil {
+				return "", err
+			}
+			_, err = indexObject.Update(ctx, storage.ObjectAttrsToUpdate{
+				CacheControl: "no-store",
+				ContentType: "application/yaml",
+			})
+			if err != nil {
+				log.Entry().Warnf("Unable to set attributes on index: %v", err)
+			}
+		}
+
+	} else {
+
+		repoClientOptions := piperhttp.ClientOptions{
+			Username:     h.config.TargetRepositoryUser,
+			Password:     h.config.TargetRepositoryPassword,
+			TrustedCerts: h.config.CustomTLSCertificateLinks,
+		}
+
+		h.utils.SetOptions(repoClientOptions)
+
+		response, err := h.utils.UploadRequest(http.MethodPut, targetURL, binary, "", nil, nil, "binary")
+		if err != nil {
+			return "", fmt.Errorf("couldn't upload artifact: %w", err)
+		}
+
+		if !(response.StatusCode == 200 || response.StatusCode == 201) {
+			return "", fmt.Errorf("couldn't upload artifact, received status code %d", response.StatusCode)
+		}
 	}
 
 	return targetURL, nil
@@ -459,6 +581,58 @@ func (h *HelmExecute) runHelmCommand(helmParams []string) error {
 		log.Entry().WithError(err).Fatalf("Helm %v call failed", h.config.HelmCommand)
 		return err
 	}
+
+	return nil
+}
+
+func uploadFileToBucket(bucket *storage.BucketHandle, srcfile string, dstobject string, ctx context.Context) (*storage.ObjectHandle, error) {
+	artifactObject := bucket.Object(dstobject)
+	if artifactObject == nil {
+		return nil, fmt.Errorf("Unable to get new storage Object %s", dstobject)
+	}
+
+	artifactWriter := artifactObject.NewWriter(ctx)
+	if artifactWriter == nil {
+		return nil, fmt.Errorf("Error while allocation writer for artifact object")
+	}
+	defer artifactWriter.Close()
+
+	infile, err := os.Open(srcfile)
+	if err != nil {
+		return nil, err
+	}
+	defer infile.Close()
+
+	_, err = io.Copy(artifactWriter, infile)
+	if err != nil {
+		return nil, err
+	}
+
+	return artifactObject, nil
+}
+func downloadFileFromBucket(bucket *storage.BucketHandle, srcobject string, dstfile string, ctx context.Context) error {
+	artifactObject := bucket.Object(srcobject)
+	if artifactObject == nil {
+		return fmt.Errorf("Unable to get storage Object %s", srcobject)
+	}
+
+	artifactReader, err := artifactObject.NewReader(ctx)
+	if err != nil {
+		return err
+	}
+	defer artifactReader.Close()
+
+	outfile, err := os.OpenFile(dstfile, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer outfile.Close()
+
+	clen, err := io.Copy(outfile, artifactReader)
+	if err != nil {
+		return err
+	}
+	log.Entry().Debugf("Downloaded %s->%s %v bytes", srcobject, dstfile, clen)
 
 	return nil
 }
