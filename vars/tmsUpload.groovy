d8bd9d43ee7020ceca747d1f87a6e46ef8409a15
Alex Gerstner <alex.gerstner@sap.com>
2023-03-30 08:11:45 +0200

    Remove useGoStep from metadata

--- vars/tmsUpload.groovy
+++ vars/tmsUpload.groovy
@@ -88,6 +88,7 @@ void call(Map parameters = [:]) {
             .withMandatoryProperty('credentialsId')
             .use()
 
+        echo "[TransportManagementService] useGoStep: '${config.useGoStep}'"
         if (config.useGoStep == true) {
             List credentials = [
                 [type: 'token', id: 'credentialsId', env: ['PIPER_tmsServiceKey']]
