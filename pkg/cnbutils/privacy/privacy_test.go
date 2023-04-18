bcc1a29987fae0a328152ffe429c31f2fc238930
Pavel Busko <pavel.busko@sap.com>
2023-04-18 09:01:10 +0200

    update unit tests

--- pkg/cnbutils/privacy/privacy_test.go
+++ pkg/cnbutils/privacy/privacy_test.go
@@ -57,6 +57,7 @@ func TestCnbPrivacy_FilterBuildpacks(t *testing.T) {
 	t.Run("filters others", func(t *testing.T) {
 		images := []string{
 			"test/nodejs:v1",
+			"test/nodejs:v1", // SHA should be the same for multiple occurences
 			"my-mirror.de/paketobuildpacks/nodejs:v1",
 			"gcr.io/my-project/paketo-buildpacks/nodejs:v1",
 		}
@@ -66,6 +67,7 @@ func TestCnbPrivacy_FilterBuildpacks(t *testing.T) {
 		require.Len(t, filtered, len(images))
 
 		assert.ElementsMatch(t, filtered, []string{
+			"6ea013d746199ccc0e48e0b4984a6d9357105b82f936ecf18d15786805ac892f",
 			"6ea013d746199ccc0e48e0b4984a6d9357105b82f936ecf18d15786805ac892f",
 			"66131ef922cf26b1500e54a74827f051b43857bcf8d0596593c182548f7d4bd6",
 			"4fd8f0a950aacd7e428c79fce6f51bb1fbf0ab15caf4aca7accc18609acd79b1",
@@ -136,7 +138,7 @@ func TestCnbPrivacy_FilterBuilder(t *testing.T) {
 
 		filteredBuilder := privacy.FilterBuilder(builder)
 
-		assert.Equal(t, "<redacted>", filteredBuilder)
+		assert.Equal(t, "70278d9360533fa4978e5c50aa79bc35a8c0167a353e00521202feeaa09a305b", filteredBuilder)
 	})
 
 }
