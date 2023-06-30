b5b5ff7f5d05b4efffef03e92431fb6e3830be27
Ralf Pannemans <ralf.pannemans@sap.com>
2023-06-30 10:10:55 +0200

    fix gitops integration test assertion
    Co-authored-by: Pavel Busko <pavel.busko@sap.com>

--- integration/integration_gitops_test.go
+++ integration/integration_gitops_test.go
@@ -17,7 +17,7 @@ func TestGitOpsIntegrationUpdateDeployment(t *testing.T) {
 		Mounts:  map[string]string{"./testdata/TestGitopsUpdateIntegration/kustomize/gitopsRepo": "/gitopsRepo-source"},
 		Setup:   []string{"cp -r /gitopsRepo-source /gitopsRepo"},
 	})
-	defer container.terminate(t)
+	// defer container.terminate(t)
 
 	err := container.whenRunningPiperCommand("gitopsUpdateDeployment", "--containerImageNameTag=image:456")
 	if err != nil {
@@ -34,5 +34,6 @@ func TestGitOpsIntegrationUpdateDeployment(t *testing.T) {
   newName: image
   newTag: "456"
 apiVersion: kustomize.config.k8s.io/v1beta1
-kind: Kustomization`)
+kind: Kustomization
+`)
 }
