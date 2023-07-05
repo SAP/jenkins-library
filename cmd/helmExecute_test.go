e967b13c38ae712d55f17cd5c5f23d3a271c7fdc
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2023-07-05 19:51:26 +0600

    Fix tests

--- cmd/helmExecute_test.go
+++ cmd/helmExecute_test.go
@@ -1,3 +1,6 @@
+//go:build unit
+// +build unit
+
 package cmd
 
 import (
@@ -62,7 +65,7 @@ func TestRunHelmUpgrade(t *testing.T) {
 
 			err := runHelmExecute(testCase.config, helmExecute, &cpe)
 			if err != nil {
-				assert.NotEqual(t, testCase.expectedErrStr, err.Error())
+				assert.Equal(t, testCase.expectedErrStr, err.Error())
 			}
 		})
 
