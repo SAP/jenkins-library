d51be66a5eb2f0d78c8ff44db8981a47bccc2542
Ashly Mathew <ashly.mathew@sap.com>
2023-07-11 12:48:38 +0200

    Change guage tests docker image to node buster

--- integration/integration_gauge_test.go
+++ integration/integration_gauge_test.go
@@ -46,7 +46,7 @@ cd /test
 	ioutil.WriteFile(filepath.Join(tempDir, "runPiper.sh"), []byte(testScript), 0700)
 
 	reqNode := testcontainers.ContainerRequest{
-		Image: "getgauge/gocd-jdk-mvn-node",
+		Image: "node:lts-buster",
 		Cmd:   []string{"tail", "-f"},
 		BindMounts: map[string]string{
 			pwd:     "/piperbin",
