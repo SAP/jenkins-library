ae53ec0322fbca0c595ba7f544bfff73771edc7b
Egor Balakin <14162703+m1ron0xFF@users.noreply.github.com>
2023-09-11 12:58:57 +0400

    feat(commonPipelineEnvironment): encrypt CPE (#4504)
    * encrypt CPE - init
    
    * fix
    
    * disable encrypt on Jenkins
    
    * get PIPER_pipelineEnv_SECRET from vault
    
    * reuse artifactPrepareVersionOptions
    
    * encrypt only with orchestrator.GitHubActions
    
    * Workaround: orchestrators expect json
    
    * add encryptedCPE flag
    
    * remove JSON workaround
    
    * throw error if stepConfigPassword is empty
    
    * fix log messages
    
    ---------
    
    Co-authored-by: Egor Balakin <egor.balakin@sap.com>

--- cmd/writePipelineEnv.go
+++ cmd/writePipelineEnv.go
@@ -2,7 +2,13 @@ package cmd
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	b64 "encoding/base64"
 	"encoding/json"
+	"fmt"
+	"github.com/SAP/jenkins-library/pkg/config"
 	"io"
 	"os"
 	"path/filepath"
@@ -14,25 +20,41 @@ import (
 
 // WritePipelineEnv Serializes the commonPipelineEnvironment JSON to disk
 func WritePipelineEnv() *cobra.Command {
-	return &cobra.Command{
+	var stepConfig artifactPrepareVersionOptions
+	var encryptedCPE bool
+	metadata := artifactPrepareVersionMetadata()
+
+	writePipelineEnv := &cobra.Command{
 		Use:   "writePipelineEnv",
 		Short: "Serializes the commonPipelineEnvironment JSON to disk",
 		PreRun: func(cmd *cobra.Command, args []string) {
 			path, _ := os.Getwd()
 			fatalHook := &log.FatalHook{CorrelationID: GeneralConfig.CorrelationID, Path: path}
 			log.RegisterHook(fatalHook)
+
+			err := PrepareConfig(cmd, &metadata, "", &stepConfig, config.OpenPiperFile)
+			if err != nil {
+				log.SetErrorCategory(log.ErrorConfiguration)
+				return
+			}
+			log.RegisterSecret(stepConfig.Password)
+			log.RegisterSecret(stepConfig.Username)
 		},
 
 		Run: func(cmd *cobra.Command, args []string) {
-			err := runWritePipelineEnv()
+			err := runWritePipelineEnv(stepConfig.Password, encryptedCPE)
 			if err != nil {
 				log.Entry().Fatalf("error when writing common Pipeline environment: %v", err)
 			}
 		},
 	}
+
+	writePipelineEnv.Flags().BoolVar(&encryptedCPE, "encryptedCPE", false, "Bool to use encryption in CPE")
+	return writePipelineEnv
 }
 
-func runWritePipelineEnv() error {
+func runWritePipelineEnv(stepConfigPassword string, encryptedCPE bool) error {
+	var err error
 	pipelineEnv, ok := os.LookupEnv("PIPER_pipelineEnv")
 	inBytes := []byte(pipelineEnv)
 	if !ok {
@@ -46,10 +68,23 @@ func runWritePipelineEnv() error {
 		return nil
 	}
 
+	// try to decrypt
+	if encryptedCPE {
+		log.Entry().Debug("trying to decrypt CPE")
+		if stepConfigPassword == "" {
+			return fmt.Errorf("empty stepConfigPassword")
+		}
+
+		inBytes, err = decrypt([]byte(stepConfigPassword), inBytes)
+		if err != nil {
+			log.Entry().Fatal(err)
+		}
+	}
+
 	commonPipelineEnv := piperenv.CPEMap{}
 	decoder := json.NewDecoder(bytes.NewReader(inBytes))
 	decoder.UseNumber()
-	err := decoder.Decode(&commonPipelineEnv)
+	err = decoder.Decode(&commonPipelineEnv)
 	if err != nil {
 		return err
 	}
@@ -69,4 +104,31 @@ func runWritePipelineEnv() error {
 		return err
 	}
 	return nil
+}
+
+func decrypt(secret, base64CipherText []byte) ([]byte, error) {
+	// decode from base64
+	cipherText, err := b64.StdEncoding.DecodeString(string(base64CipherText))
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode from base64: %v", err)
+	}
+
+	// use SHA256 as key
+	key := sha256.Sum256(secret)
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new cipher: %v", err)
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return nil, fmt.Errorf("invalid ciphertext block size")
+	}
+
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherText, cipherText)
+
+	return cipherText, nil
 }
