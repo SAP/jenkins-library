3261a2fdefc12090c61d6e8b2093b233ce4f7e27
Dmitrii Pavlukhin <dmitry.pavlukhin@gmail.com>
2023-09-14 14:30:05 +0300

    made detect8 default

--- cmd/detectExecuteScan.go
+++ cmd/detectExecuteScan.go
@@ -294,11 +294,11 @@ func getDetectScript(config detectExecuteScanOptions, utils detectUtils) error {
 
 	log.Entry().Infof("Downloading Detect Script")
 
-	if config.UseDetect8 {
-		return utils.DownloadFile("https://detect.synopsys.com/detect8.sh", "detect.sh", nil, nil)
+	if config.UseDetect7 {
+		return utils.DownloadFile("https://detect.synopsys.com/detect7.sh", "detect.sh", nil, nil)
 	}
 
-	return utils.DownloadFile("https://detect.synopsys.com/detect7.sh", "detect.sh", nil, nil)
+	return utils.DownloadFile("https://detect.synopsys.com/detect8.sh", "detect.sh", nil, nil)
 }
 
 func addDetectArgs(args []string, config detectExecuteScanOptions, utils detectUtils, sys *blackduckSystem) ([]string, error) {
@@ -339,6 +339,10 @@ func addDetectArgs(args []string, config detectExecuteScanOptions, utils detectU
 		config.ScanProperties, _ = piperutils.RemoveAll(config.ScanProperties, "--detect.project.codelocation.unmap=true")
 	}
 
+	// TEMPORARY OPTION DURING THE MIGRATION TO DETECT8
+	args = append(args, "--detect.project.codelocation.unmap=true")
+	// REMOVE AFTER 25.09.2023
+
 	args = append(args, config.ScanProperties...)
 
 	args = append(args, fmt.Sprintf("--blackduck.url=%v", config.ServerURL))
@@ -346,7 +350,7 @@ func addDetectArgs(args []string, config detectExecuteScanOptions, utils detectU
 	// ProjectNames, VersionName, GroupName etc can contain spaces and need to be escaped using double quotes in CLI
 	// Hence the string need to be surrounded by \"
 
-	// Maven Parameters
+	// Moved parameters
 	mavenArgs, err := maven.DownloadAndGetMavenParameters(config.GlobalSettingsFile, config.ProjectSettingsFile, utils)
 	if err != nil {
 		return nil, err
@@ -366,8 +370,26 @@ func addDetectArgs(args []string, config detectExecuteScanOptions, utils detectU
 	}
 
 	// Since detect8 adds quotes by default, to avoid double quotation they should be removed for several arguments
-	if config.UseDetect8 {
+	if config.UseDetect7 {
+		args = append(args, fmt.Sprintf("\"--detect.project.name='%v'\"", config.ProjectName))
+		args = append(args, fmt.Sprintf("\"--detect.project.version.name='%v'\"", detectVersionName))
 
+		// Groups parameter is added only when there is atleast one non-empty groupname provided
+		if len(config.Groups) > 0 && len(config.Groups[0]) > 0 {
+			args = append(args, fmt.Sprintf("\"--detect.project.user.groups='%v'\"", strings.Join(config.Groups, ",")))
+		}
+
+		// Atleast 1, non-empty category to fail on must be provided
+		if len(config.FailOn) > 0 && len(config.FailOn[0]) > 0 {
+			args = append(args, fmt.Sprintf("--detect.policy.check.fail.on.severities=%v", strings.Join(config.FailOn, ",")))
+		}
+
+		args = append(args, fmt.Sprintf("\"--detect.code.location.name='%v'\"", codelocation))
+
+		if len(mavenArgs) > 0 && !checkIfArgumentIsInScanProperties(config, "detect.maven.build.command") {
+			args = append(args, fmt.Sprintf("\"--detect.maven.build.command='%v'\"", strings.Join(mavenArgs, " ")))
+		}
+	} else {
 		args = append(args, fmt.Sprintf("\"--detect.project.name=%v\"", config.ProjectName))
 		args = append(args, fmt.Sprintf("\"--detect.project.version.name=%v\"", detectVersionName))
 
@@ -387,30 +409,7 @@ func addDetectArgs(args []string, config detectExecuteScanOptions, utils detectU
 			args = append(args, fmt.Sprintf("\"--detect.maven.build.command=%v\"", strings.Join(mavenArgs, " ")))
 		}
 
-	} else {
-
-		args = append(args, fmt.Sprintf("\"--detect.project.name='%v'\"", config.ProjectName))
-		args = append(args, fmt.Sprintf("\"--detect.project.version.name='%v'\"", detectVersionName))
-
-		// Groups parameter is added only when there is atleast one non-empty groupname provided
-		if len(config.Groups) > 0 && len(config.Groups[0]) > 0 {
-			args = append(args, fmt.Sprintf("\"--detect.project.user.groups='%v'\"", strings.Join(config.Groups, ",")))
-		}
-
-		// Atleast 1, non-empty category to fail on must be provided
-		if len(config.FailOn) > 0 && len(config.FailOn[0]) > 0 {
-			args = append(args, fmt.Sprintf("--detect.policy.check.fail.on.severities=%v", strings.Join(config.FailOn, ",")))
-		}
-
-		args = append(args, fmt.Sprintf("\"--detect.code.location.name='%v'\"", codelocation))
-
-		if len(mavenArgs) > 0 && !checkIfArgumentIsInScanProperties(config, "detect.maven.build.command") {
-			args = append(args, fmt.Sprintf("\"--detect.maven.build.command='%v'\"", strings.Join(mavenArgs, " ")))
-		}
-	}
-
-	if config.SuccessOnSkip {
-		args = append(args, fmt.Sprintf("\"--detect.force.success.on.skip=%v\"", config.SuccessOnSkip))
+		args = append(args, fmt.Sprintf("\"--detect.force.success.on.skip=true\""))
 	}
 
 	if len(config.ScanPaths) > 0 && len(config.ScanPaths[0]) > 0 {
