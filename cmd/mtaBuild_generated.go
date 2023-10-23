1ea467732596e1b3ff7055a059de6407837820a7
D074360 <manjunath.mandya.surendrakumar@sap.com>
2023-10-23 09:18:45 +0200

    Test docker image

--- cmd/mtaBuild_generated.go
+++ cmd/mtaBuild_generated.go
@@ -502,7 +502,7 @@ func mtaBuildMetadata() config.StepData {
 				},
 			},
 			Containers: []config.Container{
-				{Image: "devxci/mbtci-java11-node14"},
+				{Image: "cobra217/mbtci-java11-node14"},
 			},
 			Outputs: config.StepOutputs{
 				Resources: []config.StepResources{
