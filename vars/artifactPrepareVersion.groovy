d74695fe85fc1a4490c72b4503e27309782671df
I742444 <148915640+I742444@users.noreply.github.com>
2023-10-25 15:37:28 -0700

    FPA53-16217: Update service version

--- vars/artifactPrepareVersion.groovy
+++ vars/artifactPrepareVersion.groovy
@@ -18,6 +18,16 @@ void call(Map parameters = [:]) {
     // Tell dockerExecuteOnKubernetes (if used) to stash also .-folders
     // This preserves the '.git' folder into the pod and restores it from the pod with the created tag.
     parameters['stashNoDefaultExcludes'] = true
+    
+    def result = sh(script: "mvn help:evaluate -Dexpression='project.version' -q -DforceStdout", returnStdout: true)
+    
+    // x.y.z -> x.y.z+1
+    def parts = result.split("\\.")
+    def patch = parts[2].toInteger() + 1
+    parts[2] = patch.toString()
+    version = parts.join('.')
+    // Set the new version in all pom.xml
+    sh(script: "mvn versions:set -DnewVersion='$version'")
 
     parameters = DownloadCacheUtils.injectDownloadCacheInParameters(script, parameters, BuildTool.MAVEN)
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials, false, false, true)
