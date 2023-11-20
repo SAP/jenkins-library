308efec70c1f5af4d88af41e53f789cf54706b85
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2023-11-20 17:58:50 +0600

    Comment some logic

--- cmd/imagePushToRegistry.go
+++ cmd/imagePushToRegistry.go
@@ -101,18 +101,18 @@ func runImagePushToRegistry(config *imagePushToRegistryOptions, telemetryData *t
 	log.Entry().WithField("LogField", "Log field content").Info("This is just a demo for a simple step.")
 
 	// Example of calling methods from external dependencies directly on utils:
-	exists, err := utils.FileExists("file.txt")
-	if err != nil {
-		// It is good practice to set an error category.
-		// Most likely you want to do this at the place where enough context is known.
-		log.SetErrorCategory(log.ErrorConfiguration)
-		// Always wrap non-descriptive errors to enrich them with context for when they appear in the log:
-		return fmt.Errorf("failed to check for important file: %w", err)
-	}
-	if !exists {
-		log.SetErrorCategory(log.ErrorConfiguration)
-		return fmt.Errorf("cannot run without important file")
-	}
+	// exists, err := utils.FileExists("file.txt")
+	// if err != nil {
+	// It is good practice to set an error category.
+	// Most likely you want to do this at the place where enough context is known.
+	// log.SetErrorCategory(log.ErrorConfiguration)
+	// Always wrap non-descriptive errors to enrich them with context for when they appear in the log:
+	// return fmt.Errorf("failed to check for important file: %w", err)
+	// }
+	// if !exists {
+	// log.SetErrorCategory(log.ErrorConfiguration)
+	// return fmt.Errorf("cannot run without important file")
+	// }
 
 	return nil
 
