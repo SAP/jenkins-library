05f077794ef28eb287e547775ebe6ad8e7e5abd3
Muhammadali Nazarov <muhammadalinazarov@gmail.com>
2023-11-28 13:24:55 +0500

    throw a warning if value of a config is of the wrong type

--- pkg/config/vault.go
+++ pkg/config/vault.go
@@ -83,10 +83,10 @@ type vaultClient interface {
 
 func (s *StepConfig) mixinVaultConfig(parameters []StepParameters, configs ...map[string]interface{}) {
 	for _, config := range configs {
-		s.mixIn(config, vaultFilter)
+		s.mixIn(config, vaultFilter, StepData{})
 		// when an empty filter is returned we skip the mixin call since an empty filter will allow everything
 		if referencesFilter := getFilterForResourceReferences(parameters); len(referencesFilter) > 0 {
-			s.mixIn(config, referencesFilter)
+			s.mixIn(config, referencesFilter, StepData{})
 		}
 	}
 }
