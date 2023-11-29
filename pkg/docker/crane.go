82e037ddbefbb3e61ed8bd938d5b0be47855d4a9
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2023-11-29 19:04:18 +0600

    Fix: Support platform

--- pkg/docker/crane.go
+++ pkg/docker/crane.go
@@ -28,3 +28,13 @@ func (c *CraneUtilsBundle) PushImage(ctx context.Context, im v1.Image, dest, pla
 func (c *CraneUtilsBundle) LoadImage(ctx context.Context, src string) (v1.Image, error) {
 	return crane.Load(src, crane.WithContext(ctx))
 }
+
+// parsePlatform is a wrapper for v1.ParsePlatform. It is necessary because
+// v1.ParsePlatform returns an empty struct when the platform is equal to an empty string,
+// whereas we expect 'nil'
+func parsePlatform(p string) (*v1.Platform, error) {
+	if p == "" {
+		return nil, nil
+	}
+	return v1.ParsePlatform(p)
+}
