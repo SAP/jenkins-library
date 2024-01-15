3ee0feadf63570ce796068074922431a68ec2bac
Christopher Fenner <Christopher.Fenner@sap.com>
2024-01-15 09:13:39 +0100

    remove swa calls

--- vars/dockerExecuteOnKubernetes.groovy
+++ vars/dockerExecuteOnKubernetes.groovy
@@ -255,12 +255,6 @@ void call(Map parameters = [:], body) {
             .addIfEmpty('uniqueId', UUID.randomUUID().toString())
             .use()
 
-        utils.pushToSWA([
-            step         : STEP_NAME,
-            stepParamKey1: 'scriptMissing',
-            stepParam1   : parameters?.script == null
-        ], config)
-
         if (!config.containerMap && config.dockerImage) {
             config.containerName = 'container-exec'
             config.containerMap = [(config.get('dockerImage')): config.containerName]
