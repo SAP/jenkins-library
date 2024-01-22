60c4733a6f46910a20232281b13ab14f0482ddf9
ffeldmann <f.feldmann@sap.com>
2024-01-22 13:36:31 +0100

    Update cmd/piper.go

--- cmd/piper.go
+++ cmd/piper.go
@@ -407,7 +407,7 @@ func PrepareConfig(cmd *cobra.Command, metadata *config.StepData, stepName strin
 
 	// disables telemetry reporting in go
 	// follow-up cleanup needed
-	GeneralConfig.NoTelemetry = false
+	GeneralConfig.NoTelemetry = true
 
 	stepConfig.Config = checkTypes(stepConfig.Config, options)
 	confJSON, _ := json.Marshal(stepConfig.Config)
