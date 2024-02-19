f9f86b7f8085f1e79c21cb85f4ce6326b6f58fae
sumeet patil <sumeet.patil@sap.com>
2024-02-19 16:08:15 +0530

    Remove null checks as being considered in mandatory params

--- cmd/contrastExecuteScan.go
+++ cmd/contrastExecuteScan.go
@@ -43,10 +43,8 @@ func contrastExecuteScan(config contrastExecuteScanOptions, telemetryData *telem
 }
 
 func runContrastExecuteScan(config *contrastExecuteScanOptions, telemetryData *telemetry.CustomData, utils contrastExecuteScanUtils) (reports []piperutils.Path, err error) {
-	err = validateConfigs(config)
-	if err != nil {
-		log.Entry().Errorf("config is invalid: %v", err)
-		return nil, err
+	if !strings.HasPrefix(config.Server, "https://") {
+		config.Server = "https://" + config.Server
 	}
 
 	auth := getAuth(config)
@@ -98,29 +96,6 @@ func runContrastExecuteScan(config *contrastExecuteScanOptions, telemetryData *t
 	}
 
 	return reports, nil
-}
-
-func validateConfigs(config *contrastExecuteScanOptions) error {
-	validations := map[string]string{
-		"server":         config.Server,
-		"organizationId": config.OrganizationID,
-		"applicationId":  config.ApplicationID,
-		"userApiKey":     config.UserAPIKey,
-		"username":       config.Username,
-		"serviceKey":     config.ServiceKey,
-	}
-
-	for k, v := range validations {
-		if v == "" {
-			return fmt.Errorf("%s is empty", k)
-		}
-	}
-
-	if !strings.HasPrefix(config.Server, "https://") {
-		config.Server = "https://" + config.Server
-	}
-
-	return nil
 }
 
 func getApplicationUrls(config *contrastExecuteScanOptions) (string, string) {
