b644bf7e158652b4695ad880d728621c25155ab4
Vyacheslav Starostin <32613074+vstarostin@users.noreply.github.com>
2024-02-20 17:17:20 +0600

    Use Pendo as analytics tool (#4828)
    * Try pendo
    
    * Try pendo
    
    * Fix typo
    
    * Optimize data struct
    
    * Adjust kaniko and kubernetes to try pendo
    
    * Include golangBuild
    
    * Update generator helper
    
    * go generate
    
    * Delete token param from some steps && go generate
    
    * Add checking on ExportPrefix
    
    * Update telemetry struct
    
    * Try pipelineID for artifactPrepVersion
    
    * Try pipelineID as a CPE value
    
    * Update
    
    * Try CF
    
    * Read pipelineID from CPE
    
    * Enable telemetry
    
    * Fix name
    
    * Do not show token in header
    
    * Read pipelineID from CPE
    
    * Clean up
    
    * Clean up
    
    * Update template
    
    * go generate
    
    * Update template
    
    * Try Cribl
    
    * HTTPS
    
    * Update
    
    * Clean-up
    
    * Fix tests
    
    * Update

--- cmd/shellExecute_generated.go
+++ cmd/shellExecute_generated.go
@@ -109,7 +109,7 @@ func ShellExecuteCommand() *cobra.Command {
 			}
 			log.DeferExitHandler(handler)
 			defer handler()
-			telemetryClient.Initialize(GeneralConfig.NoTelemetry, STEP_NAME)
+			telemetryClient.Initialize(GeneralConfig.NoTelemetry, STEP_NAME, GeneralConfig.HookConfig.PendoConfig.Token)
 			shellExecute(stepConfig, &stepTelemetryData)
 			stepTelemetryData.ErrorCode = "0"
 			log.Entry().Info("SUCCESS")
