358f2e055c5f39db635cd979f9dc44b7640b9839
tiloKo <70266685+tiloKo@users.noreply.github.com>
2024-03-07 10:40:05 +0100

    2nd try

--- cmd/abapAddonAssemblyKitReserveNextPackages_test.go
+++ cmd/abapAddonAssemblyKitReserveNextPackages_test.go
@@ -165,7 +165,7 @@ func TestCopyFieldsToRepositoriesPackage(t *testing.T) {
 
 	t.Run("test copyFieldsToRepositories Planned success with predecessorcommitI short AAKaaS", func(t *testing.T) {
 		pckgWR[0].Package.Status = aakaas.PackageStatusPlanned
-		pckgWR[0].Package.PredecessorCommitID = "something"
+		pckgWR[0].Package.PredecessorCommitID = "predecessor"
 		pckgWR[0].Repo.CommitID = "something40charslongxxxxxxxxxxxxxxxxxxxx"
 		pckgWR[0].Package.CommitID = "something40charslongxxxxxxxxxxxxxxxxxxxx"
 		_, err := checkAndCopyFieldsToRepositories(pckgWR)
@@ -174,9 +174,9 @@ func TestCopyFieldsToRepositoriesPackage(t *testing.T) {
 
 	t.Run("test copyFieldsToRepositories Planned success with predecessorcommitID short addon.yml", func(t *testing.T) {
 		pckgWR[0].Package.Status = aakaas.PackageStatusPlanned
-		pckgWR[0].Package.PredecessorCommitID = "something40charslongPREDECESSORyyyyyyyyy"
-		pckgWR[0].Repo.CommitID = "something"
-		pckgWR[0].Package.CommitID = "something40charslongxxxxxxxxxxxxxxxxxxxx"
+		pckgWR[0].Package.PredecessorCommitID = "predecessor"
+		pckgWR[0].Repo.CommitID = "successor"
+		pckgWR[0].Package.CommitID = "successorANDsomemore"
 		_, err := checkAndCopyFieldsToRepositories(pckgWR)
 		assert.NoError(t, err)
 	})
