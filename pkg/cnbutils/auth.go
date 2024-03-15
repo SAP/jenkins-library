df2e976eaab00e3dc71f2fec117d23a67a085742
Johannes Dillmann <modulo11@users.noreply.github.com>
2024-03-15 10:44:14 +0100

    feat(cnbBuild): validate docker credentials (#4840)

--- pkg/cnbutils/auth.go
+++ pkg/cnbutils/auth.go
@@ -4,30 +4,66 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/SAP/jenkins-library/pkg/log"
 	"github.com/docker/cli/cli/config/configfile"
 )
 
-func GenerateCnbAuth(config string, utils BuildUtils) (string, error) {
+type Credentials struct {
+	utils        BuildUtils
+	dockerConfig *configfile.ConfigFile
+}
+
+func NewCredentials(utils BuildUtils) Credentials {
+	return Credentials{
+		utils:        utils,
+		dockerConfig: nil,
+	}
+}
+
+func (c *Credentials) GenerateCredentials(config string) (string, error) {
 	var err error
+	c.dockerConfig, err = c.parse(config)
+	if err != nil {
+		return "", err
+	}
+
+	return c.generate()
+}
+
+func (c *Credentials) Validate(target string) bool {
+	if c.dockerConfig == nil {
+		return false
+	}
+	_, ok := c.dockerConfig.AuthConfigs[target]
+	if !strings.HasPrefix(target, "localhost") && !ok {
+		return false
+	}
+	return true
+}
+
+func (c *Credentials) parse(config string) (*configfile.ConfigFile, error) {
 	dockerConfig := &configfile.ConfigFile{}
 
 	if config != "" {
 		log.Entry().Debugf("using docker config file %q", config)
-		dockerConfigJSON, err := utils.FileRead(config)
+		dockerConfigJSON, err := c.utils.FileRead(config)
 		if err != nil {
-			return "", err
+			return &configfile.ConfigFile{}, err
 		}
 
 		err = json.Unmarshal(dockerConfigJSON, dockerConfig)
 		if err != nil {
-			return "", err
+			return &configfile.ConfigFile{}, err
 		}
 	}
+	return dockerConfig, nil
+}
 
+func (c *Credentials) generate() (string, error) {
 	auth := map[string]string{}
-	for registry, value := range dockerConfig.AuthConfigs {
+	for registry, value := range c.dockerConfig.AuthConfigs {
 		if value.Auth == "" && value.Username == "" && value.Password == "" {
 			log.Entry().Warnf("docker config.json contains empty credentials for registry %q. Either 'auth' or 'username' and 'password' have to be provided.", registry)
 			continue
