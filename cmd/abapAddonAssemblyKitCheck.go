fb61ee0560ff20fd9c71021669494c90799e92d4
tiloKo <70266685+tiloKo@users.noreply.github.com>
2024-04-05 11:37:57 +0200

    more functions

--- cmd/abapAddonAssemblyKitCheck.go
+++ cmd/abapAddonAssemblyKitCheck.go
@@ -1,10 +1,13 @@
 package cmd
 
 import (
+	"encoding/json"
+
 	"github.com/SAP/jenkins-library/pkg/abap/aakaas"
 	abapbuild "github.com/SAP/jenkins-library/pkg/abap/build"
 	"github.com/SAP/jenkins-library/pkg/abaputils"
 	"github.com/SAP/jenkins-library/pkg/log"
+	"github.com/SAP/jenkins-library/pkg/piperutils"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
 	"github.com/pkg/errors"
 )
@@ -40,26 +43,26 @@ func runAbapAddonAssemblyKitCheck(config *abapAddonAssemblyKitCheckOptions, tele
 		return err
 	}
 
-	if err := pvh.check(); err != nil {
+	if err := pvh.checkAndResolveVersion(conn); err != nil {
 		return err
 	}
+	pvh.SyncAddonDescriptorVersionFields(&addonDescriptor)
 
-	// log.Entry().WithField("LogField", "Log field content").Info("This is just a demo for a simple step.")
-	// // Example of calling methods from external dependencies directly on utils:
-	// exists, err := utils.FileExists("file.txt")
-	// if err != nil {
-	// 	// It is good practice to set an error category.
-	// 	// Most likely you want to do this at the place where enough context is known.
-	// 	log.SetErrorCategory(log.ErrorConfiguration)
-	// 	// Always wrap non-descriptive errors to enrich them with context for when they appear in the log:
-	// 	return fmt.Errorf("failed to check for important file: %w", err)
-	// }
-	// if !exists {
-	// 	log.SetErrorCategory(log.ErrorConfiguration)
-	// 	return fmt.Errorf("cannot run without important file")
-	// }
+	commonPipelineEnvironment.abap.addonDescriptor = string(addonDescriptor.AsJSON())
+
+	publishAddonYaml(config, utils)
 
 	return nil
+}
+
+func publishAddonYaml(config *abapAddonAssemblyKitCheckOptions, utils aakaas.AakUtils) {
+	var filesToPublish []piperutils.Path
+	log.Entry().Infof("Add %s to be published", config.AddonDescriptorFileName)
+	filesToPublish = append(filesToPublish, piperutils.Path{Target: config.AddonDescriptorFileName, Name: "AddonDescriptor", Mandatory: true})
+	log.Entry().Infof("Publishing %v files", len(filesToPublish))
+	if err := piperutils.PersistReportsAndLinks("abapAddonAssemblyKitCheckPV", "", utils, filesToPublish, nil); err != nil {
+		log.Entry().WithError(err).Error("failed to persist report information")
+	}
 }
 
 type ProductVersionHeader struct {
@@ -117,7 +120,71 @@ func NewProductVersionHeader(addonDescriptor *abaputils.AddonDescriptor, conn *a
 	return &pvh, nil
 }
 
-func (pv *ProductVersionHeader) check() error {
+func (pv *ProductVersionHeader) checkAndResolveVersion(conn *abapbuild.Connector) error {
+	requestJson, err := json.Marshal(pv)
+	if err != nil {
+		return err
+	}
+	appendum := "/odata/aas_ocs_package/ProductVersionHeaderSet"
+	responseBody, err := conn.Post(appendum, string(requestJson))
+	if err != nil {
+		return errors.Wrap(err, "Checking Product Modeling in AAkaaS failed")
+	}
+	var resultPv ProductVersionHeader //wrapper struct needed???
+	if err := json.Unmarshal(responseBody, &resultPv); err != nil {
+		return errors.Wrap(err, "Unexpected AAKaaS response for checking Product Modeling "+string(responseBody))
+	}
+
+	pv.ProductVersion = resultPv.ProductVersion
+	pv.Spslevel = resultPv.Spslevel
+	pv.PatchLevel = resultPv.PatchLevel
+
+	//transfer resolved version fields (and others)
+	for pvc_index, pvc := range pv.Content {
+		foundPvc := ProductVersionContent{}
+		for _, resultPvc := range resultPv.Content {
+			if pvc.SoftwareComponentName == resultPvc.SoftwareComponentName && foundPvc.SoftwareComponentName == "" {
+				foundPvc = resultPvc
+			} else if pvc.SoftwareComponentName == resultPvc.SoftwareComponentName {
+				return errors.New("Software Component Name must be unique in the ProductVersionContent")
+			}
+		}
+		if foundPvc.SoftwareComponentName == "" {
+			return errors.New("Software Component Name not found in the ProductVersionContent")
+		}
+		pv.Content[pvc_index].PatchLevel = foundPvc.PatchLevel
+		pv.Content[pvc_index].SpLevel = foundPvc.SpLevel
+		pv.Content[pvc_index].SoftwareComponentVersion = foundPvc.SoftwareComponentVersion
+	}
+
+	return nil
+}
+
+func (pv *ProductVersionHeader) SyncAddonDescriptorVersionFields(addonDescriptor *abaputils.AddonDescriptor) error {
+	addonDescriptor.AddonVersion = pv.ProductVersion
+	addonDescriptor.AddonSpsLevel = pv.Spslevel
+	addonDescriptor.AddonPatchLevel = pv.PatchLevel
+
+	//in NewPvh function pvh was build up 1:1 based on addonDescriptor
+	//in checkAndResolve pvh was synced from AAKaaS reply assuming it does not contain more content than before(if it does it is ignored)
+	//anyway we are defenisve once more
+	for repo_index, repo := range addonDescriptor.Repositories {
+		foundPvc := ProductVersionContent{}
+		for _, pvc := range pv.Content {
+			if pvc.SoftwareComponentName == repo.Name && foundPvc.SoftwareComponentName == "" {
+				foundPvc = pvc
+			} else if pvc.SoftwareComponentName == repo.Name {
+				return errors.New("Software Component Name must be unique in addon descriptor(aka addon.yml)")
+			}
+		}
+		if foundPvc.SoftwareComponentName == "" {
+			return errors.New("ProductVersionContent & addon descriptor (aka addon.yml) out of sync")
+		}
+
+		addonDescriptor.Repositories[repo_index].PatchLevel = foundPvc.PatchLevel
+		addonDescriptor.Repositories[repo_index].SpLevel = foundPvc.SpLevel
+		addonDescriptor.Repositories[repo_index].Version = foundPvc.SoftwareComponentVersion
+	}
 
 	return nil
 }
