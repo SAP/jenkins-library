32c1ef10c717099504c9d6a23400f4175c05bce4
jliempt <>
2024-05-02 16:48:48 +0200

    implement commented tests

--- pkg/vault/oidc_test.go
+++ pkg/vault/oidc_test.go
@@ -2,7 +2,9 @@ package vault
 
 import (
 	"encoding/base64"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/SAP/jenkins-library/pkg/vault/mocks"
 	"github.com/hashicorp/vault/api"
@@ -11,7 +13,8 @@ import (
 
 func TestOIDC(t *testing.T) {
 	oidcPath := "identity/oidc/token/testRoleID"
-	mockToken := base64.StdEncoding.EncodeToString([]byte("testOIDCtoken123"))
+	mockPayload := base64.RawStdEncoding.EncodeToString([]byte("testOIDCtoken123"))
+	mockToken := fmt.Sprintf("hvs.%s", mockPayload)
 
 	mockJwt := &api.Secret{
 		Data: map[string]interface{}{
@@ -23,34 +26,62 @@ func TestOIDC(t *testing.T) {
 	t.Run("Test getting OIDC token - token non-existent in env yet", func(t *testing.T) {
 		t.Parallel()
 
+		// init
 		vaultMock := &mocks.VaultMock{}
 		client := Client{vaultMock, &Config{}}
 		vaultMock.On("Read", oidcPath).Return(mockJwt, nil)
 
+		// run
 		token, err := client.GetOIDCTokenByValidation("testRoleID")
 
+		// assert
 		assert.NoError(t, err)
 		assert.Equal(t, token, mockToken)
 	})
 
-	// t.Run("Test getting OIDC token - token exists in env and is valid", func(t *testing.T) {
-	// 	t.Parallel()
+	t.Run("Test getting OIDC token - token exists in env and is valid", func(t *testing.T) {
+		// init
+		// still valid for 10 minutes
+		expiryTime := time.Now().Local().Add(time.Minute * time.Duration(10))
+		payload := fmt.Sprintf("{\"exp\": %d}", expiryTime.Unix())
+		payloadB64 := base64.RawStdEncoding.EncodeToString([]byte(payload))
+		token := fmt.Sprintf("hvs.%s", payloadB64)
 
-	// 	t.Setenv("PIPER_OIDCIdentityToken", "testOIDCtoken123")
+		t.Setenv("PIPER_OIDCIdentityToken", token)
 
-	// 	vaultMock := &mocks.VaultMock{}
-	// 	_ = Client{vaultMock, &Config{}}
-	// 	vaultMock.On("Read", oidcPath).Return(mockJwt, nil)
-	// })
+		vaultMock := &mocks.VaultMock{}
+		client := Client{vaultMock, &Config{}}
+		vaultMock.On("Read", oidcPath).Return(mockJwt, nil)
 
-	// t.Run("Test getting OIDC token - token exists in env and is invalid", func(t *testing.T) {
-	// 	t.Parallel()
+		// run
+		tokenResult, err := client.GetOIDCTokenByValidation("testRoleID")
 
-	// 	t.Setenv("PIPER_OIDCIdentityToken", "testOIDCtoken123")
+		// assert
+		assert.Equal(t, token, tokenResult)
+		assert.NoError(t, err)
+	})
 
-	// 	vaultMock := &mocks.VaultMock{}
-	// 	_ = Client{vaultMock, &Config{}}
-	// 	vaultMock.On("Read", oidcPath).Return(mockJwt, nil)
-	// })
+	t.Run("Test getting OIDC token - token exists in env and is invalid", func(t *testing.T) {
+		//init
+		// expired 10 minutes ago (time is subtracted!)
+		expiryTime := time.Now().Add(-time.Minute * time.Duration(10))
+		payload := fmt.Sprintf("{\"exp\": %d}", expiryTime.Unix())
+		payloadB64 := base64.RawStdEncoding.EncodeToString([]byte(payload))
+		token := fmt.Sprintf("hvs.%s", payloadB64)
+
+		t.Setenv("PIPER_OIDCIdentityToken", token)
+
+		vaultMock := &mocks.VaultMock{}
+		client := Client{vaultMock, &Config{}}
+		vaultMock.On("Read", oidcPath).Return(mockJwt, nil)
+
+		// run
+		tokenResult, err := client.GetOIDCTokenByValidation("testRoleID")
+
+		// assert
+		client.GetOIDCTokenByValidation("testRoleID")
+		assert.Equal(t, mockToken, tokenResult)
+		assert.NoError(t, err)
+	})
 
 }
