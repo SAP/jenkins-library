ff9b3d429643d7e7fa96abd0255ad99dac943f67
Simon Dold <48808400+doldsimo@users.noreply.github.com>
2024-05-08 14:25:13 +0200

    [ABAP] update ConvertTime (#4919)
    * update ConvertTime
    
    * clean up
    
    * fix typo
    
    ---------
    
    Co-authored-by: Daniel Mieg <56156797+DanielMieg@users.noreply.github.com>

--- pkg/abaputils/abaputils.go
+++ pkg/abaputils/abaputils.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"strconv"
 	"strings"
 	"time"
 
@@ -242,17 +241,6 @@ func GetErrorDetailsFromResponse(resp *http.Response) (errorString string, error
 
 	return "", "", errors.New("Could not parse the JSON error response")
 
-}
-
-// ConvertTime formats an ABAP timestamp string from format /Date(1585576807000+0000)/ into a UNIX timestamp and returns it
-func ConvertTime(logTimeStamp string) time.Time {
-	seconds := strings.TrimPrefix(strings.TrimSuffix(logTimeStamp, "000+0000)/"), "/Date(")
-	n, error := strconv.ParseInt(seconds, 10, 64)
-	if error != nil {
-		return time.Unix(0, 0).UTC()
-	}
-	t := time.Unix(n, 0).UTC()
-	return t
 }
 
 // AddDefaultDashedLine adds 25 dashes
