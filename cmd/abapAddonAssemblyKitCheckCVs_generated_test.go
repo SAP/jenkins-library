dd760e7312d81e7e79c167c9f92e1c081ba1d3a0
tiloKo <70266685+tiloKo@users.noreply.github.com>
2024-06-20 14:16:37 +0200

    recreate deleted files (to be deleted later)

---
+++ cmd/abapAddonAssemblyKitCheckCVs_generated_test.go
@@ -0,0 +1,20 @@
+//go:build unit
+// +build unit
+
+package abapAddonAssemblyKit
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAbapAddonAssemblyKitCheckCVsCommand(t *testing.T) {
+	t.Parallel()
+
+	testCmd := AbapAddonAssemblyKitCheckCVsCommand()
+
+	// only high level testing performed - details are tested in step generation procedure
+	assert.Equal(t, "abapAddonAssemblyKitCheckCVs", testCmd.Use, "command name incorrect")
+
+}
