b6b366066f1ff819bb2b8f43c18e0a748ba432a4
Vyacheslav Starostin <32613074+vstarostin@users.noreply.github.com>
2024-07-05 17:23:57 +0500

    Disable telemetry reporting (#4983)

--- cmd/piper.go
+++ cmd/piper.go
@@ -420,6 +420,11 @@ func PrepareConfig(cmd *cobra.Command, metadata *config.StepData, stepName strin
 		}
 	}
 
+	// since Pendo has been sunset
+	// disable telemetry reporting in go
+	// follow-up cleanup needed
+	GeneralConfig.NoTelemetry = true
+
 	stepConfig.Config = checkTypes(stepConfig.Config, options)
 	confJSON, _ := json.Marshal(stepConfig.Config)
 	_ = json.Unmarshal(confJSON, &options)
