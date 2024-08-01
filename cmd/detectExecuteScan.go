303b124e029903c37af0e2164e7d4cdea71fd883
Vijayan T <vijayanjay@gmail.com>
2024-08-01 21:41:53 +0530

    testing ref param of vault

--- cmd/detectExecuteScan.go
+++ cmd/detectExecuteScan.go
@@ -442,6 +442,7 @@ func getDetectScript(config detectExecuteScanOptions, utils detectUtils) error {
 }
 
 func addDetectArgs(args []string, config detectExecuteScanOptions, utils detectUtils, sys *blackduckSystem, versionSuffix, locationSuffix string) ([]string, error) {
+	fmt.Println("vij addDetectArgs")
 	detectVersionName := getVersionName(config)
 	if versionSuffix != NO_VERSION_SUFFIX {
 		detectVersionName = fmt.Sprintf("%s-%s", detectVersionName, versionSuffix)
