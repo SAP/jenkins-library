e20f2685df006d02cfefc10e45b40034af817b24
jliempt <>
2024-09-06 17:06:34 +0200

    update log messages based on suggestions

--- pkg/config/trustengine.go
+++ pkg/config/trustengine.go
@@ -15,19 +15,19 @@ const RefTypeTrustengineSecret = "trustengineSecret"
 func resolveAllTrustEngineReferences(config *StepConfig, params []StepParameters, trustEngineConfiguration trustengine.Configuration, client *piperhttp.Client) {
 	for _, param := range params {
 		if ref := param.GetReference(RefTypeTrustengineSecret); ref != nil {
-			if config.Config[param.Name] == "" { // what if Jenkins set the secret?
+			if config.Config[param.Name] == "" {
 				log.Entry().Infof("Getting '%s' from Trust Engine", param.Name)
 				token, err := trustengine.GetToken(ref.Default, client, trustEngineConfiguration)
 				if err != nil {
 					log.Entry().Info(" failed")
-					log.Entry().WithError(err).Debugf("Couldn't get '%s' secret from Trust Engine", param.Name)
+					log.Entry().WithError(err).Debugf("Couldn't get '%s' token from Trust Engine", ref.Default)
 					continue
 				}
 				log.RegisterSecret(token)
 				config.Config[param.Name] = token
 				log.Entry().Info(" succeeded")
 			} else {
-				log.Entry().Debugf("Skipping getting '%s' from Trust Engine: parameter already set", param.Name)
+				log.Entry().Debugf("Skipping retrieval of '%s' from Trust Engine: parameter already set", param.Name)
 			}
 		}
 	}
@@ -42,21 +42,21 @@ func (c *Config) setTrustEngineConfiguration(hookConfig map[string]interface{}) 
 	if serverURL, ok := trustEngineHook["serverURL"].(string); ok {
 		c.trustEngineConfiguration.ServerURL = serverURL
 	} else {
-		return errors.New("no server URL found in Trust Engine hook configuration")
+		return errors.New("no Trust Engine server URL found")
 	}
 	if tokenEndPoint, ok := trustEngineHook["tokenEndPoint"].(string); ok {
 		c.trustEngineConfiguration.TokenEndPoint = tokenEndPoint
 	} else {
-		return errors.New("no token end point found in Trust Engine hook configuration")
+		return errors.New("no Trust Engine service endpoint found")
 	}
 	if tokenQueryParamName, ok := trustEngineHook["tokenQueryParamName"].(string); ok {
 		c.trustEngineConfiguration.TokenQueryParamName = tokenQueryParamName
 	} else {
-		return errors.New("no token query parameter name found in Trust Engine hook configuration")
+		return errors.New("no Trust Engine query parameter name found")
 	}
 
 	if len(c.trustEngineConfiguration.Token) == 0 {
-		log.Entry().Debug("Trust Engine token is not configured or empty string")
+		log.Entry().Debug("no Trust Engine token found")
 	}
 	return nil
 }
