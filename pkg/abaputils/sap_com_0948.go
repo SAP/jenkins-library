6d85c56cea29f4863bc4a1afb00d7a2875a83ed1
Simon Dold <simon.dold@sap.com>
2024-09-16 14:25:00 +0200

    adding stepName

--- pkg/abaputils/sap_com_0948.go
+++ pkg/abaputils/sap_com_0948.go
@@ -347,7 +347,7 @@ func (api *SAP_COM_0948) LogArchive() {
 		var reports []piperutils.Path
 		reports = append(reports, piperutils.Path{Target: fileName, Name: "Log Archive", Mandatory: true})
 
-		piperutils.PersistReportsAndLinks("", "", fileUtils, reports, nil)
+		piperutils.PersistReportsAndLinks("abapEnvironmentCloneGitRepo", "", fileUtils, reports, nil)
 	}
 }
 
