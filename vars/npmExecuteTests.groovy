17f081052179d078f1c10be6d3ab694f62538737
Philip Germanov <philip.germanov@sap.com>
2024-10-01 18:37:35 +0300

    fix: config keys

--- vars/npmExecuteTests.groovy
+++ vars/npmExecuteTests.groovy
@@ -6,20 +6,24 @@ import static com.sap.piper.Prerequisites.checkScript
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/npmExecuteTests.yaml'
 
-@Field Set CONFIG_KEYS = [
+@Field Set GLOBAL_CONFIG_KEYS = []
+
+@Field Set STEP_CONFIG_KEYS = [
     "wdi5",
     "credentialsId",
 ]
+
+@Field Set PARAMETER_KEYS = []
 
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
     String stageName = parameters.stageName ?: env.STAGE_NAME
     Map config = ConfigurationHelper.newInstance(this)
             .loadStepDefaults([:], stageName)
-            .mixinGeneralConfig(script.commonPipelineEnvironment, CONFIG_KEYS)
-            .mixinStepConfig(script.commonPipelineEnvironment, CONFIG_KEYS)
-            .mixinStageConfig(script.commonPipelineEnvironment, stageName, CONFIG_KEYS)
-            .mixin(parameters, CONFIG_KEYS)
+            .mixinGeneralConfig(script.commonPipelineEnvironment, GENERAL_CONFIG_KEYS)
+            .mixinStepConfig(script.commonPipelineEnvironment, STEP_CONFIG_KEYS)
+            .mixinStageConfig(script.commonPipelineEnvironment, stageName, PARAMETER_KEYS)
+            .mixin(parameters, STEP_CONFIG_KEYS)
             .use()
 
     List credentials = []
