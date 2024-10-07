676939f432f662c39166fedba44e3852982dc2cc
Manjunath <manjunath.mandya.surendrakumar@sap.com>
2024-10-07 18:59:35 +0200

    Update mavenBuild.go

--- cmd/mavenBuild.go
+++ cmd/mavenBuild.go
@@ -56,6 +56,7 @@ func runMavenBuild(config *mavenBuildOptions, telemetryData *telemetry.CustomDat
 	var goals []string
 
 	if config.Flatten {
+		goals = append(goals, "-U")
 		goals = append(goals, "flatten:flatten")
 		defines = append(defines, "-Dflatten.mode=resolveCiFriendliesOnly", "-DupdatePomFile=true")
 	}
