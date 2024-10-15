24e25fa6a2ae3844ed99f68669a2a8fd002f3e64
maxcask <maxcask@gmail.com>
2024-10-15 12:35:14 +0400

    more verbose

--- cmd/checkIfStepActive.go
+++ cmd/checkIfStepActive.go
@@ -90,11 +90,11 @@ func checkIfStepActive(utils piperutils.FileUtils) error {
 	currentOrchestrator := orchestrator.DetectOrchestrator().String()
 	if currentOrchestrator == "Jenkins" {
 		log.Entry().Info("CBfix config: ", runConfigV1)
-		log.Entry().Info("CBfix stages: ", runConfigV1.StageConfig)
+		log.Entry().Info("CBfix steps: ", runConfigV1.RunSteps)
 		log.Entry().Info("CBfix old runconfig: ", runConfigV1.RunConfig)
 		log.Entry().Info("CBfix runStages: ", runConfigV1.RunStages)
-		log.Entry().Info("CBfix [Central Build] config: ", runConfigV1.StageConfig.Stages["Central Build"])
-		log.Entry().Info("CBfix [Build] config: ", runConfigV1.StageConfig.Stages["Build"])
+		log.Entry().Info("CBfix [Central Build] config: ", runConfigV1.RunStages["Central Build"])
+		log.Entry().Info("CBfix [Build] config: ", runConfigV1.RunStages["Build"])
 		log.Entry().Info("CBfix: Orchestrator is Jenkins, check if stage name is Central Build")
 		if stage, ok := runConfigV1.RunConfig.StageConfig.Stages["Central Build"]; ok {
 			log.Entry().Info("CBfix: Central Build stage name was found")
