64b41075dd5f388c89dfb153580be0205c82790d
maxcask <maxcask@gmail.com>
2024-11-18 18:54:51 +0400

    rename extension file name

--- vars/piperStageWrapper.groovy
+++ vars/piperStageWrapper.groovy
@@ -104,10 +104,19 @@ private void executeStage(script, originalStage, stageName, config, utils, telem
         */
         def projectInterceptorFile = "${config.projectExtensionsDirectory}${stageName}.groovy"
         def globalInterceptorFile = "${config.globalExtensionsDirectory}${stageName}.groovy"
+        /* due to renaming stage 'Central Build' to 'Build' need to get configuration from
+        extension file name 'Central Build.groovy', once all the users will 'Build' as a stageName,
+        this reaming should be removed
+        */
+        if (stageName == 'Build'){
+            centralBuildExtensionFileName = "Central Build.groovy"
+            projectInterceptorFile = "${config.projectExtensionsDirectory}${centralBuildExtensionFileName}"
+            globalInterceptorFile = "${config.globalExtensionsDirectory}${centralBuildExtensionFileName}"
+        }
         projectExtensions = fileExists(projectInterceptorFile)
         globalExtensions = fileExists(globalInterceptorFile)
         echo "[${STEP_NAME}] Running project interceptor '${projectExtensions}' for ${stageName}."
-         echo "[${STEP_NAME}] VERBOSE: check allowed extensions '${script.env.PIPER_DISABLE_EXTENSIONS}'"
+        echo "[${STEP_NAME}] VERBOSE: check allowed extensions '${script.env.PIPER_DISABLE_EXTENSIONS}'"
         // Pre-defining the real originalStage in body variable, might be overwritten later if extensions exist
         def body = originalStage
         if (stageName == 'Central Build') {
