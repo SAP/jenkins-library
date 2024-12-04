281f09bc24289c66179c1095e78dc758b4d29931
maxcask <maxcask@gmail.com>
2024-12-04 15:46:15 +0400

    echo

--- vars/commonPipelineEnvironment.groovy
+++ vars/commonPipelineEnvironment.groovy
@@ -192,7 +192,7 @@ class commonPipelineEnvironment implements Serializable {
 
     Map getStepConfiguration(stepName, stageName = env.STAGE_NAME, includeDefaults = true) {
         Map defaults = [:]
-        echo "CBFIX: print stages ${configuration.get('stages')?.get(stageName)}"
+        this.echo "CBFIX: print stages ${configuration.get('stages')?.get(stageName)}"
         // if (stageName == 'Central Build') {
         //         stageName = 'Build'
         // }
