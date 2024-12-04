0e7443dac7b7f4e53c1b6a5bf677d193e56f9d1a
maxcask <maxcask@gmail.com>
2024-12-04 14:02:32 +0400

    revert renaming in stageNameProvider

--- src/com/sap/piper/StageNameProvider.groovy
+++ src/com/sap/piper/StageNameProvider.groovy
@@ -3,17 +3,17 @@ package com.sap.piper
 @Singleton
 class StageNameProvider implements Serializable {
     static final long serialVersionUID = 1L
-    static final String CENTRAL_BUILD = "Central Build";
-    static final String BUILD = "Build";
+    // static final String CENTRAL_BUILD = "Central Build";
+    // static final String BUILD = "Build";
 
     /** Stores a feature toggle for defaulting to technical names in stages */
     boolean useTechnicalStageNames = false
 
     String getStageName(Script script, Map parameters, Script step) {
-        String stageName = null
+        // String stageName = null
         if (parameters.stageName in CharSequence) {
-            stageName = parameters.stageName
-            stageName = replaceCentralBuild(stageName);
+            // stageName = parameters.stageName
+            // stageName = replaceCentralBuild(stageName);
             return stageName
         }
         if (this.useTechnicalStageNames) {
@@ -22,16 +22,16 @@ class StageNameProvider implements Serializable {
                 return technicalStageName
             }
         }
-        if (stageName == null) {
-            stageName = script.env.STAGE_NAME
-            stageName = replaceCentralBuild(stageName);
-        }
+        // if (stageName == null) {
+        //     stageName = script.env.STAGE_NAME
+        //     stageName = replaceCentralBuild(stageName);
+        // }
         return stageName
     }
 
-    private String replaceCentralBuild(String stageName) {
-        return CENTRAL_BUILD.equals(stageName) ? BUILD : stageName;
-    }
+    // private String replaceCentralBuild(String stageName) {
+    //     return CENTRAL_BUILD.equals(stageName) ? BUILD : stageName;
+    // }
 
     static String getTechnicalStageName(Script step) {
         try {
