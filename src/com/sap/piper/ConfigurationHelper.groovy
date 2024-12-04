4620fef171f0522a401470bf4e9fb9a5830b8f42
maxcask <maxcask@gmail.com>
2024-12-04 14:41:05 +0400

    print config

--- src/com/sap/piper/ConfigurationHelper.groovy
+++ src/com/sap/piper/ConfigurationHelper.groovy
@@ -13,7 +13,7 @@ class ConfigurationHelper implements Serializable {
         this.step.echo "CBFIX: stagename ${stageName}"
         DefaultValueCache.prepare(step)
         this.config = ConfigurationLoader.defaultGeneralConfiguration()
-        this.config.stageName
+        this.step.echo "CBFIX: print config ${this.config}"
         mixin(ConfigurationLoader.defaultGeneralConfiguration(), null, compatibleParameters)
         mixin(ConfigurationLoader.defaultStepConfiguration(null, name), null, compatibleParameters)
         mixin(ConfigurationLoader.defaultStageConfiguration(null, stageName), null, compatibleParameters)
