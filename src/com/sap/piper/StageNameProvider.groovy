6beb985d5a274bc56d86c1f6ff8af963dc6ef423
maxcask <maxcask@gmail.com>
2024-12-05 10:00:20 +0400

    rename in StageNameProvider

--- src/com/sap/piper/StageNameProvider.groovy
+++ src/com/sap/piper/StageNameProvider.groovy
@@ -10,11 +10,11 @@ class StageNameProvider implements Serializable {
     boolean useTechnicalStageNames = false
 
     String getStageName(Script script, Map parameters, Script step) {
-        // String stageName = null
+        String stageName = null
         if (parameters.stageName in CharSequence) {
-            // stageName = parameters.stageName
-            // stageName = replaceCentralBuild(stageName);
-            return parameters.stageName
+            stageName = parameters.stageName
+            stageName = replaceCentralBuild(stageName);
+            return stageName
         }
         if (this.useTechnicalStageNames) {
             String technicalStageName = getTechnicalStageName(step)
@@ -22,16 +22,16 @@ class StageNameProvider implements Serializable {
                 return technicalStageName
             }
         }
-        // if (stageName == null) {
-        //     stageName = script.env.STAGE_NAME
-        //     stageName = replaceCentralBuild(stageName);
-        // }
-        return script.env.STAGE_NAME
+        if (stageName == null) {
+            stageName = script.env.STAGE_NAME
+            stageName = replaceCentralBuild(stageName);
+        }
+        return stageName
     }
 
-    // private String replaceCentralBuild(String stageName) {
-    //     return CENTRAL_BUILD.equals(stageName) ? BUILD : stageName;
-    // }
+    private String replaceCentralBuild(String stageName) {
+        return CENTRAL_BUILD.equals(stageName) ? BUILD : stageName;
+    }
 
     static String getTechnicalStageName(Script step) {
         try {
