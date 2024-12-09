3eaf6087d8803933fe623cd32ad360ef58bd72fa
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2024-12-09 09:06:30 +0100

    Update description.go

--- pkg/documentation/generator/description.go
+++ pkg/documentation/generator/description.go
@@ -74,8 +74,8 @@ func createDescriptionSection(stepData *config.StepData) string {
 		description += headlineGHA
 		description += fmt.Sprintf("%v```\n", spacingTabBox)
 		description += fmt.Sprintf("%vsteps:\n", spacingTabBox)
-		description += fmt.Sprintf("%v  - name: %v\n", spacingTabBox, stepData.Metadata.Name)
-		description += fmt.Sprintf("%v    uses: SAP/project-piper-action@releaseCommitSHA\n", spacingTabBox)
+		description += fmt.Sprintf("%v  - uses: SAP/project-piper-action@releaseCommitSHA\n", spacingTabBox)
+		description += fmt.Sprintf("%v    name: %v\n", spacingTabBox, stepData.Metadata.Name)
 		description += fmt.Sprintf("%v    with:\n", spacingTabBox)
 		description += fmt.Sprintf("%v      step-name: %v\n", spacingTabBox, stepData.Metadata.Name)
 		description += fmt.Sprintf("%v      flags: --anyStepParameter\n", spacingTabBox)
