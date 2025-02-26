6d55504255174f95490a6493544a3251d3f99fd3
Philip Germanov <phgermanov@gmail.com>
2025-02-26 16:10:47 +0200

    feat: add groovy class

---
+++ vars/buildahExecute.groovy
@@ -0,0 +1,10 @@
+
+import groovy.transform.Field
+
+@Field String STEP_NAME = getClass().getName()
+@Field String METADATA_FILE = 'metadata/buildahExecute.yaml'
+
+void call(Map parameters = [:]) {
+    List credentials = []
+    piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
+}
