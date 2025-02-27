1d900b67a3b4031f91728bfe62ad9f65b6dc94e1
Philip Germanov <phgermanov@gmail.com>
2025-02-27 12:17:47 +0200

    fix: b

--- cmd/buildahExecute.go
+++ cmd/buildahExecute.go
@@ -66,7 +66,7 @@ func runBuildahExecute(config *buildahExecuteOptions, telemetryData *telemetry.C
 		"bud",                  // Using bud (build-using-dockerfile) for Dockerfile builds
 		"--isolation=rootless", // Use rootless isolation mode
 		"--format=docker",      // Use Docker format for compatibility
-		"--storate-driver=vfs", // Use vfs storage driver for rootless
+		"--storage-driver=vfs", // Use vfs storage driver for rootless
 		"--layers",             // Use layering for build
 	}
 
