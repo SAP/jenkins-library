542e6f256720aac34974045db5d6fff8f44cb89b
Stefan Pirtoaca <stefan.pirtoaca@onapsis.com>
2025-03-18 11:47:52 +0200

    Remove error() call in onapsisExecuteScan.groovy

--- vars/onapsisExecuteScan.groovy
+++ vars/onapsisExecuteScan.groovy
@@ -4,17 +4,11 @@ import groovy.transform.Field
 @Field String METADATA_FILE = 'metadata/onapsisExecuteScan.yaml'
 
 def call(Map parameters = [:]) {
-    List credentials = [
-        // [type: 'token', id: 'onapsisTokenCredentialsId', env: ['PIPER_accessToken']],
-        // [type: 'string', id: 'scanServiceUrl', env: ['PIPER_scanServiceUrl']],
-        // [type: 'string', id: 'onapsisUsername', env: ['PIPER_onapsisUsername']],
-        // [type: 'secret', id: 'onapsisPassword', env: ['PIPER_onapsisPassword']]
-    ]
 
     try {
-        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
+        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
     } catch (Exception e) {
-        error("An error occurred while executing the Onapsis scan: ${e.message}")
+//        error("An error occurred while executing the Onapsis scan: ${e.message}")
         currentBuild.result = 'FAILURE' // Mark the build as failed
     } finally {
         archiveArtifacts('onapsis_scan_report.zip')
