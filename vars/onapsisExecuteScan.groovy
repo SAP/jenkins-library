91f67e1a0a6e213ae01fd5544f06c7bd1ee01251
Stefan Pirtoaca <stefan.pirtoaca@onapsis.com>
2025-03-18 16:47:08 +0200

    Check if Onapsis report exists, then mark it as artifact.

--- vars/onapsisExecuteScan.groovy
+++ vars/onapsisExecuteScan.groovy
@@ -2,13 +2,14 @@ import groovy.transform.Field
 
 @Field String STEP_NAME = getClass().getName()
 @Field String METADATA_FILE = 'metadata/onapsisExecuteScan.yaml'
+@Field String ONAPSIS_REPORT_NAME = 'onapsis_scan_report.zip'
 
 def call(Map parameters = [:]) {
-    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
-        try {
-            piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
-        } finally {
-            archiveArtifacts('onapsis_scan_report.zip')
-        }
+    try {
+        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
+    } finally {
+        def artifact = new File(ONAPSIS_REPORT_NAME)
+        if (artifact.exists())
+            archiveArtifacts(ONAPSIS_REPORT_NAME)
     }
 }
