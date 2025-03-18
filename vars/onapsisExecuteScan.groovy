c4659d328fcd49ae13d2e4ede27e4c0fcda35627
Stefan Pirtoaca <stefan.pirtoaca@onapsis.com>
2025-03-18 14:37:15 +0200

    Use handlePipelineStepErrors

--- vars/onapsisExecuteScan.groovy
+++ vars/onapsisExecuteScan.groovy
@@ -4,13 +4,11 @@ import groovy.transform.Field
 @Field String METADATA_FILE = 'metadata/onapsisExecuteScan.yaml'
 
 def call(Map parameters = [:]) {
-
-    try {
-        piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
-    } catch (Exception e) {
-//        error("An error occurred while executing the Onapsis scan: ${e.message}")
-        currentBuild.result = 'FAILURE' // Mark the build as failed
-    } finally {
-        archiveArtifacts('onapsis_scan_report.zip')
+    handlePipelineStepErrors(stepName: STEP_NAME, stepParameters: parameters) {
+        try {
+            piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, [])
+        } finally {
+            archiveArtifacts('onapsis_scan_report.zip')
+        }
     }
 }
