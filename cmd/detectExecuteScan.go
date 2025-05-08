1d151555b20dd7ec70d4bfb31aa2df1a9d2f0a97
Philip Germanov <phgermanov@gmail.com>
2025-05-08 13:02:04 +0300

    fix: add quotes in step

--- cmd/detectExecuteScan.go
+++ cmd/detectExecuteScan.go
@@ -546,7 +546,7 @@ func addDetectArgs(args []string, config detectExecuteScanOptions, utils detectU
 	args = append(args, fmt.Sprintf("\"--detect.code.location.name=%v\"", codelocation))
 
 	if len(mavenArgs) > 0 && !checkIfArgumentIsInScanProperties(config, "detect.maven.build.command") {
-		args = append(args, fmt.Sprintf("\"--detect.maven.build.command=%v\"", strings.Join(mavenArgs, " ")))
+		args = append(args, fmt.Sprintf("\"--detect.maven.build.command=%s\"", quoteMavenArgs(mavenArgs)))
 	}
 
 	args = append(args, fmt.Sprintf("\"--detect.force.success.on.skip=true\""))
@@ -1187,4 +1187,17 @@ func findItemInStringSlice(slice []string, item string) int {
 		}
 	}
 	return -1
+}
+
+func quoteMavenArgs(args []string) string {
+	// Quote any argument containing spaces to handle paths properly
+	quotedArgs := make([]string, len(args))
+	for i, arg := range args {
+		if strings.Contains(arg, " ") {
+			quotedArgs[i] = fmt.Sprintf("'%s'", arg)
+		} else {
+			quotedArgs[i] = arg
+		}
+	}
+	return strings.Join(quotedArgs, " ")
 }
