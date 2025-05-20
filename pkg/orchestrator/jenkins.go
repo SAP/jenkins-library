041143473f2e7529168f81c783a25f4f04a9a2ea
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2025-05-20 08:28:05 +0200

    Update jenkins.go

--- pkg/orchestrator/jenkins.go
+++ pkg/orchestrator/jenkins.go
@@ -2,6 +2,7 @@ package orchestrator
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"strings"
 	"time"
@@ -9,7 +10,6 @@ import (
 	"github.com/Jeffail/gabs/v2"
 	piperHttp "github.com/SAP/jenkins-library/pkg/http"
 	"github.com/SAP/jenkins-library/pkg/log"
-	"github.com/pkg/errors"
 )
 
 type jenkinsConfigProvider struct {
@@ -128,14 +128,14 @@ func (j *jenkinsConfigProvider) FullLogs() ([]byte, error) {
 
 	response, err := j.client.GetRequest(URL, nil, nil)
 	if err != nil {
-		return []byte{}, errors.Wrapf(err, "could not GET Jenkins log file %v", err)
+		return []byte{}, fmt.Errorsf("could not GET Jenkins log file %v", err)
 	} else if response.StatusCode != 200 {
 		log.Entry().Error("response code !=200 could not get log information from Jenkins, returning with empty log.")
 		return []byte{}, nil
 	}
 	logFile, err := io.ReadAll(response.Body)
 	if err != nil {
-		return []byte{}, errors.Wrapf(err, "could not read Jenkins log file from request %v", err)
+		return []byte{}, fmt.Errorsf"could not read Jenkins log file from request %v", err)
 	}
 	defer response.Body.Close()
 	return logFile, nil
@@ -151,13 +151,13 @@ func (j *jenkinsConfigProvider) PipelineStartTime() time.Time {
 	}
 
 	if response.StatusCode != 200 { //http.StatusNoContent -> also empty log!
-		log.Entry().WithError(err).Error(errors.Wrapf(err, "failed to fetch build information with status code %d, returning empty time", response.StatusCode))
+		log.Entry().WithError(err).Errorf("failed to fetch build information with status code %d, returning empty time: %v", response.StatusCode, err))
 		return time.Time{}.UTC()
 	}
 
 	var responseInterface map[string]interface{}
 	if err = piperHttp.ParseHTTPResponseBodyJSON(response, &responseInterface); err != nil {
-		log.Entry().WithError(err).Error(errors.Wrap(err, "failed to parse response, returning empty time"))
+		log.Entry().WithError(err).Errorf("failed to parse response, returning empty time: %v", err))
 		return time.Time{}.UTC()
 	}
 	defer response.Body.Close()
