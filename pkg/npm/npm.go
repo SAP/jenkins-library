bcdc7151f4213a37ddc0df643a29ffe98e212b44
Marcus Holl <marcus.holl@sap.com>
2025-07-04 09:21:54 +0200

    Provide log message in case externally provided package list is used.

--- pkg/npm/npm.go
+++ pkg/npm/npm.go
@@ -146,6 +146,7 @@ func (exec *Execute) RunScriptsInAllPackages(runScripts []string, runOptions []s
 
 	if len(packagesList) > 0 {
 		packageJSONFiles = packagesList
+		log.Entry().Info("Using provided package-list: " + strings.Join(packageJSONFiles, ", "))
 	} else {
 		packageJSONFiles, err = exec.FindPackageJSONFilesWithExcludes(excludeList)
 		if err != nil {
