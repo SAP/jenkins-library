55c695bb8b346e9abfb56539150e48f93b889f42
Nikolay Angelov <nikolay.angelov@sap.com>
2025-07-30 12:58:54 +0300

    update from yuriy fork

--- cmd/sonarExecuteScan.go
+++ cmd/sonarExecuteScan.go
@@ -250,39 +250,60 @@ func runSonar(config sonarExecuteScanOptions, client piperhttp.Downloader, runne
 	if err != nil {
 		return err
 	}
-	// fetch number of issues by severity
-	issueService := SonarUtils.NewIssuesService(serverUrl, config.Token, taskReport.ProjectKey, config.Organization, config.BranchName, config.ChangeID, apiClient)
-	var categories []SonarUtils.Severity
-	influx.sonarqube_data.fields.blocker_issues, err = issueService.GetNumberOfBlockerIssues(&categories)
-	if err != nil {
-		return err
-	}
-	influx.sonarqube_data.fields.critical_issues, err = issueService.GetNumberOfCriticalIssues(&categories)
-	if err != nil {
-		return err
-	}
-	influx.sonarqube_data.fields.major_issues, err = issueService.GetNumberOfMajorIssues(&categories)
-	if err != nil {
-		return err
-	}
-	influx.sonarqube_data.fields.minor_issues, err = issueService.GetNumberOfMinorIssues(&categories)
-	if err != nil {
-		return err
-	}
-	influx.sonarqube_data.fields.info_issues, err = issueService.GetNumberOfInfoIssues(&categories)
+
+	codeCheckData, err := getStaticCodeCheckResults(config, &taskReport, serverUrl, influx, apiClient)
 	if err != nil {
 		return err
 	}
 
-	reportData := SonarUtils.ReportData{
+	hotSpotData, err := getHotSpotSecurityCheckResults(config, &taskReport, serverUrl, apiClient)
+	if err != nil {
+		return err
+	}
+
+	err = getCombinedReport(&codeCheckData, &hotSpotData)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getStaticCodeCheckResults(config sonarExecuteScanOptions, taskReport *SonarUtils.TaskReportData, serverUrl string, influx *sonarExecuteScanInflux, apiClient SonarUtils.Sender) (SonarUtils.ReportCodeCheckData, error) {
+	// fetch number of issues by severity
+	issueService := SonarUtils.NewIssuesService(serverUrl, config.Token, taskReport.ProjectKey, config.Organization, config.BranchName, config.ChangeID, apiClient)
+	var categories []SonarUtils.Severity
+	var err error
+	influx.sonarqube_data.fields.blocker_issues, err = issueService.GetNumberOfBlockerIssues(&categories)
+	if err != nil {
+		return SonarUtils.ReportCodeCheckData{}, err
+	}
+	influx.sonarqube_data.fields.critical_issues, err = issueService.GetNumberOfCriticalIssues(&categories)
+	if err != nil {
+		return SonarUtils.ReportCodeCheckData{}, err
+	}
+	influx.sonarqube_data.fields.major_issues, err = issueService.GetNumberOfMajorIssues(&categories)
+	if err != nil {
+		return SonarUtils.ReportCodeCheckData{}, err
+	}
+	influx.sonarqube_data.fields.minor_issues, err = issueService.GetNumberOfMinorIssues(&categories)
+	if err != nil {
+		return SonarUtils.ReportCodeCheckData{}, err
+	}
+	influx.sonarqube_data.fields.info_issues, err = issueService.GetNumberOfInfoIssues(&categories)
+	if err != nil {
+		return SonarUtils.ReportCodeCheckData{}, err
+	}
+
+	reportData := SonarUtils.ReportCodeCheckData{
 		ServerURL:    taskReport.ServerURL,
 		ProjectKey:   taskReport.ProjectKey,
 		TaskID:       taskReport.TaskID,
 		ChangeID:     config.ChangeID,
 		BranchName:   config.BranchName,
 		Organization: config.Organization,
-		Errors:       categories[:],
-		NumberOfIssues: SonarUtils.Issues{
+		ScanResults:  categories[:],
+		NumberOfIssues: &SonarUtils.Issues{
 			Blocker:  influx.sonarqube_data.fields.blocker_issues,
 			Critical: influx.sonarqube_data.fields.critical_issues,
 			Major:    influx.sonarqube_data.fields.major_issues,
@@ -307,12 +328,39 @@ func runSonar(config sonarExecuteScanOptions, client piperhttp.Downloader, runne
 
 	log.Entry().Debugf("Influx values: %v", influx.sonarqube_data.fields)
 
-	err = SonarUtils.WriteReport(reportData, sonar.workingDir, os.WriteFile)
+	return reportData, SonarUtils.WriteCodeCheckReport(reportData, sonar.workingDir, os.WriteFile)
+}
 
+func getHotSpotSecurityCheckResults(config sonarExecuteScanOptions, taskReport *SonarUtils.TaskReportData, serverUrl string, apiClient SonarUtils.Sender) (SonarUtils.ReportHotSpotData, error) {
+	// fetch number of issues by severity
+	issueService := SonarUtils.NewIssuesService(serverUrl, config.Token, taskReport.ProjectKey, config.Organization, config.BranchName, config.ChangeID, apiClient)
+	var hotspotissues []SonarUtils.SecurityHotspot
+	err := issueService.GetHotSpotSecurityIssues(&hotspotissues)
 	if err != nil {
-		return err
+		return SonarUtils.ReportHotSpotData{}, err
 	}
-	return nil
+
+	reportData := SonarUtils.ReportHotSpotData{
+		ServerURL:        taskReport.ServerURL,
+		ProjectKey:       taskReport.ProjectKey,
+		TaskID:           taskReport.TaskID,
+		ChangeID:         config.ChangeID,
+		BranchName:       config.BranchName,
+		Organization:     config.Organization,
+		SecurityHotspots: hotspotissues[:],
+	}
+
+	return reportData, SonarUtils.WriteHotSpotReport(reportData, sonar.workingDir, os.WriteFile)
+}
+
+func getCombinedReport(codeCheckData *SonarUtils.ReportCodeCheckData, hotSpotData *SonarUtils.ReportHotSpotData) error {
+	reportData := SonarUtils.ReportCombinedData{
+		NumberOfIssues:   codeCheckData.NumberOfIssues,
+		ScanResults:      codeCheckData.ScanResults,
+		SecurityHotspots: hotSpotData.SecurityHotspots,
+	}
+
+	return SonarUtils.WriteCombinedReport(reportData, sonar.workingDir, os.WriteFile)
 }
 
 // isInOptions returns true, if the given property is already provided in config.Options.
--- pkg/sonar/report.go
+++ pkg/sonar/report.go
@@ -6,20 +6,45 @@ import (
 	"path/filepath"
 )
 
-const reportFileName = "sonarscan.json"
+const reportCodeCheckFileName = "codecheck.json"
+const reportCombinedFileName = "sonarscan.json"
+const reportHotSpotFileName = "hotspot.json"
 
-// ReportData is representing the data of the step report JSON
-type ReportData struct {
+// ReportCodeCheckData is representing the data of the step report JSON
+type ReportCodeCheckData struct {
 	ServerURL      string            `json:"serverUrl"`
 	ProjectKey     string            `json:"projectKey"`
 	TaskID         string            `json:"taskId"`
 	ChangeID       string            `json:"changeID,omitempty"`
 	BranchName     string            `json:"branchName,omitempty"`
 	Organization   string            `json:"organization,omitempty"`
-	NumberOfIssues Issues            `json:"numberOfIssues"`
-	Errors         []Severity        `json:"errors"`
+	NumberOfIssues *Issues           `json:"numberOfIssues"`
+	ScanResults    []Severity        `json:"scanResults"`
 	Coverage       *SonarCoverage    `json:"coverage,omitempty"`
 	LinesOfCode    *SonarLinesOfCode `json:"linesOfCode,omitempty"`
+}
+
+// ReportCodeCheckData is representing the data of the step report JSON
+type ReportHotSpotData struct {
+	ServerURL        string            `json:"serverUrl"`
+	ProjectKey       string            `json:"projectKey"`
+	TaskID           string            `json:"taskId"`
+	ChangeID         string            `json:"changeID,omitempty"`
+	BranchName       string            `json:"branchName,omitempty"`
+	Organization     string            `json:"organization,omitempty"`
+	SecurityHotspots []SecurityHotspot `json:"securityHotspots"`
+}
+
+type ReportCombinedData struct {
+	NumberOfIssues   *Issues           `json:"numberOfIssues"`
+	ScanResults      []Severity        `json:"scanResults"`
+	SecurityHotspots []SecurityHotspot `json:"securityHotspots"`
+}
+
+// HotSpot Security Issues
+type SecurityHotspot struct {
+	Priority string `json:"priority"`
+	Hotspots int    `json:"hotspots"`
 }
 
 // Issues ...
@@ -38,10 +63,26 @@ type Severity struct {
 }
 
 // WriteReport ...
-func WriteReport(data ReportData, reportPath string, writeToFile func(f string, d []byte, p os.FileMode) error) error {
+func WriteCodeCheckReport(data ReportCodeCheckData, reportPath string, writeToFile func(f string, d []byte, p os.FileMode) error) error {
 	jsonData, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
-	return writeToFile(filepath.Join(reportPath, reportFileName), jsonData, 0644)
+	return writeToFile(filepath.Join(reportPath, reportCodeCheckFileName), jsonData, 0644)
+}
+
+func WriteHotSpotReport(data ReportHotSpotData, reportPath string, writeToFile func(f string, d []byte, p os.FileMode) error) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	return writeToFile(filepath.Join(reportPath, reportHotSpotFileName), jsonData, 0644)
+}
+
+func WriteCombinedReport(data ReportCombinedData, reportPath string, writeToFile func(f string, d []byte, p os.FileMode) error) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	return writeToFile(filepath.Join(reportPath, reportCombinedFileName), jsonData, 0644)
 }
