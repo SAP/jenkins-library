601595b950ded77753e68b486dfdace1dd08a726
yuriy-tereshchuk-sap <yuriy.tereshchuk@sap.com>
2025-08-01 12:05:10 +0200

    feat(sonar): create combined report for Sonar scans (#5427)
    Co-authored-by: Yuriy.Tereshchuk <astro.lutsk.aa@gmail.com>

--- pkg/sonar/report_test.go
+++ pkg/sonar/report_test.go
@@ -81,3 +81,33 @@ func TestWriteHotSpotReport(t *testing.T) {
 	assert.Equal(t, expected, fileContent)
 	assert.Equal(t, reportHotSpotFileName, fileName)
 }
+
+func TestWriteCombinedReport(t *testing.T) {
+	// init
+	const expected = `{"numberOfIssues":{"blocker":0,"critical":1,"major":2,"minor":3,"info":4},"errors":[{"severity":"CRITICAL","error_type":"CODE_SMELL","issues":10}],"securityHotspots":[{"priority":"HIGH","hotspots":1},{"priority":"LOW","hotspots":4}]}`
+	combinedData := ReportCombinedData{
+		Errors: []Severity{
+			{
+				SeverityType: "CRITICAL",
+				IssueType:    "CODE_SMELL",
+				IssueCount:   10,
+			},
+		},
+		NumberOfIssues: &Issues{
+			Critical: 1,
+			Major:    2,
+			Minor:    3,
+			Info:     4,
+		},
+		SecurityHotspots: []SecurityHotspot{
+			{Priority: "HIGH", Hotspots: 1},
+			{Priority: "LOW", Hotspots: 4},
+		},
+	}
+	// test
+	err := WriteCombinedReport(combinedData, "", writeToFileMock)
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, expected, fileContent)
+	assert.Equal(t, reportCombinedFileName, fileName)
+}
