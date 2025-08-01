eedfeccc88a91bc3df229b4ec727291863aa8a9c
yuriy-tereshchuk-sap <yuriy.tereshchuk@sap.com>
2025-08-01 12:05:10 +0200

    feat(sonar): create combined report for Sonar scans (#5427)
    Co-authored-by: Yuriy.Tereshchuk <astro.lutsk.aa@gmail.com>

--- pkg/sonar/report_test.go
+++ pkg/sonar/report_test.go
@@ -22,12 +22,12 @@ func writeToFileMock(f string, d []byte, p os.FileMode) error {
 
 func TestWriteCodeCheckReport(t *testing.T) {
 	// init
-	const expected = `{"serverUrl":"https://sonarcloud.io","projectKey":"Piper-Validation/Golang","taskId":"mock.Anything","numberOfIssues":{"blocker":0,"critical":1,"major":2,"minor":3,"info":4},"errors":[{"severity":"CRITICAL","error_type":"CODE_SMELL","issues":10}],"coverage":{"coverage":13.7,"lineCoverage":37.1,"linesToCover":123,"uncoveredLines":23,"branchCoverage":42,"branchesToCover":30,"uncoveredBranches":3},"linesOfCode":{"total":327,"languageDistribution":[{"languageKey":"java","linesOfCode":327}]}}`
+	const expected = `{"serverUrl":"https://sonarcloud.io","projectKey":"Piper-Validation/Golang","taskId":"mock.Anything","numberOfIssues":{"blocker":0,"critical":1,"major":2,"minor":3,"info":4},"scanResults":[{"severity":"CRITICAL","error_type":"CODE_SMELL","issues":10}],"coverage":{"coverage":13.7,"lineCoverage":37.1,"linesToCover":123,"uncoveredLines":23,"branchCoverage":42,"branchesToCover":30,"uncoveredBranches":3},"linesOfCode":{"total":327,"languageDistribution":[{"languageKey":"java","linesOfCode":327}]}}`
 	testData := ReportCodeCheckData{
 		ServerURL:  "https://sonarcloud.io",
 		ProjectKey: "Piper-Validation/Golang",
 		TaskID:     mock.Anything,
-		Errors: []Severity{
+		ScanResults: []Severity{
 			{
 				SeverityType: "CRITICAL",
 				IssueType:    "CODE_SMELL",
@@ -80,4 +80,34 @@ func TestWriteHotSpotReport(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, expected, fileContent)
 	assert.Equal(t, reportHotSpotFileName, fileName)
+}
+
+func TestWriteCombinedReport(t *testing.T) {
+	// init
+	const expected = `{"numberOfIssues":{"blocker":0,"critical":1,"major":2,"minor":3,"info":4},"scanResults":[{"severity":"CRITICAL","error_type":"CODE_SMELL","issues":10}],"securityHotspots":[{"priority":"HIGH","hotspots":1},{"priority":"LOW","hotspots":4}]}`
+	combinedData := ReportCombinedData{
+		ScanResults: []Severity{
+			{
+				SeverityType: "CRITICAL",
+				IssueType:    "CODE_SMELL",
+				IssueCount:   10,
+			},
+		},
+		NumberOfIssues: &Issues{
+			Critical: 1,
+			Major:    2,
+			Minor:    3,
+			Info:     4,
+		},
+		SecurityHotspots: []SecurityHotspot{
+			{Priority: "HIGH", Hotspots: 1},
+			{Priority: "LOW", Hotspots: 4},
+		},
+	}
+	// test
+	err := WriteCombinedReport(combinedData, "", writeToFileMock)
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, expected, fileContent)
+	assert.Equal(t, reportCombinedFileName, fileName)
 }
