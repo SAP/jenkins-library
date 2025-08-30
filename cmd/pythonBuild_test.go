ebbafeb7056104c3fc851c5ae6560dbbdd83dcdd
Christopher Fenner <Christopher.Fenner@sap.com>
2025-08-30 10:18:25 +0200

    update docs

--- cmd/pythonBuild_test.go
+++ cmd/pythonBuild_test.go
@@ -109,3 +109,34 @@ func TestRunPythonBuild(t *testing.T) {
 		assert.Equal(t, []string{"env", "--output-file", "bom-pip.xml", "--output-format", "XML", "--spec-version", "1.4"}, utils.ExecMockRunner.Calls[5].Params)
 	})
 }
+
+func TestPythonBuildExecute(t *testing.T) {
+	t.Run("Test build with flags", func(t *testing.T) {
+		config := pythonBuildOptions{
+			BuildFlags:             []string{"--verbose"},
+			SetupFlags:             []string{"egg_info", "--tag-build=pr13"},
+			VirutalEnvironmentName: "venv",
+		}
+
+		utils := pythonBuildMockUtils{
+			ExecMockRunner: &mock.ExecMockRunner{},
+		}
+
+		virutalEnvironmentPathMap := map[string]string{
+			"python": "python",
+		}
+
+		err := buildExecute(&config, &utils, virutalEnvironmentPathMap)
+
+		assert.NoError(t, err)
+		assert.Equal(t, "python", utils.ExecMockRunner.Calls[0].Exec)
+		assert.Equal(t, []string{
+			"--verbose",
+			"setup.py",
+			"egg_info",
+			"--tag-build=pr13",
+			"sdist",
+			"bdist_wheel",
+		}, utils.ExecMockRunner.Calls[0].Params)
+	})
+}
