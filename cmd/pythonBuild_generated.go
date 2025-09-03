71dc8d3b600c0c81d851996c5aa3d7c7d8f08fe1
Christopher Fenner <Christopher.Fenner@sap.com>
2025-09-03 09:49:09 +0200

    update python to 3.10

--- cmd/pythonBuild_generated.go
+++ cmd/pythonBuild_generated.go
@@ -330,7 +330,7 @@ func pythonBuildMetadata() config.StepData {
 				},
 			},
 			Containers: []config.Container{
-				{Name: "python", Image: "python:3.9"},
+				{Name: "python", Image: "python:3.10"},
 			},
 			Outputs: config.StepOutputs{
 				Resources: []config.StepResources{
