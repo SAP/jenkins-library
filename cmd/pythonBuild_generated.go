2132251e02aa2df926ec1ce61522c83bc705cdd8
Christopher Fenner <Christopher.Fenner@sap.com>
2025-09-04 09:30:00 +0200

    update python to 3.12

--- cmd/pythonBuild_generated.go
+++ cmd/pythonBuild_generated.go
@@ -341,7 +341,7 @@ func pythonBuildMetadata() config.StepData {
 				},
 			},
 			Containers: []config.Container{
-				{Name: "python", Image: "python:3.11"},
+				{Name: "python", Image: "python:3.12"},
 			},
 			Outputs: config.StepOutputs{
 				Resources: []config.StepResources{
