0645fcc40e36fb88695bd527bbdec5d6175dc497
Christopher Fenner <Christopher.Fenner@sap.com>
2025-09-17 11:19:12 +0200

    add test cases

--- pkg/python/pip_test.go
+++ pkg/python/pip_test.go
@@ -7,6 +7,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestInstallProjectDependencies(t *testing.T) {
+	// init
+	mockRunner := mock.ExecMockRunner{}
+
+	// test
+	err := InstallProjectDependencies(mockRunner.RunExecutable, "pip")
+
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, "pip", mockRunner.Calls[0].Exec)
+	assert.Equal(t, []string{"install", "--upgrade", "--root-user-action=ignore", "."}, mockRunner.Calls[0].Params)
+}
+
+func TestInstallBuild(t *testing.T) {
+	// init
+	mockRunner := mock.ExecMockRunner{}
+
+	// test
+	err := InstallBuild(mockRunner.RunExecutable, "pip")
+
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, "pip", mockRunner.Calls[0].Exec)
+	assert.Equal(t, []string{"install", "--upgrade", "--root-user-action=ignore", "build"}, mockRunner.Calls[0].Params)
+}
+
 func TestInstallWheel(t *testing.T) {
 	// init
 	mockRunner := mock.ExecMockRunner{}
@@ -18,4 +44,17 @@ func TestInstallWheel(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "pip", mockRunner.Calls[0].Exec)
 	assert.Equal(t, []string{"install", "--upgrade", "--root-user-action=ignore", "wheel"}, mockRunner.Calls[0].Params)
+}
+
+func TestInstallPip(t *testing.T) {
+	// init
+	mockRunner := mock.ExecMockRunner{}
+
+	// test
+	err := InstallPip(mockRunner.RunExecutable, "pip")
+
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, "pip", mockRunner.Calls[0].Exec)
+	assert.Equal(t, []string{"install", "--upgrade", "--root-user-action=ignore", "pip"}, mockRunner.Calls[0].Params)
 }
