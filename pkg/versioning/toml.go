e0ffd5c10a0935445aa6642eb7b769ae5d3ad4f5
Christopher Fenner <Christopher.Fenner@sap.com>
2025-10-02 12:00:17 +0200

    use toml pkg

--- pkg/versioning/toml.go
+++ pkg/versioning/toml.go
@@ -2,68 +2,73 @@ package versioning
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
+
+	"github.com/BurntSushi/toml"
 )
 
 const (
 	TomlBuildDescriptor = "pyproject.toml"
-	// TomlNameRegex is used to match the pip descriptor artifact name
-	TomlNameRegex = "(?s).*?name = ['\"](.*?)['\"].*"
-	// TomlVersionRegex is used to match the pip descriptor artifact version
-	TomlVersionRegex = "(?s).*?version = ['\"](.*?)['\"].*"
 )
 
 // Pip utility to interact with Python specific versioning
 type Toml struct {
 	Pip
+	coordinates tomlCoordinates
+}
+
+type tomlCoordinates struct {
+	Project struct {
+		Name    string `toml:"name"`
+		Version string `toml:"version"`
+	} `toml:"project"`
 }
 
 func (p *Toml) init() error {
-	return p.Pip.init()
+	var coordinates tomlCoordinates
+
+	if !strings.Contains(p.Pip.path, TomlBuildDescriptor) {
+		return fmt.Errorf("file '%v' is not a %s", p.Pip.path, TomlBuildDescriptor)
+	}
+
+	if err := p.Pip.init(); err != nil {
+		return err
+	}
+
+	if _, err := toml.Decode(p.Pip.buildDescriptorContent, &coordinates); err != nil {
+		return err
+	}
+	p.coordinates = coordinates
+	return nil
 }
 
 // GetName returns the name from the build descriptor
 func (p *Toml) GetName() (string, error) {
-	if !strings.Contains(p.Pip.path, TomlBuildDescriptor) {
-		return "", fmt.Errorf("file '%v' is not a %s", p.Pip.path, TomlBuildDescriptor)
-	}
-
 	if err := p.init(); err != nil {
 		return "", fmt.Errorf("failed to read file '%v': %w", p.Pip.path, err)
 	}
-	if !hasMatch(p.Pip.buildDescriptorContent, TomlNameRegex) {
+	if len(p.coordinates.Project.Name) == 0 {
 		return "", fmt.Errorf("no name information found in file '%v'", p.Pip.path)
 	}
-	values := regexp.MustCompile(TomlNameRegex).FindStringSubmatch(p.Pip.buildDescriptorContent)
-	if len(values) < 2 {
-		return "", fmt.Errorf("no name information found in file '%v'", p.Pip.path)
-	}
-	return values[1], nil
+	return p.coordinates.Project.Name, nil
 }
 
-// GetVersion returns the current version from the build descriptor
+// // GetVersion returns the current version from the build descriptor
 func (p *Toml) GetVersion() (string, error) {
-	if !strings.Contains(p.Pip.path, TomlBuildDescriptor) {
-		return "", fmt.Errorf("file '%v' is not a %s", p.Pip.path, TomlBuildDescriptor)
-	}
-
 	if err := p.init(); err != nil {
 		return "", fmt.Errorf("failed to read file '%v': %w", p.Pip.path, err)
 	}
-	if !hasMatch(p.Pip.buildDescriptorContent, TomlVersionRegex) {
+	if len(p.coordinates.Project.Version) == 0 {
 		return "", fmt.Errorf("no version information found in file '%v'", p.Pip.path)
 	}
-	values := regexp.MustCompile(TomlVersionRegex).FindStringSubmatch(p.Pip.buildDescriptorContent)
-	if len(values) < 2 {
-		return "", fmt.Errorf("no version information found in file '%v'", p.Pip.path)
-	}
-	return values[1], nil
+	return p.coordinates.Project.Version, nil
 }
 
 // SetVersion updates the version in the build descriptor
 func (p *Toml) SetVersion(new string) error {
 	if current, err := p.GetVersion(); err != nil {
+		return err
+	} else {
 		// replace with single quotes
 		p.Pip.buildDescriptorContent = strings.ReplaceAll(
 			p.Pip.buildDescriptorContent,
@@ -74,10 +79,11 @@ func (p *Toml) SetVersion(new string) error {
 			p.Pip.buildDescriptorContent,
 			fmt.Sprintf("version = \"%v\"", current),
 			fmt.Sprintf("version = \"%v\"", new))
-		p.Pip.writeFile(p.Pip.path, []byte(p.Pip.buildDescriptorContent), 0600)
+		err = p.Pip.writeFile(p.Pip.path, []byte(p.Pip.buildDescriptorContent), 0600)
+		if err != nil {
+			return fmt.Errorf("failed to write file '%v': %w", p.Pip.path, err)
+		}
 		return nil
-	} else {
-		return err
 	}
 }
 
@@ -86,7 +92,7 @@ func (p *Toml) GetCoordinates() (Coordinates, error) {
 	result := Coordinates{}
 	// get name
 	if name, err := p.GetName(); err != nil {
-		result.ArtifactID = ""
+		return result, fmt.Errorf("failed to retrieve coordinates: %w", err)
 	} else {
 		result.ArtifactID = name
 	}
@@ -98,8 +104,4 @@ func (p *Toml) GetCoordinates() (Coordinates, error) {
 	}
 
 	return result, nil
-}
-
-func hasMatch(value, regex string) bool {
-	return evaluateResult(value, regex)
 }
