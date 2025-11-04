ffb9c658bb4d5f1fc476529875b67d28b1dfa23e
Christopher Fenner <26137398+CCFenner@users.noreply.github.com>
2025-11-04 14:51:09 +0100

    feat(python): handle python build for pyproject.toml based projects (#5463)
    Co-authored-by: Petko Dimitrov <petko.dimitrov@sap.com>

--- cmd/pythonBuild_generated.go
+++ cmd/pythonBuild_generated.go
@@ -205,7 +205,7 @@ and are exposed are environment variables that must be present in the environmen
 
 func addPythonBuildFlags(cmd *cobra.Command, stepConfig *pythonBuildOptions) {
 	cmd.Flags().StringSliceVar(&stepConfig.BuildFlags, "buildFlags", []string{}, "Defines list of build flags passed to python binary.")
-	cmd.Flags().StringSliceVar(&stepConfig.SetupFlags, "setupFlags", []string{}, "Defines list of flags passed to setup.py.")
+	cmd.Flags().StringSliceVar(&stepConfig.SetupFlags, "setupFlags", []string{}, "Defines list of flags passed to setup.py / build module.")
 	cmd.Flags().BoolVar(&stepConfig.CreateBOM, "createBOM", false, "Creates the bill of materials (BOM) using CycloneDX plugin.")
 	cmd.Flags().BoolVar(&stepConfig.Publish, "publish", false, "Configures the build to publish artifacts to a repository.")
 	cmd.Flags().StringVar(&stepConfig.TargetRepositoryPassword, "targetRepositoryPassword", os.Getenv("PIPER_targetRepositoryPassword"), "Password for the target repository where the compiled binaries shall be uploaded - typically provided by the CI/CD environment.")
