794fae2a9b6ed26411cc0140980693f3d0b6770c
maksim bondarenko <maksim.bondarenko@sap.com>
2025-12-01 09:59:53 +0200

    small typo

--- cmd/kubernetesDeploy.go
+++ cmd/kubernetesDeploy.go
@@ -134,7 +134,7 @@ func runHelmDeploy(config kubernetesDeployOptions, utils kubernetes.DeployUtils,
 			log.Entry().Infof("Calling kubectl create secret --dry-run=true ...")
 			// print dockerconfigjson file contents in debug log (without secrets)
 			log.Entry().Infof("Using Docker config.json content: %v", dockerRegistrySecret.String())
-			log.Entry().Infofs("kubectl parameters %v", kubeSecretParams)
+			log.Entry().Infof("kubectl parameters %v", kubeSecretParams)
 			if err := utils.RunExecutable("kubectl", kubeSecretParams...); err != nil {
 				log.Entry().WithError(err).Fatal("Retrieving Docker config via kubectl failed")
 			}
