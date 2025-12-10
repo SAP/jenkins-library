5b0f3b8529d3d5b3c2ea5dd3263819e2368d2632
Dannick Arnold Kwengang Tankeu <dannick.arnold.kwengang.tankeu@sap.com>
2025-12-10 09:11:47 +0100

    feat: implement btpCreateServiceBinding command and associated functionality

---
+++ cmd/btpCreateServiceBinding_test.go
@@ -0,0 +1,112 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/SAP/jenkins-library/pkg/btp"
+	"github.com/SAP/jenkins-library/pkg/telemetry"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunBtpCreateServiceBinding(t *testing.T) {
+	t.Parallel()
+	InstanceName := "test_instance"
+	BindingName := "testServiceBinding"
+	m := &btp.BtpExecutorMock{}
+	m.Stdout(new(bytes.Buffer))
+
+	var telemetryData telemetry.CustomData
+
+	t.Run("Create service binding: no tenant", func(t *testing.T) {
+		defer btpMockCleanup(m)
+
+		utils := btp.NewBTPUtils(m)
+		m.StdoutReturn = map[string]string{
+			"btp login .*": "Authentication successful",
+			"btp get services/binding": fmt.Sprintf(`
+				{
+				"id": "xxxx",
+				"name": "%s",
+				"ready": true
+				}`, BindingName),
+		}
+
+		// init
+		config := btpCreateServiceBindingOptions{
+			Url:                 "https://api.endpoint.com",
+			Subdomain:           "testSubdomain",
+			Subaccount:          "testSubaccount",
+			ServiceInstanceName: InstanceName,
+			ServiceBindingName:  BindingName,
+			CreateServiceConfig: "testCreateServiceConfig",
+			Timeout:             60,
+			PollInterval:        5,
+			User:                "testUser",
+			Password:            "testPassword",
+		}
+
+		// test
+		err := runBtpCreateServiceBinding(&config, &telemetryData, *utils)
+
+		// assert
+		if assert.NoError(t, err) {
+			assert.Equal(t,
+				btp.BtpExecCall{Exec: "btp", Params: []string{"login", "--url", config.Url, "--subdomain", config.Subdomain, "--user", config.User, "--password", config.Password}},
+				m.Calls[1])
+			assert.Equal(t,
+				btp.BtpExecCall{Exec: "btp", Params: []string{"create", "services/binding", "--name", config.ServiceBindingName, "--instance-name", config.ServiceInstanceName, "--subaccount", config.Subaccount, "--parameters", config.CreateServiceConfig}},
+				m.Calls[2])
+			assert.Equal(t,
+				btp.BtpExecCall{Exec: "btp", Params: []string{"logout"}},
+				m.Calls[len(m.Calls)-1])
+		}
+	})
+
+	t.Run("Create service binding: full parameters", func(t *testing.T) {
+		defer btpMockCleanup(m)
+
+		utils := btp.NewBTPUtils(m)
+		m.StdoutReturn = map[string]string{
+			"btp login .*": "Authentication successful",
+			"btp get services/binding": fmt.Sprintf(`
+				{
+					"id": "xxx",
+					"name": "%s",
+					"ready": true
+				}`, InstanceName),
+		}
+
+		// init
+		config := btpCreateServiceBindingOptions{
+			Url:                 "https://api.endpoint.com",
+			Subdomain:           "testSubdomain",
+			Tenant:              "testTenant",
+			Subaccount:          "testSubaccount",
+			ServiceInstanceName: InstanceName,
+			ServiceBindingName:  BindingName,
+			CreateServiceConfig: "testCreateServiceConfig",
+			Timeout:             60,
+			PollInterval:        5,
+			User:                "testUser",
+			Password:            "testPassword",
+		}
+
+		// test
+		err := runBtpCreateServiceBinding(&config, &telemetryData, *utils)
+
+		// assert
+		if assert.NoError(t, err) {
+			assert.Equal(t,
+				btp.BtpExecCall{Exec: "btp", Params: []string{"login", "--url", config.Url, "--subdomain", config.Subdomain, "--user", config.User, "--password", config.Password, "--idp", config.Tenant}},
+				m.Calls[1])
+			assert.Equal(t,
+				btp.BtpExecCall{Exec: "btp", Params: []string{"create", "services/binding", "--name", config.ServiceBindingName, "--instance-name", config.ServiceInstanceName, "--subaccount", config.Subaccount, "--parameters", config.CreateServiceConfig}},
+				m.Calls[2])
+			assert.Equal(t,
+				btp.BtpExecCall{Exec: "btp", Params: []string{"logout"}},
+				m.Calls[len(m.Calls)-1])
+		}
+	})
+}
