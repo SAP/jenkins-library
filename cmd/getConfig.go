86d4b5d3f67f12b4e6c3791eac28e9950c59fc6a
lekuehn <leon.kuehn@sap.com>
2026-01-26 09:51:21 +0100

    feedback: add new flag setVaultCredentials

--- cmd/getConfig.go
+++ cmd/getConfig.go
@@ -27,6 +27,7 @@ type ConfigCommandOptions struct {
 	StepName                      string
 	ContextConfig                 bool
 	OpenFile                      func(s string, t map[string]string) (io.ReadCloser, error)
+	SetVaultCredentials           bool
 }
 
 var configOptions ConfigCommandOptions
@@ -41,6 +42,7 @@ func SetConfigOptions(c ConfigCommandOptions) {
 	configOptions.StageConfigAcceptedParameters = c.StageConfigAcceptedParameters
 	configOptions.StepMetadata = c.StepMetadata
 	configOptions.StepName = c.StepName
+	configOptions.SetVaultCredentials = c.SetVaultCredentials
 }
 
 type getConfigUtils interface {
@@ -226,17 +228,19 @@ func getConfigWithFlagValues(cmd *cobra.Command) (config.StepConfig, error) {
 			flagValues = config.AvailableFlagValues(cmd, &paramFilter)
 		}
 
-		// add vault credentials so that configuration can be fetched from vault
-		if GeneralConfig.VaultRoleID == "" {
-			GeneralConfig.VaultRoleID = os.Getenv("PIPER_vaultAppRoleID")
+		if configOptions.SetVaultCredentials {
+			// add vault credentials so that configuration can be fetched from vault
+			if GeneralConfig.VaultRoleID == "" {
+				GeneralConfig.VaultRoleID = os.Getenv("PIPER_vaultAppRoleID")
+			}
+			if GeneralConfig.VaultRoleSecretID == "" {
+				GeneralConfig.VaultRoleSecretID = os.Getenv("PIPER_vaultAppRoleSecretID")
+			}
+			if GeneralConfig.VaultToken == "" {
+				GeneralConfig.VaultToken = os.Getenv("PIPER_vaultToken")
+			}
+			myConfig.SetVaultCredentials(GeneralConfig.VaultRoleID, GeneralConfig.VaultRoleSecretID, GeneralConfig.VaultToken)
 		}
-		if GeneralConfig.VaultRoleSecretID == "" {
-			GeneralConfig.VaultRoleSecretID = os.Getenv("PIPER_vaultAppRoleSecretID")
-		}
-		if GeneralConfig.VaultToken == "" {
-			GeneralConfig.VaultToken = os.Getenv("PIPER_vaultToken")
-		}
-		myConfig.SetVaultCredentials(GeneralConfig.VaultRoleID, GeneralConfig.VaultRoleSecretID, GeneralConfig.VaultToken)
 
 		stepConfig, err = myConfig.GetStepConfig(flagValues, GeneralConfig.ParametersJSON, customConfig, defaultConfig, GeneralConfig.IgnoreCustomDefaults, paramFilter, metadata, resourceParams, GeneralConfig.StageName, metadata.Metadata.Name)
 		if err != nil {
@@ -299,6 +303,7 @@ func addConfigFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&configOptions.StepMetadata, "stepMetadata", "", "Step metadata, passed as path to yaml")
 	cmd.Flags().StringVar(&configOptions.StepName, "stepName", "", "Step name, used to get step metadata if yaml path is not set")
 	cmd.Flags().BoolVar(&configOptions.ContextConfig, "contextConfig", false, "Defines if step context configuration should be loaded instead of step config")
+	cmd.Flags().BoolVar(&configOptions.SetVaultCredentials, "setVaultCredentials", false, "Defines whether to set Vault credentials to enable fetching credentials from Vault or not")
 }
 
 func defaultsAndFilters(metadata *config.StepData, stepName string) ([]io.ReadCloser, config.StepFilters, error) {
