a3fbe1de5e92d1f3b0d8cfd10dd0274af717f605
Valentin <16714035+valfz@users.noreply.github.com>
2026-02-01 10:30:40 +0200

    Update pkg/vault/vault_test.go
    Co-authored-by: Christopher Fenner <26137398+CCFenner@users.noreply.github.com>

--- pkg/vault/vault_test.go
+++ pkg/vault/vault_test.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/SAP/jenkins-library/pkg/vault/mocks"
+
 	"github.com/hashicorp/vault/api"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
