b1ab5b20cf2088ef1d330b1e68b87d04a59dac5c
Christopher Fenner <Christopher.Fenner@sap.com>
2026-02-02 11:34:52 +0100

    add event data type

---
+++ pkg/events/data.go
@@ -0,0 +1,21 @@
+package events
+
+import (
+	"encoding/json"
+
+	"github.com/SAP/jenkins-library/pkg/log"
+)
+
+type TaskRunEventData struct {
+	TaskName string `json:"taskName"`
+	Outcome  string `json:"outcome"`
+}
+
+func (data *TaskRunEventData) toJSON() string {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		log.Entry().WithError(err).Error("Failed to marshal TaskRunEventData to JSON")
+		return ""
+	}
+	return string(jsonBytes)
+}
