8f6f871d5b24996665728f443a7721b85e67037f
Christopher Fenner <Christopher.Fenner@sap.com>
2026-02-02 11:51:54 +0100

    update generated sources

--- cmd/pythonBuild_generated.go
+++ cmd/pythonBuild_generated.go
@@ -183,6 +183,16 @@ The variables ` + "`" + `PIPER_VAULTCREDENTIAL_USERNAME` + "`" + ` and ` + "`" +
 				}
 				if GeneralConfig.HookConfig.GCPPubSubConfig.Enabled {
 					log.Entry().Debug("publishing event to GCP Pub/Sub...")
+					// prepare taskrunfinished event data
+					outcome := "failure"
+					if stepTelemetryData.ErrorCode == "0" {
+						outcome = "success"
+					}
+					payload := events.TaskRunEventData{
+						TaskName:  STEP_NAME,
+						StageName: telemetryClient.GetData().StageName,
+						Outcome:   outcome,
+					}
 					// create GCP Pub/Sub client
 					gcpClient := gcp.NewGcpPubsubClient(
 						vaultClient,
@@ -193,17 +203,11 @@ The variables ` + "`" + `PIPER_VAULTCREDENTIAL_USERNAME` + "`" + ` and ` + "`" +
 						GeneralConfig.HookConfig.OIDCConfig.RoleID,
 					)
 					// send event
-					// Build a safe JSON payload for the event via events package
-					payload, mErr := events.SafeDataFromTaskName(STEP_NAME)
-					if mErr != nil {
-						log.Entry().WithError(mErr).Warn("failed to marshal Pub/Sub event payload")
-					}
 					if err := events.SendTaskRunFinished(
 						GeneralConfig.HookConfig.GCPPubSubConfig.Source,
 						GeneralConfig.HookConfig.GCPPubSubConfig.TypePrefix,
 						GeneralConfig.HookConfig.GCPPubSubConfig.TopicPrefix,
 						payload,
-						"",
 						gcpClient); err != nil {
 						log.Entry().WithError(err).Warn("  failed")
 					} else {
--- cmd/sonarExecuteScan_generated.go
+++ cmd/sonarExecuteScan_generated.go
@@ -250,6 +250,16 @@ func SonarExecuteScanCommand() *cobra.Command {
 				}
 				if GeneralConfig.HookConfig.GCPPubSubConfig.Enabled {
 					log.Entry().Debug("publishing event to GCP Pub/Sub...")
+					// prepare taskrunfinished event data
+					outcome := "failure"
+					if stepTelemetryData.ErrorCode == "0" {
+						outcome = "success"
+					}
+					payload := events.TaskRunEventData{
+						TaskName:  STEP_NAME,
+						StageName: telemetryClient.GetData().StageName,
+						Outcome:   outcome,
+					}
 					// create GCP Pub/Sub client
 					gcpClient := gcp.NewGcpPubsubClient(
 						vaultClient,
@@ -264,8 +274,7 @@ func SonarExecuteScanCommand() *cobra.Command {
 						GeneralConfig.HookConfig.GCPPubSubConfig.Source,
 						GeneralConfig.HookConfig.GCPPubSubConfig.TypePrefix,
 						GeneralConfig.HookConfig.GCPPubSubConfig.TopicPrefix,
-						"{\"taskName\":\""+STEP_NAME+"\"}",
-						"",
+						payload,
 						gcpClient); err != nil {
 						log.Entry().WithError(err).Warn("  failed")
 					} else {
