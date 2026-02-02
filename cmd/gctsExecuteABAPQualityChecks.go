6199a5239912d616c0adad1166ce3443cff9388c
Marcus Holl <marcus.holl@sap.com>
2026-02-02 11:14:57 +0100

    refactor: avoid duplicate code
    The new util for getting the http-options is not applied inside function
    getLastSuccessfullCommit since we do not work with user/password here, but with
    a token.
    
    That function seems to be un-used by the way (... used only by tests)

--- cmd/gctsExecuteABAPQualityChecks.go
+++ cmd/gctsExecuteABAPQualityChecks.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"html"
 	"net/http"
-	"net/http/cookiejar"
 	"net/url"
 	"os"
 	"regexp"
@@ -14,6 +13,7 @@ import (
 	"strings"
 
 	"github.com/SAP/jenkins-library/pkg/command"
+	"github.com/SAP/jenkins-library/pkg/gcts"
 	piperhttp "github.com/SAP/jenkins-library/pkg/http"
 	"github.com/SAP/jenkins-library/pkg/log"
 	"github.com/SAP/jenkins-library/pkg/telemetry"
@@ -55,34 +55,15 @@ func rungctsExecuteABAPQualityChecks(config *gctsExecuteABAPQualityChecksOptions
 	const repository = "repository"
 	const packages = "packages"
 
-	cookieJar, cookieErr := cookiejar.New(nil)
-	if cookieErr != nil {
-		return errors.Wrap(cookieErr, "creating a cookie jar failed")
-	}
-
-	maxRetries := -1
-	clientOptions := piperhttp.ClientOptions{
-		CookieJar:                 cookieJar,
-		Username:                  config.Username,
-		Password:                  config.Password,
-		MaxRetries:                maxRetries,
-		TransportSkipVerification: config.SkipSSLVerification,
-	}
-
-	if config.Proxy != "" {
-		proxyURL, err := url.Parse(config.Proxy)
-		if err != nil {
-			return errors.Wrap(err, "failed to parse proxy URL")
-		}
-		clientOptions.TransportProxy = proxyURL
-		log.Entry().Infof("Using proxy: %v", config.Proxy)
+	clientOptions, err := gcts.NewHttpClientOptions(config.Username, config.Password, config.Proxy, config.SkipSSLVerification)
+	if err != nil {
+		return err
 	}
 	httpClient.SetOptions(clientOptions)
 
 	log.Entry().Infof("start of gctsExecuteABAPQualityChecks step with configuration values: %v", config)
 
 	var objects []repoObject
-	var err error
 
 	log.Entry().Info("scope:", config.Scope)
 
