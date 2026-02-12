68c1c584b64e8c5e23d120f0c4f4bede967ff9c0
Oliver Feldmann <oliver.feldmann@sap.com>
2026-02-12 13:28:03 +0100

    include other options in test

--- pkg/gcts/gctsUtils_test.go
+++ pkg/gcts/gctsUtils_test.go
@@ -8,12 +8,22 @@ import (
 
 func TestGctsUtils(t *testing.T) {
 
-	t.Run("valid proxy is used in config", func(t *testing.T) {
-		options, err := NewHttpClientOptions("password", "userName", "https://example.org/my-proxy", false)
-		assert.NoError(t, err)
-		assert.Equal(t, options.TransportProxy.String(), "https://example.org/my-proxy")
-	})
+	t.Run("parameters are correctly assigned to client options", func(t *testing.T) {
+		username := "testUser"
+		password := "testPassword"
+		proxy := "https://proxy.example.com:8080"
+		skipSSLVerification := true
 
+		options, err := NewHttpClientOptions(username, password, proxy, skipSSLVerification)
+		
+		assert.NoError(t, err)
+		assert.Equal(t, username, options.Username)
+		assert.Equal(t, password, options.Password)
+		assert.Equal(t, skipSSLVerification, options.TransportSkipVerification)
+		assert.NotNil(t, options.TransportProxy)
+		assert.Equal(t, proxy, options.TransportProxy.String())
+	})
+	
 	t.Run("no transport proxy set when proxy is not defined", func(t *testing.T) {
 		options, err := NewHttpClientOptions("password", "userName", "", false)
 		assert.NoError(t, err)
