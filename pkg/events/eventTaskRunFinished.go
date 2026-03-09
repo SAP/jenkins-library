eefa227cc34bdbaeec7b22a8edae1ef8e45998c3
jliempt <35920075+jliempt@users.noreply.github.com>
2026-03-09 21:01:07 +0100

    log better

--- pkg/events/eventTaskRunFinished.go
+++ pkg/events/eventTaskRunFinished.go
@@ -1,20 +1,27 @@
 package events
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/SAP/jenkins-library/pkg/log"
 )
 
 func NewEventTaskRunFinished(eventTypePrefix, eventSource string, payload PayloadTaskRunFinished) ([]byte, error) {
+	// Log the payload in human-readable form
+	payloadJSON, _ := json.MarshalIndent(payload, "", "  ")
+	log.Entry().Infof("Event payload:\n%s", string(payloadJSON))
+
 	eventType := fmt.Sprintf("%seventTypeTaskRunFinished", eventTypePrefix)
 	// create cloud event
 	event := NewEvent(eventType, eventSource, "").Create(payload)
-	log.Entry().Debugf("event %+v", event)
-	// log event payload
-	var newEventData map[string]interface{}
-	event.cloudEvent.DataAs(&newEventData)
-	log.Entry().Debugf("event data %+v", newEventData)
+
+	// Log the event data in human-readable form
+	var eventData map[string]interface{}
+	event.cloudEvent.DataAs(&eventData)
+	eventDataJSON, _ := json.MarshalIndent(eventData, "", "  ")
+	log.Entry().Infof("Event data:\n%s", string(eventDataJSON))
+
 	// convert event to bytes
 	eventBytes, err := event.ToBytes()
 	if err != nil {
