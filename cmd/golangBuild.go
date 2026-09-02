247b36808105936a61990f476142425436b09f48
And-tab-8889 <andrei.tabolin@sap.com>
2026-09-02 12:47:23 +0200

    fix(ctp): flip createBuildArtifactsMetadata default to true for mtaBuild, kanikoExecute, golangBuild, pythonBuild (#5898)
    Co-authored-by: fskhiri <faycal.skhiri@sap.com>

--- cmd/golangBuild.go
+++ cmd/golangBuild.go
@@ -257,6 +257,30 @@ func runGolangBuild(config *golangBuildOptions, telemetryData *telemetry.CustomD
 	}
 	commonPipelineEnvironment.custom.buildSettingsInfo = buildSettingsInfo
 
+	if config.CreateBuildArtifactsMetadata {
+		var buildCoordinates []versioning.Coordinates
+		for _, binary := range binaries {
+			coordinate, err := createGoBuildArtifactsMetadata(binary, utils)
+			if err != nil {
+				log.Entry().Warnf("unable to create build artifact metadata: %v", err)
+				continue
+			}
+			buildCoordinates = append(buildCoordinates, coordinate)
+		}
+		if len(buildCoordinates) > 0 {
+			var buildArtifacts build.BuildArtifacts
+			buildArtifacts.Coordinates = buildCoordinates
+			jsonResult, err := json.Marshal(buildArtifacts)
+			if err != nil {
+				log.Entry().Warnf("unable to marshal build artifacts metadata: %v", err)
+			} else {
+				commonPipelineEnvironment.custom.goBuildArtifacts = string(jsonResult)
+			}
+		} else {
+			log.Entry().Warnf("unable to identify artifact coordinates for the go binary(s)")
+		}
+	}
+
 	if config.Publish {
 		if len(config.TargetRepositoryURL) == 0 {
 			return fmt.Errorf("there's no target repository for binary publishing configured")
@@ -295,7 +319,6 @@ func runGolangBuild(config *golangBuildOptions, telemetryData *telemetry.CustomD
 		utils.SetOptions(repoClientOptions)
 
 		var binaryArtifacts piperenv.Artifacts
-		buildCoordinates := []versioning.Coordinates{}
 
 		for _, binary := range binaries {
 
@@ -323,51 +346,34 @@ func runGolangBuild(config *golangBuildOptions, telemetryData *telemetry.CustomD
 			binaryArtifacts = append(binaryArtifacts, piperenv.Artifact{
 				Name: binary,
 			})
-
-			if config.CreateBuildArtifactsMetadata {
-				err, coordinate := createGoBuildArtifactsMetadata(binary, config.TargetRepositoryURL, artifactVersion, utils)
-				if err != nil {
-					log.Entry().Warnf("unable to create build artifact metadata : %v", err)
-				}
-				buildCoordinates = append(buildCoordinates, coordinate)
-			}
 		}
 		commonPipelineEnvironment.custom.artifacts = binaryArtifacts
-
-		if len(buildCoordinates) == 0 {
-			log.Entry().Warnf("unable to identify artifact coordinates for the go binary(s) published")
-			return nil
-		}
-
-		var buildArtifacts build.BuildArtifacts
-		buildArtifacts.Coordinates = buildCoordinates
-		jsonResult, _ := json.Marshal(buildArtifacts)
-		commonPipelineEnvironment.custom.goBuildArtifacts = string(jsonResult)
 
 	}
 
 	return nil
 }
 
-func createGoBuildArtifactsMetadata(binary string, repositoryURL string, artifactVersion string, utils golangBuildUtils) (error, versioning.Coordinates) {
+func createGoBuildArtifactsMetadata(binary string, utils golangBuildUtils) (versioning.Coordinates, error) {
 	options := versioning.Options{}
 	builtArtifact, err := versioning.GetArtifact("golang", "", &options, utils)
+	if err != nil {
+		return versioning.Coordinates{}, err
+	}
 	coordinate, err := builtArtifact.GetCoordinates()
+	if err != nil {
+		return versioning.Coordinates{}, err
+	}
 	component := piperutils.GetComponent(filepath.Join(filepath.Dir("go.mod"), sbomFilename))
 
 	purl := component.Purl
-	// golang purls contain the hex code for & with GOOS and GOARC and should be reomved from the PURL
+	// golang purls contain the hex code for & with GOOS and GOARC and should be removed from the PURL
 	purl = strings.ReplaceAll(purl, "\\u0026", "&")
-	if err != nil {
-		return err, coordinate
-	}
 	coordinate.ArtifactID = binary
-	coordinate.URL = repositoryURL
 	coordinate.BuildPath = filepath.Dir(binary)
 	coordinate.PURL = purl
-	coordinate.Version = artifactVersion
 
-	return nil, coordinate
+	return coordinate, nil
 }
 
 func prepareGolangEnvironment(config *golangBuildOptions, goModFile *modfile.File, utils golangBuildUtils) error {
--- cmd/pythonBuild_generated.go
+++ cmd/pythonBuild_generated.go
@@ -24,23 +24,25 @@ import (
 )
 
 type pythonBuildOptions struct {
-	BuildFlags               []string `json:"buildFlags,omitempty"`
-	SetupFlags               []string `json:"setupFlags,omitempty"`
-	CreateBOM                bool     `json:"createBOM,omitempty"`
-	Publish                  bool     `json:"publish,omitempty"`
-	TargetRepositoryPassword string   `json:"targetRepositoryPassword,omitempty"`
-	TargetRepositoryUser     string   `json:"targetRepositoryUser,omitempty"`
-	TargetRepositoryURL      string   `json:"targetRepositoryURL,omitempty"`
-	BuildSettingsInfo        string   `json:"buildSettingsInfo,omitempty"`
-	VirtualEnvironmentName   string   `json:"virtualEnvironmentName,omitempty"`
-	RequirementsFilePath     string   `json:"requirementsFilePath,omitempty"`
-	RunTests                 bool     `json:"runTests,omitempty"`
-	TestOptions              []string `json:"testOptions,omitempty"`
+	BuildFlags                   []string `json:"buildFlags,omitempty"`
+	SetupFlags                   []string `json:"setupFlags,omitempty"`
+	CreateBOM                    bool     `json:"createBOM,omitempty"`
+	CreateBuildArtifactsMetadata bool     `json:"createBuildArtifactsMetadata,omitempty"`
+	Publish                      bool     `json:"publish,omitempty"`
+	TargetRepositoryPassword     string   `json:"targetRepositoryPassword,omitempty"`
+	TargetRepositoryUser         string   `json:"targetRepositoryUser,omitempty"`
+	TargetRepositoryURL          string   `json:"targetRepositoryURL,omitempty"`
+	BuildSettingsInfo            string   `json:"buildSettingsInfo,omitempty"`
+	VirtualEnvironmentName       string   `json:"virtualEnvironmentName,omitempty"`
+	RequirementsFilePath         string   `json:"requirementsFilePath,omitempty"`
+	RunTests                     bool     `json:"runTests,omitempty"`
+	TestOptions                  []string `json:"testOptions,omitempty"`
 }
 
 type pythonBuildCommonPipelineEnvironment struct {
 	custom struct {
-		buildSettingsInfo string
+		buildSettingsInfo    string
+		pythonBuildArtifacts string
 	}
 }
 
@@ -51,6 +53,7 @@ func (p *pythonBuildCommonPipelineEnvironment) persist(path, resourceName string
 		value    any
 	}{
 		{category: "custom", name: "buildSettingsInfo", value: p.custom.buildSettingsInfo},
+		{category: "custom", name: "pythonBuildArtifacts", value: p.custom.pythonBuildArtifacts},
 	}
 
 	errCount := 0
@@ -256,6 +259,7 @@ func addPythonBuildFlags(cmd *cobra.Command, stepConfig *pythonBuildOptions) {
 	cmd.Flags().StringSliceVar(&stepConfig.BuildFlags, "buildFlags", []string{}, "Defines list of build flags passed to python binary.")
 	cmd.Flags().StringSliceVar(&stepConfig.SetupFlags, "setupFlags", []string{}, "Defines list of flags passed to setup.py / build module.")
 	cmd.Flags().BoolVar(&stepConfig.CreateBOM, "createBOM", false, "Creates the bill of materials (BOM) using CycloneDX plugin.")
+	cmd.Flags().BoolVar(&stepConfig.CreateBuildArtifactsMetadata, "createBuildArtifactsMetadata", true, "When true, writes artifact coordinates (name, version, and PURL if a CycloneDX BOM was produced) to commonPipelineEnvironment/custom/pythonBuildArtifacts. This data is consumed by downstream steps such as OSC CTP scans.")
 	cmd.Flags().BoolVar(&stepConfig.Publish, "publish", false, "Configures the build to publish artifacts to a repository.")
 	cmd.Flags().StringVar(&stepConfig.TargetRepositoryPassword, "targetRepositoryPassword", os.Getenv("PIPER_targetRepositoryPassword"), "Password for the target repository where the compiled binaries shall be uploaded - typically provided by the CI/CD environment.")
 	cmd.Flags().StringVar(&stepConfig.TargetRepositoryUser, "targetRepositoryUser", os.Getenv("PIPER_targetRepositoryUser"), "Username for the target repository where the compiled binaries shall be uploaded - typically provided by the CI/CD environment.")
@@ -305,6 +309,15 @@ func pythonBuildMetadata() config.StepData {
 						Mandatory:   false,
 						Aliases:     []config.Alias{},
 						Default:     false,
+					},
+					{
+						Name:        "createBuildArtifactsMetadata",
+						ResourceRef: []config.ResourceReference{},
+						Scope:       []string{"STEPS", "STAGES", "PARAMETERS"},
+						Type:        "bool",
+						Mandatory:   false,
+						Aliases:     []config.Alias{},
+						Default:     true,
 					},
 					{
 						Name:        "publish",
@@ -419,6 +432,7 @@ func pythonBuildMetadata() config.StepData {
 						Type: "piperEnvironment",
 						Parameters: []map[string]any{
 							{"name": "custom/buildSettingsInfo"},
+							{"name": "custom/pythonBuildArtifacts"},
 						},
 					},
 					{
